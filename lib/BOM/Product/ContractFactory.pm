00d54a21c89a013812631ed9aa42897c7320b37a
Jun Bon Kok <junbon@binary.com>
2015-06-17 11:02:09 +0800

    changed factory to handle spreads as well.

--- lib/BOM/Product/ContractFactory.pm
+++ lib/BOM/Product/ContractFactory.pm
@@ -79,144 +79,7 @@ sub produce_contract {
         croak $missing. ' is required.';
     }
 
-    delete $input_params{expiry_daily};
-    if (not $input_params{date_start}) {
-        # An undefined or missing date_start implies that we want a bet which starts now.
-        $input_params{date_start} = Date::Utility->new;
-        # Force date_pricing to be similarly set, but make sure we know below that we did this, for speed reasons.
-        $input_params{pricing_new} = 1;
-    }
-    # Still need the available amount_types somewhere visible.
-    my @available_amount_types = qw(payout stake);
-    foreach my $at (@available_amount_types) {
-        delete $input_params{$at} if ($input_params{amount_type});    # looks like ambiguous hash ref reuse.
-                                                                      # Use the amount_type and make them work it out.
-        if ($input_params{$at}) {
-            # Support pre-stake parameters and how people might think it should work.
-            $input_params{amount_type} = $at;                         # Replace these wholesale.
-            $input_params{amount}      = $input_params{$at};
-            delete $input_params{$at};
-        }
-    }
-    if ($input_params{amount} && first { $_ eq $input_params{amount_type} } (@available_amount_types)) {
-        if ($input_params{amount_type} eq 'payout') {
-            $input_params{payout} = $input_params{amount};
-        } elsif ($input_params{amount_type} eq 'stake') {
-            $input_params{ask_price} = $input_params{amount};
-        }
-    } else {
-        # Dunno what this is, so set the payout to zero and let it fail validation.
-        $input_params{payout} = 0;
-    }
-
-    # We might need this for build so, pre-coerce;
-    if ((ref $input_params{underlying}) !~ /BOM::Market::Underlying/) {
-        $input_params{underlying} = BOM::Market::Underlying->new($input_params{underlying});
-    }
-
-    $input_params{date_start} = Date::Utility->new($input_params{date_start});
-
-    if (defined $input_params{tick_expiry}) {
-        $input_params{date_expiry} = $input_params{date_start}->plus_time_interval(2 * $input_params{tick_count});
-    }
-
-    if (defined $input_params{duration}) {
-        if (my ($number_of_ticks) = $input_params{duration} =~ /(\d+)t$/) {
-            $input_params{tick_expiry} = 1;
-            $input_params{tick_count}  = $number_of_ticks;
-            $input_params{date_expiry} = $input_params{date_start}->plus_time_interval(2 * $input_params{tick_count});
-        } else {
-            # The thinking here is that duration is only added on purpose, but
-            # date_expiry might be hanging around from a poorly reused hashref.
-            my $duration    = $input_params{duration};
-            my $underlying  = $input_params{underlying};
-            my $start_epoch = $input_params{date_start}->epoch;
-            my $expiry;
-            if ($duration =~ /d$/) {
-                # Since we return the day AFTER, we pass one day ahead of expiry.
-                my $expiry_date = Date::Utility->new($start_epoch)->plus_time_interval($duration);
-                # Daily bet expires at the end of day, so here you go
-                if (my $closing = $underlying->exchange->closing_on($expiry_date)) {
-                    $expiry = $closing->epoch;
-                } else {
-                    $expiry = $expiry_date->epoch;
-                    my $regular_day   = $underlying->exchange->regular_trading_day_after($expiry_date);
-                    my $regular_close = $underlying->exchange->closing_on($regular_day);
-                    $expiry = Date::Utility->new($expiry_date->date_yyyymmdd . ' ' . $regular_close->time_hhmmss)->epoch;
-                }
-            } else {
-                $expiry = $start_epoch + Time::Duration::Concise->new(interval => $duration)->seconds;
-            }
-            $input_params{date_expiry} = Date::Utility->new($expiry);
-        }
-    }
-    # If they gave us a date for start and pricing, then we need to do some magic.
-    if (defined $input_params{date_pricing}) {
-        my $pricing = Date::Utility->new($input_params{date_pricing});
-        $input_params{underlying} = BOM::Market::Underlying->new($input_params{underlying}->symbol, $pricing)
-            if (not($input_params{underlying}->for_date and $input_params{underlying}->for_date->is_same_as($pricing)));
-    }
-
-    $input_params{date_start}  //= 1;    # Error conditions if it's not legacy or run, I guess.
-    $input_params{date_expiry} //= 1;
-
-    $input_params{date_expiry} = Date::Utility->new($input_params{date_expiry});
-
-    # This convenience which may also be a bad idea, but it makes testing easier.
-    # You may also add hit and exit, if you like, but those seem like even worse ideas.
-    foreach my $which (qw(current entry)) {
-        my ($spot, $tick) = ($which . '_spot', $which . '_tick');
-        next unless ($input_params{$spot} and not $input_params{$tick});
-
-        $input_params{$tick} = BOM::Market::Data::Tick->new({
-            quote  => $input_params{$spot},
-            epoch  => 1,                                   # Intentionally very old for recognizability.
-            symbol => $input_params{underlying}->symbol,
-        });
-        delete $input_params{$spot};
-    }
-
-    my @barriers = qw(barrier high_barrier low_barrier);
-    foreach my $barrier_name (grep { defined $input_params{$_} } @barriers) {
-        my $possible = $input_params{$barrier_name};
-        if (ref($possible) !~ /BOM::Product::Contract::Strike/) {
-            my $barrier_string_name = 'supplied_' . $barrier_name;
-            if ($possible && (uc(substr($possible, -1)) eq 'D')) {
-                $input_params{'supplied_delta_' . $barrier_name} = $possible;
-                # They gave us a delta instead of a Strike string
-                substr($possible, -1, 1, '');    # Now just the number.
-                my $underlying = $input_params{underlying};
-                # A close enough approximation if they are using deltas.
-                my $tid  = Time::Duration::Concise->new(interval => $input_params{date_expiry}->epoch - $input_params{date_start}->epoch)->days;
-                my $tiy  = $tid / 365;
-                my $tick = $input_params{entry_tick} // $underlying->tick_at($input_params{date_start}, {allow_inconsistent => 1});
-                $input_params{$barrier_string_name} = get_strike_for_spot_delta({
-                    delta            => $possible,
-                    option_type      => 'VANILLA_CALL',
-                    atm_vol          => 0.10,                                                       # Everything here is an approximation.
-                    t                => $tiy,
-                    r_rate           => ($tid > 1) ? $underlying->interest_rate_for($tiy) : 0,
-                    q_rate           => ($tid > 1) ? $underlying->dividend_rate_for($tiy) : 0,
-                    spot             => $tick->quote,
-                    premium_adjusted => $underlying->market_convention->{delta_premium_adjusted},
-                });
-            } else {
-                # Some sort of string which Strike can presumably use.
-                $input_params{$barrier_string_name} = $possible;
-            }
-            delete $input_params{$barrier_name};
-        }
-    }
-
-    # just to make sure that we don't accidentally pass in undef barriers
-    delete $input_params{$_} for @barriers;
-
-    # Add any new validation methods here.
-    # Looking them up can be too slow for pricing speed constraints.
-    $input_params{validation_methods} = [
-        qw(_validate_volsurface _validate_contract _validate_expiry_date _validate_start_date _validate_stake _validate_barrier _validate_underlying _validate_payout _validate_lifetime)
-    ];
-
+    # common initialization for spreads and derivatives
     if (defined $OVERRIDE_LIST{$input_params{bet_type}}) {
         my $override_params = $OVERRIDE_LIST{$input_params{bet_type}};
         delete $input_params{bet_type};
@@ -230,12 +93,157 @@ sub produce_contract {
         can_load(modules => {$contract_class => undef});    # No idea what to do if this fails.
     }
 
-    $input_params{'build_parameters'} = {%input_params};    # Do not self-cycle.
+    # We might need this for build so, pre-coerce;
+    if ((ref $input_params{underlying}) !~ /BOM::Market::Underlying/) {
+        $input_params{underlying} = BOM::Market::Underlying->new($input_params{underlying});
+    }
+    # If they gave us a date for start and pricing, then we need to do some magic.
+    if (defined $input_params{date_pricing}) {
+        my $pricing = Date::Utility->new($input_params{date_pricing});
+        $input_params{underlying} = BOM::Market::Underlying->new($input_params{underlying}->symbol, $pricing)
+            if (not($input_params{underlying}->for_date and $input_params{underlying}->for_date->is_same_as($pricing)));
+    }
 
-    # This occurs after to hopefully make it more annoying to bypass the Factory.
-    $input_params{'_produce_contract_ref'} = \&produce_contract;
 
-    return $contract_class->new(\%input_params);
+    my $contract_obj;
+    if ($contract_class->contract_category eq 'spreads') {
+        $contract_obj = $contract_class->new(\%input_params);
+    } else {
+        delete $input_params{expiry_daily};
+        if (not $input_params{date_start}) {
+            # An undefined or missing date_start implies that we want a bet which starts now.
+            $input_params{date_start} = Date::Utility->new;
+            # Force date_pricing to be similarly set, but make sure we know below that we did this, for speed reasons.
+            $input_params{pricing_new} = 1;
+        }
+        # Still need the available amount_types somewhere visible.
+        my @available_amount_types = qw(payout stake);
+        foreach my $at (@available_amount_types) {
+            delete $input_params{$at} if ($input_params{amount_type});    # looks like ambiguous hash ref reuse.
+                                                                          # Use the amount_type and make them work it out.
+            if ($input_params{$at}) {
+                # Support pre-stake parameters and how people might think it should work.
+                $input_params{amount_type} = $at;                         # Replace these wholesale.
+                $input_params{amount}      = $input_params{$at};
+                delete $input_params{$at};
+            }
+        }
+        if ($input_params{amount} && first { $_ eq $input_params{amount_type} } (@available_amount_types)) {
+            if ($input_params{amount_type} eq 'payout') {
+                $input_params{payout} = $input_params{amount};
+            } elsif ($input_params{amount_type} eq 'stake') {
+                $input_params{ask_price} = $input_params{amount};
+            }
+        } else {
+            # Dunno what this is, so set the payout to zero and let it fail validation.
+            $input_params{payout} = 0;
+        }
+
+        $input_params{date_start} = Date::Utility->new($input_params{date_start});
+
+        if (defined $input_params{tick_expiry}) {
+            $input_params{date_expiry} = $input_params{date_start}->plus_time_interval(2 * $input_params{tick_count});
+        }
+
+        if (defined $input_params{duration}) {
+            if (my ($number_of_ticks) = $input_params{duration} =~ /(\d+)t$/) {
+                $input_params{tick_expiry} = 1;
+                $input_params{tick_count}  = $number_of_ticks;
+                $input_params{date_expiry} = $input_params{date_start}->plus_time_interval(2 * $input_params{tick_count});
+            } else {
+                # The thinking here is that duration is only added on purpose, but
+                # date_expiry might be hanging around from a poorly reused hashref.
+                my $duration    = $input_params{duration};
+                my $underlying  = $input_params{underlying};
+                my $start_epoch = $input_params{date_start}->epoch;
+                my $expiry;
+                if ($duration =~ /d$/) {
+                    # Since we return the day AFTER, we pass one day ahead of expiry.
+                    my $expiry_date = Date::Utility->new($start_epoch)->plus_time_interval($duration);
+                    # Daily bet expires at the end of day, so here you go
+                    if (my $closing = $underlying->exchange->closing_on($expiry_date)) {
+                        $expiry = $closing->epoch;
+                    } else {
+                        $expiry = $expiry_date->epoch;
+                        my $regular_day   = $underlying->exchange->regular_trading_day_after($expiry_date);
+                        my $regular_close = $underlying->exchange->closing_on($regular_day);
+                        $expiry = Date::Utility->new($expiry_date->date_yyyymmdd . ' ' . $regular_close->time_hhmmss)->epoch;
+                    }
+                } else {
+                    $expiry = $start_epoch + Time::Duration::Concise->new(interval => $duration)->seconds;
+                }
+                $input_params{date_expiry} = Date::Utility->new($expiry);
+            }
+        }
+        $input_params{date_start}  //= 1;    # Error conditions if it's not legacy or run, I guess.
+        $input_params{date_expiry} //= 1;
+
+        $input_params{date_expiry} = Date::Utility->new($input_params{date_expiry});
+
+        # This convenience which may also be a bad idea, but it makes testing easier.
+        # You may also add hit and exit, if you like, but those seem like even worse ideas.
+        foreach my $which (qw(current entry)) {
+            my ($spot, $tick) = ($which . '_spot', $which . '_tick');
+            next unless ($input_params{$spot} and not $input_params{$tick});
+
+            $input_params{$tick} = BOM::Market::Data::Tick->new({
+                quote  => $input_params{$spot},
+                epoch  => 1,                                   # Intentionally very old for recognizability.
+                symbol => $input_params{underlying}->symbol,
+            });
+            delete $input_params{$spot};
+        }
+
+        my @barriers = qw(barrier high_barrier low_barrier);
+        foreach my $barrier_name (grep { defined $input_params{$_} } @barriers) {
+            my $possible = $input_params{$barrier_name};
+            if (ref($possible) !~ /BOM::Product::Contract::Strike/) {
+                my $barrier_string_name = 'supplied_' . $barrier_name;
+                if ($possible && (uc(substr($possible, -1)) eq 'D')) {
+                    $input_params{'supplied_delta_' . $barrier_name} = $possible;
+                    # They gave us a delta instead of a Strike string
+                    substr($possible, -1, 1, '');    # Now just the number.
+                    my $underlying = $input_params{underlying};
+                    # A close enough approximation if they are using deltas.
+                    my $tid  = Time::Duration::Concise->new(interval => $input_params{date_expiry}->epoch - $input_params{date_start}->epoch)->days;
+                    my $tiy  = $tid / 365;
+                    my $tick = $input_params{entry_tick} // $underlying->tick_at($input_params{date_start}, {allow_inconsistent => 1});
+                    $input_params{$barrier_string_name} = get_strike_for_spot_delta({
+                        delta            => $possible,
+                        option_type      => 'VANILLA_CALL',
+                        atm_vol          => 0.10,                                                       # Everything here is an approximation.
+                        t                => $tiy,
+                        r_rate           => ($tid > 1) ? $underlying->interest_rate_for($tiy) : 0,
+                        q_rate           => ($tid > 1) ? $underlying->dividend_rate_for($tiy) : 0,
+                        spot             => $tick->quote,
+                        premium_adjusted => $underlying->market_convention->{delta_premium_adjusted},
+                    });
+                } else {
+                    # Some sort of string which Strike can presumably use.
+                    $input_params{$barrier_string_name} = $possible;
+                }
+                delete $input_params{$barrier_name};
+            }
+        }
+
+        # just to make sure that we don't accidentally pass in undef barriers
+        delete $input_params{$_} for @barriers;
+
+        # Add any new validation methods here.
+        # Looking them up can be too slow for pricing speed constraints.
+        $input_params{validation_methods} = [
+            qw(_validate_volsurface _validate_contract _validate_expiry_date _validate_start_date _validate_stake _validate_barrier _validate_underlying _validate_payout _validate_lifetime)
+        ];
+
+        $input_params{'build_parameters'} = {%input_params};    # Do not self-cycle.
+
+        # This occurs after to hopefully make it more annoying to bypass the Factory.
+        $input_params{'_produce_contract_ref'} = \&produce_contract;
+
+        $contract_obj = $contract_class->new(\%input_params);
+    }
+
+    return $contract_obj;
 }
 
 sub _args_to_ref {
