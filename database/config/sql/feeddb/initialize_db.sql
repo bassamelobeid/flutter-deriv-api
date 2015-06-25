cb9670a7d2af42e4dbd5f75011f5d83b6e829bf0
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-06-25 14:18:23 +0800

    cleanup

--- database/config/sql/feeddb/initialize_db.sql
+++
@@ -1,8 +0,0 @@
--- This will create Database, schema & Roles for feed db
-CREATE DATABASE feed;
-
-CREATE ROLE read LOGIN PASSWORD 'letmein';
-CREATE ROLE write LOGIN PASSWORD	'letmein';
-CREATE ROLE monitor LOGIN PASSWORD	'letmein';
-CREATE ROLE replicator LOGIN REPLICATION PASSWORD	'letmein';
-
--- database/config/sql/feeddb/temporary_insert_delay_ohlc.sql
+++
@@ -1,6 +0,0 @@
-SET client_min_messages TO warning;
-
-DROP TRIGGER tick_insert_trigger ON feed.tick;
-DROP TRIGGER ohlc_minutely_insert_trigger ON feed.ohlc_minutely;
-DROP TRIGGER ohlc_hourly_insert_trigger ON feed.ohlc_hourly;
-DROP TRIGGER ohlc_daily_insert_trigger ON feed.ohlc_daily;
