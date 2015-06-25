d2c34d6a590835c87ec7eb7b2a972671dde4620a
Jun Bon Kok <junbon@binary.com>
2015-06-25 15:11:03 +0800

    Added news_adjusted_pricing_vol to pricing_engine_parameters.
    This should be safe to reuse.

--- lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
+++ lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
@@ -576,7 +576,7 @@ sub _build__attrs_safe_for_eq_ticks_reuse {
 
 # This is not a comprehensive list of safe attributes, but includes the ones which
 # are slow enough to make use want to reuse them
-    return [qw(ticks_for_trend pricing_vol)];
+    return [qw(ticks_for_trend pricing_vol news_adjusted_pricing_vol)];
 }
 
 has economic_events_volatility_risk_markup => (
