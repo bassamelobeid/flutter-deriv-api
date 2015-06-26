25d1291875a02f3aed7502d4ec6b7ced37825379
Torsten Förtsch <torsten@binary.com>
2015-06-26 04:04:19 +0000

    improve test-translation.pl

--- bin/test-translation.pl
+++ bin/test-translation.pl
@@ -8,14 +8,92 @@ use Try::Tiny;
 use Test::MockModule;
 use BOM::Platform::Context::I18N;
 use Term::ANSIColor qw/color/;
+use Getopt::Long;
+
+my $dir = '/home/git/binary-com/binary-static/config/locales';
+my $opt_help;
+
+sub usage {
+    return <<"USAGE";
+test-translation.pl [--directory=DIR] [--help] PO_FILE_OR_LANGUAGE ...
+
+OPTIONS
+
+  --directory   specify the directory where PO files are to be found
+                default: $dir
+  --help        print this help and exit
+
+DESCRIPTION
+
+This script reads all message ids from the specified PO files and tries to
+translate them into the destination language. PO files can be specified either
+as file name (extension .po) or by providing the language. In the latter case
+the PO file is found in the directory given by the --directory option.
+
+TYPES OF ERRORS FOUND
+
+* unknown %func() calls
+  Translations can contain function calls in the form of %func(parameters).
+  These functions must be defined in our code. Sometimes translators try to
+  translate the function name which then calls an undefined function.
+
+* incorrect number of %plural() parameters
+  Different languages have different numbers of plural forms. Some, like Malay,
+  don't have any plural forms. Some, like English or French, have just 2 forms,
+  singular and one plural. Others like Arabic or Russian have more forms.
+  Whenever a translator uses the %plural() function, he must specify the correct
+  number of plural forms as parameters.
+
+* incorrect usage of %d in %plural() parameters
+  In some languages, like English or German, singular is applicable only to the
+  quantity of 1. That means the German translator could come up for instance
+  with the following valid %plural call:
+
+    %plural(%5,ein Stein,%d Steine)
+
+  In other languages, like French or Russian, this would be an error. French
+  uses singular also for 0 quantities. So, if the French translator calls:
+
+    %plural(%5,une porte,%d portes)
+
+  and in the actual call the quantity of 0 is passed the output is still
+  "une porte". In Russian the problem is even more critical because singular
+  is used for instance also for the quantity of 121.
+
+  Thus, this test checks if a) the target language is similar to English in
+  having only 2 plural forms, singular and one plural, and in applying
+  singular only to the quantity of 1. If both of these conditions are met
+  %plural calls like the above are allowed. Otherwise, if at least one of
+  the parameters passed to %plural contains a %d, all of the parameters must
+  contain the %d as well.
+
+  That means the following 2 %plural calls are allowed in Russian:
+
+    %plural(%3,%d книга,%d книги,%d книг)
+    %3 %plural(%3,книга,книги,книг)
+
+  while this is forbidden:
+
+    %plural(%3,одна книга,%d книги,%d книг)
+USAGE
+}
+
+binmode STDOUT, ':utf8';
+binmode STDERR, ':utf8';
+
+GetOptions 'directory=s' => \$dir,
+           'help'        => \$opt_help,
+    or die usage;
+
+if ($opt_help) {
+    print usage;
+    exit 0;
+}
 
 select STDERR;
 $| = 1;
 select STDOUT;
 
-binmode STDOUT, ':utf8';
-
-my $dir = '/home/git/binary-com/binary-static/config/locales';
 my @lang = sort do {
     if (opendir my $dh, $dir) {
         grep {s/^(\w+)\.po$/$1/} readdir $dh
@@ -68,20 +146,37 @@ sub cstring {
 sub bstring {
     my @params;
     return $_[0] =~ s!
-                         (?>
-                             %(\w+)\(%([0-9]+)([^\)]*)\)
+                         (?>                       # matches %func(%N,parameters...)
+                             %
+                             (?<func>\w+)
+                             \(
+                             %
+                             (?<p0>[0-9]+)
+                             (?<prest>[^\)]*)
+                             \)
                          )
                      |
-                         %([0-9]+)
-                     |
-                         ([\[\]~])
+                         (?>                       # matches %func(parameters)
+                             %
+                             (?<simplefunc>\w+)
+                             \(
+                             (?<simpleparm>[^\)]*)
+                             \)
+                         )
+                     |                             # matches %N
+                         %
+                         (?<simple>[0-9]+)
+                     |                             # [, ] and ~ should be escaped as ~[, ~] and ~~
+                         (?<esc>[\[\]~])
                      !
-                         if ($5) {
-                             "~$5";
+                         if ($+{esc}) {
+                             "~$+{esc}";
+                         } elsif ($+{simplefunc}) {
+                             "[$+{simplefunc},$+{simpleparm}]";
                          } else {
-                             my $pos = ($1 ? $2 : $4) - 1;
-                             $params[$pos] = $1 && $1 eq 'plural' ? 'plural' : ($params[$pos] // 'text');
-                             $1 ? "[$1,_$2$3]" : "[_$4]";
+                             my $pos = ($+{func} ? $+{p0} : $+{simple}) - 1;
+                             $params[$pos] = $+{func} && $+{func} eq 'plural' ? 'plural' : ($params[$pos] // 'text');
+                             $+{func} ? "[$+{func},_$+{p0}$+{prest}]" : "[_$+{simple}]";
                          }
                      !regx,
            \@params;
@@ -119,7 +214,8 @@ sub get_trans {
 }
 
 sub get_po {
-    my $lang = shift;
+    my $lang        = shift;
+    my $header_only = shift;
 
     unless ($lang =~ /\.po$/) {
         $lang = $dir . '/'. $lang . '.po';
@@ -131,6 +227,7 @@ sub get_po {
     my $ln;
 
     open my $f, '<:utf8', $lang or die "Cannot open $lang: $!\n";
+ READ:
     while (defined (my $l = nextline $f)) {
         if ($l =~ /^\s*msgid\s*"(.*)"/) {
             my $line = $1;
@@ -143,6 +240,7 @@ sub get_po {
                     if ($first) {
                         undef $first;
                         %header = map {split /\s*:\s*/, lc($_), 2} split /\n/, get_trans($f);
+                        last READ if $header_only;
                     } elsif (length $line) {
                         push @ids, [bstring(cstring($line)), get_trans($f), $ln];
                     }
@@ -155,8 +253,26 @@ sub get_po {
     return {
             header => \%header,
             ids    => \@ids,
+            lang   => $header{language},
+            file   => $lang,
            };
 }
+
+my $orig_configs_for = \&BOM::Platform::Context::I18N::configs_for;
+my $mock = Test::MockModule->new('BOM::Platform::Context::I18N', no_auto => 1);
+$mock->mock(configs_for => sub {
+    my $config = $orig_configs_for->(@_);
+
+    delete @{$config}{grep {!/^_/} keys %$config};
+    for my $lang (@lang) {
+        my $po = get_po $lang, 'header_only';
+        $config->{uc $po->{lang}} = [Gettext => $po->{file}];
+    }
+
+    #use Data::Dumper; print Data::Dumper->new([$config])->Useqq(1)->Sortkeys(1)->Dump;
+
+    return $config;
+});
 
 for my $lang (@lang) {
     my $po = get_po $lang;
@@ -179,27 +295,71 @@ for my $lang (@lang) {
         }
     }
 
+    # $lang_plural_is_like_english==1 means the language has exactly 2 plural forms
+    # and singular is applied only to the quantity of 1. That means something like
+    # %plural(%d,ein Stern,%d Sterne) is allowed. In French for instance, singular is
+    # also applied to the quantity of 0. In that case the singular form should also
+    # contain a %d sequence.
+    my $lang_plural_is_like_english = ($nplurals == 2);
+    if ($lang_plural_is_like_english) {
+        for (my $i = 0; $i <= 100_000; $i++) {
+            next if $i == 1;
+            if ($plural_sub->($i) == 0) {
+                $lang_plural_is_like_english = 0;
+                last;
+            }
+        }
+    }
+
     my $ln;
 
     $plural_sub = $hnd->can('plural');
     my $mock = Test::MockModule->new(ref($hnd), no_auto => 1);
     $mock->mock(plural => sub {
-                    elog(1, $lg, $ln, "insufficient number of parameters in \%plural()")
+                    # The plural call should provide exactly the number of forms required by the language
+                    elog(1, $lg, $ln, "\%plural() requires $nplurals parameters for this language (provided: @{[@_ - 2]})")
                         unless @_ == $nplurals+2;
+
+                    # %plural() can be used like
+                    #
+                    #     %plural(%3,word,words)
+                    #
+                    # or like
+                    #
+                    #     %plural(%3,%d word,%d words)
+                    #
+                    # In the first case we are only looking for the correct plural form
+                    # providing the actual quantity elsewhere.
+                    #
+                    # The code below checks that either all parameters of the current call contain %d
+                    # or none of them. That means something like %plural(%15,one word,%d words) is an
+                    # error as singular is in many languages also applied to other quantities than 1.
+
+
+                    my $found_percent_d = 0;
+                    my @no_percent_d;
                     for (my $i = 2; $i < @_; $i++) {
-                        unless ($_[$i] =~ /%d/) {
-                            if ($_[$i] =~ /\d/) {
-                                elog(1, $lg, $ln, "\%plural() parameter " . ($i-1) . " misses %d")
-                            } else {
-                                elog(0, $lg, $ln, "\%plural() parameter " . ($i-1) . " misses %d")
-                            }
+                        if ($_[$i] =~ /%d/) {
+                            $found_percent_d++;
+                        } else {
+                            # 
+                            push @no_percent_d, $i - 1 unless ($i == 2 and $lang_plural_is_like_english);
                         }
                     }
+                    if ($found_percent_d) {
+                        if (@no_percent_d > 1) {
+                            my $s = join(', ', @no_percent_d[0 .. $#no_percent_d - 1]) . ' and ' . $no_percent_d[-1];
+                            elog(1, $lg, $ln, "\%plural() parameters $s miss %d");
+                        } elsif (@no_percent_d == 1) {
+                            elog(1, $lg, $ln, "\%plural() parameter $no_percent_d[0] misses %d");
+                        }
+                    }
+
                     goto $plural_sub;
                 });
 
     for my $test (@{$po->{ids}}) {
-        # use Data::Dumper; print Data::Dumper->new([$test])->Useqq(1)->Sortkeys(1)->Dump;
+        #use Data::Dumper; print Data::Dumper->new([$test])->Useqq(1)->Sortkeys(1)->Dump;
         $ln = $test->[3];
         my $i = 0;
         my $j = 0;
