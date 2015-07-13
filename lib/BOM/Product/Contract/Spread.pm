a39ff08917f1dfec6ccf70e8468bddf7566a05a8
Jun Bon Kok <junbon@binary.com>
2015-07-13 18:55:15 +0800

    breaching tick screw up

--- lib/BOM/Product/Contract/Spread.pm
+++ lib/BOM/Product/Contract/Spread.pm
@@ -291,8 +291,8 @@ sub breaching_tick {
     # we use the original high/low here because the feed DB stores mid instead of buy/sell.
     my ($higher_barrier, $lower_barrier) = @{$self->_highlow_args};
     my $tick = $self->underlying->breaching_tick((
-        start  => $start,
-        end    => $self->date_pricing->epoch,
+        start_time  => $start,
+        end_time    => $self->date_pricing->epoch,
         higher => $higher_barrier,
         lower  => $lower_barrier
     ));
