af56b6063026510582ffcb765e4a372ff022b44d
Chris Travers <christravers@binary.com>
2015-07-17 03:24:47 +0000

    tidy

--- lib/BOM/Platform/SessionCookie.pm
+++ lib/BOM/Platform/SessionCookie.pm
@@ -9,12 +9,12 @@ use warnings;
 sub new {
     my ($package, $self) = shift;
     if ($self->{token}) {
-        $self = JSON::from_json(BOM::System::Chronicle->_redis_read->get('LOGIN_SESSIN::'.$self->{token})) || {};  
+        $self = JSON::from_json(BOM::System::Chronicle->_redis_read->get('LOGIN_SESSIN::' . $self->{token})) || {};
     } else {
-        $self->{token} = Data::Random::String->create_random_string(length=>'128');
-        BOM::System::Chronicle->_redis_write->set('LOGIN_SESSIN::'.$self->{token},JSON::to_json($self));
+        $self->{token} = Data::Random::String->create_random_string(length => '128');
+        BOM::System::Chronicle->_redis_write->set('LOGIN_SESSIN::' . $self->{token}, JSON::to_json($self));
     }
-    BOM::System::Chronicle->_redis_write->ttl('LOGIN_SESSIN::'.$self->{token}, 3600 * 24);       
+    BOM::System::Chronicle->_redis_write->ttl('LOGIN_SESSIN::' . $self->{token}, 3600 * 24);
     return bless $self, $package;
 }
 
@@ -24,20 +24,19 @@ sub session_data {
 }
 
 sub validate_session {
-    my $self = shift;
+    my $self  = shift;
     my $scope = shift;
     return unless $self->{token};
-    return not $scope or grep { $_ eq $scope } @{$self->{scopes}}
+    return not $scope or grep { $_ eq $scope } @{$self->{scopes}};
 }
 
-sub end_session { ## no critic
+sub end_session {    ## no critic
     my $self = shift;
-    BOM::System::Chronicle->_redis_write->set('LOGIN_SESSIN::'.$self->{token});
+    BOM::System::Chronicle->_redis_write->set('LOGIN_SESSIN::' . $self->{token});
 }
 
 # accessors for very frequently used attributes
-sub loginid { $_[0]->{loginid} } ## no critic
-sub email { $_[0]->{email} } ## no critic
-
+sub loginid { $_[0]->{loginid} }    ## no critic
+sub email   { $_[0]->{email} }      ## no critic
 
 1;
