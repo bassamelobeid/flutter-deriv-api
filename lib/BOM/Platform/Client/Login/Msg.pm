09f439b86645201f997d9bc6b082b167be6d6813
Chris Travers <christravers@binary.com>
2015-07-20 03:19:33 +0000

    So Perl was giving me an error message based on a very non-standard syntax....

--- lib/BOM/Platform/Client/Login/Msg.pm
+++ lib/BOM/Platform/Client/Login/Msg.pm
@@ -3,7 +3,7 @@ package BOM::Platform::Client::Login::Msg;
 use strict;
 use warnings;
 
-user overload (
+use overload (
     bool => sub { return exists $_[0]->{success} },    ## no critic
     neg  => sub { return exists $_[0]->{error} },      ## no critic
 );
