18d5a4287877451129299be737f3b00bf11f9506
Chris Travers <christravers@binary.com>
2015-07-20 03:00:09 +0000

    making list a little more explicit

--- lib/BOM/Platform/Client/Login/Msg.pm
+++ lib/BOM/Platform/Client/Login/Msg.pm
@@ -3,9 +3,10 @@ package BOM::Platform::Client::Login::Msg;
 use strict;
 use warnings;
 
-user overload
+user overload (
     bool => sub { return exists $_->[0]->{success} },    ## no critic
-    neg  => sub { return exists $_->[0]->{error} };      ## no critic
+    neg  => sub { return exists $_->[0]->{error} },      ## no critic
+);
 
 sub new { bless shift }                                  ## no critic
 
