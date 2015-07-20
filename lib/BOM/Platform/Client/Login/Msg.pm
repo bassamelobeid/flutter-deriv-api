d0d16fc6e77c81eb2b098106eb46e8ab0948562e
Chris Travers <christravers@binary.com>
2015-07-20 03:11:00 +0000

    fixing syntax error

--- lib/BOM/Platform/Client/Login/Msg.pm
+++ lib/BOM/Platform/Client/Login/Msg.pm
@@ -4,8 +4,8 @@ use strict;
 use warnings;
 
 user overload (
-    bool => sub { return exists $_->[0]->{success} },    ## no critic
-    neg  => sub { return exists $_->[0]->{error} },      ## no critic
+    bool => sub { return exists $_[0]->{success} },    ## no critic
+    neg  => sub { return exists $_[0]->{error} },      ## no critic
 );
 
 sub new { bless shift }                                  ## no critic
