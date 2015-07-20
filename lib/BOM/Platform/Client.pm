7a7af9053801d7de3dfd08029bcc6d907a4f0f9c
Chris Travers <christravers@binary.com>
2015-07-20 02:06:51 +0000

    tidy

--- lib/BOM/Platform/Client.pm
+++ lib/BOM/Platform/Client.pm
@@ -900,11 +900,11 @@ sub login {
 
 package BOM::Platform::Client::Login::Msg;
 
-user overload 
-   bool => sub { return exists $_->[0]->{success} }, ## no critic
-   neg  => sub { return exists $_->[0]->{error} }; ## no critic
+user overload
+    bool => sub { return exists $_->[0]->{success} },    ## no critic
+    neg  => sub { return exists $_->[0]->{error} };      ## no critic
 
-sub new { bless shift, BOM::Platform::Client::Login } ## no critic
+sub new { bless shift, BOM::Platform::Client::Login }    ## no critic
 
 1;
 
