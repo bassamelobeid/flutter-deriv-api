3143b7af18a0fc8128c882a3572d48fa11cdac87
Chris Travers <christravers@binary.com>
2015-07-20 05:11:16 +0000

    tidy

--- lib/BOM/Platform/SessionCookie.pm
+++ lib/BOM/Platform/SessionCookie.pm
@@ -49,8 +49,8 @@ hashref).
 
 # accessors for very frequently used attributes
 sub loginid { $_[0]->{loginid} if ref $_[0] }    ## no critic
-sub email   { $_[0]->{email} if ref $_[0] }      ## no critic
-sub token   { $_[0]->{token} if ref $_[0] }      ## no critic
+sub email   { $_[0]->{email}   if ref $_[0] }    ## no critic
+sub token   { $_[0]->{token}   if ref $_[0] }    ## no critic
 
 =head1 CONSTRUCTOR
 
