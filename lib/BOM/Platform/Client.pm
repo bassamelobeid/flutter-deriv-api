b1df5c4190ea03884f3a49cd61935ae36ead47f2
Chris Travers <christravers@binary.com>
2015-07-20 02:08:39 +0000

    fixing syntax

--- lib/BOM/Platform/Client.pm
+++ lib/BOM/Platform/Client.pm
@@ -904,7 +904,7 @@ user overload
     bool => sub { return exists $_->[0]->{success} },    ## no critic
     neg  => sub { return exists $_->[0]->{error} };      ## no critic
 
-sub new { bless shift, BOM::Platform::Client::Login }    ## no critic
+sub new { bless shift }    ## no critic
 
 1;
 
