4f631d511cdcc745b50f6ff87efd51f7c65204b4
Jun Bon Kok <junbon@binary.com>
2015-07-23 18:13:12 +0800

    small changes here and there.

--- lib/BOM/Product/Contract/Spread.pm
+++ lib/BOM/Product/Contract/Spread.pm
@@ -40,7 +40,7 @@ has currency => (
     required => 1,
 );
 
-# supplied_stop_loss & supplied_stop_profit can be in points or dollar amount
+# supplied_stop_loss & supplied_stop_profit can be in POINT or DOLLAR amount
 # we need the untouch input for longcode.
 has [qw(supplied_stop_loss supplied_stop_profit stop_type amount_per_point)] => (
     is       => 'ro',
@@ -55,12 +55,12 @@ has [qw(stop_profit stop_loss)] => (
 
 sub _build_stop_profit {
     my $self = shift;
-    return $self->stop_type eq 'dollar' ? $self->supplied_stop_profit / $self->amount_per_point : $self->supplied_stop_profit;
+    return $self->stop_type eq 'DOLLAR' ? $self->supplied_stop_profit / $self->amount_per_point : $self->supplied_stop_profit;
 }
 
 sub _build_stop_loss {
     my $self = shift;
-    return $self->stop_type eq 'dollar' ? $self->supplied_stop_loss / $self->amount_per_point : $self->supplied_stop_loss;
+    return $self->stop_type eq 'DOLLAR' ? $self->supplied_stop_loss / $self->amount_per_point : $self->supplied_stop_loss;
 }
 
 has underlying => (
@@ -256,12 +256,12 @@ sub _build_longcode {
     my $self        = shift;
     my $description = $self->longcode_description;
     my @other       = ($self->supplied_stop_loss, $self->supplied_stop_profit);
-    if ($self->stop_type eq 'dollar') {
+    if ($self->stop_type eq 'DOLLAR') {
         push @other, $self->currency;
-        $description .= ' with stop loss of <strong>[_6] [_4]</strong> and limit of <strong>[_6] [_5]</strong>.';
+        $description .= ' with stop loss of <strong>[_6] [_4]</strong> and stop profit of <strong>[_6] [_5]</strong>.';
     } else {
         push @other, 'points';
-        $description .= ' with stop loss of <strong>[_4] [_6]</strong> and limit of <strong>[_5] [_6]</strong>.';
+        $description .= ' with stop loss of <strong>[_4] [_6]</strong> and stop profit of <strong>[_5] [_6]</strong>.';
     }
 
     return localize($description, ($self->currency, $self->amount_per_point, $self->underlying->translated_display_name, @other));
