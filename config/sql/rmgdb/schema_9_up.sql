05d047efdde852cbb3de68e9e1e2509ca958211e
Jun Bon Kok <junbon@binary.com>
2015-07-28 12:56:28 +0800

    Edited schema 9 according to torsten's review.
    Drop and create instead of rename-create-drop.

--- config/sql/rmgdb/schema_9_up.sql
+++ config/sql/rmgdb/schema_9_up.sql
@@ -5,6 +5,7 @@ CREATE TABLE bet.spread_bet (
     amount_per_point numeric,
     stop_profit numeric,
     stop_loss numeric,
+    stop_type VARCHAR,
     spread numeric,
     spread_divisor numeric,
     CONSTRAINT basic_validation CHECK ((amount_per_point > (0)::numeric) AND (stop_profit > (0)::numeric) AND (stop_loss > (0)::numeric))
@@ -14,40 +15,28 @@ ALTER TABLE ONLY bet.spread_bet
     ADD CONSTRAINT fk_spread_bet_financial_market_bet_id FOREIGN KEY (financial_market_bet_id) REFERENCES bet.financial_market_bet(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 ALTER TABLE ONLY bet.bet_dictionary
-    RENAME CONSTRAINT bet_dictionary_table_name_check TO bet_dictionary_table_name_check_old;
+    DROP CONSTRAINT IF EXISTS bet_dictionary_table_name_check RESTRICT;
 
 ALTER TABLE ONLY bet.bet_dictionary
     ADD CONSTRAINT bet_dictionary_table_name_check CHECK (((table_name)::text = ANY ((ARRAY['touch_bet'::character varying, 'range_bet'::character varying, 'higher_lower_bet'::character varying, 'run_bet'::character varying, 'legacy_bet'::character varying, 'digit_bet'::character varying, 'spread_bet'::character varying])::text[])));
 
-ALTER TABLE ONLY bet.bet_dictionary
-    DROP CONSTRAINT IF EXISTS  bet_dictionary_table_name_check_old RESTRICT;
-
 ALTER TABLE ONLY bet.financial_market_bet
-    RENAME CONSTRAINT pk_check_bet_class_value TO pk_check_bet_class_value_old;
+    DROP CONSTRAINT IF EXISTS pk_check_bet_class_value RESTRICT;
 
 ALTER TABLE ONLY bet.financial_market_bet
     ADD CONSTRAINT pk_check_bet_class_value CHECK (((bet_class)::text = ANY ((ARRAY['higher_lower_bet'::character varying, 'range_bet'::character varying, 'touch_bet'::character varying, 'run_bet'::character varying, 'legacy_bet'::character varying, 'digit_bet'::character varying, 'spread_bet'::character varying])::text[]))) NOT VALID;
 
 ALTER TABLE ONLY bet.financial_market_bet
-    DROP CONSTRAINT IF EXISTS  pk_check_bet_class_value_old RESTRICT;
-
-ALTER TABLE ONLY bet.financial_market_bet
-    RENAME CONSTRAINT basic_validation TO basic_validation_old;
+    DROP CONSTRAINT IF EXISTS basic_validation RESTRICT;
 
 ALTER TABLE ONLY bet.financial_market_bet
     ADD CONSTRAINT basic_validation CHECK (purchase_time < '2014-05-09 00:00:00'::timestamp without time zone OR (NOT is_sold OR 0::numeric <= sell_price AND (bet_class = 'spread_bet' OR sell_price <= payout_price) AND round(sell_price, 2) = sell_price AND purchase_time < sell_time) AND 0::numeric < buy_price AND (bet_class = 'spread_bet' OR 0::numeric < payout_price) AND round(buy_price, 2) = buy_price AND round(payout_price, 2) = payout_price AND purchase_time <= start_time AND ((bet_class = 'spread_bet' AND NOT is_sold) OR (start_time <= expiry_time AND purchase_time <= settlement_time))) NOT VALID;
 
 ALTER TABLE ONLY bet.financial_market_bet
-   DROP CONSTRAINT IF EXISTS basic_validation_old RESTRICT;
-
-ALTER TABLE ONLY bet.financial_market_bet
-   RENAME CONSTRAINT pk_check_bet_params_payout_price to pk_check_bet_params_payout_price_old;
+   DROP CONSTRAINT IF EXISTS pk_check_bet_params_payout_price RESTRICT;
 
 ALTER TABLE ONLY bet.financial_market_bet
    ADD CONSTRAINT pk_check_bet_params_payout_price CHECK ((((bet_class)::text = 'legacy_bet'::text) OR (bet_class = 'spread_bet' OR payout_price IS NOT NULL)));
-
-ALTER TABLE ONLY bet.financial_market_bet
-   DROP CONSTRAINT IF EXISTS pk_check_bet_params_payout_price_old RESTRICT;
 
 CREATE TRIGGER prevent_action BEFORE DELETE ON bet.spread_bet FOR EACH STATEMENT EXECUTE PROCEDURE public.prevent_action();
 
