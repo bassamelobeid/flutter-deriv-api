fb3a09a3775cc5828530eeba4546c190fd9eb7a3
Jun Bon Kok <junbon@binary.com>
2015-07-28 15:56:19 +0800

    supplied_stop_(loss|profit) to spread bet table.

--- lib/BOM/Product/Transaction.pm
+++ lib/BOM/Product/Transaction.pm
@@ -433,7 +433,9 @@ sub prepare_bet_data_for_buy {
     }
 
     if ($bet_params->{bet_class} eq $BOM::Database::Model::Constants::BET_CLASS_SPREAD_BET) {
-        $bet_params->{$_} = $contract->$_ for qw(amount_per_point spread stop_profit stop_loss stop_type spread_divisor);
+        $bet_params->{$_} = $contract->$_ for qw(amount_per_point spread stop_type spread_divisor);
+        $bet_params->{stop_loss}   = $contract->supplied_stop_loss;
+        $bet_params->{stop_profit} = $contract->supplied_stop_profit;
     } elsif ($bet_params->{bet_class} eq $BOM::Database::Model::Constants::BET_CLASS_HIGHER_LOWER_BET) {
         # only store barrier in the database if it is defined.
         # asian contracts have barriers at/after expiry.
