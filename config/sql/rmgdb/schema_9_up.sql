5d514c96b6b4ad1062b3f84d9c92f02721823ce6
Jun Bon Kok <junbon@binary.com>
2015-07-31 19:24:56 +0800

    arrange sql in schema 9 to be readable, at least i try.

--- config/sql/rmgdb/schema_9_up.sql
+++ config/sql/rmgdb/schema_9_up.sql
@@ -18,19 +18,36 @@ ALTER TABLE ONLY bet.bet_dictionary
     DROP CONSTRAINT IF EXISTS bet_dictionary_table_name_check RESTRICT;
 
 ALTER TABLE ONLY bet.bet_dictionary
-    ADD CONSTRAINT bet_dictionary_table_name_check CHECK (((table_name)::text = ANY ((ARRAY['touch_bet'::character varying, 'range_bet'::character varying, 'higher_lower_bet'::character varying, 'run_bet'::character varying, 'legacy_bet'::character varying, 'digit_bet'::character varying, 'spread_bet'::character varying])::text[])));
+    ADD CONSTRAINT bet_dictionary_table_name_check
+        CHECK (((table_name)::text = ANY ((ARRAY['touch_bet'::character varying, 'range_bet'::character varying, 'higher_lower_bet'::character varying, 'run_bet'::character varying, 'legacy_bet'::character varying, 'digit_bet'::character varying, 'spread_bet'::character varying])::text[])));
 
 ALTER TABLE ONLY bet.financial_market_bet
     DROP CONSTRAINT IF EXISTS pk_check_bet_class_value RESTRICT;
 
 ALTER TABLE ONLY bet.financial_market_bet
-    ADD CONSTRAINT pk_check_bet_class_value CHECK (((bet_class)::text = ANY ((ARRAY['higher_lower_bet'::character varying, 'range_bet'::character varying, 'touch_bet'::character varying, 'run_bet'::character varying, 'legacy_bet'::character varying, 'digit_bet'::character varying, 'spread_bet'::character varying])::text[]))) NOT VALID;
+    ADD CONSTRAINT pk_check_bet_class_value
+        CHECK (((bet_class)::text = ANY ((ARRAY['higher_lower_bet'::character varying, 'range_bet'::character varying, 'touch_bet'::character varying, 'run_bet'::character varying, 'legacy_bet'::character varying, 'digit_bet'::character varying, 'spread_bet'::character varying])::text[]))) NOT VALID;
 
 ALTER TABLE ONLY bet.financial_market_bet
     DROP CONSTRAINT IF EXISTS basic_validation RESTRICT;
 
 ALTER TABLE ONLY bet.financial_market_bet
-    ADD CONSTRAINT basic_validation CHECK (purchase_time < '2014-05-09 00:00:00'::timestamp without time zone OR (NOT is_sold OR 0::numeric <= sell_price AND (bet_class = 'spread_bet' OR sell_price <= payout_price) AND round(sell_price, 2) = sell_price AND purchase_time < sell_time) AND 0::numeric < buy_price AND (bet_class = 'spread_bet' OR 0::numeric < payout_price) AND round(buy_price, 2) = buy_price AND round(payout_price, 2) = payout_price AND purchase_time <= start_time AND ((bet_class = 'spread_bet' AND NOT is_sold) OR (start_time <= expiry_time AND purchase_time <= settlement_time))) NOT VALID;
+    ADD CONSTRAINT basic_validation
+        CHECK (
+            purchase_time < '2014-05-09 00:00:00'::timestamp without time zone
+            OR (NOT is_sold
+                OR 0::numeric <= sell_price
+                    AND (bet_class = 'spread_bet' OR sell_price <= payout_price)
+                    AND round(sell_price, 2) = sell_price
+                    AND purchase_time < sell_time)
+            AND 0::numeric < buy_price
+            AND (bet_class = 'spread_bet' OR 0::numeric < payout_price)
+            AND round(buy_price, 2) = buy_price
+            AND round(payout_price, 2) = payout_price
+            AND purchase_time <= start_time
+            AND ((bet_class = 'spread_bet' AND NOT is_sold)
+                OR (start_time <= expiry_time AND purchase_time <= settlement_time))
+        ) NOT VALID;
 
 ALTER TABLE ONLY bet.financial_market_bet
    DROP CONSTRAINT IF EXISTS pk_check_bet_params_payout_price RESTRICT;
