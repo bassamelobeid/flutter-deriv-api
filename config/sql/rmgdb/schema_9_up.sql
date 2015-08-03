8b55c6efac1b9c9bae6dcf44b54db3eceb90eac8
Torsten Förtsch <torsten@binary.com>
2015-08-03 04:23:33 +0000

    FMB constraints

--- config/sql/rmgdb/schema_9_up.sql
+++ config/sql/rmgdb/schema_9_up.sql
@@ -25,26 +25,29 @@ ALTER TABLE ONLY bet.bet_dictionary
 
 ALTER TABLE ONLY bet.bet_dictionary
     ADD CONSTRAINT bet_dictionary_table_name_check
-        CHECK (table_name IN ('touch_bet',
-                              'range_bet',
-                              'higher_lower_bet',
-                              'run_bet',
-                              'legacy_bet',
-                              'digit_bet',
-                              'spread_bet'));
+        CHECK (
+               (table_name IN ('touch_bet',
+                               'range_bet',
+                               'higher_lower_bet',
+                               'run_bet',
+                               'legacy_bet',
+                               'digit_bet',
+                               'spread_bet')) IS TRUE
+        );
 
 ALTER TABLE ONLY bet.financial_market_bet
     DROP CONSTRAINT IF EXISTS pk_check_bet_class_value;
 
 ALTER TABLE ONLY bet.financial_market_bet
     ADD CONSTRAINT pk_check_bet_class_value
-        CHECK (bet_class IN ('higher_lower_bet',
-                             'range_bet',
-                             'touch_bet',
-                             'run_bet',
-                             'legacy_bet',
-                             'digit_bet',
-                             'spread_bet')
+        CHECK (
+               (bet_class IN ('higher_lower_bet',
+                              'range_bet',
+                              'touch_bet',
+                              'run_bet',
+                              'legacy_bet',
+                              'digit_bet',
+                              'spread_bet')) IS TRUE
         ) NOT VALID;
 
 ALTER TABLE ONLY bet.financial_market_bet
@@ -53,31 +56,26 @@ ALTER TABLE ONLY bet.financial_market_bet
 ALTER TABLE ONLY bet.financial_market_bet
     ADD CONSTRAINT basic_validation
         CHECK (
+           (
             purchase_time < '2014-05-09 00:00:00'::timestamp without time zone
             OR (NOT is_sold
-                OR 0::numeric <= sell_price
-                    AND (bet_class = 'spread_bet' OR sell_price <= payout_price)
-                    AND round(sell_price, 2) = sell_price
-                    AND purchase_time < sell_time)
-            AND 0::numeric < buy_price
-            AND (bet_class = 'spread_bet' OR 0::numeric < payout_price)
+                OR 0 <= sell_price
+                   AND (bet_class = 'spread_bet' OR sell_price <= payout_price)
+                   AND round(sell_price, 2) = sell_price
+                   AND purchase_time < sell_time)
+            AND 0 < buy_price
             AND round(buy_price, 2) = buy_price
-            AND round(payout_price, 2) = payout_price
             AND purchase_time <= start_time
-            AND (bet_class = 'spread_bet' OR
-                 (start_time <= expiry_time AND
-                  purchase_time <= settlement_time))
+            AND (bet_class = 'spread_bet'
+                 OR start_time <= expiry_time
+                    AND purchase_time <= settlement_time
+                    AND 0 < payout_price
+                    AND round(payout_price, 2) = payout_price)
+           ) IS TRUE
         ) NOT VALID;
 
 ALTER TABLE ONLY bet.financial_market_bet
    DROP CONSTRAINT IF EXISTS pk_check_bet_params_payout_price;
-
-ALTER TABLE ONLY bet.financial_market_bet
-   ADD CONSTRAINT pk_check_bet_params_payout_price
-        CHECK (bet_class IN ('legacy_bet',
-                             'spread_bet')
-                         OR (payout_price IS NOT NULL)
-        ) NOT VALID;
 
 CREATE TRIGGER prevent_action BEFORE DELETE ON bet.spread_bet
    FOR EACH STATEMENT EXECUTE PROCEDURE public.prevent_action();
@@ -87,5 +85,58 @@ VALUES ('SPREADU', false, 'spread_bet'),
        ('SPREADD', false, 'spread_bet');
 
 GRANT SELECT, UPDATE, INSERT ON bet.spread_bet to read, write;
+
+COMMIT;
+
+BEGIN;
+
+UPDATE pg_constraint
+   SET convalidated=NOT EXISTS (
+           SELECT *
+             FROM bet.financial_market_bet
+            WHERE NOT (
+               (bet_class IN ('higher_lower_bet',
+                              'range_bet',
+                              'touch_bet',
+                              'run_bet',
+                              'legacy_bet',
+                              'digit_bet',
+                              'spread_bet')) IS TRUE
+        )
+       )
+ WHERE NOT convalidated
+   AND conrelid='bet.financial_market_bet'::REGCLASS::OID
+   AND conname='pk_check_bet_class_value';
+
+COMMIT;
+
+BEGIN;
+
+UPDATE pg_constraint
+   SET convalidated=NOT EXISTS (
+           SELECT *
+             FROM bet.financial_market_bet
+            WHERE NOT (
+           (
+            purchase_time < '2014-05-09 00:00:00'::timestamp without time zone
+            OR (NOT is_sold
+                OR 0 <= sell_price
+                   AND (bet_class = 'spread_bet' OR sell_price <= payout_price)
+                   AND round(sell_price, 2) = sell_price
+                   AND purchase_time < sell_time)
+            AND 0 < buy_price
+            AND round(buy_price, 2) = buy_price
+            AND purchase_time <= start_time
+            AND (bet_class = 'spread_bet'
+                 OR start_time <= expiry_time
+                    AND purchase_time <= settlement_time
+                    AND 0 < payout_price
+                    AND round(payout_price, 2) = payout_price)
+           ) IS TRUE
+        )
+       )
+ WHERE NOT convalidated
+   AND conrelid='bet.financial_market_bet'::REGCLASS::OID
+   AND conname='basic_validation';
 
 COMMIT;
