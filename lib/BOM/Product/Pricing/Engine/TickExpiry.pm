5a4a33106a34fc497c11ebeda1795c78a41e4245
Jun Bon Kok <junbon@binary.com>
2015-08-05 15:26:03 +0800

    tie_factor is zero when there's economic events 10 minutes before and after the contract start time.

--- lib/BOM/Product/Pricing/Engine/TickExpiry.pm
+++ lib/BOM/Product/Pricing/Engine/TickExpiry.pm
@@ -225,12 +225,21 @@ sub _build_risk_markup {
 }
 
 sub _build_tie_factor {
+    my $self = shift;
+
+    my $ten_minutes_int = Time::Duration::Concise->new(interval => '10m');
+    my $contract_start = $self->effective_start;
+    my $start_period = $contract_start->epoch - $ten_minutes_int->seconds;
+    my $end_period = $contract_start->epoch + $ten_minutes_int->seconds;
+    my @economic_events = $self->get_applicable_economic_events($start_period, $end_period);
+    my $factor_base = (@economic_events) ? 0 :0.75;
+
     return Math::Util::CalculatedValue::Validatable->new({
         #This is the fraction of tie value that we return to clients.
         name        => 'tie_factor',
         description => 'A constant multiplier to ties coefficient',
         set_by      => __PACKAGE__,
-        base_amount => 0.75,
+        base_amount => $factor_base,
     });
 }
 
