9213268126fe721682eee9c8a5332dba6a594743
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-08-06 22:21:19 +0800

    it became ugly in this step

--- lib/BOM/WebSocketAPI/Websocket.pm
+++ lib/BOM/WebSocketAPI/Websocket.pm
@@ -9,8 +9,8 @@ use BOM::Platform::Client;
 use BOM::Product::Transaction;
 use BOM::Product::Contract::Finder;
 use BOM::Product::ContractFactory qw(produce_contract make_similar_contract);
-use BOM::WebSocketAPI::Symbols;
-use BOM::WebSocketAPI::Offerings;
+use BOM::WebAPI::Symbols;
+use BOM::WebAPI::Offerings;
 
 =head1 DESCRIPTION
 
@@ -287,7 +287,7 @@ my $json_receiver = sub {
                 json => {
                     msg_type => 'symbols',
                     echo_req => $p1,
-                    symbols  => BOM::WebSocketAPI::Symbols->active_symbols($by)}});
+                    symbols  => BOM::WebAPI::Symbols->active_symbols($by)}});
     }
 
     if (my $symbol = $p1->{contracts_for}) {
@@ -301,7 +301,7 @@ my $json_receiver = sub {
     }
 
     if (my $options = $p1->{offerings}) {
-        my $results = BOM::WebSocketAPI::Offerings::query($c, $options);
+        my $results = BOM::WebAPI::Offerings::query($c, $options);
         return $c->send({
                 json => {
                     msg_type  => 'offerings',
@@ -350,7 +350,7 @@ my $json_receiver = sub {
                     error    => "symbol $symbol invalid"
                 }});
         if ($p1->{end}) {
-            my $ticks = $c->BOM::WebSocketAPI::Symbols::_ticks(%$p1, ul => $ul);
+            my $ticks = $c->BOM::WebAPI::Symbols::_ticks(%$p1, ul => $ul);
             my $history = {
                 prices => [map { $_->{price} } @$ticks],
                 times  => [map { $_->{time} } @$ticks],
