bd90c6a1586147fc598d94542471f9863c6259fd
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-08-18 16:12:41 +0800

    Ugliness

---
+++ bin/json_2_html.pl
@@ -0,0 +1,57 @@
+use JSON;
+use File::Slurp;
+	use Data::Dumper;
+
+
+opendir(my $dh, 'config') || die;
+my @f = ();
+while(my $f = readdir $dh) {
+	push @f, "config/$f";
+}
+
+foreach my $f (reverse sort @f) {
+	if (-f $f and $f =~ /\_send.json$/) {
+		print_doc_send(JSON::from_json(File::Slurp::read_file($f)));
+
+	}
+	print "\n\n";
+	if (-f $f and $f =~ /\_receive.json$/) {
+		print_doc_receive(JSON::from_json(File::Slurp::read_file($f)));
+
+	}
+    
+}
+
+sub print_doc_receive {
+	my $data = shift;
+	print "<h1>".$data->{title}, "</h1>\n";
+	print $data->{description}, "\n";
+	print "<pre>{\n";
+	for my $p (keys %{$data->{properties}}) {
+		if ($data->{properties}->{$p}->{default}) {
+			print "\t".$p, ": ", $data->{properties}->{$p}->{default}, ",  // ", $data->{properties}->{$p}->{description}, "\n";
+		}else { 
+			print "\t".$p, ": ", ",  // ", $data->{properties}->{$p}->{description}, "\n";
+		}
+		print "\t"."{\n" if (keys %{$data->{properties}->{$p}->{properties}});
+		for my $t (keys %{$data->{properties}->{$p}->{properties}}) {
+			print	"\t"."\t".$t, ": ", $data->{properties}->{$p}->{properties}->{$t}->{default}, ",  // ", $data->{properties}->{$p}->{properties}->{$t}->{description}, "\n";
+
+		}
+		print "\t"."}\n" if (keys %{$data->{properties}->{$p}->{properties}});
+	}
+	print "}</pre>\n";
+
+}
+sub print_doc_send {
+	my $data = shift;
+	print "<h1>".$data->{title}, "</h1>\n";
+	print $data->{description}, "\n";
+	print "<pre>{\n";
+	for my $p (keys %{$data->{properties}}) {
+		print "\t".$p, ": ", $data->{properties}->{$p}->{default}, ",  // ", $data->{properties}->{$p}->{description}, "\n";
+	}
+	print "}</pre>\n";
+
+}
+closedir $dh;
