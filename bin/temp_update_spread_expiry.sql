c201841dd1e2e8ea0f7f5f37132dfff2f3540e74
Jun Bon Kok <junbon@binary.com>
2015-08-19 11:39:08 +0800

    update the sql script

--- bin/temp_update_spread_expiry.sql
+++ bin/temp_update_spread_expiry.sql
@@ -1,9 +1,42 @@
+SET statement_timeout = 0;
+
 BEGIN;
 
-UPDATE bet.financial_market_bet
+UPDATE bet.financial_market_bet fmb
     SET expiry_time = start_time::timestamp + interval '365 days',
-        settlement_time= start_time::timestamp + interval '365 days'
+        settlement_time = start_time::timestamp + interval '365 days',
+        payout_price = (
+            SELECT
+                CASE
+                    WHEN chld.stop_type='dollar' THEN chld.stop_profit
+                    WHEN chld.stop_type='point' THEN chld.stop_profit * chld.amount_per_point
+                END
+            FROM bet.spread_bet chld
+            WHERE fmb.id = chld.financial_market_bet_id
+        )
     WHERE bet_class='spread_bet'
-        AND expiry_time is null;
+        AND expiry_time IS NULL
+        AND payout_price IS NULL
+        AND is_sold IS TRUE;
+
+COMMIT;
+
+BEGIN;
+
+UPDATE bet.financial_market_bet fmb
+    SET expiry_time = start_time::timestamp + interval '365 days',
+        settlement_time = start_time::timestamp + interval '365 days',
+        payout_price = (
+            SELECT
+                CASE
+                    WHEN chld.stop_type='dollar' THEN chld.stop_profit
+                    WHEN chld.stop_type='point' THEN chld.stop_profit * chld.amount_per_point
+                END
+            FROM bet.spread_bet chld
+            WHERE fmb.id = chld.financial_market_bet_id
+        )
+    WHERE bet_class='spread_bet'
+        AND expiry_time IS NULL
+        AND payout_price IS NULL;
 
 COMMIT;
