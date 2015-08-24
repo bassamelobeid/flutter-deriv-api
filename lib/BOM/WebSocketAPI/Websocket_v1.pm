ca59e014033571f56f933b3445ffb119f2e103c3
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-08-24 21:53:36 +0800

    adding origin to data dog

--- lib/BOM/WebSocketAPI/Websocket_v1.pm
+++ lib/BOM/WebSocketAPI/Websocket_v1.pm
@@ -65,6 +65,12 @@ sub __handle {
     foreach my $dispatch (@dispatch) {
         next unless $p1->{$dispatch->[0]};
         DataDog::DogStatsd::Helper::stats_inc('websocket_api.call.' . $dispatch->[0]);
+        if (my $origin = $c->req->headers->header("Origin")) {
+            if (my $origin = $c->req->headers->header("Origin") and $origin =~ /https?:\/\/([a-zA-Z0-9\.]+)$/ and $origin = $1 and $origin =~ s/\./_/g ) {
+                DataDog::DogStatsd::Helper::stats_inc('websocket_api.origin.' . $origin);
+                DataDog::DogStatsd::Helper::stats_inc('websocket_api.origin.' . $origin. '.call.' . $dispatch->[0]);
+            }
+        }
         if ($dispatch->[2] and not $c->stash('client')) {
             return __authorize_error($dispatch->[3] || $dispatch->[0]);
         }
