93adc78afae7fc949ad7d16997e895a0314ea54d
Jun Bon Kok <junbon@binary.com>
2015-08-24 09:57:10 +0800

    error is in "$_"

--- lib/BOM/DailySummaryReport.pm
+++ lib/BOM/DailySummaryReport.pm
@@ -124,7 +124,7 @@ sub generate_report {
                         }
                         catch {
                             $logger->warn(
-                                "theo price error[$@], bet_id[" . $bet_id . "], account_id[$account_id], end_of_day_balance_id[" . $eod_id[0] . "]");
+                                "theo price error[$_], bet_id[" . $bet_id . "], account_id[$account_id], end_of_day_balance_id[" . $eod_id[0] . "]");
                             return 1;
                         } and next;
 
