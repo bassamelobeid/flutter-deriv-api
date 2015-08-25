712a41c66462975bddb53bda55525e78e60b358a
Jun Bon Kok <junbon@binary.com>
2015-08-25 15:12:38 +0800

    fixes spread shortcode parser

--- lib/BOM/Product/ContractFactory/Parser.pm
+++ lib/BOM/Product/ContractFactory/Parser.pm
@@ -143,7 +143,7 @@ sub shortcode_to_parameters {
         };
     }
 
-    if ($shortcode =~ /^(SPREADU|SPREADD)_([\w\d]+)_(\d*.?\d*)_(\d+)_(\d+.?\d*)_(\d+.?\d*)_(DOLLAR|POINT)/) {
+    if ($shortcode =~ /^(SPREADU|SPREADD)_([\w\d]+)_(\d*.?\d*)_(\d+)_(\d*.?\d*)_(\d*.?\d*)_(DOLLAR|POINT)/) {
         return {
             shortcode        => $shortcode,
             bet_type         => $1,
