8c1db53e5682e9e8bf0ec28d24330bccfcc7f351
Torsten Förtsch <torsten@binary.com>
2015-08-26 15:16:32 +0000

    tidy

--- lib/BOM/Product/Contract/Spreadu.pm
+++ lib/BOM/Product/Contract/Spreadu.pm
@@ -77,7 +77,7 @@ sub _recalculate_value {
     if ($level) {
         my $point_diff = $level - $self->barrier->as_absolute;
         my $value      = $point_diff * $self->amount_per_point;
-        $self->value(roundnear(0.01,$value));
+        $self->value(roundnear(0.01, $value));
         $self->point_value($self->underlying->pipsized_value($point_diff));
     }
 
