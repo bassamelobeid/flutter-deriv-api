eecda34fd17bc65d91d14482be2c168a8fa7cf58
Jun Bon Kok <junbon@binary.com>
2015-08-26 15:24:28 +0800

    changed longcode.

--- lib/BOM/Product/Contract/Spreadu.pm
+++ lib/BOM/Product/Contract/Spreadu.pm
@@ -96,9 +96,9 @@ sub _build__highlow_args {
 
 sub localizable_description {
     return {
-        dollar => 'Payout of [_1] [_2] for every point [_3] rises from entry level, with stop loss of [_6] [_4] and stop profit of [_6] [_5].',
+        dollar => '[_1] [_2] payout for every point [_3] rises from entry level, with stop loss of [_6] [_4] and stop profit of [_6] [_5].',
         point =>
-            'Payout of [_1] [_2] for every point [_3] rises from entry level, with stop loss of [_4] [plural,_4,point,points] and stop profit of [_5] [plural,_5,point,points].',
+            '[_1] [_2] payout for every point [_3] rises from entry level, with stop loss of [_4] [plural,_4,point,points] and stop profit of [_5] [plural,_5,point,points].',
     };
 }
 
