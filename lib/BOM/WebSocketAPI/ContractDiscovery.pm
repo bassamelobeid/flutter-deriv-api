90ec18794ae0800ab458209d9ae39abc0c11bca8
Yng Shan <yngshan@binary.com>
2015-08-27 14:29:07 +0000

    include region on contractDiscovery

--- lib/BOM/WebSocketAPI/ContractDiscovery.pm
+++ lib/BOM/WebSocketAPI/ContractDiscovery.pm
@@ -26,9 +26,8 @@ sub payout_currencies {
 
 sub contracts_for {
     my ($c, $args) = @_;
-    my $args_contracts_for = $args->{contracts_for};
-    my $symbol             = $args_contracts_for->{symbol};
-    my $region             = $args_contracts_for->{region} || 'other';
+    my $symbol = $args->{contracts_for};
+    my $region = $args->{region} || 'other';
     my $contracts_for;
     if ($region eq 'japan') {
         $contracts_for = BOM::Product::Contract::Finder::Japan::predefined_contracts_for_symbol({symbol => $symbol});
