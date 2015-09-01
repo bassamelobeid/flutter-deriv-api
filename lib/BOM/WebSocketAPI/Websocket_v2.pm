c00831880e15f67c2d2982c14380d4796e6a3e48
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-09-01 13:37:52 +0800

    test

--- lib/BOM/WebSocketAPI/Websocket_v2.pm
+++ lib/BOM/WebSocketAPI/Websocket_v2.pm
@@ -102,10 +102,10 @@ sub __handle {
         }
         my $result = $dispatch->[1]->($c, $p1);
 
-        $validator = JSON::Schema->new(JSON::from_json(File::Slurp::read_file("$f/receive.json")));
-        if (not $validator->validate($result)) {
-            die "Invalid results parameters.";
-        }
+#        $validator = JSON::Schema->new(JSON::from_json(File::Slurp::read_file("$f/receive.json")));
+#        if (not $validator->validate($result)) {
+#            die "Invalid results parameters.";
+#        }
 
         return $result;
     }
