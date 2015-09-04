44a49c9e4018df5af0c46dc203bde5e61c4d1bdd
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-09-04 12:00:31 +0800

    cleanupg

--- lib/BOM/WebSocketAPI/Websocket_v2.pm
+++ lib/BOM/WebSocketAPI/Websocket_v2.pm
@@ -96,9 +96,9 @@ sub __handle {
         my $validator = JSON::Schema->new(JSON::from_json(File::Slurp::read_file("$f/send.json")));
         if (not $validator->validate($p1)) {
             my $result = $validator->validate($p1);
-            print " - $_\n" foreach $result->errors;
-            print Data::Dumper::Dumper($p1);
-            die "Invalid input parameter for [" . $dispatch->[0] . "]";
+            my $error;
+            $error .= " - $_" foreach $result->errors;
+            die "Invalid input parameter for [" . $dispatch->[0] . " $error]";
         }
 
         my $tag = 'origin:';
