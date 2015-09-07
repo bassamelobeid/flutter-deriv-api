64d1dc077995d0271f441d98c39ae1b3cb6f706a
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-09-07 11:43:41 +0800

    was for test

--- lib/BOM/WebSocketAPI/Websocket_v2.pm
+++ lib/BOM/WebSocketAPI/Websocket_v2.pm
@@ -143,7 +143,6 @@ sub __handle {
             my $result = $validator->validate($p1);
             my $error;
             $error .= " - $_" foreach $result->errors;
-            File::Slurp::append_file('/tmp/a', $result."\n".$error);
             die "Invalid output parameter for [" . $dispatch->[0] . " $error]";
         }
 
