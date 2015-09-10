67cd5c2b39cc7826f85b2774f4a5c734c62c2bd5
Kaveh Mousavi Zamani <kaveh@binary.com>
2015-09-10 17:00:38 +0800

    return them all

--- lib/BOM/WebSocketAPI/v2/Symbols.pm
+++ lib/BOM/WebSocketAPI/v2/Symbols.pm
@@ -45,21 +45,17 @@ sub _description {
             submarket_display_name    => $ul->submarket->translated_display_name
         };
     } else {
-        if (!$ul->is_trading_suspended && $exchange_is_open) {
-            return {
-                symbol                 => $symbol,
-                display_name           => $ul->display_name,
-                symbol_type            => $ul->instrument_type,
-                market_display_name    => $ul->market->translated_display_name,
-                market                 => $ul->market->name,
-                submarket              => $ul->submarket->name,
-                submarket_display_name => $ul->submarket->translated_display_name,
-                exchange_is_open       => $exchange_is_open,
-                is_trading_suspended   => $ul->is_trading_suspended,
-            };
-        } else {
-            return;
-        }
+        return {
+            symbol                 => $symbol,
+            display_name           => $ul->display_name,
+            symbol_type            => $ul->instrument_type,
+            market_display_name    => $ul->market->translated_display_name,
+            market                 => $ul->market->name,
+            submarket              => $ul->submarket->name,
+            submarket_display_name => $ul->submarket->translated_display_name,
+            exchange_is_open       => $exchange_is_open,
+            is_trading_suspended   => $ul->is_trading_suspended,
+        };
     }
 }
 
