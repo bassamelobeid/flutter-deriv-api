b92a255d45a266b8cc859dc78b281aeb4264c6c3
Tee Shuwn Yuan <shuwnyuan@binary.com>
2015-09-14 16:19:06 +0800

    move websites.yml here

--- lib/BOM/Platform/Runtime.pm
+++ lib/BOM/Platform/Runtime.pm
@@ -195,7 +195,7 @@ sub _build_website_list {
     my $self = shift;
     return BOM::Platform::Runtime::Website::List->new(
         broker_codes => $self->broker_codes,
-        definitions  => YAML::CacheLoader::LoadFile('/home/git/regentmarkets/bom/config/files/websites.yml'),
+        definitions  => YAML::CacheLoader::LoadFile('/home/git/regentmarkets/bom-platform/config/websites.yml'),
         localhost    => $self->hosts->localhost,
     );
 }
