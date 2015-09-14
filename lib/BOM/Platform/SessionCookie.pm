de620562088ee5c7f5ad01aef1532f4d23204aed
Chris Travers <christravers@binary.com>
2015-09-13 22:18:14 -0700

    changing to situation where we invalidate cookies on login etc.

--- lib/BOM/Platform/SessionCookie.pm
+++ lib/BOM/Platform/SessionCookie.pm
@@ -69,20 +69,24 @@ my $TOKEN_LENGTH = 48;
 
 sub new {    ## no critic RequireArgUnpack
     my ($package) = shift;
+    local $@;
     my $self = ref $_[0] ? $_[0] : {@_};
     if ($self->{token}) {
         $self = eval { JSON::from_json(BOM::System::Chronicle->_redis_read->get('LOGIN_SESSION::' . $self->{token})) } || {};
         return bless {}, $package unless $self->{token};
+        my $email_cookie = eval { JSON::from_json(BOM::System::Chronicle->_redis_read->get('LOGIN_SESSION::BY_EMAIL::' . $self->{email}; }
+        if ($email_cookie and $email_cookie->{token} ne $email_cookie->{token}){
+            # ensures that once logging in, other session keys used are invalid.
+            bless $self, $package;
+            $self->end_session;
+            return bless {}, $package;
+        }
+        BOM::System::Chronicle->_redis_write->set('LOGIN_SESSION::BY_EMAIL::' . $self->{email}, JSON::to_json($self)) unless $email_cookie;
     } else {
         my @missing = grep { !$self->{$_} } @REQUIRED;
         croak "Error adding new session, missing: " . join(',', @missing)
             if @missing;
 
-        my @tokens = BOM::System::Chronicle->_redis_read->keys('LOGIN_SESSION::*');
-        for my $token (@tokens) {
-            my $cookie = BOM::Platform::SessionCookie->new({token => $token});
-            $cookie->end_session if $cookie->email and $cookie->email eq $self->{email};
-        }
         # NOTE, we need to use the object interface here. Bytes::Random::Secure also offers a function interface
         # but that uses a RNG which is initialized only once. If we happen to generate a session cookie for
         # whatever reason before forking children, all children would then generate the same random sequence.
@@ -91,10 +95,12 @@ sub new {    ## no critic RequireArgUnpack
             Bits        => 160,
             NonBlocking => 1,
         )->string_from($STRING, $TOKEN_LENGTH);
+        BOM::System::Chronicle->_redis_write->set('LOGIN_SESSION::BY_EMAIL' . $self->{email}, JSON::to_json($self));
         BOM::System::Chronicle->_redis_write->set('LOGIN_SESSION::' . $self->{token}, JSON::to_json($self));
     }
     $self->{expires_in} ||= $EXPIRES_IN;
     $self->{issued_at} = time;
+    BOM::System::Chronicle->_redis_write->expire('LOGIN_SESSION::BY_EMAIL::' . $self->{email}, $self->{expires_in});
     BOM::System::Chronicle->_redis_write->expire('LOGIN_SESSION::' . $self->{token}, $self->{expires_in});
     return bless $self, $package;
 }
