59e183e509e592ffd199231992dacddb5da0097b
Yng Shan <yngshan@binary.com>
2015-09-21 08:28:53 +0000

    fix typo

--- lib/BOM/Product/Contract/Finder/Japan.pm
+++ lib/BOM/Product/Contract/Finder/Japan.pm
@@ -161,7 +161,7 @@ sub _predefined_trading_period {
                 map { $today->plus_time_interval($_ . 'h') } ($odd_hour, $odd_hour - 2, $odd_hour - 4);
 
         }
-        my $key_expiry = $now_minute < 45 ? Date::Utility->new($now_date.' '.$now_hour.':45:00')->epoch : Date::Utility->new($now_date.' '.$now_hour + 1.':00:00')->epoch;
+        my $key_expiry = $now_minute < 45 ? Date::Utility->new($now_date.' '.$now_hour.':45:00')->epoch : Date::Utility->new($now_date.' '.$now_hour + 1 .':00:00')->epoch;
         Cache::RedisDB->set($cache_keyspace, $trading_key, $trading_periods, $key_expiry - $now->epoch);
     }
 
