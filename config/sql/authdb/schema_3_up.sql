8b019b9dfc3ff112e0f467b2396052c58c10607e
Fayland Lam <fayland@gmail.com>
2015-10-06 20:35:11 +0800

    with an id might be better

--- config/sql/authdb/schema_3_up.sql
+++ config/sql/authdb/schema_3_up.sql
@@ -1,7 +1,8 @@
 BEGIN;
 
 CREATE TABLE auth.access_token (
-    token CHAR(8) NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    token CHAR(8) NOT NULL UNIQUE,
     display_name VARCHAR(48) NOT NULL,
     client_loginid character varying(12) NOT NULL,
     last_used timestamp DEFAULT NULL
@@ -9,5 +10,6 @@ CREATE TABLE auth.access_token (
 CREATE INDEX idx_access_token_client_loginid ON auth.access_token USING btree (client_loginid);
 GRANT SELECT, INSERT, UPDATE, DELETE ON auth.access_token TO write;
 GRANT SELECT ON auth.access_token TO read;
+GRANT USAGE ON auth.access_token_id_seq TO write;
 
 COMMIT;
