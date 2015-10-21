ddd72687e0441355034a187ce8013bf1ab298efa
Raunak Kathuria <raunakkathuria@gmail.com>
2015-10-21 06:46:43 +0000

    remove routing for v1

--- lib/BOM/WebSocketAPI.pm
+++ lib/BOM/WebSocketAPI.pm
@@ -25,11 +25,6 @@ sub startup {
 
     my $r = $app->routes;
 
-    for ($r->under('/websockets/v1')) {
-        $_->to('Websocket_v1#ok');
-        $_->websocket('/')->to('#entry_point');
-    }
-
     for ($r->under('/websockets/v2')) {
         $_->to('Websocket_v2#ok');
         $_->websocket('/')->to('#entry_point');
@@ -37,12 +32,6 @@ sub startup {
 
     for ($r->under('/websockets/v3')) {
         $_->to('Websocket_v3#ok');
-        $_->websocket('/')->to('#entry_point');
-    }
-
-    # Alias, to be deprecated.
-    for ($r->under('/websockets/contracts')) {
-        $_->to('Websocket_v1#ok');
         $_->websocket('/')->to('#entry_point');
     }
 
