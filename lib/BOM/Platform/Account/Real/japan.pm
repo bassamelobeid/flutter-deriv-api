e8f87bfef34d46f37e1b525456d6520a3a16bfcb
Tee Shuwn Yuan <shuwnyuan@binary.com>
2015-10-21 17:36:50 +0800

    tidy

--- lib/BOM/Platform/Account/Real/japan.pm
+++ lib/BOM/Platform/Account/Real/japan.pm
@@ -21,8 +21,8 @@ sub _validate {
     return;
 #    return if ($from_client->residence eq 'jp');
 
-    get_logger()->warn("japan acc opening err: loginid:" . $from_client->loginid . " wrong residence:" . $from_client->residence);  ## no critic
-    return {err => 'invalid'};                                                                                                      ## no critic
+    get_logger()->warn("japan acc opening err: loginid:" . $from_client->loginid . " wrong residence:" . $from_client->residence);    ## no critic
+    return {err => 'invalid'};                                                                                                        ## no critic
 }
 
 sub create_account {
