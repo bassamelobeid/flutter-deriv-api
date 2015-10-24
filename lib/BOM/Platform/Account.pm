5317f92febd10083245bd026dca3fd3825f461f9
Tee Shuwn Yuan <shuwnyuan@binary.com>
2015-10-24 15:07:04 +0800

    minor change

--- lib/BOM/Platform/Account.pm
+++ lib/BOM/Platform/Account.pm
@@ -6,11 +6,11 @@ use warnings;
 use BOM::Platform::Runtime;
 
 sub get_real_acc_opening_type {
-    my $args = shift;
-    my ($residence, $from_client) = @{$args}{'residence', 'from_client'};
+    my $args        = shift;
+    my $from_client = $args->{from_client};
 
-    my $gaming_company    = BOM::Platform::Runtime->instance->gaming_company_for_country($residence);
-    my $financial_company = BOM::Platform::Runtime->instance->financial_company_for_country($residence);
+    my $gaming_company    = BOM::Platform::Runtime->instance->gaming_company_for_country($client->residence);
+    my $financial_company = BOM::Platform::Runtime->instance->financial_company_for_country($client->residence);
 
     if ($from_client->is_virtual) {
         if ($gaming_company) {
