17b758dd32d19b67a888e65f784ecc3f4eef9da9
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-10-28 09:56:45 +0800

    Adding begin/commit to make the strcuture change transactional

--- config/sql/rmgdb/schema_13_up.sql
+++ config/sql/rmgdb/schema_13_up.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 CREATE OR REPLACE FUNCTION notify_transaction_trigger() RETURNS trigger AS $$
 DECLARE
 BEGIN
@@ -7,3 +9,5 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER watched_transaction_trigger AFTER UPDATE OR INSERT ON transaction.transaction FOR EACH ROW EXECUTE PROCEDURE notify_transaction_trigger();
+
+COMMIT;
