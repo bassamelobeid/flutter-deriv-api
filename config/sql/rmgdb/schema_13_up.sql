6b2c5d18c59880b34e4e0adf993420e595a7a03f
Tee Shuwn Yuan <shuwnyuan@binary.com>
2015-10-28 12:40:28 +0800

    jp loginid sequence start: 1009

--- config/sql/rmgdb/schema_13_up.sql
+++ config/sql/rmgdb/schema_13_up.sql
@@ -3,16 +3,16 @@ BEGIN;
 SET search_path = sequences, pg_catalog;
 
 CREATE SEQUENCE loginid_sequence_vrtj
-    START WITH 1000
+    START WITH 1009
     INCREMENT BY 1
-    MINVALUE 1000
+    MINVALUE 1009
     NO MAXVALUE
     CACHE 1;
 
 CREATE SEQUENCE loginid_sequence_jp
-    START WITH 1000
+    START WITH 1009
     INCREMENT BY 1
-    MINVALUE 1000
+    MINVALUE 1009
     NO MAXVALUE
     CACHE 1;
 
