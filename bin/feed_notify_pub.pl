99b98fc91634cf741245c0520f0aa5930731a485
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-11-06 16:37:33 +0800

    listen and publish for feed

---
+++ bin/feed_notify_pub.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.010;
+use YAML::XS;
+use DBI;
+use DBD::Pg;
+use IO::Select;
+use Try::Tiny;
+use RedisDB;
+use JSON;
+use BOM::Database::FeedDB;
+
+my $conn = _master_db_connections();
+
+
+
+while (1) {
+    try {
+        my $redis = _redis();
+        my $dbh   = BOM::Database::FeedDB::write_dbh();
+
+        $dbh->do("LISTEN ohlc_watchers");
+
+        my $sel = IO::Select->new;
+        $sel->add($dbh->{pg_socket});
+        while ($sel->can_read) {
+            while (my $notify = $dbh->pg_notifies) {
+                my ($name, $pid, $payload) = @$notify;
+                _publish($redis, _msg($payload));
+            }
+        }
+    }
+    catch {
+        warn "$0 ($$): saw exception: $_";
+        sleep 1;
+    };
+}
+exit;
+
+sub _publish {
+    my $redis = shift;
+    my $msg   = shift;
+
+    $redis->publish('FEED::' . $msg->{type}. '_'.$msg->{underlying}, JSON::to_json($msg));
+
+}
+
+sub _msg {
+    my $payload = shift;
+
+    my %msg;
+    if ($payload ~= /tick/) {
+        @msg{qw/type underlying ts spot/} = split(',', $payload);
+    } else {
+        @msg{qw/type underlying ts open high low close/} = split(',', $payload);
+    }
+
+    return \%msg;
+}
+
+sub _redis {
+    my $config = YAML::XS::LoadFile('/etc/rmg/chronicle.yml');
+    return RedisDB->new(
+        host     => $config->{write}->{host},
+        port     => $config->{write}->{port},
+        password => $config->{write}->{password});
+}
