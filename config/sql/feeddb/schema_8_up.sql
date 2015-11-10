6fb4aedf608d01428004809477c2caafaab4eaad
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-11-10 14:11:39 +0800

    faster ohlc streaming means combining a using a faster pl lang that is suprisingly perl!

--- config/sql/feeddb/schema_8_up.sql
+++ config/sql/feeddb/schema_8_up.sql
@@ -1,50 +1,65 @@
 BEGIN;
+CREATE OR REPLACE PROCEDURAL LANGUAGE plperl;
 
-CREATE TABLE feed.realtime_ohlc (
-    granuality BIGINT NOT NULL,
+CREATE UNLOGGED TABLE feed.realtime_ohlc (
     underlying VARCHAR(128) NOT NULL,
-    ts TIMESTAMP NOT NULL,
-    open DOUBLE PRECISION NOT NULL,
-    high DOUBLE PRECISION NOT NULL,
-    low DOUBLE PRECISION NOT NULL,
-    close DOUBLE PRECISION NOT NULL,
-    PRIMARY KEY (granuality,underlying)
+    ts BIGINT NOT NULL,
+    ohlc TEXT NOT NULL,
+    PRIMARY KEY (underlying,ts)
 );
 
+-- underlying VARCHAR(128),ts EPOCH, spot DOUBLE PRECISION
+CREATE OR REPLACE FUNCTION tick_notify(VARCHAR(128),BIGINT,DOUBLE PRECISION) RETURNS TEXT AS
+$tick_notify$
+  my $underlying = $_[0];
+  my $ts = $_[1];
+  my $spot = $_[2];
+  my @grans = qw(60 120 300 600 900 1800 3600 7200 14400 28800 86400);
 
-CREATE OR REPLACE FUNCTION update_realtime_ohlc(fgranuality BIGINT, funderlying VARCHAR(128),fts TIMESTAMP, fspot DOUBLE PRECISION)
-RETURNS INT AS $$
-DECLARE last_aggregation_period BIGINT;
-DECLARE tick_period BIGINT;
-BEGIN
-    SELECT EXTRACT(EPOCH FROM ts)::BIGINT -  EXTRACT(EPOCH FROM ts)::BIGINT % (fgranuality) INTO last_aggregation_period FROM feed.realtime_ohlc where underlying=funderlying and granuality=fgranuality;
-    SELECT EXTRACT(EPOCH FROM fts)::BIGINT -  EXTRACT(EPOCH FROM fts)::BIGINT % (fgranuality) INTO tick_period;
+  # Get the only record which should exist for each underlying
+  $rv = spi_exec_query("SELECT * FROM feed.realtime_ohlc where underlying='$underlying'", 1);
 
-    IF last_aggregation_period IS NOT NULL and tick_period = last_aggregation_period THEN
-      UPDATE feed.realtime_ohlc SET
-          ts=fts,
-          high = GREATEST(fspot, high),
-          low  = LEAST(fspot, low),
-          close = fspot
-      WHERE
-          underlying = funderlying and granuality = fgranuality;
-    ELSE
-      DELETE FROM feed.realtime_ohlc WHERE underlying = funderlying and granuality = fgranuality;
-      INSERT INTO feed.realtime_ohlc VALUES (fgranuality, funderlying, fts, fspot, fspot, fspot, fspot);
-    END IF;
-    RETURN 0;
-END;
-$$
-LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION notify_realtime_ohlc_trigger() RETURNS trigger AS $$
-DECLARE
-BEGIN
-  PERFORM pg_notify('feed_watchers' , NEW.granuality || ',' || NEW.underlying || ',' || NEW.ts || ',' || NEW."open" || ',' || NEW.high || ',' || NEW.low || ',' || NEW."close" );
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER watched_ohlc_trigger BEFORE UPDATE OR INSERT ON feed.realtime_ohlc FOR EACH ROW EXECUTE PROCEDURE notify_realtime_ohlc_trigger();
+  # If there is not record insert one.
+  if (!$rv->{rows}[0]->{ohlc}) {
+    $all_same = "$spot,$spot,$spot,$spot";
+    $ohlc_val='';
+    foreach (@grans) {$ohlc_val .= "$_:$all_same;"}
+    $rv = spi_exec_query("INSERT INTO feed.realtime_ohlc VALUES ('$underlying', $ts, '$ohlc_val')");
+  # If there is any record update it for each granuality
+  } else {
+    # Find array of all ohlc values saved in ohlc field in text type
+    $N = '([.0-9+-]+),([.0-9+-]+),([.0-9+-]+),([.0-9+-]+)';
+    $pattern = "";
+    foreach (@grans) {$pattern .= "$_:$N;"}
+    @match = ($rv->{rows}[0]->{ohlc} =~ /$pattern/g);
+
+    # convert the array to hash to using it a bit less error prone
+    my $m;
+    $c=0;
+    foreach (@grans) {$m->{"o_$_"}=$match[$c];$c++;$m->{"h_$_"}=$match[$c];$c++;$m->{"l_$_"}=$match[$c];$c++;$m->{"c_$_"}=$match[$c];$c++;}
+
+    # go through all granualities and update them
+    $ohlc_val='';
+    foreach $g (@grans) {
+      # if last ohlc ts is still in same time period of new tick ts for that granuality
+      if (($ts - $ts % $g) == ($rv->{rows}[0]->{ts} - $rv->{rows}[0]->{ts} % $g)) {
+        $ohlc_val.="$g:".$m->{"o_$g"}.",";
+        $ohlc_val.=($spot>$m->{"h_$g"})? "$spot,":$m->{"h_$g"}.",";
+        $ohlc_val.=($spot<$m->{"l_$g"})? "$spot,":$m->{"l_$g"}.",";
+        $ohlc_val.="$spot;";
+      # if this is a new period reset all ohlc values for that period
+      } else {
+        $ohlc_val.="$g:$spot,$spot,$spot,$spot;";
+      }
+    }
+    $rv = spi_exec_query("UPDATE feed.realtime_ohlc SET ts=$ts, ohlc='$ohlc_val' where underlying='$underlying'");
+    $rv = spi_exec_query("SELECT pg_notify('feed_watchers', '$ts;$spot;$ohlc_val');");
+  }
+
+return $ohlc_val;
+$tick_notify$
+LANGUAGE 'plperl';
 
 GRANT SELECT, INSERT, UPDATE, DELETE, TRIGGER ON ALL TABLES IN SCHEMA feed TO write;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA feed TO write;
