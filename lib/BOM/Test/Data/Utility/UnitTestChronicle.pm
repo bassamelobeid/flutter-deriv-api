557e6d27a2177d9d2401422a703da2142ea4e568
Mahdi Mohammadi <mm@binary.com>
2015-11-16 07:51:32 +0000

    create_doc same as couchdb

--- lib/BOM/Test/Data/Utility/UnitTestChronicle.pm
+++ lib/BOM/Test/Data/Utility/UnitTestChronicle.pm
@@ -63,4 +63,42 @@ sub init_chronicle {
     $pg->disconnect();
 }
 
+=head2 create doc()
+
+    Create a new document in the test database
+
+    params:
+    $yaml_db  => The name of the entity in the YAML file (eg. promo_code)
+    $data_mod       => hasref of modifictions required to the data (optional)
+
+=cut
+
+sub create_doc {
+    my ($yaml_db, $data_mod) = @_;
+
+    my $save = 1;
+    if (exists $data_mod->{save}) {
+        $save = delete $data_mod->{save};
+    }
+
+    # get data to insert
+    my $fixture = YAML::XS::LoadFile('/home/git/regentmarkets/bom-test/data/couch_unit_test.yml');
+    my $data    = $fixture->{$yaml_db}{data};
+
+    # modify data?
+    for (keys %$data_mod) {
+        $data->{$_} = $data_mod->{$_};
+    }
+
+    # use class to create the Couch doc
+    my $class_name = $fixture->{$yaml_db}{class_name};
+    my $obj        = $class_name->new($data);
+
+    if ($save) {
+        $obj->save;
+    }
+
+    return $obj;
+}
+
 1;
