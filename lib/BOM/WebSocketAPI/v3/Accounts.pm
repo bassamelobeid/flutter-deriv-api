04459efc5db7a1f9b7af190a2c0b9b53a402b6d6
Fayland Lam <fayland@gmail.com>
2015-11-19 20:28:12 +0800

    fix replacement error

--- lib/BOM/WebSocketAPI/v3/Accounts.pm
+++ lib/BOM/WebSocketAPI/v3/Accounts.pm
@@ -326,7 +326,7 @@ sub get_settings {
         get_settings => {
             email         => $client->email,
             date_of_birth => Date::Utility->new($client->date_of_birth)->epoch,
-            country       => BOM::Platform::Runtime->instance->countries->$c->ld_code2country($client->residence, $r->language),
+            country       => BOM::Platform::Runtime->instance->countries->localized_code2country($client->residence, $r->language),
             $client->is_virtual
             ? ()
             : (
