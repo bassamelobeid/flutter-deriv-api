01332792f9aba8257f98330ea809cd62269aeb52
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-11-23 09:22:43 +0800

    correct check and handling tickS

--- lib/BOM/WebSocketAPI/v3/System.pm
+++ lib/BOM/WebSocketAPI/v3/System.pm
@@ -36,7 +36,8 @@ sub forget_all {
                 $k =~ /(.*);(.*)/;
                 my $fsymbol = $1;
                 my $ftype   = $2;
-                if ($type =~ /^$ftype/) {
+                # . 's' while we are still using tickS in this calls. backward compatibility that must be removed.
+                if ($ftype . 's' =~ /^$type/) {
                     push @removed_ids, $c->stash('feed_channel_type')->{$k}->{uuid};
                     BOM::WebSocketAPI::v3::MarketDiscovery::_feed_channel($c, 'unsubscribe', $fsymbol, $ftype);
                 }
