e5a20b08e22798daf17fb84b4bfa9c20f7c78e41
Raunak Kathuria <raunakkathuria@gmail.com>
2015-11-24 03:25:29 +0000

    add wrapper for Authorize that will handle stash and runtime thing

---
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Authorize.pm
@@ -0,0 +1,40 @@
+package BOM::WebSocketAPI::v3::Wrapper::Authorize;
+
+use strict;
+use warnings;
+
+use BOM::WebSocketAPI::v3::Authorize;
+use BOM::Platform::Client;
+
+sub authorize {
+    my ($c, $args) = @_;
+    my $token = $args->{authorize};
+
+    my $response = BOM::WebSocketAPI::v3::Authorize::authorize($token);
+
+    if (exists $response->{error}) {
+        return $c->new_error('authorize', $response->{error}->{code}, $response->{error}->{message});
+    } else {
+        my $client = BOM::Platform::Client->new({loginid => $response->{loginid}});
+
+        my $token_type = 'session_token';
+        if (length $token == 15) {
+            $token_type = 'api_token';
+        }
+
+        $c->stash(
+            loginid    => $loginid,
+            token_type => $token_type,
+            client     => $client,
+            account    => $client->default_account,
+        );
+
+        return {
+            msg_type  => 'authorize',
+            authorize => {%$response}};
+    }
+
+    return;
+}
+
+1;
