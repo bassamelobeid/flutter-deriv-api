9b434465330fae0af48283f7ca71a811b5d0f912
Mahdi Mohammadi <mm@binary.com>
2015-11-25 07:59:30 +0000

    remove tests

--- lib/BOM/Market/Underlying.pm
+++ lib/BOM/Market/Underlying.pm
@@ -1990,17 +1990,6 @@ sub _build_corporate_actions {
     foreach my $id (keys %$available_actions) {
         my $act      = $available_actions->{$id};
         my $eff_date = $act->{effective_date};
-
-        if ( not defined $eff_date ) {
-           print STDERR "err_date is not defined!";
-           use Data::Dumper;
-
-           print  STDERR Dumper($corp);
-           print STDERR $self->symbol;
-           die "ERROR effective_date is undefined! " . Dumper($corp);
-
-        }
-
         push @{$grouped_by_date{$eff_date}}, $act;
     }
 
