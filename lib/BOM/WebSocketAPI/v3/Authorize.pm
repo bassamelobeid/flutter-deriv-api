a3d2a57c0d846f34df8d6eb079bf6bb73d82d932
Raunak Kathuria <raunakkathuria@gmail.com>
2015-11-25 03:52:28 +0000

    make Authorize use new create_error format

--- lib/BOM/WebSocketAPI/v3/Authorize.pm
+++ lib/BOM/WebSocketAPI/v3/Authorize.pm
@@ -13,7 +13,9 @@ use BOM::Database::Model::AccessToken;
 sub authorize {
     my $token = shift;
 
-    my $err = BOM::WebSocketAPI::v3::Utility::create_error('InvalidToken', BOM::Platform::Context::localize('The token is invalid.'));
+    my $err = BOM::WebSocketAPI::v3::Utility::create_error({
+            code              => 'InvalidToken',
+            message_to_client => BOM::Platform::Context::localize('The token is invalid.')});
 
     my $loginid;
     if (length $token == 15) {    # access token
