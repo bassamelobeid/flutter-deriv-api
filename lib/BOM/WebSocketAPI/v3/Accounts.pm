79ec9d2d2ae82908bbc9919197982619022ac5ba
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-11-25 16:11:50 +0800

    quiet tests

--- lib/BOM/WebSocketAPI/v3/Accounts.pm
+++ lib/BOM/WebSocketAPI/v3/Accounts.pm
@@ -121,7 +121,7 @@ sub get_transactions {
 
         if ($txn->{short_code}) {
             ($struct->{longcode}, undef, undef) = try { simple_contract_info($txn->{short_code}, $acc->currency_code) };
-            $struct->{longcode} = Mojo::DOM->new->parse($struct->{longcode})->all_text;
+            $struct->{longcode} = Mojo::DOM->new->parse($struct->{longcode})->all_text if $struct->{longcode};
         }
         push @txns, $struct;
     }
