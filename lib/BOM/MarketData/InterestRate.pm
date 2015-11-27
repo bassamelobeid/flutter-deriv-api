8c95900aaaff113a2bde92d2fe7f208bdc6e362a
Mahdi Mohammadi <mm@binary.com>
2015-11-27 03:40:38 +0000

    initial commit

--- lib/BOM/MarketData/InterestRate.pm
+++ lib/BOM/MarketData/InterestRate.pm
@@ -15,9 +15,16 @@ extends 'BOM::MarketData::Rates';
 
 use Math::Function::Interpolator;
 
-has '_data_location' => (
+=head2 for_date
+
+The date for which we wish data
+
+=cut
+
+has for_date => (
     is      => 'ro',
-    default => 'interest_rates',
+    isa     => 'Maybe[Date::Utility]',
+    default => undef,
 );
 
 around _document_content => sub {
@@ -31,25 +38,44 @@ around _document_content => sub {
     };
 };
 
-=head2 VersionedSymbolData
+=head2 document
 
-As this module inherits from VersionedSymbolData we need to manipulate the inheritance so that we can "inject"
-our Chronicle saving code in the inherited "save" subroutine.
+The CouchDB document that this object is tied to.
 
 =cut
 
-with 'BOM::MarketData::Role::VersionedSymbolData' => {
-    -alias    => {save => '_save'},
-    -excludes => ['save']};
+has document => (
+    is         => 'rw',
+    lazy_build => 1,
+);
+
+sub _build_document {
+    my $self = shift;
+
+    my $document = BOM::System::Chronicle::get('interest_rates', $self->symbol);
+
+    if ($self->for_date and $self->for_date->datetime_iso8601 lt $document->{date}) {
+        $document = BOM::System::Chronicle::get_for('interest_rates', $self->symbol, $self->for_date->epoch);
+
+        # This works around a problem with Volatility surfaces and negative dates to expiry.
+        # We have to use the oldest available surface.. and we don't really know when it
+        # was relative to where we are now.. so just say it's from the requested day.
+        # We do not allow saving of historical surfaces, so this should be fine.
+        $document //= {};
+        $document->{date} = $self->for_date->datetime_iso8601;
+    }
+
+    return $document;
+}
 
 sub save {
     my $self = shift;
 
-    #first call original save method to save all data into CouchDB just like before
-    my $result = $self->_save();
+    if (not defined BOM::System::Chronicle::get('interest_rates', $self->symbol)) {
+        BOM::System::Chronicle::set('interest_rates', $self->symbol, {});
+    }
 
-    BOM::System::Chronicle::set('interest_rates', $self->symbol, $self->_document_content);
-    return $result;
+    return BOM::System::Chronicle::set('interest_rates', $self->symbol, $self->_document_content);
 }
 
 has type => (
