ce3e02160f29285f312122cea2aa16cecb728b4d
Raunak Kathuria <raunakkathuria@gmail.com>
2015-12-02 04:05:00 +0000

    move proposal_open_contract to wrapper, get_bid as only lib function

--- lib/BOM/WebSocketAPI/v3/PortfolioManagement.pm
+++ lib/BOM/WebSocketAPI/v3/PortfolioManagement.pm
@@ -111,37 +111,6 @@ sub sell {
     };
 }
 
-sub proposal_open_contract {    ## no critic (Subroutines::RequireFinalReturn)
-    my ($c, $args) = @_;
-
-    my $client = $c->stash('client');
-    my $source = $c->stash('source');
-
-    my @fmbs = ();
-    if ($args->{contract_id}) {
-        @fmbs = grep { $args->{contract_id} eq $_->id } $client->open_bets;
-    } else {
-        @fmbs = $client->open_bets;
-    }
-
-    if (scalar @fmbs > 0) {
-        foreach my $fmb (@fmbs) {
-            my $details = {%$args};
-            $details->{short_code} = $fmb->short_code;
-            $details->{fmb_id}     = $fmb->id;
-            $details->{currency}   = $client->currency;
-
-            my $id = BOM::WebSocketAPI::v3::MarketDiscovery::_feed_channel($c, 'subscribe', $fmb->underlying_symbol,
-                'proposal_open_contract:' . JSON::to_json($args));
-            send_bid($c, $id, $args, $details);
-        }
-    } else {
-        return {
-            msg_type               => 'proposal_open_contract',
-            proposal_open_contract => {}};
-    }
-}
-
 sub portfolio {
     my $client = shift;
 
@@ -233,27 +202,6 @@ sub get_bid {
     }
 
     return \%returnhash;
-}
-
-sub send_bid {
-    my ($c, $id, $args, $details) = @_;
-
-    my $latest = get_bid($details->{short_code}, $details->{fmb_id}, $details->{currency});
-
-    my $response = {
-        msg_type => 'proposal_open_contract',
-        echo_req => $args,
-    };
-
-    $c->send({
-            json => {
-                %$response,
-                proposal_open_contract => {
-                    id => $id,
-                    %$latest
-                }}});
-
-    return;
 }
 
 1;
