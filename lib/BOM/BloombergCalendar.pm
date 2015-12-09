c998b5b0f451b626a705e66e9c0e2bab5115e144
Jun Bon Kok <junbon@binary.com>
2015-12-09 13:36:09 +0800

    modified the backoffice holiday upload tool

--- lib/BOM/BloombergCalendar.pm
+++ lib/BOM/BloombergCalendar.pm
@@ -5,29 +5,80 @@ use strict;
 use warnings;
 
 use BOM::System::Chronicle;
+use File::Temp ();
+use Try::Tiny;
+use File::Copy;
 use Text::CSV::Slurp;
 use Date::Utility;
 use YAML::CacheLoader qw(LoadFile);
 
-state $calendar_code_mapper = LoadFile('/home/git/regentmarkets/bom-backoffice/config/bloomberg_calendar_code_mapper.yml');
-
 sub save_calendar {
-    my ($data_ref, $type) = @_;
+    my ($calendar, $calendar_type) = @_;
 
-    my $calendar;
-    foreach my $date (keys %$data_ref) {
-        my $data = $data_ref->{$date};
-        foreach my $ref (@$data) {
-            foreach my $exchange (@{$calendar_code_mapper->{$ref->{calendar_code}}}) {
-                $calendar->{$exchange}->{$date} = $ref->{desc};
-            }
-        }
-    }
     # don't have to include synthetics for country holidays
-    _include_synthetic($calendar) if $type ne 'country';
-    my $updated = map { BOM::System::Chronicle::set($type, $_, $calendar->{$_}) } keys %$calendar;
+    _include_synthetic($calendar) if $calendar_type ne 'country-holiday';
+    my $updated = map { BOM::System::Chronicle::set($calendar_type, $_, $calendar->{$_}) } keys %$calendar;
 
     return $updated;
+}
+
+sub parse_calendar {
+    my ($file, $calendar_type) = @_;
+
+    my $csv = Text::CSV::Slurp->load(file => $file);
+    my @holiday_data;
+    if ($calendar_type eq 'exchange-holiday') {
+        @holiday_data = grep {defined $_->{Trading} and $_->{Trading} =~ /No/} @$csv
+    } elsif ($calendar_type eq 'country-holiday') {
+        @holiday_data = grep { defined $_->{Settle} and $_->{Settle} =~ /No/} @$csv
+    } elsif ($calendar_type eq 'trading-time') {
+        @holiday_data = grep {defined $_->{Trading} and $_->{Trading} =~ /Partial/} @$csv;
+    }
+
+    my $data = _process(@holiday_data);
+
+    return $data;
+}
+
+sub backup_file {
+    my $file = shift;
+
+    my $fh = File::Temp->new(SUFFIX => '.csv');
+    my $filename = $fh->filename;
+    copy($file, $filename);
+
+    return;
+}
+
+sub _process {
+    my @data = @_;
+
+    state $calendar_code_mapper = LoadFile('/home/git/regentmarkets/bom-backoffice/config/bloomberg_calendar_code_mapper.yml');
+
+    my $output;
+    foreach my $data (@data) {
+        my $date          = $data->{'Date'};
+        next unless $date;
+        my $calendar_code = $data->{'Code'};
+        my $description       = $data->{'Holidays/Events'};
+
+        if ($date =~ /(\d{1,2})\/(\d{1,2})\/(\d{2})$/) {
+            my $year = $3;
+            if ($year < 99) {
+                $year = '20' . $year;
+            }
+            $date = Date::Utility->new($year . '-' . sprintf('%02d', $1) . '-' . sprintf('%02d', $2));
+        } elsif ($date =~ /(\d{1,2})\/(\d{1,2})\/(\d{4})$/) {
+            $date = Date::Utility->new($3 . '-' . sprintf('%02d', $1) . '-' . sprintf('%02d', $2));
+        }
+
+        my @affected_symbols = @{$calendar_code_mapper->{$calendar_code}};
+        foreach my $symbol (@affected_symbols) {
+            $output->{$symbol}{$date->date_ddmmmyyyy} = $description;
+        }
+    }
+
+    return $output;
 }
 
 sub _include_synthetic {
@@ -53,59 +104,4 @@ sub _include_synthetic {
 
     return;
 }
-
-sub parse_exchange_holiday {
-    my $file = shift;
-
-    my $csv = Text::CSV::Slurp->load(file => $file);
-    my @holiday_data = grep {defined $_->{Trading} and $_->{Trading} =~ /No/} @$csv;
-    my $data = _process(@holiday_data);
-
-    return $data;
-}
-
-sub parse_country_holiday {
-    my $file = shift;
-
-    my $csv = Text::CSV::Slurp->load(file => $file);
-    my @holiday_data = grep { defined $_->{Settle} and $_->{Settle} =~ /No/} @$csv;
-    my $data = _process(@holiday_data);
-
-    return $data;
-}
-
-sub parse_early_close_calendar {
-    my $file = shift;
-
-    my $csv = Text::CSV::Slurp->load(file => $file);
-    my @early_close_data = grep {defined $_->{Trading} and $_->{Trading} =~ /Partial/} @$csv;
-    my $data = _process(@early_close_data);
-
-    return $data;
-}
-
-sub _process {
-    my @data = @_;
-
-    my $output;
-    foreach my $data (@data) {
-        my $date          = $data->{'Date'};
-        next unless $date;
-        my $calendar_code = $data->{'Code'};
-        my $description       = $data->{'Holidays/Events'};
-
-        if ($date =~ /(\d{1,2})\/(\d{1,2})\/(\d{2})$/) {
-            my $year = $3;
-            if ($year < 99) {
-                $year = '20' . $year;
-            }
-            $date = Date::Utility->new($year . '-' . sprintf('%02d', $1) . '-' . sprintf('%02d', $2));
-        } elsif ($date =~ /(\d{1,2})\/(\d{1,2})\/(\d{4})$/) {
-            $date = Date::Utility->new($3 . '-' . sprintf('%02d', $1) . '-' . sprintf('%02d', $2));
-        }
-
-        push @{$output->{$date->truncate_to_day->date_ddmmmyyyy}}, {calendar_code => $calendar_code, desc => $description};
-    }
-
-    return $output;
-}
+1;
