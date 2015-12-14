081425ad094fb435850bd00daf24752f9695cccb
Fayland Lam <fayland@gmail.com>
2015-12-14 20:23:28 +0800

    Add logout

--- lib/BOM/RPC/v3/Authorize.pm
+++ lib/BOM/RPC/v3/Authorize.pm
@@ -5,7 +5,10 @@ use warnings;
 
 use BOM::RPC::v3::Utility;
 
+use Date::Utility;
+use BOM::System::AuditLog;
 use BOM::Platform::Client;
+use BOM::Platform::User;
 use BOM::Platform::SessionCookie;
 use BOM::Platform::Context qw (localize);
 use BOM::Database::Model::AccessToken;
@@ -42,6 +45,39 @@ sub authorize {
         currency => ($account ? $account->currency_code : ''),
         email    => $client->email,
     };
+}
+
+sub logout {
+    my ($r, $ua) = @_;
+
+    my $email   = $r->email   // '';
+    my $loginid = $r->loginid // '';
+
+    # Invalidates token, but we can only do this if we have a cookie
+    $r->session_cookie->end_session if $r->session_cookie;
+
+    if ($email) {
+        if (my $user = BOM::Platform::User->new({email => $email})) {
+            $user->add_login_history({
+                environment => login_env($r, $ua),
+                successful  => 't',
+                action      => 'logout',
+            });
+            $user->save;
+        }
+        BOM::System::AuditLog::log("user logout", "$email,$loginid");
+    }
+}
+
+sub login_env {
+    my ($r, $ua) = @_;
+
+    my $now                = Date::Utility->new->datetime_ddmmmyy_hhmmss_TZ;
+    my $ip_address         = $r->client_ip || '';
+    my $ip_address_country = uc $r->country_code || '';
+    my $lang               = uc $r->language || '';
+    my $environment        = "$now IP=$ip_address IP_COUNTRY=$ip_address_country User_AGENT=$ua LANG=$lang";
+    return $environment;
 }
 
 1;
