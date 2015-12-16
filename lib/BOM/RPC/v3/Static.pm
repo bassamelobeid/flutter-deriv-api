90b9e3cfd0901b6ad45730b4af323a6ab9f193ae
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-12-16 15:27:49 +0800

    and lang

--- lib/BOM/RPC/v3/Static.pm
+++ lib/BOM/RPC/v3/Static.pm
@@ -4,8 +4,13 @@ use strict;
 use warnings;
 
 use BOM::Platform::Locale;
+use BOM::Platform::Context;
 
 sub residence_list {
+    my $params = shift;
+
+    BOM::Platform::Context::request()->language($params->{language});
+
     my $residence_list = BOM::Platform::Locale::generate_residence_countries_list();
     $residence_list = [grep { $_->{value} ne '' } @$residence_list];
     return $residence_list,;
@@ -13,6 +18,8 @@ sub residence_list {
 
 sub states_list {
     my $params = shift;
+
+    BOM::Platform::Context::request()->language($params->{language});
 
     my $args    = $params->{args};
     my $country = $args->{states_list};
