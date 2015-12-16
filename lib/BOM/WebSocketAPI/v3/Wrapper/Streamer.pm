011380c7a10397a0f61d90a10b8ba483c612a68f
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-12-16 16:46:27 +0800

    one async then sync?!

--- lib/BOM/WebSocketAPI/v3/Wrapper/Streamer.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Streamer.pm
@@ -73,7 +73,39 @@ sub proposal {
         return $c->new_error('proposal', $response->{error}->{code}, $response->{error}->{message_to_client});
     } else {
         my $id = _feed_channel($c, 'subscribe', $symbol, 'proposal:' . JSON::to_json($args));
-        send_ask($c, $id, $args);
+        async_send_ask($c, $id, $args);
+    }
+}
+
+sub async_send_ask {
+    my ($c, $id, $args) = @_;
+
+    my %details  = %{$args};
+    my $response = BOM::RPC::v3::Contract::get_ask(BOM::RPC::v3::Contract::prepare_ask(\%details));
+    if ($response->{error}) {
+        BOM::WebSocketAPI::v3::Wrapper::System::forget_one($c, $id);
+
+        my $proposal = {id => $id};
+        $proposal->{longcode}  = delete $response->{longcode}  if $response->{longcode};
+        $proposal->{ask_price} = delete $response->{ask_price} if $response->{ask_price};
+        $c->send({
+                json => {
+                    msg_type => 'proposal',
+                    echo_req => $args,
+                    (exists $args->{req_id}) ? (req_id => $args->{req_id}) : (),
+                    proposal => $proposal,
+                    %$response
+                }});
+    } else {
+        $c->send({
+                json => {
+                    msg_type => 'proposal',
+                    echo_req => $args,
+                    (exists $args->{req_id}) ? (req_id => $args->{req_id}) : (),
+                    proposal => {
+                        id => $id,
+                        %$response
+                    }}});
     }
     return;
 }
