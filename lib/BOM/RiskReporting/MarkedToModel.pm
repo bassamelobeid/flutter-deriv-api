cd04be39b0b76a87d5fdef5e57308d20ad549225
Matt Miller <matt@binary.com>
2015-12-19 06:25:32 +0200

    Add error in reporting of processing trouble.
    I am fairly certain this used to be in there.  I cannot imagine why it
    was removed, if so.. unless these will turn out to be very long (maybe
    traceback-style) strings.
    
    Regardless, we kinda need to know what's happening especially now that
    the errors are so frequent.

--- lib/BOM/RiskReporting/MarkedToModel.pm
+++ lib/BOM/RiskReporting/MarkedToModel.pm
@@ -134,8 +134,7 @@ sub generate {
             }
             catch {
                 $error_count++;
-                $mail_content .=
-                    "Unable to process bet [ $last_fmb_id, " . $open_fmb->{short_code} . "]\n";
+                $mail_content .= "Unable to process bet [ $last_fmb_id, " . $open_fmb->{short_code} . ", $_ ]\n";
             };
         }
 
