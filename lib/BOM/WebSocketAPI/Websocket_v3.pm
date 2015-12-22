7c0130e03e0688fd2ee1c8138c74dc39fc4b2648
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-12-22 11:52:16 +0800

    no version

--- lib/BOM/WebSocketAPI/Websocket_v3.pm
+++ lib/BOM/WebSocketAPI/Websocket_v3.pm
@@ -356,7 +356,6 @@ sub rpc {
 
             my $args = $params->{args};
             $data->{echo_req} = $args;
-            $data->{version}  = 3;
             $data->{req_id}   = $args->{req_id} if ($args and exists $args->{req_id});
 
             my $l = length JSON::to_json($data);
