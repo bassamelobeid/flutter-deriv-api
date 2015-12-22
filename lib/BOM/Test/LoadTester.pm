663134e3f3844a16896b5ed336fceb121babfd57
Raunak Kathuria <raunakkathuria@gmail.com>
2015-12-22 08:19:00 +0000

    remove check for free memory as it varies a lot

--- lib/BOM/Test/LoadTester.pm
+++ lib/BOM/Test/LoadTester.pm
@@ -23,14 +23,9 @@ use File::Slurp;
 use Test::Mojo;
 use Test::Most;
 use Devel::Gladiator qw(walk_arena arena_ref_counts arena_table);
-use Sys::MemInfo qw(totalmem freemem totalswap);
 
 sub load_test {
     my $func = shift;
-
-    # take snapshot of memory before executing
-    my $free_memory = (&freemem / 1024);
-    my $free_swap   = (Sys::MemInfo::get("freeswap") / 1024);
 
     # check for redis (Application)
     my $app_count = `netstat -nat | grep 6381 | grep EST |  wc -l`;
@@ -57,12 +52,6 @@ sub load_test {
     my %dump2 = map { $dump1{$_} ? () : ("$_" => $_) } walk_arena();
     use Devel::Peek;
     Dump \%dump2;
-
-    my $current_mem  = (&freemem / 1024);
-    my $current_swap = (Sys::MemInfo::get("freeswap") / 1024);
-
-    is $current_mem,  $free_memory, 'Free memory ok after process';
-    is $current_swap, $free_swap,   'Free swap memory ok after process';
 
     return;
 }
