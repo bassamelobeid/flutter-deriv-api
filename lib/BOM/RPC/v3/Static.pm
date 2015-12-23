c2e3661033224512f322c2004034511725818cfa
Fayland Lam <fayland@gmail.com>
2015-12-23 16:20:21 +0800

    add phone_idd

--- lib/BOM/RPC/v3/Static.pm
+++ lib/BOM/RPC/v3/Static.pm
@@ -3,12 +3,19 @@ package BOM::RPC::v3::Static;
 use strict;
 use warnings;
 
+use BOM::Platform::Runtime;
 use BOM::Platform::Locale;
 
 sub residence_list {
     my $residence_list = BOM::Platform::Locale::generate_residence_countries_list();
     $residence_list = [grep { $_->{value} ne '' } @$residence_list];
-    return $residence_list,;
+
+    # plus phone_idd
+    foreach (@$residence_list) {
+        $_->{phone_idd} = BOM::Platform::Runtime->instance->countries->idd_from_code($_->{value});
+    }
+
+    return $residence_list;
 }
 
 sub states_list {
