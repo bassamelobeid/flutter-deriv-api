f3819c411acb31157633d8a7399a96e356a7b633
ssteo <teo@binary.com>
2015-12-23 17:25:47 +0800

    fixes

--- lib/BOM/WebSocketAPI/Websocket_v1.pm
+++ lib/BOM/WebSocketAPI/Websocket_v1.pm
@@ -99,7 +99,7 @@ sub __handle {
             return __authorize_error($dispatch->[3] || $dispatch->[0]);
         }
 
-        DataDog::DogStatsd::Helper::stats_inc('websocket_api_v1.authenticated_call.all', {tags => [$tag, $dispatch->[0], $c->stash('client')->loginid]});
+        DataDog::DogStatsd::Helper::stats_inc('websocket_api_v1.authenticated_call.all', {tags => [$tag, $dispatch->[0], $c->stash('client')->{loginid}]});
         return $dispatch->[1]->($c, $p1);
     }
 
