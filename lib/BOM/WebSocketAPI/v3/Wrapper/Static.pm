c46a90718dd40c1f013ddc3425334530a68d03c7
Raunak Kathuria <raunakkathuria@gmail.com>
2015-12-28 02:14:34 +0000

    residence and states list to RPC

--- lib/BOM/WebSocketAPI/v3/Wrapper/Static.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Static.pm
@@ -8,19 +8,37 @@ use BOM::RPC::v3::Static;
 sub residence_list {
     my ($c, $args) = @_;
 
-    return {
-        msg_type       => 'residence_list',
-        residence_list => BOM::RPC::v3::Static::residence_list(),
-    };
+    BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c,
+        'residence_list',
+        sub {
+            my $response = shift;
+            return {
+                msg_type       => 'residence_list',
+                residence_list => $response,
+            };
+        },
+        {args => $args});
+
+    return;
 }
 
 sub states_list {
     my ($c, $args) = @_;
 
-    return {
-        msg_type    => 'states_list',
-        states_list => BOM::RPC::v3::Static::states_list($args->{states_list}),
-    };
+    BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c,
+        'states_list',
+        sub {
+            my $response = shift;
+            return {
+                msg_type    => 'states_list',
+                states_list => $response,
+            };
+        },
+        {args => $args});
+
+    return;
 }
 
 1;
