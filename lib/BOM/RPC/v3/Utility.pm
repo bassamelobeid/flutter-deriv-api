3f3696ac93200b6eb2d18a8f6180055a11314044
Raunak Kathuria <raunakkathuria@gmail.com>
2015-12-29 15:38:10 +0000

    create sub for permission error

--- lib/BOM/RPC/v3/Utility.pm
+++ lib/BOM/RPC/v3/Utility.pm
@@ -3,6 +3,8 @@ package BOM::RPC::v3::Utility;
 use strict;
 use warnings;
 
+use BOM::Platform::Context qw (localize);
+
 sub create_error {
     my $args = shift;
     return {
@@ -11,6 +13,12 @@ sub create_error {
             message_to_client => $args->{message_to_client},
             $args->{message} ? (message => $args->{message}) : (),
             $args->{details} ? (details => $args->{details}) : ()}};
+}
+
+sub permission_error {
+    return create_error({
+            code              => 'PermissionDenied',
+            message_to_client => localize('Permission denied.')});
 }
 
 sub ping {
