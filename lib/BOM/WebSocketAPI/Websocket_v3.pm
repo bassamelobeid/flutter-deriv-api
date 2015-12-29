70a9b316942b7acd1cbb674009c63a500ffeb6c4
ssteo <teo@binary.com>
2015-12-29 14:58:48 +0800

    merge master

--- lib/BOM/WebSocketAPI/Websocket_v3.pm
+++ lib/BOM/WebSocketAPI/Websocket_v3.pm
@@ -469,7 +469,9 @@ sub rpc {
 
             my $args = $params->{args};
             $data->{echo_req} = $args;
-            $data->{req_id} = $args->{req_id} if ($args and exists $args->{req_id});
+            $data->{version}  = 3;
+            $data->{req_id}   = $args->{req_id}
+                if ($args and exists $args->{req_id});
 
             my $l = length JSON::to_json($data);
             if ($l > 328000) {
