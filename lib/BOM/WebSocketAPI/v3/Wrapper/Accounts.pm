c373a6b1d92f5a45c84f5ac20d980efff2f650ef
Raunak Kathuria <raunakkathuria@gmail.com>
2015-12-30 06:51:13 +0000

    call profit_table RPC way

--- lib/BOM/WebSocketAPI/v3/Wrapper/Accounts.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Accounts.pm
@@ -91,9 +91,20 @@ sub statement {
 sub profit_table {
     my ($c, $args) = @_;
 
-    return {
-        msg_type => 'profit_table',
-        profit_table => BOM::RPC::v3::Accounts::profit_table($c->stash('client'), $args)};
+    BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c,
+        'profit_table',
+        sub {
+            my $response = shift;
+            return {
+                msg_type     => 'profit_table',
+                profit_table => $response,
+            };
+        },
+        {
+            args           => $args,
+            client_loginid => $c->stash('loginid')});
+    return;
 }
 
 sub get_account_status {
