2f7c7ad251e02275679bc2c045c9716bb9e73406
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-12-30 16:00:04 +0800

    tidy

--- lib/BOM/Platform/Email.pm
+++ lib/BOM/Platform/Email.pm
@@ -20,7 +20,7 @@ our @EXPORT_OK = qw(send_email);
 $Mail::Sender::NO_X_MAILER = 1;    # avoid hostname/IP leak
 
 sub send_email {
-    my $args_ref           = shift;
+    my $args_ref = shift;
     return if -e 'etc/rmg/travis';
     my $fromemail          = $args_ref->{'from'};
     my $email              = $args_ref->{'to'};
