96cf6f50b8a971f18579e0fff052179f24e18dec
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-12-30 12:05:55 +0800

    first draft

--- lib/BOM/WebSocketAPI/v3/Wrapper/NewAccount.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/NewAccount.pm
@@ -4,28 +4,38 @@ use strict;
 use warnings;
 
 use BOM::RPC::v3::NewAccount;
+use BOM::WebSocketAPI::Websocket_v3;
 use BOM::Platform::SessionCookie;
 
 sub new_account_virtual {
     my ($c, $args) = @_;
 
     my $token = $c->cookie('verify_token') || $args->{verification_code};
-
-    my $response = BOM::RPC::v3::NewAccount::new_account_virtual($args, $token, $args->{email});
-    if (exists $response->{error}) {
-        return $c->new_error('new_account_virtual', $response->{error}->{code}, $response->{error}->{message_to_client});
-    } else {
-        return {
-            msg_type            => 'new_account_virtual',
-            new_account_virtual => $response
-        };
-    }
+    BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c,
+        'new_account_virtual',
+        sub {
+            my $response = shift;
+            if (exists $response->{error}) {
+                return $c->new_error('new_account_virtual', $response->{error}->{code}, $response->{error}->{message_to_client});
+            } else {
+                return {
+                    msg_type            => 'new_account_virtual',
+                    new_account_virtual => $response
+                };
+            }
+        },
+        {
+            args  => $args,
+            token => $token
+        });
+    return;
 }
 
 sub verify_email {
     my ($c, $args) = @_;
-    my $r = $c->stash('request');
 
+    my $r     = $c->stash('request');
     my $email = $args->{verify_email};
 
     my $code = BOM::Platform::SessionCookie->new({
@@ -40,41 +50,68 @@ sub verify_email {
             verify_token => $code,
         });
 
-    my $response = BOM::RPC::v3::NewAccount::verify_email($email, $c->stash('request')->website, $link);
-
-    return {
-        msg_type     => 'verify_email',
-        verify_email => 1
-    };
+    BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c,
+        'verify_email',
+        sub {
+            my $response = shift;
+            return {
+                msg_type     => 'verify_email',
+                verify_email => $response->{status}};
+        },
+        {
+            args         => $args,
+            email        => $email,
+            cs_email     => $r->website->config->get('customer_support.email'),
+            website_name => $r->website->display_name,
+            link         => $link->to_string
+        });
+    return;
 }
 
 sub new_account_real {
     my ($c, $args) = @_;
 
-    my $response = BOM::RPC::v3::NewAccount::new_account_real($c->stash('client'), $args);
-    if (exists $response->{error}) {
-        return $c->new_error('new_account_real', $response->{error}->{code}, $response->{error}->{message_to_client});
-    } else {
-        return {
-            msg_type         => 'new_account_real',
-            new_account_real => $response
-        };
-    }
+    BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c,
+        'new_account_real',
+        sub {
+            my $response = shift;
+            if (exists $response->{error}) {
+                return $c->new_error('new_account_real', $response->{error}->{code}, $response->{error}->{message_to_client});
+            } else {
+                return {
+                    msg_type         => 'new_account_real',
+                    new_account_real => $response
+                };
+            }
+        },
+        {
+            args           => $args,
+            client_loginid => $c->stash('loginid')});
     return;
 }
 
 sub new_account_maltainvest {
     my ($c, $args) = @_;
 
-    my $response = BOM::RPC::v3::NewAccount::new_account_maltainvest($c->stash('client'), $args);
-    if (exists $response->{error}) {
-        return $c->new_error('new_account_maltainvest', $response->{error}->{code}, $response->{error}->{message_to_client});
-    } else {
-        return {
-            msg_type                => 'new_account_maltainvest',
-            new_account_maltainvest => $response
-        };
-    }
+    BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c,
+        'new_account_maltainvest',
+        sub {
+            my $response = shift;
+            if (exists $response->{error}) {
+                return $c->new_error('new_account_maltainvest', $response->{error}->{code}, $response->{error}->{message_to_client});
+            } else {
+                return {
+                    msg_type                => 'new_account_maltainvest',
+                    new_account_maltainvest => $response
+                };
+            }
+        },
+        {
+            args           => $args,
+            client_loginid => $c->stash('loginid')});
     return;
 }
 
