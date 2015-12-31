0f32a425a3ea4bee250c0076b80f4eb579c988a8
Raunak Kathuria <raunakkathuria@gmail.com>
2015-12-31 08:30:41 +0000

    fix proposal open contract, remove req_id from send_ask

--- lib/BOM/WebSocketAPI/v3/Wrapper/PortfolioManagement.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/PortfolioManagement.pm
@@ -39,16 +39,16 @@ sub proposal_open_contract {    ## no critic (Subroutines::RequireFinalReturn)
         @fmbs = $client->open_bets;
     }
 
-    # don't touch args as we send them in echo_req
-    my $details = {%$args};
     if (scalar @fmbs > 0) {
         foreach my $fmb (@fmbs) {
-            $details->{short_code}  = $fmb->short_code;
-            $details->{contract_id} = $fmb->id;
-            $details->{currency}    = $client->currency;
+            # these keys needs to be deleted from args (check send_proposal)
+            # populating here cos we stash them in redis channel
+            $args->{short_code}  = $fmb->short_code;
+            $args->{contract_id} = $fmb->id;
+            $args->{currency}    = $client->currency;
             my $id = BOM::WebSocketAPI::v3::Wrapper::Streamer::_feed_channel($c, 'subscribe', $fmb->underlying_symbol,
-                'proposal_open_contract:' . JSON::to_json($details));
-            send_proposal($c, $id, $args, $details);
+                'proposal_open_contract:' . JSON::to_json($args));
+            send_proposal($c, $id, $args);
         }
     } else {
         return {
@@ -58,8 +58,9 @@ sub proposal_open_contract {    ## no critic (Subroutines::RequireFinalReturn)
 }
 
 sub send_proposal {
-    my ($c, $id, $args, $details) = @_;
+    my ($c, $id, $args) = @_;
 
+    my $details = {%$args};
     BOM::WebSocketAPI::Websocket_v3::rpc(
         $c,
         'get_bid',
@@ -73,10 +74,11 @@ sub send_proposal {
                 }};
         },
         {
-            args        => $args,
-            short_code  => $details->{short_code},
-            contract_id => $details->{contract_id},
-            currency    => $details->{currency}});
+            short_code  => delete $details->{short_code},
+            contract_id => delete $details->{contract_id},
+            currency    => delete $details->{currency},
+            args        => $details
+        });
     return;
 }
 
