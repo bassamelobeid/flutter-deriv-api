e9efae0c52aa41934481fb288142ef4dbeba4de0
root <root@qa15.regentmarkets.com>
2016-01-06 03:28:34 +0000

    tidy

--- lib/BOM/WebSocketAPI/v3/Wrapper/Streamer.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Streamer.pm
@@ -12,31 +12,21 @@ use BOM::WebSocketAPI::v3::Wrapper::PortfolioManagement;
 use BOM::WebSocketAPI::v3::Wrapper::System;
 
 sub ticks {
-    my ( $c, $args ) = @_;
+    my ($c, $args) = @_;
 
-    my @symbols
-        = ( ref $args->{ticks} ) ? @{ $args->{ticks} } : ( $args->{ticks} );
+    my @symbols =
+        (ref $args->{ticks}) ? @{$args->{ticks}} : ($args->{ticks});
     foreach my $symbol (@symbols) {
         my $response = BOM::RPC::v3::Contract::validate_underlying($symbol);
-        if ( $response and exists $response->{error} ) {
-            return $c->new_error(
-                'ticks',
-                $response->{error}->{code},
-                $response->{error}->{message_to_client} );
+        if ($response and exists $response->{error}) {
+            return $c->new_error('ticks', $response->{error}->{code}, $response->{error}->{message_to_client});
         } else {
-            if ( exists $args->{subscribe} and $args->{subscribe} eq '0' ) {
-                _feed_channel( $c, 'unsubscribe', $symbol, 'tick' );
+            if (exists $args->{subscribe} and $args->{subscribe} eq '0') {
+                _feed_channel($c, 'unsubscribe', $symbol, 'tick');
             } else {
                 my $uuid;
-                if ( not $uuid
-                    = _feed_channel( $c, 'subscribe', $symbol, 'tick' ) )
-                {
-                    return $c->new_error(
-                        'ticks',
-                        'AlreadySubscribed',
-                        $c->l(
-                            'You are already subscribed to [_1]', $symbol
-                        ) );
+                if (not $uuid = _feed_channel($c, 'subscribe', $symbol, 'tick')) {
+                    return $c->new_error('ticks', 'AlreadySubscribed', $c->l('You are already subscribed to [_1]', $symbol));
                 }
             }
         }
@@ -45,144 +35,122 @@ sub ticks {
 }
 
 sub ticks_history {
-    my ( $c, $args ) = @_;
+    my ($c, $args) = @_;
 
     BOM::WebSocketAPI::Websocket_v3::rpc(
         $c,
         'ticks_history',
         sub {
             my $response = shift;
-            if ( $response and exists $response->{error} ) {
-                return $c->new_error(
-                    'ticks_history',
-                    $response->{error}->{code},
-                    $response->{error}->{message_to_client} );
+            if ($response and exists $response->{error}) {
+                return $c->new_error('ticks_history', $response->{error}->{code}, $response->{error}->{message_to_client});
             }
 
-            if ( exists $args->{subscribe} ) {
-                if ( $args->{subscribe} eq '1' ) {
-                    if (not _feed_channel(
-                            $c,                     'subscribe',
-                            $args->{ticks_history}, $response->{publish} ) )
-                    {
-                        return $c->new_error(
-                            'ticks_history',
-                            'AlreadySubscribed',
-                            $c->l(
-                                'You are already subscribed to [_1]',
-                                $args->{ticks_history} ) );
+            if (exists $args->{subscribe}) {
+                if ($args->{subscribe} eq '1') {
+                    if (not _feed_channel($c, 'subscribe', $args->{ticks_history}, $response->{publish})) {
+                        return $c->new_error('ticks_history',
+                            'AlreadySubscribed', $c->l('You are already subscribed to [_1]', $args->{ticks_history}));
                     }
                 }
             }
             return {
                 msg_type => $response->{type},
-                %{ $response->{data} } };
+                %{$response->{data}}};
         },
-        { args => $args } );
+        {args => $args});
 
     return;
 }
 
 sub proposal {
-    my ( $c, $args ) = @_;
+    my ($c, $args) = @_;
 
     my $symbol   = $args->{symbol};
     my $response = BOM::RPC::v3::Contract::validate_symbol($symbol);
-    if ( $response and exists $response->{error} ) {
-        return $c->new_error(
-            'proposal',
-            $response->{error}->{code},
-            $response->{error}->{message_to_client} );
+    if ($response and exists $response->{error}) {
+        return $c->new_error('proposal', $response->{error}->{code}, $response->{error}->{message_to_client});
     } else {
         my $id;
-        if ( $args->{subscribe} eq '1' ) {
-            $id = _feed_channel( $c, 'subscribe', $symbol,
-                'proposal:' . JSON::to_json($args) );
+        if ($args->{subscribe} eq '1') {
+            $id = _feed_channel($c, 'subscribe', $symbol, 'proposal:' . JSON::to_json($args));
         }
-        send_ask( $c, $id, $args );
+        send_ask($c, $id, $args);
     }
     return;
 }
 
 sub send_ask {
-    my ( $c, $id, $args ) = @_;
+    my ($c, $id, $args) = @_;
 
     BOM::WebSocketAPI::Websocket_v3::rpc(
         $c,
         'send_ask',
         sub {
             my $response = shift;
-            if ( $response and exists $response->{error} ) {
-                BOM::WebSocketAPI::v3::Wrapper::System::forget_one( $c, $id );
-                return $c->new_error(
-                    'proposal',
-                    $response->{error}->{code},
-                    $response->{error}->{message_to_client} );
+            if ($response and exists $response->{error}) {
+                BOM::WebSocketAPI::v3::Wrapper::System::forget_one($c, $id);
+                return $c->new_error('proposal', $response->{error}->{code}, $response->{error}->{message_to_client});
             }
             return {
                 msg_type => 'proposal',
-                proposal => { ( $id ? ( id => $id ) : () ), %$response } };
+                proposal => {($id ? (id => $id) : ()), %$response}};
         },
-        { args => $args } );
+        {args => $args});
     return;
 }
 
 sub process_realtime_events {
-    my ( $c, $message ) = @_;
+    my ($c, $message) = @_;
 
-    my @m = split( ';', $message );
+    my @m = split(';', $message);
     my $feed_channels_type = $c->stash('feed_channel_type');
 
-    foreach my $channel ( keys %{$feed_channels_type} ) {
+    foreach my $channel (keys %{$feed_channels_type}) {
         $channel =~ /(.*);(.*)/;
         my $symbol    = $1;
         my $type      = $2;
         my $arguments = $feed_channels_type->{$channel}->{args};
 
-        if ( $type eq 'tick' and $m[0] eq $symbol ) {
-            $c->send(
-                {   json => {
+        if ($type eq 'tick' and $m[0] eq $symbol) {
+            $c->send({
+                    json => {
                         msg_type => 'tick',
                         echo_req => $arguments,
-                        ( exists $arguments->{req_id} )
-                        ? ( req_id => $arguments->{req_id} )
+                        (exists $arguments->{req_id})
+                        ? (req_id => $arguments->{req_id})
                         : (),
                         tick => {
                             id     => $feed_channels_type->{$channel}->{uuid},
                             symbol => $symbol,
                             epoch  => $m[1],
-                            quote  => BOM::Market::Underlying->new($symbol)
-                                ->pipsized_value( $m[2] ) } } } ) if $c->tx;
-        } elsif ( $type =~ /^proposal:/ and $m[0] eq $symbol ) {
-            send_ask( $c, $feed_channels_type->{$channel}->{uuid},
-                $arguments )
+                            quote  => BOM::Market::Underlying->new($symbol)->pipsized_value($m[2])}}}) if $c->tx;
+        } elsif ($type =~ /^proposal:/ and $m[0] eq $symbol) {
+            send_ask($c, $feed_channels_type->{$channel}->{uuid}, $arguments)
                 if $c->tx;
-        } elsif ( $type =~ /^proposal_open_contract:/ and $m[0] eq $symbol ) {
-            BOM::WebSocketAPI::v3::Wrapper::PortfolioManagement::send_proposal(
-                $c, $feed_channels_type->{$channel}->{uuid}, $arguments )
+        } elsif ($type =~ /^proposal_open_contract:/ and $m[0] eq $symbol) {
+            BOM::WebSocketAPI::v3::Wrapper::PortfolioManagement::send_proposal($c, $feed_channels_type->{$channel}->{uuid}, $arguments)
                 if $c->tx;
-        } elsif ( $m[0] eq $symbol ) {
+        } elsif ($m[0] eq $symbol) {
             my $u = BOM::Market::Underlying->new($symbol);
-            $message
-                =~ /;$type:([.0-9+-]+),([.0-9+-]+),([.0-9+-]+),([.0-9+-]+);/;
-            $c->send(
-                {   json => {
+            $message =~ /;$type:([.0-9+-]+),([.0-9+-]+),([.0-9+-]+),([.0-9+-]+);/;
+            $c->send({
+                    json => {
                         msg_type => 'ohlc',
                         echo_req => $arguments,
-                        ( exists $arguments->{req_id} )
-                        ? ( req_id => $arguments->{req_id} )
+                        (exists $arguments->{req_id})
+                        ? (req_id => $arguments->{req_id})
                         : (),
                         ohlc => {
-                            id    => $feed_channels_type->{$channel}->{uuid},
-                            epoch => $m[1],
+                            id          => $feed_channels_type->{$channel}->{uuid},
+                            epoch       => $m[1],
                             open_time   => $m[1] - $m[1] % $type,
                             symbol      => $symbol,
                             granularity => $type,
                             open        => $u->pipsized_value($1),
                             high        => $u->pipsized_value($2),
                             low         => $u->pipsized_value($3),
-                            close       => $u->pipsized_value($4) } } }
-            ) if $c->tx;
+                            close       => $u->pipsized_value($4)}}}) if $c->tx;
         }
     }
 
@@ -190,35 +158,35 @@ sub process_realtime_events {
 }
 
 sub _feed_channel {
-    my ( $c, $subs, $symbol, $type ) = @_;
+    my ($c, $subs, $symbol, $type) = @_;
     my $uuid;
 
     my $feed_channel      = $c->stash('feed_channel');
     my $feed_channel_type = $c->stash('feed_channel_type');
 
     my $redis = $c->stash('redis');
-    if ( $subs eq 'subscribe' ) {
-        if ( exists $feed_channel_type->{"$symbol;$type"} ) {
+    if ($subs eq 'subscribe') {
+        if (exists $feed_channel_type->{"$symbol;$type"}) {
             return;
         }
         $uuid = Data::UUID->new->create_str();
         $feed_channel->{$symbol} += 1;
         $feed_channel_type->{"$symbol;$type"}->{args} = $c->stash('args');
         $feed_channel_type->{"$symbol;$type"}->{uuid} = $uuid;
-        $redis->subscribe( ["FEED::$symbol"], sub { } );
+        $redis->subscribe(["FEED::$symbol"], sub { });
     }
 
-    if ( $subs eq 'unsubscribe' ) {
+    if ($subs eq 'unsubscribe') {
         $feed_channel->{$symbol} -= 1;
         delete $feed_channel_type->{"$symbol;$type"};
-        if ( $feed_channel->{$symbol} <= 0 ) {
-            $redis->unsubscribe( ["FEED::$symbol"], sub { } );
+        if ($feed_channel->{$symbol} <= 0) {
+            $redis->unsubscribe(["FEED::$symbol"], sub { });
             delete $feed_channel->{$symbol};
         }
     }
 
-    $c->stash( 'feed_channel'      => $feed_channel );
-    $c->stash( 'feed_channel_type' => $feed_channel_type );
+    $c->stash('feed_channel'      => $feed_channel);
+    $c->stash('feed_channel_type' => $feed_channel_type);
 
     return $uuid;
 }
