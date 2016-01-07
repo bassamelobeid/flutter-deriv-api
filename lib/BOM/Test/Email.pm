cf156de9a5474f2920d358885d65506bfbebf295
Chylli <chylli.email@gmail.com>
2016-01-07 14:55:50 +0800

    get email

--- lib/BOM/Test/Email.pm
+++ lib/BOM/Test/Email.pm
@@ -22,8 +22,38 @@ our @EXPORT_OK = qw(get_email clear_mailbox);
 
 our $mailbox = "/tmp/default.mailbox";
 
-sub get_email {
-    my %conditions = @_;
+sub get_email_by_address_subject {
+    my %cond = @_;
+
+    die 'Need email address and subject regexp' unless $cond{email} && $cond{subject} && ref($cond{subject}) eq 'Regexp';
+
+    my $email          = $cond{email};
+    my $subject_regexp = $subject;
+
+    my $mgr = Mail::Box::Manager->new;
+    my ($folder, %msg);
+    #mailbox maybe late, so we wait 3 seconds
+    WAIT: for (0 .. 5) {
+        $folder = $mgr->open(
+            folder => $mailbox,
+        );
+
+        MSG: for my $tmsg ($folder->messages) {
+            my @to      = $tmsg->to;
+            my $address = $to[0]->address();
+            my $subject = $tmsg->subject();
+
+            if ($address eq $email && $subject =~ $subject_regexp) {
+                $msg{body}    = $tmsg->body->decoded();
+                $msg{address} = $address;
+                $msg{subject} = $subject;
+                last WAIT;
+            }
+        }
+        $folder->close();
+        sleep 1;
+    }
+    return %msg;
 }
 
 sub init {
