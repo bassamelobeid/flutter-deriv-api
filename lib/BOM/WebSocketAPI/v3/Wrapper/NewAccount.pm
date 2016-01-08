8690baf6d3556b6f848aa2b5af02dd74f5fbbdcf
negar-binary <negar@binary.com>
2016-01-08 08:32:12 +0000

    send type

--- lib/BOM/WebSocketAPI/v3/Wrapper/NewAccount.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/NewAccount.pm
@@ -71,11 +71,11 @@ sub verify_email {
                 verify_email => $response->{status}};
         },
         {
-            args         => $args,
             email        => $email,
             cs_email     => $r->website->config->get('customer_support.email'),
             website_name => $r->website->display_name,
-            link         => $link->to_string
+            link         => $link->to_string,
+            type         => $type
         });
     return;
 }
