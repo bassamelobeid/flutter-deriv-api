be23bc306bbdbaf711e3498612ec56faf0d7c451
Fayland Lam <fayland@gmail.com>
2016-01-10 13:20:02 +0800

    for new apis

---
+++ lib/BOM/WebSocketAPI/v3/Wrapper/App.pm
@@ -0,0 +1,42 @@
+package BOM::WebSocketAPI::v3::Wrapper::App;
+
+use strict;
+use warnings;
+use BOM::WebSocketAPI::Websocket_v3;
+
+sub __call_rpc {
+    my ($c, $method, $args) = @_;
+
+    return BOM::WebSocketAPI::Websocket_v3::rpc(
+        $c, $method,
+        sub {
+            my $response = shift;
+            if (exists $response->{error}) {
+                return $c->new_error($method, $response->{error}->{code}, $response->{error}->{message_to_client});
+            } else {
+                return {
+                    msg_type => $method,
+                    $method  => $response,
+                };
+            }
+        },
+        {
+            args           => $args,
+            client_loginid => $c->stash('loginid'),
+            language       => $c->stash('request')->language
+        });
+}
+
+sub register {
+    return __call_rpc(shift, 'app_register', @_);
+}
+
+sub list {
+    return __call_rpc(shift, 'app_list', @_);
+}
+
+sub get {
+    return __call_rpc(shift, 'app_get', @_);
+}
+
+1;
