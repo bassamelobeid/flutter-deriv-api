c834805b42d3c1c622aa7055693a0b2c0cdd7580
negar-binary <negar@binary.com>
2016-01-12 09:26:51 +0000

    check again

--- lib/BOM/RPC/v3/NewAccount.pm
+++ lib/BOM/RPC/v3/NewAccount.pm
@@ -81,13 +81,15 @@ sub verify_email {
                 use_email_template => 1
             });
     } elsif ($params->{type} eq 'account_opening') {
-        send_email({
-            from               => $params->{cs_email},
-            to                 => $params->{email},
-            subject            => BOM::Platform::Context::localize('Verify your email address - [_1]', $params->{website_name}),
-            message            => [BOM::Platform::Context::localize('Your email address verification link is: ' . $params->{link})],
-            use_email_template => 1
-        });
+        unless (BOM::Platform::User->new({email => $params->{email}})) {
+            send_email({
+                from               => $params->{cs_email},
+                to                 => $params->{email},
+                subject            => bom::platform::context::localize('verify your email address - [_1]', $params->{website_name}),
+                message            => [bom::platform::context::localize('your email address verification link is: ' . $params->{link})],
+                use_email_template => 1
+            });
+        }
     }
 
     return {status => 1};    # always return 1, so not to leak client's email
