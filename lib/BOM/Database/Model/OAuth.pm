a4810e0f870cfabbfb3f5230f95c453e0601d50d
Fayland Lam <fayland@gmail.com>
2016-01-13 20:35:17 +0800

    new subs and new tests

--- lib/BOM/Database/Model/OAuth.pm
+++ lib/BOM/Database/Model/OAuth.pm
@@ -23,15 +23,61 @@ sub verify_client {
     ", undef, $client_id);
 }
 
+sub confirm_scope {
+    my ($self, $client_id, $loginid, @scopes) = @_;
+
+    my $dbh           = $self->dbh;
+    my $get_scope_sth = $dbh->prepare("SELECT id FROM oauth.scopes WHERE scope = ?");
+    my $insert_sth    = $dbh->prepare("
+       INSERT INTO oauth.user_scope_confirm (client_id, loginid, scope_id) VALUES (?, ?, ?)
+    ");
+
+    foreach my $scope (@scopes) {
+        $get_scope_sth->execute($scope);
+        my ($scope_id) = $get_scope_sth->fetchrow_array;
+        next unless $scope_id;
+        $insert_sth->execute($client_id, $loginid, $scope_id);
+    }
+
+    return 1;
+}
+
+sub is_scope_confirmed {
+    my ($self, $client_id, $loginid, @scopes) = @_;
+
+    my $dbh = $self->dbh;
+    my $sth = $dbh->prepare("
+        SELECT 1 FROM oauth.user_scope_confirm JOIN oauth.scopes ON user_scope_confirm.scope_id=scopes.id
+        WHERE client_id = ? AND loginid = ? AND scopes.scope = ?
+    ");
+
+    foreach my $scope (@scopes) {
+        $sth->execute($client_id, $loginid, $scope);
+        my ($is_approved) = $sth->fetchrow_array;
+        return 0 unless $is_approved;
+    }
+
+    return 1;
+}
+
 ## store auth code
 sub store_auth_code {
-    my ($self, $client_id, $loginid) = @_;
+    my ($self, $client_id, $loginid, @scopes) = @_;
 
     my $dbh          = $self->dbh;
     my $auth_code    = String::Random::random_regex('[a-zA-Z0-9]{32}');
     my $expires_time = Date::Utility->new({epoch => (Date::Utility->new->epoch + 600)})->datetime_yyyymmdd_hhmmss;    # 10 minutes max
     $dbh->do("INSERT INTO oauth.auth_code (auth_code, client_id, loginid, expires, verified) VALUES (?, ?, ?, ?, false)",
         undef, $auth_code, $client_id, $loginid, $expires_time);
+
+    my $get_scope_sth    = $dbh->prepare("SELECT id FROM oauth.scopes WHERE scope = ?");
+    my $insert_scope_sth = $dbh->prepare("INSERT INTO oauth.auth_code_scope (auth_code, scope_id) VALUES (?, ?)");
+    foreach my $scope (@scopes) {
+        $get_scope_sth->execute($scope);
+        my ($scope_id) = $get_scope_sth->fetchrow_array;
+        next unless $scope_id;
+        $insert_scope_sth->execute($auth_code, $scope_id);
+    }
 
     return $auth_code;
 }
@@ -55,9 +101,21 @@ sub verify_auth_code {
     return $auth_row->{loginid};
 }
 
+sub get_scope_ids_by_auth_code {
+    my ($self, $auth_code) = @_;
+
+    my @scope_ids;
+    my $sth = $self->dbh->prepare("SELECT scope_id FROM oauth.auth_code_scope WHERE auth_code = ?");
+    $sth->execute($auth_code);
+    while (my ($sid) = $sth->fetchrow_array) {
+        push @scope_ids, $sid;
+    }
+    return @scope_ids;
+}
+
 ## store access token
 sub store_access_token {
-    my ($self, $client_id, $loginid) = @_;
+    my ($self, $client_id, $loginid, @scope_ids) = @_;
 
     my $dbh           = $self->dbh;
     my $expires_in    = 3600;
@@ -69,6 +127,13 @@ sub store_access_token {
         undef, $access_token, $client_id, $loginid, $expires_time);
 
     $dbh->do("INSERT INTO oauth.refresh_token (refresh_token, client_id, loginid) VALUES (?, ?, ?)", undef, $refresh_token, $client_id, $loginid);
+
+    foreach my $related ('access_token', 'refresh_token') {
+        my $insert_sth = $dbh->prepare("INSERT INTO oauth.${related}_scope ($related, scope_id) VALUES (?, ?)");
+        foreach my $scope_id (@scope_ids) {
+            $insert_sth->execute($related eq 'access_token' ? $access_token : $refresh_token, $scope_id);
+        }
+    }
 
     return ($access_token, $refresh_token, $expires_in);
 }
@@ -95,6 +160,18 @@ sub verify_refresh_token {
     $dbh->do("UPDATE oauth.refresh_token SET revoked=true WHERE refresh_token = ?", undef, $refresh_token);
 
     return $loginid;
+}
+
+sub get_scope_ids_by_refresh_token {
+    my ($self, $refresh_token) = @_;
+
+    my @scope_ids;
+    my $sth = $self->dbh->prepare("SELECT scope_id FROM oauth.refresh_token_scope WHERE refresh_token = ?");
+    $sth->execute($refresh_token);
+    while (my ($sid) = $sth->fetchrow_array) {
+        push @scope_ids, $sid;
+    }
+    return @scope_ids;
 }
 
 sub is_name_taken {
--- t/BOM/Database/Model/oauth.t
+++ t/BOM/Database/Model/oauth.t
@@ -13,26 +13,39 @@ my $test_clientid = 'binarycom';
 my $client = $m->verify_client($test_clientid);
 is $client->{id}, $test_clientid;
 
+$m->dbh->do("DELETE FROM oauth.user_scope_confirm");    # clear
+my $is_confirmed = $m->is_scope_confirmed($test_clientid, $test_loginid, 'trade');
+is $is_confirmed, 0, 'not confirmed';
+
+ok $m->confirm_scope($test_clientid, $test_loginid, 'trade'), 'confirm scope';
+$is_confirmed = $m->is_scope_confirmed($test_clientid, $test_loginid, 'trade');
+is $is_confirmed, 1, 'confirmed after confirm_scope';
+
 # create then verify
-my $code = $m->store_auth_code($test_clientid, $test_loginid);
+my $code = $m->store_auth_code($test_clientid, $test_loginid, 'trade');
 ok $code, 'code created';
 
 my $loginid = $m->verify_auth_code($test_clientid, $code);
 is $loginid, $test_loginid, 'verify ok';
 
+my @scope_ids = $m->get_scope_ids_by_auth_code($code);
+is_deeply(\@scope_ids, [1], 'scope_ids by auth_code');
+
 # you can't re-use the code
 ok(!$m->verify_auth_code($test_clientid, $code), 'can not re-use');
 
 ## try access_token
-my ($access_token, $refresh_token) = $m->store_access_token($test_clientid, $test_loginid);
+my ($access_token, $refresh_token) = $m->store_access_token($test_clientid, $test_loginid, 1);
 ok $access_token;
 ok $refresh_token;
 ok $access_token ne $refresh_token;
-diag $access_token;
 is $m->get_loginid_by_access_token($access_token), $test_loginid, 'get_loginid_by_access_token';
 
 $loginid = $m->verify_refresh_token($test_clientid, $refresh_token);
 is $loginid, $test_loginid, 'refresh_token ok';
+
+@scope_ids = $m->get_scope_ids_by_refresh_token($refresh_token);
+is_deeply(\@scope_ids, [1], 'scope_ids by refresh_token');
 
 ok(!$m->verify_refresh_token($test_clientid, $refresh_token), 'can not re-use');
 ok(!$m->verify_refresh_token($test_clientid, $access_token),  'access_token is not for refresh');
