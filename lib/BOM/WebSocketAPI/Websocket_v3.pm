6af4af92a4b791b3081e9ab8c9039a7b749a03ae
Raunak Kathuria <raunakkathuria@gmail.com>
2016-01-15 07:49:09 +0000

    fix Cashier subs for error handling and not using stash client

--- lib/BOM/WebSocketAPI/Websocket_v3.pm
+++ lib/BOM/WebSocketAPI/Websocket_v3.pm
@@ -256,13 +256,10 @@ my @dispatch = (
         'cashier_password',
         \&BOM::WebSocketAPI::v3::Wrapper::Accounts::cashier_password, 1
     ],
-    [
-        'topup_virtual',
-        \&BOM::WebSocketAPI::v3::Wrapper::Cashier::topup_virtual, 1
-    ],
-    ['api_token',    \&BOM::WebSocketAPI::v3::Wrapper::Accounts::api_token,    1],
-    ['get_limits',   \&BOM::WebSocketAPI::v3::Wrapper::Cashier::get_limits,    1],
-    ['tnc_approval', \&BOM::WebSocketAPI::v3::Wrapper::Accounts::tnc_approval, 1],
+    ['api_token',     \&BOM::WebSocketAPI::v3::Wrapper::Accounts::api_token,    1],
+    ['tnc_approval',  \&BOM::WebSocketAPI::v3::Wrapper::Accounts::tnc_approval, 1],
+    ['topup_virtual', \&BOM::WebSocketAPI::v3::Wrapper::Cashier::topup_virtual, 1],
+    ['get_limits',    \&BOM::WebSocketAPI::v3::Wrapper::Cashier::get_limits,    1],
     [
         'paymentagent_withdraw',
         \&BOM::WebSocketAPI::v3::Wrapper::Cashier::paymentagent_withdraw, 1
