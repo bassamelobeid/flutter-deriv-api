552f927e1db6e3b8f4351fef5821a5a59ee558dd
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-01-15 03:57:08 +0100

    adding txn time

--- config/sql/rmgdb/schema_15_up.sql
+++ config/sql/rmgdb/schema_15_up.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE OR REPLACE FUNCTION notify_transaction_trigger() RETURNS trigger AS $$
 DECLARE
 BEGIN
-  PERFORM pg_notify('transaction_watchers', NEW.id || ',' || NEW.account_id || ',' || NEW.action_type || ',' || NEW.referrer_type || ',' || COALESCE(NEW.financial_market_bet_id,0) || ',' || COALESCE(NEW.payment_id,0) || ',' || NEW.amount || ',' || NEW.balance_after );
+  PERFORM pg_notify('transaction_watchers', NEW.id || ',' || NEW.account_id || ',' || NEW.action_type || ',' || NEW.referrer_type || ',' || COALESCE(NEW.financial_market_bet_id,0) || ',' || COALESCE(NEW.payment_id,0) || ',' || NEW.amount || ',' || NEW.balance_after || ',' || NEW.transaction_time );
   RETURN new;
 END;
 $$ LANGUAGE plpgsql;
