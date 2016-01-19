cdbcaf56b439c8c878eab6da630f06cfe1d656f2
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-01-19 06:04:40 +0100

    test

--- config/sql/rmgdb/schema_15_up.sql
+++ config/sql/rmgdb/schema_15_up.sql
@@ -7,11 +7,16 @@ DECLARE
   payment_remark VARCHAR(255) :='';
 BEGIN
     PERFORM 1 FROM pg_class
-        WHERE (relname = 'session_bet_details' or relname='session_payment_details') AND relnamespace = pg_my_temp_schema();
+        WHERE relname = 'session_bet_details' AND relnamespace = pg_my_temp_schema();
     IF FOUND THEN
         IF NEW.action_type = 'buy'::VARCHAR OR NEW.action_type = 'sell'::VARCHAR THEN
             SELECT s.currency_code, s.short_code INTO currency_code,short_code FROM session_bet_details s WHERE fmb_id = NEW.financial_market_bet_id AND action_type = NEW.action_type;
-        ELSE
+        END IF;
+    END IF;
+    PERFORM 1 FROM pg_class
+        WHERE relname='session_payment_details' AND relnamespace = pg_my_temp_schema();
+    IF FOUND THEN
+        IF NEW.action_type = 'withdraw'::VARCHAR OR NEW.action_type = 'deposit'::VARCHAR THEN
             SELECT s.remark INTO payment_remark FROM session_payment_details s WHERE payment_id=NEW.payment_id;
         END IF;
     END IF;
