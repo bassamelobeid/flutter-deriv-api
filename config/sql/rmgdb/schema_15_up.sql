a1dbb31f0c4153fb3e243306e2a8cb64c290fa8a
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-01-19 04:14:01 +0100

    adding payment remark

--- config/sql/rmgdb/schema_15_up.sql
+++ config/sql/rmgdb/schema_15_up.sql
@@ -12,7 +12,7 @@ BEGIN
         IF NEW.action_type = 'buy'::VARCHAR OR NEW.action_type = 'sell'::VARCHAR THEN
             SELECT s.currency_code, s.short_code INTO currency_code,short_code FROM session_bet_details s WHERE fmb_id = NEW.financial_market_bet_id AND action_type = NEW.action_type;
         ELSE
-        --    SELECT s.remark INTO payment_remark FROM session_payment_details s WHERE payment_id=NEW.payment_id;
+            SELECT s.remark INTO payment_remark FROM session_payment_details s WHERE payment_id=NEW.payment_id;
         END IF;
     END IF;
     PERFORM pg_notify('transaction_watchers', NEW.id || ',' || NEW.account_id || ',' || NEW.action_type || ',' || NEW.referrer_type || ',' || COALESCE(NEW.financial_market_bet_id,0) || ',' || COALESCE(NEW.payment_id,0) || ',' || NEW.amount || ',' || NEW.balance_after || ',' || NEW.transaction_time  || ',' || short_code || ',' || currency_code || ',' || payment_remark);
