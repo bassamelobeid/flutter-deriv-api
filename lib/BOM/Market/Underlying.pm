f094816c88fc24c60b9ff4182bce990233d95b72
Shahrizal Shaari <shahrizal@binary.com>
2016-01-21 06:24:25 +0000

    Change delay amount logic

--- lib/BOM/Market/Underlying.pm
+++ lib/BOM/Market/Underlying.pm
@@ -173,7 +173,9 @@ has delay_amount => (
 sub _build_delay_amount {
     my $self = shift;
 
-    my $delay_amount = $self->exchange->delay_amount;
+    my $lic = $self->feed_license;
+
+    my $delay_amount = ($lic eq 'realtime') ? 0 : $self->exchange->delay_amount;
 
     return $delay_amount;
 }
