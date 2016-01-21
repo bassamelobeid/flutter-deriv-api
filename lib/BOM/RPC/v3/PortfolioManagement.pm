6b1da73bc40afff8197ee7da56129fbe6eb35342
Raunak Kathuria <raunakkathuria@gmail.com>
2016-01-21 09:04:17 +0000

    access as object rather than key

--- lib/BOM/RPC/v3/PortfolioManagement.pm
+++ lib/BOM/RPC/v3/PortfolioManagement.pm
@@ -130,14 +130,15 @@ sub proposal_open_contract {
     my $response = {};
     if (scalar @fmbs > 0) {
         foreach my $fmb (@fmbs) {
-            my $id = $fmb->{id};
+            my $id = $fmb->id;
             $response->{$id} = {
-                short_code => $fmb->{short_code},
+                short_code => $fmb->short_code,
                 currency   => $client->currency,
-                underlying => $fmb->{underlying_symbol},
-                buy_price  => $fmb->{buy_price},
-                sell_price => $fmb->{sell_price},
-                is_expired => $fmb->{is_expired}};
+                underlying => $fmb->underlying_symbol,
+                buy_price  => $fmb->buy_price,
+                sell_price => $fmb->sell_price,
+                is_expired => $fmb->is_expired
+            };
         }
     }
 
