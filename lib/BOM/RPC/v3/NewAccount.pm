ef70f1d129306ebbb397f3c7361764cc0ea7df9b
negar-binary <negar@binary.com>
2016-01-25 10:23:32 +0000

    clean up

--- lib/BOM/RPC/v3/NewAccount.pm
+++ lib/BOM/RPC/v3/NewAccount.pm
@@ -83,7 +83,7 @@ sub verify_email {
 
     BOM::Platform::Context::request()->language($params->{language});
 
-    if ($params->{type} eq 'lost_password' && BOM::Platform::User->new({email => $params->{email}})) {
+    if (BOM::Platform::User->new({email => $params->{email}}) && $params->{type} eq 'lost_password') {
         send_email({
                 from    => $params->{cs_email},
                 to      => $params->{email},
@@ -95,26 +95,28 @@ sub verify_email {
                 ],
                 use_email_template => 1
             });
-    } elsif ($params->{type} eq 'account_opening' && BOM::Platform::User->new({email => $params->{email}})) {
-        send_email({
-                from    => $params->{cs_email},
-                to      => $params->{email},
-                subject => BOM::Platform::Context::localize('A Duplicate Email Address Has Been Submitted - [_1]', $params->{website_name}),
-                message => [
-                    BOM::Platform::Context::localize(
-                        'Dear Valued Customer, <p style="margin-top:1em;line-height:200%;">It appears that you have tried to register an email address that is already included in our system. If it was not you, simply ignore this email, or contact our customer support if you have any concerns.</p>'
-                    )
-                ],
+    } elsif ($params->{type} eq 'account_opening') {
+        unless (BOM::Platform::User->new({email => $params->{email}})) {
+            send_email({
+                from               => $params->{cs_email},
+                to                 => $params->{email},
+                subject            => BOM::Platform::Context::localize('Verify your email address - [_1]', $params->{website_name}),
+                message            => [BOM::Platform::Context::localize('Your email address verification link is: ' . $params->{link})],
                 use_email_template => 1
             });
-    } else {
-        send_email({
-            from               => $params->{cs_email},
-            to                 => $params->{email},
-            subject            => BOM::Platform::Context::localize('Verify your email address - [_1]', $params->{website_name}),
-            message            => [BOM::Platform::Context::localize('Your email address verification link is: ' . $params->{link})],
-            use_email_template => 1
-        });
+        } else {
+            send_email({
+                    from    => $params->{cs_email},
+                    to      => $params->{email},
+                    subject => BOM::Platform::Context::localize('A Duplicate Email Address Has Been Submitted - [_1]', $params->{website_name}),
+                    message => [
+                        BOM::Platform::Context::localize(
+                            'Dear Valued Customer, <p style="margin-top:1em;line-height:200%;">It appears that you have tried to register an email address that is already included in our system. If it was not you, simply ignore this email, or contact our customer support if you have any concerns.</p>'
+                        )
+                    ],
+                    use_email_template => 1
+                });
+        }
     }
     return {status => 1};    # always return 1, so not to leak client's email
 }
