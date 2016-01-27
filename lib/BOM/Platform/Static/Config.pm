6f8cac1c9938c645d7ef1977bddb242ae5afba24
Raunak Kathuria <raunakkathuria@gmail.com>
2016-01-27 05:35:00 +0000

    add sub to load config from config.json only at startup

--- lib/BOM/Platform/Static/Config.pm
+++ lib/BOM/Platform/Static/Config.pm
@@ -2,6 +2,7 @@ package BOM::Platform::Static::Config;
 
 use strict;
 use warnings;
+use feature 'state';
 
 sub get_display_languages {
     return ['EN', 'ID', 'RU', 'ES', 'FR', 'PT', 'DE', 'ZH_CN', 'PL', 'AR', 'ZH_TW', 'VI', 'IT'];
@@ -17,6 +18,19 @@ sub get_static_url {
 
 sub get_customer_support_email {
     return 'support@binary.com';
+}
+
+sub get_config {
+    state $config = {};
+    unless (exists $config->{binary_js_hash}) {
+        my $config_file = path(BOM::Platform::Static::Config::get_static_path())->child('config.json');
+        my $config_json = File::Slurp::read_file($config_file);
+        my $config_data = decode_json($config_json);
+
+        $config->{binary_js_hash}  = $config_data->{binary_js_hash};
+        $config->{binary_css_hash} = $config_data->{binary_css_hash};
+    }
+    return $config;
 }
 
 1;
