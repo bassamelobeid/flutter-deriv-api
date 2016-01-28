e7fb37c458cb42b5939c3c562f5f480bf88f56f0
Jun Bon <junbon@binary.com>
2016-01-28 05:43:25 +0000

    digits commission floored at 0.5%

--- lib/BOM/Product/Pricing/Engine/Digits.pm
+++ lib/BOM/Product/Pricing/Engine/Digits.pm
@@ -129,6 +129,7 @@ sub _build_model_markup {
         name        => 'commission_markup',
         description => 'equivalent to tick trades',
         set_by      => __PACKAGE__,
+        minimum     => 0.005,
         base_amount => $winning_digits_commission[$self->winning_digits],
     });
 
