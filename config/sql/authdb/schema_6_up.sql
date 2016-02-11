712ee54cfa4fdea22b98fddc32794b530b6da1a2
Fayland Lam <fayland@gmail.com>
2016-02-11 16:34:36 +0800

    add read as well

--- config/sql/authdb/schema_6_up.sql
+++ config/sql/authdb/schema_6_up.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+INSERT INTO oauth.scopes (scope) VALUES ('read');
 INSERT INTO oauth.scopes (scope) VALUES ('admin');
 INSERT INTO oauth.scopes (scope) VALUES ('payments');
 DELETE FROM oauth.scopes WHERE scope IN ('cashier');
