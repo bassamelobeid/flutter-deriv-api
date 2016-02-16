b2dc613618a2e252d665113546dcc74932694f0e
Yng Shan <yngshan@binary.com>
2016-02-16 02:16:13 +0000

    add comment

--- lib/BOM/MarketData/AutoUpdater/Forex.pm
+++ lib/BOM/MarketData/AutoUpdater/Forex.pm
@@ -52,6 +52,8 @@ sub _build_file {
     }
     @non_quanto_filename;
 
+    #  On weekend, we only subscribe the volsurface file at 23:40GMT. So anytime before this, there is no file available
+    # On weekday, the first response file of the day is at 00:45GMT, hence at the first 45 minutes of the day, there is no file
     if (not $file and $now->hour > 0 and $now->is_a_weekday) {
 
         die('Could not find volatility source file for time[' . $now->datetime . ']');
