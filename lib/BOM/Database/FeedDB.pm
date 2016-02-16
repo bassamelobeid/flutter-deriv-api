cfa6f35ec1a012bfd1efe1cead11ec0e79880dc2
ssteo <teo@binary.com>
2016-02-16 18:09:05 +0800

    test

--- lib/BOM/Database/FeedDB.pm
+++ lib/BOM/Database/FeedDB.pm
@@ -3,33 +3,25 @@ package BOM::Database::FeedDB;
 use strict;
 use warnings;
 
-use YAML::XS;
 use DBI;
 use feature "state";
 
 sub read_dbh {
-    state $config = YAML::XS::LoadFile('/etc/rmg/feeddb.yml');
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed;port=6433;host=" . $config->{replica}->{ip},
-        "read", $config->{password} )
+        "dbi:Pg:dbname=feed;port=6433;host=/var/run/postgresql",
+        "read", '' )
       || die($DBI::errstr);
 }
 
 sub write_dbh {
-    state $config = YAML::XS::LoadFile('/etc/rmg/feeddb.yml');
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed;port=6433;host=" . $config->{write}->{ip},
-        "write", $config->{password} )
+        "dbi:Pg:dbname=feed;port=6433;host=/var/run/postgresql",
+        "write", '' )
       || die($DBI::errstr);
 }
 
 sub any_event_connection_str {
-    state $config = YAML::XS::LoadFile('/etc/rmg/feeddb.yml');
-    return
-        'host='
-      . $config->{replica}->{ip}
-      . ' port=6433 dbname=feed user=write password='
-      . $config->{password};
+    return 'host=/var/run/postgresql port=6433 dbname=feed user=write';
 }
 
 1;
