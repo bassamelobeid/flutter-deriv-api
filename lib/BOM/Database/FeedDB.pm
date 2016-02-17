c4870bbb6d9143d5b02925058f615e8b7d9b7656
ssteo <teo@binary.com>
2016-02-17 10:17:45 +0800

    test

--- lib/BOM/Database/FeedDB.pm
+++ lib/BOM/Database/FeedDB.pm
@@ -8,20 +8,20 @@ use feature "state";
 
 sub read_dbh {
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed;port=6433;host=/var/run/postgresql",
+        "dbi:Pg:dbname=feed-replica;port=6433;host=/var/run/postgresql",
         "read", '' )
       || die($DBI::errstr);
 }
 
 sub write_dbh {
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed;port=6433;host=/var/run/postgresql",
+        "dbi:Pg:dbname=feed-write;port=6433;host=/var/run/postgresql",
         "write", '' )
       || die($DBI::errstr);
 }
 
 sub any_event_connection_str {
-    return 'host=/var/run/postgresql port=6433 dbname=feed user=write';
+    return 'host=/var/run/postgresql port=6433 dbname=feed-write user=write';
 }
 
 1;
