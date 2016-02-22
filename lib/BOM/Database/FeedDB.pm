96e0ad9a9614e7728a6928d228edff0bef844577
ssteo <teo@binary.com>
2016-02-22 17:36:20 +0800

    revert

--- lib/BOM/Database/FeedDB.pm
+++ lib/BOM/Database/FeedDB.pm
@@ -7,20 +7,20 @@ use DBI;
 
 sub read_dbh {
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed-replica;port=6433;host=/var/run/postgresql;sslmode=disable",
+        "dbi:Pg:dbname=feed-replica;port=6433;host=/var/run/postgresql",
         "read", "" )
       || die($DBI::errstr);
 }
 
 sub write_dbh {
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed-write;port=6433;host=/var/run/postgresql;sslmode=disable",
+        "dbi:Pg:dbname=feed-write;port=6433;host=/var/run/postgresql",
         "write", "" )
       || die($DBI::errstr);
 }
 
 sub any_event_connection_str {
-    return 'host=/var/run/postgresql port=6433 dbname=feed-write user=write sslmode=disable';
+    return 'host=/var/run/postgresql port=6433 dbname=feed-write user=write';
 }
 
 1;
