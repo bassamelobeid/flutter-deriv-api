9e60a9eef1a68860364eb912e3386e355698b662
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-02-22 09:06:39 +0000

    quants settings

--- lib/BOM/Test/Data/Utility/UnitTestCouchDB.pm
+++ lib/BOM/Test/Data/Utility/UnitTestCouchDB.pm
@@ -156,9 +156,6 @@ sub _init {
         ));
     BOM::System::Chronicle::set('economic_events', 'economic_events', {events => []});
 
-    BOM::Platform::Static::Config->quants->{client_limits}->{smartfx_turnover_limit} = 100000;
-    BOM::Platform::Static::Config->quants->{market_data}->{interest_rates_source}    = 'market';
-
     return 1;
 }
 
