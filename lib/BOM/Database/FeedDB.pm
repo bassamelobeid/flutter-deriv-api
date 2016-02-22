1114ce0cd6948b3fa9f8e3f0bbc915215e036d2f
ssteo <teo@binary.com>
2016-02-22 17:29:49 +0800

    Disable SSL mode

--- lib/BOM/Database/FeedDB.pm
+++ lib/BOM/Database/FeedDB.pm
@@ -7,20 +7,20 @@ use DBI;
 
 sub read_dbh {
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed-replica;port=6433;host=/var/run/postgresql",
+        "dbi:Pg:dbname=feed-replica;port=6433;host=/var/run/postgresql;sslmode=disable",
         "read", "" )
       || die($DBI::errstr);
 }
 
 sub write_dbh {
     return DBI->connect_cached(
-        "dbi:Pg:dbname=feed-write;port=6433;host=/var/run/postgresql",
+        "dbi:Pg:dbname=feed-write;port=6433;host=/var/run/postgresql;sslmode=disable",
         "write", "" )
       || die($DBI::errstr);
 }
 
 sub any_event_connection_str {
-    return 'host=/var/run/postgresql port=6433 dbname=feed-write user=write';
+    return 'host=/var/run/postgresql port=6433 dbname=feed-write user=write sslmode=disable';
 }
 
 1;
