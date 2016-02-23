38ca5042a72ee07e9bbe49830e15e486b2a515ad
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-02-23 07:40:18 +0000

    fix bug

--- config/sql/rmgdb/functions/025_calculate_potential_losses.sql
+++ config/sql/rmgdb/functions/025_calculate_potential_losses.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 CREATE OR REPLACE FUNCTION bet_v1.calculate_potential_losses(p_account transaction.account)
 RETURNS NUMERIC AS $def$
-    SELECT coalesce(sum(b.buy_price), 0)
+    SELECT coalesce(sum(buy_price), 0)
       FROM bet.financial_market_bet
      WHERE account_id=p_account.id
        AND NOT is_sold;
