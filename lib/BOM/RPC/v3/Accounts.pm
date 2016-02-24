4e1e30aa7f10b4386c299b9339c1f1be4a3bc69d
Chylli <chylli.email@gmail.com>
2016-02-24 14:16:13 +0800

    localize email subject

--- lib/BOM/RPC/v3/Accounts.pm
+++ lib/BOM/RPC/v3/Accounts.pm
@@ -416,7 +416,7 @@ sub cashier_password {
             send_email({
                     'from'    => BOM::Platform::Static::Config::get_customer_support_email(),
                     'to'      => $client->email,
-                    'subject' => $client->loginid . " cashier password updated",
+                    'subject' => localize("[_1] cashier password updated", $client->loginid),
                     'message' => [
                         localize(
                             "This is an automated message to alert you that a change was made to your cashier settings section of your account [_1] from IP address [_2]. If you did not perform this update please login to your account and update settings.",
