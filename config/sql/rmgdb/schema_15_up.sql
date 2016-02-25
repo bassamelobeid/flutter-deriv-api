8bebdea9e75a5a69d2587f1d611b0646af7ee4ae
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-02-25 10:42:46 +0800

    better

--- config/sql/rmgdb/schema_15_up.sql
+++ config/sql/rmgdb/schema_15_up.sql
@@ -16,6 +16,7 @@ BEGIN
           IF NEW.action_type = 'buy'::VARCHAR OR NEW.action_type = 'sell'::VARCHAR THEN
               SELECT s.currency_code, s.short_code, s.purchase_time::TIMESTAMP(0)::TEXT, s.purchase_price, COALESCE(s.sell_time::TIMESTAMP(0)::TEXT,'') INTO currency_code, short_code, purchase_time, purchase_price, sell_time FROM session_bet_details s WHERE fmb_id = NEW.financial_market_bet_id AND action_type = NEW.action_type;
           END IF;
+          PERFORM pg_notify('transaction_watchers', NEW.id || ',' || NEW.account_id || ',' || NEW.action_type || ',' || NEW.referrer_type || ',' || COALESCE(NEW.financial_market_bet_id,0) || ',' || COALESCE(NEW.payment_id,0) || ',' || NEW.amount || ',' || NEW.balance_after || ',' || NEW.transaction_time  || ',' || short_code ||  ',' || currency_code || ',' || purchase_time || ',' || purchase_price || ',' || sell_time || ',' || payment_remark);
       END IF;
     END IF;
 
@@ -26,10 +27,10 @@ BEGIN
           IF NEW.action_type = 'withdraw'::VARCHAR OR NEW.action_type = 'deposit'::VARCHAR THEN
               SELECT s.remark INTO payment_remark FROM session_payment_details s WHERE payment_id=NEW.payment_id;
           END IF;
+          PERFORM pg_notify('transaction_watchers', NEW.id || ',' || NEW.account_id || ',' || NEW.action_type || ',' || NEW.referrer_type || ',' || COALESCE(NEW.financial_market_bet_id,0) || ',' || COALESCE(NEW.payment_id,0) || ',' || NEW.amount || ',' || NEW.balance_after || ',' || NEW.transaction_time  || ',' || short_code ||  ',' || currency_code || ',' || purchase_time || ',' || purchase_price || ',' || sell_time || ',' || payment_remark);
       END IF;
     END IF;
 
-    PERFORM pg_notify('transaction_watchers', NEW.id || ',' || NEW.account_id || ',' || NEW.action_type || ',' || NEW.referrer_type || ',' || COALESCE(NEW.financial_market_bet_id,0) || ',' || COALESCE(NEW.payment_id,0) || ',' || NEW.amount || ',' || NEW.balance_after || ',' || NEW.transaction_time  || ',' || short_code ||  ',' || currency_code || ',' || purchase_time || ',' || purchase_price || ',' || sell_time || ',' || payment_remark);
   RETURN new;
 END;
 $$ LANGUAGE plpgsql;
