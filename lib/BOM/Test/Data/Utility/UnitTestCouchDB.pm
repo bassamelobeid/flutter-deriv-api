9c10b8c91cc36b69f1968d9bb023732e2c0f906a
Mahdi Mohammadi <mm@binary.com>
2016-02-26 03:34:23 +0000

    remove couch

--- lib/BOM/Test/Data/Utility/UnitTestCouchDB.pm
+++ lib/BOM/Test/Data/Utility/UnitTestCouchDB.pm
@@ -130,19 +130,19 @@ sub _init {
     Create a new document in the test database
 
     params:
-    $yaml_couch_db  => The name of the entity in the YAML file (eg. promo_code)
+    $yaml_db  => The name of the entity in the YAML file (eg. promo_code)
     $data_mod       => hasref of modifictions required to the data (optional)
 
 =cut
 
 sub create_doc {
-    my ($yaml_couch_db, $data_mod) = @_;
+    my ($yaml_db, $data_mod) = @_;
 
-    if (grep { $_ eq $yaml_couch_db } qw{currency}) {
+    if (grep { $_ eq $yaml_db } qw{currency}) {
         $data_mod->{chronicle_reader} = BOM::System::Chronicle::get_chronicle_reader();
         $data_mod->{chronicle_writer} = BOM::System::Chronicle::get_chronicle_writer();
 
-        return Quant::Framework::Utils::Test::create_doc($yaml_couch_db, $data_mod);
+        return Quant::Framework::Utils::Test::create_doc($yaml_db, $data_mod);
     }
 
     my $save = 1;
@@ -152,17 +152,17 @@ sub create_doc {
 
     # get data to insert
     my $fixture = YAML::XS::LoadFile('/home/git/regentmarkets/bom-test/data/couch_unit_test.yml');
-    my $data    = $fixture->{$yaml_couch_db}{data};
+    my $data    = $fixture->{$yaml_db}{data};
 
-    die "Invalid yaml couch db name: $yaml_couch_db" if not defined $data;
+    die "Invalid yaml db name: $yaml_db" if not defined $data;
 
     # modify data?
     for (keys %$data_mod) {
         $data->{$_} = $data_mod->{$_};
     }
 
-    # use class to create the Couch doc
-    my $class_name = $fixture->{$yaml_couch_db}{class_name};
+    # use class to create the doc
+    my $class_name = $fixture->{$yaml_db}{class_name};
     my $obj        = $class_name->new($data);
 
     if ($save) {
