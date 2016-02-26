19a9d3b651fe6a5a04fa28cd27963a98b8db962b
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-02-26 08:57:00 +0000

    minimum payout to yml

--- lib/BOM/Product/Contract.pm
+++ lib/BOM/Product/Contract.pm
@@ -1525,17 +1525,18 @@ sub _build_staking_limits {
 
     my @possible_payout_maxes = ($self->_payout_limit);
 
-    push @possible_payout_maxes, BOM::Platform::Static::Config::quants->{bet_limits}->{maximum_payout};
-    push @possible_payout_maxes, BOM::Platform::Static::Config::quants->{bet_limits}->{maximum_payout_on_new_markets}
+    my $bet_limits = BOM::Platform::Static::Config::quants->{bet_limits};
+    push @possible_payout_maxes, $bet_limits->{maximum_payout};
+    push @possible_payout_maxes, $bet_limits->{maximum_payout_on_new_markets}
         if ($underlying->is_newly_added);
-    push @possible_payout_maxes, BOM::Platform::Static::Config::quants->{bet_limits}->{maximum_payout_on_less_than_7day_indices_call_put}
+    push @possible_payout_maxes, $bet_limits->{maximum_payout_on_less_than_7day_indices_call_put}
         if ($self->underlying->market->name eq 'indices' and not $self->is_atm_bet and $self->timeindays->amount < 7);
 
     my $payout_max = min(grep { looks_like_number($_) } @possible_payout_maxes);
     my $stake_max = $payout_max;
 
     # Client likes lower stake/payout limit on random market.
-    my $payout_min = $self->underlying->market->name eq 'random' ? 0.7 : 1;
+    my $payout_min = ($self->underlying->market->name eq 'random') ? $bet_limits->{minimum_payout_for_random} : $bet_limits->{minimum_payout};
     my $stake_min = ($self->built_with_bom_parameters) ? $payout_min / 20 : $payout_min / 2;
 
     # err is included here to allow the web front-end access to the same message generated in the back-end.
