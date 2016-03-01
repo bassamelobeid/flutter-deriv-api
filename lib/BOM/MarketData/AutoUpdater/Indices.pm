384cdacdbf9d67d7cf4251e4be7037377fe90125
Yng Shan <yngshan@binary.com>
2016-03-01 06:26:24 +0100

    fix wrong symbol

--- lib/BOM/MarketData/AutoUpdater/Indices.pm
+++ lib/BOM/MarketData/AutoUpdater/Indices.pm
@@ -87,7 +87,7 @@ sub _build_symbols_to_update {
             submarket         => ['france', 'belgium', 'amsterdam']);
         # Update vol of those plan to offer underlyings. This will be remove on yngshan/enable_stocks
         push @symbols_to_update,
-            qw(USAAPL USAMZN USCT USFB USGE USGOOG USKO USMSFT USPFE USXOM UKBARC UKBATS UKGSK UKHSBA UKVOD DEALV DEBAYN DEDAI DESIE DEVOW AUANZ AUBHP AUCBA AUMQG AUQAN);
+            qw(USAAPL USAMZN USCT USFB USGE USGOOG USKO USMSFT USPFE USXOM UKBARC UKBATS UKGSK UKHSBA UKVOD DEALV DEBAYER DEDAI DESIE DEVOWGEN AUANZ AUBHP AUCBA AUMQG AUQAN);
     }
 
     return \@symbols_to_update;
