e17bac18a30108167e0b729b244d73efa99a7b63
Chylli <chylli.email@gmail.com>
2016-03-21 06:35:15 +0000

    try to add barrier

--- lib/BOM/Test/Data/Utility/Product.pm
+++ lib/BOM/Test/Data/Utility/Product.pm
@@ -128,6 +128,8 @@ sub create_contract {
         current_tick => $ticks[1],
         entry_tick   => $ticks[0],
         exit_tick    => $ticks[-1],
+        barrier  => 'S0P',
+       
     };
     if ($args{is_spread}) {
         delete $contract_data->{date_expiry};
