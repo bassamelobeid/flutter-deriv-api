8a75296556abb74468d1df6103cb4ee8f06563c2
Yng Shan <yngshan@binary.com>
2016-03-25 06:27:33 +0000

    better ordering

--- lib/BOM/Database/DataMapper/Transaction.pm
+++ lib/BOM/Database/DataMapper/Transaction.pm
@@ -659,14 +659,16 @@ sub get_details_by_transaction_ref {
     my $transaction_id = shift;
     my $sql            = q{
     SELECT 
-    a.client_loginid AS loginid,
-    b.short_code AS shortcode,
-    a.currency_code AS currency_code,
-    FROM bet.financial_market_bet b
-    LEFT JOIN transaction.transaction t ON t.financial_market_bet_id=b.id
-    LEFT JOIN transaction.account a on a.id=b.account_id
-    where t.id = $1
-    };
+        a.client_loginid AS loginid,
+        b.short_code AS shortcode,
+        a.currency_code AS currency_code
+    FROM
+        transaction.transaction t
+        JOIN bet.financial_market_bet b ON t.financial_market_bet_id=b.id
+        JOIN transaction.account a on a.id=t.account_id
+    WHERE
+        t.id = $1
+   };
 
     my $sth = $self->db->dbh->prepare($sql);
     $sth->execute($transaction_id);
