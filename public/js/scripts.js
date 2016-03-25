6e5eec70a0b18ee4155b846fa2ecf59af66bb73a
binary-hamedanchi <hamedanchi@gmail.com>
2016-03-25 11:16:47 +0800

    Update login template

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,23 +4,25 @@ var frmLogin,
 var errClass = 'err';
 
 window.onload = function() {
-	frmLogin = document.getElementById('frmLogin');
-	txtEmail = document.getElementById('txtEmail');
-	txtPass  = document.getElementById('txtPass' );
+    frmLogin = document.getElementById('frmLogin');
+    txtEmail = document.getElementById('txtEmail');
+    txtPass  = document.getElementById('txtPass' );
 
     if (frmLogin) {
         frmLogin.addEventListener('submit', function(e) {
             if(!validate()) {
-	            e.preventDefault();
-	            return false;
-	        }
+                e.preventDefault();
+                return false;
+            } else {
+                $('#btnLogin').attr('disabled', 'disabled');
+            }
         });
     }
     if (txtEmail) {
         txtEmail.addEventListener('input', function(e) {
-        	if(txtEmail.classList.contains('hadError')) {
-	            validateEmail();
-	        }
+            if(txtEmail.classList.contains('hadError')) {
+                validateEmail();
+            }
         });
         txtEmail.addEventListener('change', function(e) {
             validateEmail();
@@ -28,9 +30,9 @@ window.onload = function() {
     }
     if (txtPass) {
         txtPass.addEventListener('input', function(e) {
-        	if(txtPass.classList.contains('hadError')) {
-	            validatePass();
-	        }
+            if(txtPass.classList.contains('hadError')) {
+                validatePass();
+            }
         });
         txtPass.addEventListener('change', function(e) {
             validatePass();
@@ -39,39 +41,39 @@ window.onload = function() {
 };
 
 function validate() {
-	var validEmail = validateEmail(),
-	    validPass  = validatePass();
+    var validEmail = validateEmail(),
+        validPass  = validatePass();
     return validEmail && validPass;
 }
 
 function validateEmail() {
-	return validateField(txtEmail, new RegExp(/^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/));
+    return validateField(txtEmail, new RegExp(/^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/));
 }
 
 function validatePass() {
-	return validateField(txtPass, new RegExp(/^[ -~]{6,25}$/));
+    return validateField(txtPass, new RegExp(/^[ -~]{6,25}$/));
 }
 
 function validateField(element, regex) {
-	var hasError = true;
-	if(element) {
-		var value = (element.value || '').trim();
-	    if (value && regex.test(value)) {
-			hasError = false;
-	    }
+    var hasError = true;
+    if(element) {
+        var value = (element.value || '').trim();
+        if (value && regex.test(value)) {
+            hasError = false;
+        }
     }
     setValidationStyle(element, hasError);
     return !hasError;
 }
 
 function setValidationStyle(element, hasError) {
-	if(hasError) {
-		element.classList.add(errClass);
-	}
-	else {
-		element.classList.remove(errClass);
-	}
+    if(hasError) {
+        element.classList.add(errClass);
+    }
+    else {
+        element.classList.remove(errClass);
+    }
     if(element.classList.contains(errClass)) {
-    	element.classList.add('hadError');
+        element.classList.add('hadError');
     }
 }
