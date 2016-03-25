6419f46e275cedba56f49be5753d103cfbf34826
Raunak Kathuria <raunakkathuria@gmail.com>
2016-03-25 03:40:30 +0000

    add creation_time to oauth access tokens

---
+++ config/sql/authdb/schema_8_up.sql
@@ -0,0 +1,6 @@
+BEGIN;
+
+ALTER TABLE oauth.access_token ADD COLUMN creation_time timestamp NOT NULL DEFAULT now();
+UPDATE TABLE oauth.access_token SET creation_time = expires - INTERVAL '1 day';
+
+COMMIT;
