16519326671c57b6474605d1a9148d139dcbbc10
Raunak Kathuria <raunakkathuria@gmail.com>
2016-03-28 07:06:09 +0000

    add JPY as allowed currency for payment api

--- lib/BOM/API/Payment/Role/Plack.pm
+++ lib/BOM/API/Payment/Role/Plack.pm
@@ -92,7 +92,7 @@ sub validate {
     }
 
     if (my $currency_code = $c->request_parameters->{'currency_code'}) {
-        my @currencies = qw(AUD EUR GBP USD);
+        my @currencies = qw(AUD EUR GBP USD JPY);
         my $regex = '(' . join('|', @currencies) . ')';
         return "Invalid currency $currency_code. Must be one of: " . join(', ', @currencies)
             unless $currency_code =~ /^$regex$/;
