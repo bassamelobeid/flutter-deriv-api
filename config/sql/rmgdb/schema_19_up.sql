c6516f1b203f2818295244774cd778f145a279d9
Jun Bon <junbon@binary.com>
2016-03-29 06:45:23 +0000

    syntax

--- config/sql/rmgdb/schema_19_up.sql
+++ config/sql/rmgdb/schema_19_up.sql
@@ -4,6 +4,6 @@ ALTER TABLE data_collection.quants_bet_variables
     ADD COLUMN entry_spot numeric,
     ADD COLUMN entry_spot_epoch bigint,
     ADD COLUMN exit_spot numeric,
-    ADD COLUMN exit_spot_epoch bigint,
+    ADD COLUMN exit_spot_epoch bigint;
 
 COMMIT;
