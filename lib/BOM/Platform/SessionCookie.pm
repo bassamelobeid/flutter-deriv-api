3d131a6f4fd80d0ddfe3c3efb92984f0481f6169
Raunak Kathuria <raunakkathuria@gmail.com>
2016-03-30 02:12:34 +0000

    add test case for multiple sessions

--- lib/BOM/Platform/SessionCookie.pm
+++ lib/BOM/Platform/SessionCookie.pm
@@ -171,7 +171,7 @@ sub have_multiple_sessions {
     return unless $self->{token};
 
     ## no critic
-    return BOM::System::RedisReplicated::redis_write()->eval(<<LUA_SCRIPT, 0, "LOGIN_SESSION_COLLECTION::" . md5_hex($self->{email}), $self->{token});
+    return BOM::System::RedisReplicated::redis_read()->eval(<<LUA_SCRIPT, 0, "LOGIN_SESSION_COLLECTION::" . md5_hex($self->{email}), $self->{token});
 for k,v in pairs(redis.call("SMEMBERS", ARGV[1])) do
     if v ~= ARGV[2] and redis.call("GET", "LOGIN_SESSION::" .. v)then
        return 1;
