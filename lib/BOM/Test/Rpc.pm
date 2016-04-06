4e010e35f3450044c177e0288c0453a4a122ec19
Chylli <chylli@binary.com>
2016-04-06 15:23:06 +0800

    perltidy and unlink file

--- lib/BOM/Test/Rpc.pm
+++ lib/BOM/Test/Rpc.pm
@@ -8,18 +8,17 @@ use Path::Tiny;
 use Net::EmptyPort qw( check_port );
 use Time::HiRes qw( usleep );
 
-
-sub config{
-  return {} unless $ENV{RPC_URL};
-  my $port = (split /:/, $ENV{RPC_URL})[-1];
-  $port =~ s{/}{}g;
-  my $cfg = {
-          url => $ENV{RPC_URL},
-          port => $port,
-          config_file => '/tmp/rpc.cfg',
-          pid_file => '/tmp/bom-rpc.pid',
-               };
-  my $config = <<EOC;
+sub config {
+    return {} unless $ENV{RPC_URL};
+    my $port = (split /:/, $ENV{RPC_URL})[-1];
+    $port =~ s{/}{}g;
+    my $cfg = {
+        url         => $ENV{RPC_URL},
+        port        => $port,
+        config_file => '/tmp/rpc.cfg',
+        pid_file    => '/tmp/bom-rpc.pid',
+    };
+    my $config = <<EOC;
   app->log(Mojo::Log->new(
                             level => 'info',
                             path  => '/tmp/bom-rpc_trace.log'
@@ -39,68 +38,72 @@ sub config{
   };
 EOC
 
-  $cfg->{config} = $config;
-  return $cfg;
-};
-
-sub start_rpc_if_not_running{
-  my $cfg = config();
-  return unless $cfg->{url};
-  return check_port($cfg->{port}) || start_rpc();
+    $cfg->{config} = $config;
+    return $cfg;
 }
 
-sub start_rpc{
-  my $cfg = config();
-  path($cfg->{config_file})->spew($cfg->{config});
+sub start_rpc_if_not_running {
+    my $cfg = config();
+    return unless $cfg->{url};
+    return check_port($cfg->{port}) || start_rpc();
+}
 
-  my $pid = fork;
-      if (not defined $pid) {
+sub start_rpc {
+    my $cfg = config();
+    path($cfg->{config_file})->spew($cfg->{config});
+
+    my $pid = fork;
+    if (not defined $pid) {
         die 'Could not fork process to start rpc service: ' . $!;
-      } elsif ($pid == 0) {
-        exec "/usr/bin/env RPC_CONFIG=$cfg->{config_file} perl -MBOM::Test /home/git/regentmarkets/bom-rpc/bin/binary_rpc.pl daemon -m production -l $cfg->{url}jsonrpc";
+    } elsif ($pid == 0) {
+        exec
+            "/usr/bin/env RPC_CONFIG=$cfg->{config_file} perl -MBOM::Test /home/git/regentmarkets/bom-rpc/bin/binary_rpc.pl daemon -m production -l $cfg->{url}jsonrpc";
         die "Oops... Couldn't start rpc service: $!";
-      }
-  #waitpid $pid, 0;
-  Net::EmptyPort::wait_port($cfg->{port}, 10);
-  path($cfg->{pid_file})->spew($pid);
-  #unlink $cfg->{config_file};
+    }
 
-  return;
+    Net::EmptyPort::wait_port($cfg->{port}, 10);
+    path($cfg->{pid_file})->spew($pid);
 
+    return;
 
 }
 
-sub stop_rpc{
-  my $cfg = config();
-  my $pfile = path($cfg->{pid_file});
+sub stop_rpc {
+    my $cfg   = config();
+    my $pfile = path($cfg->{pid_file});
 
-  if ($pfile->exists) {
-    chomp(my $pid = $pfile->slurp);
-    if (kill(0, $pid)) {
-      my $cmd = path("/proc/$pid/cmdline")->slurp;
-      kill 9, $pid if $cmd =~ /rpc/;
-      wait_till_exit($pid, 3);
+    if ($pfile->exists) {
+        chomp(my $pid = $pfile->slurp);
+        if (kill(0, $pid)) {
+            my $cmd = path("/proc/$pid/cmdline")->slurp;
+            kill 9, $pid if $cmd =~ /rpc/;
+            wait_till_exit($pid, 3);
+        }
+        if (kill(0, $pid)) {
+            die "cannot stop rpc service!";
+        } else {
+            unlink($cfg->{pid_file});
+            unlink $cfg->{config_file};
+        }
     }
-  }
 
 }
 
 sub wait_till_exit {
-  my ($pid, $timeout) = @_;
-  my $start = time;
-  while ((time - $start < $timeout) and kill ZERO => $pid) {
-    usleep 1e5;
-  }
-  return;
+    my ($pid, $timeout) = @_;
+    my $start = time;
+    while ((time - $start < $timeout) and kill ZERO => $pid) {
+        usleep 1e5;
+    }
+    return;
 }
-
 
 BEGIN {
-  start_rpc_if_not_running();
+    start_rpc_if_not_running();
 }
 
-END{
-  stop_rpc();
+END {
+    stop_rpc();
 }
 
 1;
