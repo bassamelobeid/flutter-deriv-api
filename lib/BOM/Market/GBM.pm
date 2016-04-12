272f8525307bf0cfb7889e7242f50c546706b359
Evgeniy Vostrov <evgeniy@binary.com>
2016-04-12 12:47:51 +0000

    Remove harcode

--- lib/BOM/Market/GBM.pm
+++ lib/BOM/Market/GBM.pm
@@ -99,7 +99,7 @@ sub crand {
 
 sub _redis {
     state $redis_read = RedisDB->new(
-        host => BOM::System::Config::env =~ /^qa/ ? BOM::System::Config::randsrv()->{rand_server}->{fqdn} : 'localhost',
+        host => BOM::System::Config::randsrv()->{rand_server}->{fqdn},
         port => BOM::System::Config::randsrv()->{rand_server}->{port},
         password => BOM::System::Config::randsrv()->{rand_server}->{password},
     );
