27d81aaab18ca18ba793a325ba44b978b9d5287c
Evgeniy Vostrov <evgeniy@binary.com>
2016-04-13 08:31:15 +0000

    Use preconfigured test redis servers

--- lib/BOM/Test.pm
+++ lib/BOM/Test.pm
@@ -3,13 +3,18 @@ package BOM::Test;
 use strict;
 use warnings;
 
+use File::Spec;
+use Cwd qw/abs_path/;
+
+use BOM::System::Config;
+
 =head1 NAME
 
 BOM::Test - Do things before test
 
 =head1 DESCRIPTION
 
-This module is used to prepare test. It should be used before any other bom modules in the test file.
+This module is used to prepare test environment. It should be used before any other bom modules in the test file.
 
 =over 4
 
@@ -19,15 +24,46 @@ This variable will be set if test is running on qa devbox. If it is set the syst
 
 =cut
 
-BEGIN {
-    my $environment = '';
-    if (open(my $fh, "<", "/etc/rmg/environment")) {
-        $environment = <$fh>;
-        close($fh);
-    }
+=item $ENV{REDIS_CACHE_SERVER}
 
-    if ($environment =~ /^qa/) {
-        ## no critic (RequireLocalizedPunctuationVars)
+This variable will be set if test is running on qa devbox. If it is set the Cache::RedisDB will use test redis instance instead of development.
+
+=cu
+
+t=item $ENV{BOM_TEST_REDIS_RAND}
+
+This variable will be set if test is running on qa devbox. If it is set the BOM::System::Config::randsrv will use test redis instance instead of development.
+
+=cut
+
+=item $ENV{BOM_TEST_REDIS_REPLICATED}
+
+This variable will be set if test is running on qa devbox. If it is set the BOM::System::RedisReplicated and other bom services
+will use test redis instance instead of development.
+
+=cut
+
+=item $ENV{BOM_TEST_DATA_DIR}
+
+This variable will be set if test is running on qa devbox. Variable can be use in any test to find test data dir.
+
+=cut
+
+BEGIN {
+    ## no critic (Variables::RequireLocalizedPunctuationVars)
+    my (undef, $file_path, undef) = File::Spec->splitpath(__FILE__);
+    $ENV{BOM_TEST_DATA_DIR} = abs_path("$file_path../../data");
+
+    my $config_dir = $ENV{BOM_TEST_DATA_DIR} . '/config';
+    if (BOM::System::Config::env =~ /^qa/) {
+
+        # Redis rand and replicated servers config
+        $ENV{BOM_TEST_REDIS_REPLICATED} = $config_dir . '/redis-replicated.yml';
+        $ENV{BOM_TEST_REDIS_RAND}       = $config_dir . '/redis.yml';
+
+        # Cache redis server
+        $ENV{REDIS_CACHE_SERVER} = $ENV{BOM_CACHE_SERVER} = '127.0.1.3:6385';
+
         $ENV{DB_POSTFIX} = '_test';
         $ENV{RPC_URL}    = 'http://127.0.0.1:5006/';
     }
