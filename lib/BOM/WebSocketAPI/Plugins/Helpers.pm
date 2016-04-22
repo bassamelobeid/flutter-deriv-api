964aa266e559de526a408c4ac511ecfa98d27afe
Evgeniy Vostrov <evgeniy@binary.com>
2016-04-22 13:27:41 +0000

    Minor

--- lib/BOM/WebSocketAPI/Plugins/Helpers.pm
+++ lib/BOM/WebSocketAPI/Plugins/Helpers.pm
@@ -12,7 +12,14 @@ sub register {
 
     $app->helper(server_name => sub { return [split(/\./, Sys::Hostname::hostname)]->[0] });
 
-    $app->helper(l => sub { shift; return localize(@_) });
+    $app->helper(l => sub {
+        my $c = shift;
+
+        my $lh = BOM::Platform::Context::I18N::handle_for($c->stash->{language})
+            || die("could not build locale for language " . $c->stash->{language});
+
+        return $lh->maketext(@_);
+    });
 
     $app->helper(
         country_code => sub {
