3d9bdcd96ec4f8732b668d846ce3e3bca4a5a665
Jun Bon <junbon@binary.com>
2016-04-28 11:02:38 +0000

    more test fixes

--- lib/BOM/Product/ContractFactory.pm
+++ lib/BOM/Product/ContractFactory.pm
@@ -66,10 +66,12 @@ Produce a Contract Object from a set of parameters
 my %OVERRIDE_LIST = (
     INTRADU => {
         bet_type                   => 'CALL',
+        is_forward_starting        => 1,
         starts_as_forward_starting => 1
     },
     INTRADD => {
         bet_type                   => 'PUT',
+        is_forward_starting        => 1,
         starts_as_forward_starting => 1
     },
     FLASHU => {
