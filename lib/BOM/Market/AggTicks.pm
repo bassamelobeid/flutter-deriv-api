e323a7e87242d1891bb9f7e165e680f9e7d538ac
Jun Bon <junbon@binary.com>
2016-04-28 06:50:45 +0000

    syntax

--- lib/BOM/Market/AggTicks.pm
+++ lib/BOM/Market/AggTicks.pm
@@ -373,7 +373,7 @@ sub flush {
 sub check_delay {
     my ($self, $underlying) = @_;
 
-    my ($unagg_key, $agg_key) = map { $self->_make_key($ul, $_) } (0 .. 1);
+    my ($unagg_key, $agg_key) = map { $self->_make_key($underlying, $_) } (0 .. 1);
     my $redis = $self->_redis;
     my $current_time = time;
 
