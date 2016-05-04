414b844b4ace4d4649e18730ee7354908b2e6418
Chylli <chylli@binary.com>
2016-05-05 07:07:28 +0800

    redefine yaml::loadfile also

--- bin/binary_rpc.pl
+++ bin/binary_rpc.pl
@@ -6,6 +6,7 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 use Test::MockModule;
 use YAML::XS;
+use YAML;
 BEGIN {
   my $orig=\&YAML::XS::LoadFile;
   *YAML::XS::LoadFile=sub {
@@ -14,7 +15,17 @@ BEGIN {
     print $fh "$$: loading @_ at $file $line\n";
     close($fh);
     return $orig->(@_);
+  };
+
+  my $orig2=\&YAML::LoadFile;
+  *YAML::LoadFile=sub {
+    my ($package, $file, $line) = caller;
+    open(my $fh,">>", "/tmp/log.log");
+    print $fh "$$: loading @_ at $file $line\n";
+    close($fh);
+    return $orig2->(@_);
   }
+
 }
 
 # Start command line interface for application
