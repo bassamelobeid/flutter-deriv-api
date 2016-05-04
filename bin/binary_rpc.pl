a6371270d6308114a22ebcc0685eb1327935de83
Chylli <chylli@binary.com>
2016-05-04 17:12:33 +0800

    mock loadfile

--- bin/binary_rpc.pl
+++ bin/binary_rpc.pl
@@ -5,6 +5,14 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
+BEGIN {
+  my $orig=\&YAML::XS::LoadFile;
+  *YAML::XS::LoadFile=sub {
+    my ($package, $file, $line) = caller;
+    warn "$$: loading @_ at $file $line";
+    return $orig->(@_);
+  }
+
 # Start command line interface for application
 require Mojolicious::Commands;
 Mojolicious::Commands->start_app('BOM::RPC');
