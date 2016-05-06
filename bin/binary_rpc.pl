9e1135c9fd93508b86113d573b532476e9abc429
Chylli <chylli@binary.com>
2016-05-06 10:13:25 +0800

    revert back rpc.pl

--- bin/binary_rpc.pl
+++ bin/binary_rpc.pl
@@ -4,34 +4,6 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-use Test::MockModule;
-use YAML::XS;
-use YAML;
-use Carp qw(confess);
-BEGIN {
-  my $orig=\&YAML::XS::LoadFile;
-  *YAML::XS::LoadFile=sub {
-    my ($package, $file, $line) = caller;
-    open(my $fh,">>", "/tmp/log.log");
-    print $fh "$$: loading @_ at $file $line\n";
-    eval {confess()};
-    print $fh $@;
-    close($fh);
-    return $orig->(@_);
-  };
-
-  my $orig2=\&YAML::LoadFile;
-  *YAML::LoadFile=sub {
-    my ($package, $file, $line) = caller;
-    open(my $fh,">>", "/tmp/log.log");
-    print $fh "$$: loading @_ at $file $line\n";
-    eval {confess()};
-    print $fh $@;
-    close($fh);
-    return $orig2->(@_);
-  }
-
-}
 
 # Start command line interface for application
 require Mojolicious::Commands;
