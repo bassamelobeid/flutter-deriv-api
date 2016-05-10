f66a57856edef9528a33c6eb83513bd5f82e7188
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-05-10 15:32:23 +0800

    in daemon

--- lib/BOM/WebSocketAPI/v3/Wrapper/Streamer.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Streamer.pm
@@ -122,7 +122,6 @@ sub process_pricing_events {
 
     delete $response->{data};
     delete $response->{key};
-    delete $response->{longcode};
 
     foreach my $amount (keys %{$pricing_channel->{$serialized_args}}) {
         next if $amount eq 'channel_name';
