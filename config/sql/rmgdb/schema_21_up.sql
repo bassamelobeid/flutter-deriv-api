1fc6f2f22e73f7c82372e1cc6c6c6046c8e88f74
Raunak Kathuria <raunakkathuria@gmail.com>
2016-05-10 06:02:32 +0000

    add aml risk classification column to client table

---
+++ config/sql/rmgdb/schema_21_up.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+CREATE TYPE aml_risk_classification AS ENUM ('low', 'standard', 'high', 'manual override - low', 'manual override - standard', 'manual override - high');
+
+ALTER TABLE betonmarkets.client
+    ADD COLUMN aml_risk_classification aml_risk_classification[];
+
+COMMIT;
