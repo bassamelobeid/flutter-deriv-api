2faf1338c3276e07a2a7b0d43526cdcc4a84eb29
Jun <junbon@binary.com>
2016-05-12 06:27:40 +0000

    typo

--- lib/BOM/Product/Contract.pm
+++ lib/BOM/Product/Contract.pm
@@ -2830,7 +2830,7 @@ sub confirm_validity {
     my @validation_methods =
         qw(_validate_input_parameters _validate_offerings _validate_lifetime  _validate_barrier _validate_feed _validate_stake _validate_payout);
 
-    push @validation_methods, '_validate_volsurface' if ($self->volsurface->type eq 'flat');
+    push @validation_methods, '_validate_volsurface' if (not $self->volsurface->type eq 'flat');
     push @validation_methods, qw(_validate_trading_times _validate_start_and_expiry_date) if not $self->underlying->always_available;
 
     foreach my $method (@validation_methods) {
