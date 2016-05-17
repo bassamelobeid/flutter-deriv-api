d158d2382ed8c8c625ce99b7d70aef12931f891a
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-05-17 15:40:16 +0800

    syntax

--- lib/BOM/RPC/PricerDaemon.pm
+++ lib/BOM/RPC/PricerDaemon.pm
@@ -53,9 +53,9 @@ sub price {
 
     my $t                        = gettimeofday;
     my $response                 = BOM::RPC::v3::Contract::send_ask({args => $self->{params}});
-    my $response->{pricing_time} = tv_interval($t)
+    my $response->{pricing_time} = tv_interval($t);
 
-        delete $response->{longcode};
+    delete $response->{longcode};
 
     DataDog::DogStatsd::Helper::stats_inc('pricer_daemon.price.call');
     DataDog::DogStatsd::Helper::stats_timing('pricer_daemon.price.time', $response->{rpc_time});
