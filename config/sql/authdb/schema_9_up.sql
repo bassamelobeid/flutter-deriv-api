455d3520cdbe417cc80f3c188652736916fca843
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-05-18 10:14:21 +0000

    app id

--- config/sql/authdb/schema_9_up.sql
+++ config/sql/authdb/schema_9_up.sql
@@ -70,6 +70,8 @@ UPDATE oauth.apps   SET id = 34 WHERE __id = 'id-FwnhrVstk9kPBnDfocVpk8ZDtNs1V';
 UPDATE oauth.apps   SET id = 35 WHERE __id = 'id-lzNzcmvdgbB99jBFl3IGO3yLgmUSK';
 UPDATE oauth.apps   SET id = 36 WHERE __id = 'id-EmcupPkdLUKfScM8vsM6Hc4httJrL';
 UPDATE oauth.apps   SET id = 37 WHERE __id = 'id-yfBPXh3678sX8W1q6xDvr71pk1VJK';
+UPDATE oauth.apps   SET id = 39 WHERE __id = 'binary-expiryd';
+
 
 -- populate app_id
 UPDATE oauth.access_token               SET app_id = a.id FROM oauth.apps a WHERE __app_id = a.__id;
