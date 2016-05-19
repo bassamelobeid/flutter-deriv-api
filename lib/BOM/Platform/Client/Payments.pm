7a212dbd06054b966210f120b36115bf132ae988
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-05-19 05:49:16 +0000

    pa transfer

--- lib/BOM/Platform/Client/Payments.pm
+++ lib/BOM/Platform/Client/Payments.pm
@@ -328,8 +328,8 @@ sub payment_account_transfer {
         my $dbh = $fmClient->db->dbh;
         my $response;
         try {
-            my $sth = $dbh->prepare('SELECT (v_from_trans).id FROM payment.payment_account_transfer(?,?,?,?,?,?,?,?,NULL)');
-            $sth->execute($fmClient->loginid, $toClient->loginid, $currency, $amount, $fmStaff, $toStaff, $fmRemark, $toRemark);
+            my $sth = $dbh->prepare('SELECT (v_from_trans).id FROM payment.payment_account_transfer(?,?,?,?,?,?,?,?,?,NULL)');
+            $sth->execute($fmClient->loginid, $toClient->loginid, $currency, $amount, $fmStaff, $toStaff, $fmRemark, $toRemark, $source);
             my $records = $sth->fetchall_arrayref({});
             if (scalar @{$records}) {
                 $response->{transaction_id} = $records->[0]->{id};
