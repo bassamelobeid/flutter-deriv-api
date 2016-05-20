acc548a9a6b1920d09c6aa552b4903775e147b71
Roman Tkachuk <roman.a.tkachuk@gmail.com>
2016-05-20 14:44:23 +0300

    schema

--- config/sql/rmgdb/schema_23_up.sql
+++ config/sql/rmgdb/schema_23_up.sql
@@ -12,20 +12,20 @@ CREATE TABLE bet.daily_aggregates (
 GRANT  SELECT ON bet.daily_aggregates to read;
 GRANT  SELECT, UPDATE, INSERT, DELETE ON bet.daily_aggregates to write;
 
-CREATE OR REPLACE FUNCTION update_daily_aggregates_ins() RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION bet.update_daily_aggregates_ins() RETURNS trigger AS $$
 BEGIN
   RETURN new;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION update_daily_aggregates_del() RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION bet.update_daily_aggregates_del() RETURNS trigger AS $$
 BEGIN
   RETURN new;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER watched_fmbo_trigger_ins AFTER INSERT ON bet.financial_market_bet_open FOR EACH ROW EXECUTE PROCEDURE update_daily_aggregates_ins();
-CREATE TRIGGER watched_fmbo_trigger_del AFTER DELETE ON bet.financial_market_bet_open FOR EACH ROW EXECUTE PROCEDURE update_daily_aggregates_del();
-CREATE TRIGGER watched_fmb_trigger AFTER INSERT OR DELETE ON bet.financial_market_bet FOR EACH ROW EXECUTE PROCEDURE update_daily_aggregates_ins();
+CREATE TRIGGER watched_fmbo_trigger_ins AFTER INSERT ON bet.financial_market_bet_open FOR EACH ROW EXECUTE PROCEDURE bet.update_daily_aggregates_ins();
+CREATE TRIGGER watched_fmbo_trigger_del AFTER DELETE ON bet.financial_market_bet_open FOR EACH ROW EXECUTE PROCEDURE bet.update_daily_aggregates_del();
+CREATE TRIGGER watched_fmb_trigger AFTER INSERT OR DELETE ON bet.financial_market_bet FOR EACH ROW EXECUTE PROCEDURE bet.update_daily_aggregates_ins();
 
 COMMIT;
