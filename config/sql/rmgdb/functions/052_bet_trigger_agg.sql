8287c488c4e5120f0f8ce25917984d27d7e71ec1
Roman Tkachuk <roman.a.tkachuk@gmail.com>
2016-05-20 22:33:31 +0300

    Update 052_bet_trigger_agg.sql

--- config/sql/rmgdb/functions/052_bet_trigger_agg.sql
+++ config/sql/rmgdb/functions/052_bet_trigger_agg.sql
@@ -12,7 +12,7 @@ BEGIN
 
     UPDATE bet.daily_aggregates
       SET
-        turnover = turnover + NEW.buy_price,
+        turnover = turnover + NEW.buy_price
       WHERE
           day = NEW.purchase_time::date
           AND account_id = NEW.account_id
@@ -27,26 +27,19 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION bet.update_daily_aggregates_sell() RETURNS trigger AS $$
 BEGIN
-  LOOP
-    BEGIN
-      INSERT INTO bet.daily_aggregates VALUES (NEW.purchase_time::date, NEW.account_id, 0, NEW.buy_price - NEW.sell_price);
-      RETURN new;
-    EXCEPTION WHEN unique_violation THEN
-      -- nothing
-    END;
+  UPDATE bet.daily_aggregates
+    SET
+      loss = loss + (NEW.buy_price - NEW.sell_price)
+    WHERE
+        day = NEW.purchase_time::date
+        AND account_id = NEW.account_id
+    ;
 
-    UPDATE bet.daily_aggregates
-      SET
-        loss = loss + (NEW.buy_price - NEW.sell_price)
-      WHERE
-          day = NEW.purchase_time::date
-          AND account_id = NEW.account_id
-      ;
-
-    IF FOUND THEN
-      RETURN new;
-    END IF;
-  END LOOP;
+  IF FOUND THEN
+    RETURN new;
+  ELSE
+    RETURN NULL;
+  END IF;
 END;
 $$ LANGUAGE plpgsql;
 
