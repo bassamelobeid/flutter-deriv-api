06124c6ee47758f550a3ab17452956e1a8c9d91a
Roman Tkachuk <roman.a.tkachuk@gmail.com>
2016-05-20 10:46:04 +0300

    Update schema_23_up.sql

--- config/sql/rmgdb/schema_23_up.sql
+++ config/sql/rmgdb/schema_23_up.sql
@@ -12,19 +12,13 @@ CREATE TABLE bet.daily_aggregates (
 GRANT  SELECT ON bet.daily_aggregates to read;
 GRANT  SELECT, UPDATE, INSERT, DELETE ON bet.daily_aggregates to write;
 
-CREATE OR REPLACE FUNCTION insert_daily_aggregates() RETURNS trigger AS $$
-BEGIN
-  RETURN new;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION update_daily_aggregates() RETURNS trigger AS $$
 BEGIN
   RETURN new;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER watched_fmbo_trigger AFTER INSERT ON bet.financial_market_bet_open FOR EACH ROW EXECUTE PROCEDURE insert_daily_aggregates();
-CREATE TRIGGER watched_fmb_trigger AFTER INSERT ON bet.financial_market_bet FOR EACH ROW EXECUTE PROCEDURE update_daily_aggregates();
+CREATE TRIGGER watched_fmbo_trigger AFTER INSERT OR DELETE ON bet.financial_market_bet_open FOR EACH ROW EXECUTE PROCEDURE update_daily_aggregates();
+CREATE TRIGGER watched_fmb_trigger AFTER INSERT OR DELETE ON bet.financial_market_bet FOR EACH ROW EXECUTE PROCEDURE update_daily_aggregates();
 
 COMMIT;
