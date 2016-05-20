68f3f896030176b9a8fb5e5e3dbb35a539ec8ddc
Jun <junbon@binary.com>
2016-05-20 02:33:53 +0000

    do not die if barrier is not integer for digits

--- lib/BOM/Product/Contract/Strike/Digit.pm
+++ lib/BOM/Product/Contract/Strike/Digit.pm
@@ -2,6 +2,8 @@ package BOM::Product::Contract::Strike::Digit;
 
 use Moose;
 use namespace::autoclean;
+use Scalar::Util qw(looks_like_number);
+use BOM::Platform::Context qw(localize);
 
 extends 'BOM::Product::Contract::Strike';
 
@@ -9,12 +11,22 @@ has '+basis_tick' => (
     required => 0,
 );
 
+has '+supplied_barrier' => (
+    writer => '_set_supplied_barrier',
+);
+
 sub BUILD {
     my $self = shift;
 
-    die(__PACKAGE__ . ' can only accept supplied_barriers of 1 digit: [' . $self->supplied_barrier . ']')
-        unless (defined $self->supplied_barrier
-        && $self->supplied_barrier =~ /^[0-9]$/);
+    if (not(defined $self->supplied_barrier and looks_like_number($self->supplied_barrier) and $self->supplied_barrier =~ /^[0-9]$/)) {
+        $self->add_errors({
+            severity          => 110,
+            message           => 'supplied barrier for digits is undefined',
+            message_to_client => localize('We could not process this contract at this time.'),
+        });
+        # setting supplied barrier to zero
+        $self->_set_supplied_barrier(0);
+    }
 
     return;
 }
