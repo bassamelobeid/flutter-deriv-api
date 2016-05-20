6d618f54bd8a63c0293be346a9a6039a10379ecd
Roman Tkachuk <roman.a.tkachuk@gmail.com>
2016-05-20 12:12:09 +0300

    Update 052_bet_trigger_agg.sql

--- config/sql/rmgdb/functions/052_bet_trigger_agg.sql
+++ config/sql/rmgdb/functions/052_bet_trigger_agg.sql
@@ -1,50 +1,52 @@
 BEGIN;
 
-
-CREATE OR REPLACE FUNCTION update_daily_aggregates() RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION update_daily_aggregates_ins() RETURNS trigger AS $$
 BEGIN
   LOOP
-    -- when delete in same period
-    -- when delete in new period
-  
-    IF TG_OP = 'INSERT' THEN
-      BEGIN
-        INSERT INTO bet.daily_aggregates VALUES (NEW.purchase_time::date, NEW.account_id, NEW.buy_price, NEW.buy_price - coalesce(NEW.sell_price, 0));
-        RETURN new;
-      EXCEPTION WHEN unique_violation THEN
-        -- nothing
-      END;
-    ELSIF TG_OP = 'DELETE' THEN
-      BEGIN
-        INSERT INTO bet.daily_aggregates VALUES (OLD.purchase_time::date, OLD.account_id, OLD.buy_price, OLD.buy_price - coalesce(OLD.sell_price, 0));
-        RETURN old;
-      EXCEPTION WHEN unique_violation THEN
-        -- nothing
-      END;
-    END IF;
-    
+    BEGIN
+      INSERT INTO bet.daily_aggregates VALUES (NEW.purchase_time::date, NEW.account_id, NEW.buy_price, NEW.buy_price - coalesce(NEW.sell_price, 0));
+      RETURN new;
+    EXCEPTION WHEN unique_violation THEN
+      -- nothing
+    END;
+
     UPDATE bet.daily_aggregates
       SET
-        turnover = turnover
-            + CASE WHEN TG_OP = 'INSERT' THEN coalesce(NEW.buy_price, 0) ELSE 0 END                     -- add new
-            - CASE WHEN TG_OP = 'DELETE' THEN coalesce(OLD.buy_price, 0) ELSE 0 END                     -- del old
-        ,loss = loss 
-            + CASE WHEN TG_OP = 'INSERT' THEN (NEW.buy_price - coalesce(NEW.sell_price, 0)) ELSE 0 END  -- add new loss
-            - CASE WHEN TG_OP = 'DELETE' THEN (OLD.buy_price - coalesce(OLD.sell_price, 0)) ELSE 0 END  -- del old loss
+        turnover = turnover + coalesce(NEW.buy_price, 0),
+        loss = loss + (NEW.buy_price - coalesce(NEW.sell_price, 0))
       WHERE
-        (
-          TG_OP = 'INSERT'
-          AND day = NEW.purchase_time::date
+          day = NEW.purchase_time::date
           AND account_id = NEW.account_id
-        ) OR (
-          TG_OP = 'DELETE'
-          AND day = OLD.purchase_time::date
-          AND account_id = OLD.account_id
-        )
       ;
 
     IF FOUND THEN
-      RETURN CASE WHEN TG_OP = 'INSERT' THEN new WHEN TG_OP = 'DELETE' THEN old END;
+      RETURN new;
+    END IF;
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_daily_aggregates_del() RETURNS trigger AS $$
+BEGIN
+  LOOP
+    BEGIN
+      INSERT INTO bet.daily_aggregates VALUES (OLD.purchase_time::date, OLD.account_id, OLD.buy_price, OLD.buy_price - coalesce(OLD.sell_price, 0));
+      RETURN old;
+    EXCEPTION WHEN unique_violation THEN
+      -- nothing
+    END;
+
+    UPDATE bet.daily_aggregates
+      SET
+        turnover = turnover - coalesce(OLD.buy_price, 0),
+        loss = loss - (OLD.buy_price - coalesce(OLD.sell_price, 0))
+      WHERE
+          day = OLD.purchase_time::date
+          AND account_id = OLD.account_id
+      ;
+
+    IF FOUND THEN
+      RETURN old;
     END IF;
   END LOOP;
 END;
