e0b8f0993bec172d702b2e53c9c6c77444b2fbea
binary-hamedanchi <hamedanchi@gmail.com>
2016-05-23 14:29:19 +0800

    Update redirect_uri for binarycom

--- config/sql/authdb/schema_5_up.sql
+++ config/sql/authdb/schema_5_up.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 ALTER TABLE oauth.apps ADD COLUMN redirect_uri VARCHAR(255);
 
-UPDATE oauth.apps SET redirect_uri='/logged_inws' WHERE id='binarycom';
+UPDATE oauth.apps SET redirect_uri='https://www.binary.com/en/logged_inws.html' WHERE id='1';
 
 DROP TABLE oauth.app_redirect_uri;
 
