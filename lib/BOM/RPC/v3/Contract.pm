31833ad3cbdf86ac5b48f9cb03f1bc3bf566a96d
Jun Bon <junbon@binary.com>
2016-05-25 07:05:49 +0000

    use roundnear

--- lib/BOM/RPC/v3/Contract.pm
+++ lib/BOM/RPC/v3/Contract.pm
@@ -12,6 +12,7 @@ use BOM::Platform::Context qw (localize request);
 use BOM::Product::Offerings qw(get_offerings_with_filter);
 use BOM::Product::ContractFactory qw(produce_contract);
 use BOM::Product::Contract::Helper;
+use Format::Util::Numbers qw(roundnear);
 use Time::HiRes;
 use DataDog::DogStatsd::Helper qw(stats_timing);
 
