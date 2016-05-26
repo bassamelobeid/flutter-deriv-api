c622b25649092ea8e043faeeee4e1a8e01db79f0
Mahdi Mohammadi <mm@binary.com>
2016-05-26 09:18:20 +0000

    add default_vol_duration to underlying_config

--- lib/BOM/Market/Underlying.pm
+++ lib/BOM/Market/Underlying.pm
@@ -265,6 +265,9 @@ sub _build_config {
         $default_dividend_rate = pop @rates;
     }
 
+    my $default_vol_duration = undef;
+    $default_vol_duration = 2 / 86400 if $self->submarket_name eq 'random_daily';
+
     my $default_interest_rate = undef;
 
     # list of markets that have zero rate
@@ -295,6 +298,7 @@ sub _build_config {
         asset_class                           => $asset_class,
         default_interest_rate                 => $default_interest_rate,
         default_dividend_rate                 => $default_dividend_rate,
+        default_volatility_duration           => $default_vol_duration,
     });
 }
 
