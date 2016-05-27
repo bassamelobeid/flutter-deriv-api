9b1bbdcba2aa01305c0eaa66747f2d246bec5f31
Evgeniy Vostrov <evgeniy@binary.com>
2016-05-27 14:07:19 +0000

    Move out parsing code to separate module

--- lib/BOM/WebSocketAPI/Dispatcher.pm
+++ lib/BOM/WebSocketAPI/Dispatcher.pm
@@ -1,6 +1,7 @@
 package BOM::WebSocketAPI::Dispatcher;
 
 use Mojo::Base 'Mojolicious::Controller';
+use BOM::WebSocketAPI::Dispatcher::Parser; # TODO
 use BOM::WebSocketAPI::Dispatcher::Config;
 use BOM::WebSocketAPI::CallingEngine;
 use BOM::WebSocketAPI::v3::Wrapper::System;
@@ -45,7 +46,7 @@ sub on_message {
     my $req_storage = {};
     $req_storage->{args} = $args;
     timeout 15 => sub {
-        $result = $c->parse_req($req_storage);
+        $result = BOM::WebSocketAPI::Dispatcher::Parser::parse_req($c, $req_storage);
         if (!$result
             && (my $action = $c->dispatch($args)))
         {
@@ -156,80 +157,6 @@ sub send_api_response {
         $req_storage->{$hook} ||= [grep { $_ } (ref $config->{$hook} eq 'ARRAY' ? @{$config->{$hook}} : $config->{$hook})];
     }
     BOM::WebSocketAPI::CallingEngine::send_api_response($c, $req_storage, $result);
-    return;
-}
-
-sub parse_req {
-    my ($c, $req_storage) = @_;
-
-    my $result;
-    my $args = $req_storage->{args};
-    if (ref $args ne 'HASH') {
-        # for invalid call, eg: not json
-        $req_storage->{args} = {};
-        $result = $c->new_error('error', 'BadRequest', $c->l('The application sent an invalid request.'));
-    }
-
-    $result = $c->_check_sanity($req_storage) unless $result;
-
-    return $result;
-}
-
-sub _check_sanity {
-    my ($c, $req_storage) = @_;
-
-    my @failed;
-    my $args = $req_storage->{args};
-
-    OUTER:
-    foreach my $k (keys %$args) {
-        if (not ref $args->{$k}) {
-            last OUTER if (@failed = _failed_key_value($k, $args->{$k}));
-        } else {
-            if (ref $args->{$k} eq 'HASH') {
-                foreach my $l (keys %{$args->{$k}}) {
-                    last OUTER
-                        if (@failed = _failed_key_value($l, $args->{$k}->{$l}));
-                }
-            } elsif (ref $args->{$k} eq 'ARRAY') {
-                foreach my $l (@{$args->{$k}}) {
-                    last OUTER if (@failed = _failed_key_value($k, $l));
-                }
-            }
-        }
-    }
-
-    if (@failed) {
-        $c->app->log->warn("Sanity check failed: " . $failed[0] . " -> " . ($failed[1] // "undefined"));
-        my $result = $c->new_error('sanity_check', 'SanityCheckFailed', $c->l("Parameters sanity check failed."));
-        if (    $result->{error}
-            and $result->{error}->{code} eq 'SanityCheckFailed')
-        {
-            $req_storage->{args} = {};
-        }
-        return $result;
-    }
-    return;
-}
-
-sub _failed_key_value {
-    my ($key, $value) = @_;
-
-    if ($key =~ /password/) {    # TODO review carefully
-        return;
-    } elsif (
-        $key !~ /^[A-Za-z0-9_-]{1,50}$/
-        # !-~ to allow a range of acceptable characters. To find what is the range, look at ascii table
-
-        # please don't remove: \p{Script=Common}\p{L}
-        # \p{L} is to match utf-8 characters
-        # \p{Script=Common} is to match double byte characters in Japanese keyboards, eg: '１−１−１'
-        # refer: http://perldoc.perl.org/perlunicode.html
-        # null-values are allowed
-        or ($value and $value !~ /^[\p{Script=Common}\p{L}\s\w\@_:!-~]{0,300}$/))
-    {
-        return ($key, $value);
-    }
     return;
 }
 
