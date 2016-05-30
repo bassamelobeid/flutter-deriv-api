6735c94ef9aab056c7b67268a6a6db85e740317b
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-05-30 14:43:32 +0800

    for spread

--- lib/BOM/WebSocketAPI/v3/Wrapper/Pricer.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Pricer.pm
@@ -219,7 +219,11 @@ sub _price_stream_results_adjustment {
         $results->{ask_price}     = roundnear(0.01, $amount);
         $results->{display_value} = roundnear(0.01, $amount);
         $results->{payout}        = roundnear(0.01, $payout);
+    } else {
+        # in case that it is a spread
+        return $results;
     }
+
 
     if (
         my $error = BOM::RPC::v3::Contract::validate_price({
