9ec8d9ba23bb2a142704a18b2a085eff53e69e9c
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-06-01 05:38:04 +0000

    move cron

---
+++ cron/backfill_affiliate_data.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+package main;
+
+=head1 NAME
+
+myaffiliates/backfill_affiliate_data.pl
+
+=head1 DESCRIPTION
+
+su nobody -c "perl /home/git/regentmarkets/bom-backoffice/crons/myaffiliates/backfill_affiliate_data.pl"
+
+=cut
+
+use strict;
+use warnings;
+
+use Date::Utility;
+use BOM::System::Localhost;
+use BOM::Platform::Email qw(send_email);
+use BOM::Platform::MyAffiliates::BackfillManager;
+use BOM::Platform::Sysinit ();
+use BOM::Platform::Runtime;
+
+BOM::Platform::Sysinit::init();
+
+my $runtime = Date::Utility->new;
+
+my $backfill_manager            = BOM::Platform::MyAffiliates::BackfillManager->new;
+my @backfill_promo_codes_report = $backfill_manager->backfill_promo_codes;
+
+my $full_report = ['Mark First Deposits:', ''];
+push @{$full_report}, ('', 'Promo Codes:', '');
+push @{$full_report}, @backfill_promo_codes_report;
+
+send_email({
+    from    => 'system@binary.com',
+    to      => BOM::Platform::Runtime->instance->app_config->marketing->myaffiliates_email,
+    subject => 'CRON backfill_affiliate_data: Report from ' . BOM::System::Localhost::name() . ' for ' . $runtime->datetime_yyyymmdd_hhmmss_TZ,
+    message => $full_report,
+});
