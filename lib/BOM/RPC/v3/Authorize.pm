421b862c10a80be58b9dcf9d3a76bf2c59332f1a
Raunak Kathuria <raunakkathuria@gmail.com>
2016-06-01 07:12:37 +0000

    skip login history for logout in case of impersonate

--- lib/BOM/RPC/v3/Authorize.pm
+++ lib/BOM/RPC/v3/Authorize.pm
@@ -77,23 +77,30 @@ sub logout {
 
     if (my $email = $params->{email}) {
         my $token_details = $params->{token_details};
-        my $loginid = ($token_details and exists $token_details->{loginid}) ? $token_details->{loginid} : '';
+        my ($loginid, $scopes) = ($token_details and exists $token_details->{loginid}) ? @{$token_details}{qw/loginid scopes/} : ();
+
         if (my $user = BOM::Platform::User->new({email => $email})) {
-            $user->add_login_history({
-                environment => _login_env($params),
-                successful  => 't',
-                action      => 'logout',
-            });
-            $user->save;
+            my $skip_login_history;
 
             if ($params->{token_type} eq 'oauth_token') {
                 # revoke tokens for user per app_id
                 my $oauth  = BOM::Database::Model::OAuth->new;
                 my $app_id = $oauth->get_app_id_by_token($params->{token});
 
+                $skip_login_history = 1 if ($scopes and scalar(@$scopes) == 1 and grep { $_ eq 'read' } @$scopes);
+
                 foreach my $c1 ($user->clients) {
                     $oauth->revoke_tokens_by_loginid_app($c1->loginid, $app_id);
                 }
+            }
+
+            unless ($skip_login_history) {
+                $user->add_login_history({
+                    environment => _login_env($params),
+                    successful  => 't',
+                    action      => 'logout',
+                });
+                $user->save;
             }
         }
         BOM::System::AuditLog::log("user logout", "$email,$loginid");
