54181934aea02148c0c43b25f24dbe8126d86a64
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-06-03 05:47:26 +0800

    copy&paste

--- bin/price_queue.pl
+++ bin/price_queue.pl
@@ -9,11 +9,15 @@ use Time::HiRes::Sleep::Until;
 my $redis = BOM::System::RedisReplicated::redis_pricer;
 
 while (Time::HiRes::Sleep::Until->new->epoch(time+1)) {
-    $keys = $redis->scan_all(MATCH=>'Redis::Processor::*', COUNT=>1000000);
-
-    DataDog::DogStatsd::Helper::stats_gauge('pricer_daemon.queue.size', (scalar @$keys));
-    DataDog::DogStatsd::Helper::stats_gauge('pricer_daemon.queue.not_processed', $redis->llen('pricer_jobs'));
-    $redis->del('pricer_jobs');
-    $redis->push('pricer_jobs', uniq @{$keys});
+    my $rc = $redis->eval(<<'LUA', 0);
+    local ql=redis.call('LLEN', 'pricer_jobs')
+    redis.call('DEL', 'pricer_jobs')
+    return {
+        ql,
+        redis.call('RPUSH', 'pricer_jobs',
+                   unpack(redis.call('KEYS', 'Redis::Processor::*')))
+    }
+LUA
+    DataDog::DogStatsd::Helper::stats_gauge('pricer_daemon.queue.not_processed', $rc->[0]);
+    DataDog::DogStatsd::Helper::stats_gauge('pricer_daemon.queue.size', $rc->[1]);
 }
-
