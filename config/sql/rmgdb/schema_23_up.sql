835ebad66d7d5871e251375222a011e9c8766193
Raunak Kathuria <raunakkathuria@gmail.com>
2016-06-02 01:26:43 +0000

    remove default 0 for transaction markup

--- config/sql/rmgdb/schema_23_up.sql
+++ config/sql/rmgdb/schema_23_up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE transaction.transaction ADD COLUMN markup NUMERIC DEFAULT 0;
+ALTER TABLE transaction.transaction ADD COLUMN markup NUMERIC;
 GRANT INSERT (markup) ON TABLE transaction.transaction TO read, write;
 
 COMMIT;
