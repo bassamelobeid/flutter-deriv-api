a74f9f95894f6250516a7cb68232b4aeb57130b2
Sasha Murzin <sasha@binary.com>
2016-06-07 08:08:43 +0000

    Move BOM::Platform::System::Plack::App to BOM::Backoffice::PlackApp

--- lib/BOM/Platform/Sysinit.pm
+++ lib/BOM/Platform/Sysinit.pm
@@ -10,7 +10,7 @@ use BOM::Platform::Context::Request;
 use BOM::Platform::Context qw(request);
 use BOM::Platform::Plack qw( PrintContentType );
 use BOM::Platform::Static::Config;    # called here as we generate hash for static files as it does not change on every request
-use Try::Tiny::Except ();             # should be preloaded as early as possible (BOM::System::Plack::App).
+use Try::Tiny::Except ();             # should be preloaded as early as possible (BOM::Backoffice::PlackApp).
                                       # this statement here is merely a comment.
 
 sub init {
