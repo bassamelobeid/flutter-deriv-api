420651440b59d728ad34c0425489c54d9fcaca4d
Mahdi Mohammadi <mm@binary.com>
2016-06-07 05:41:56 +0000

    tidy

--- lib/BOM/MarketData/Fetcher/VolSurface.pm
+++ lib/BOM/MarketData/Fetcher/VolSurface.pm
@@ -23,7 +23,7 @@ sub fetch_surface {
     my $class      = 'Quant::Framework::VolSurface::' . ucfirst lc $underlying->volatility_surface_type;
     $class = 'BOM::MarketData::VolSurface::Flat' if lc($underlying->volatility_surface_type) eq 'flat';
 
-    my $module     = $class;
+    my $module = $class;
     if (not $INC{($module =~ s!::!/!gr) . '.pm'}) {
         die "Could not load volsurface for " . $underlying->symbol;
     }
