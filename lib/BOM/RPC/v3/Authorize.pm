e7fabefb126fd869cec0a3349ec04d16585a7d09
Raunak Kathuria <raunakkathuria@gmail.com>
2016-06-07 06:40:42 +0000

    fix concat unitialized warning

--- lib/BOM/RPC/v3/Authorize.pm
+++ lib/BOM/RPC/v3/Authorize.pm
@@ -103,8 +103,8 @@ sub logout {
                 });
                 $user->save;
             }
+            BOM::System::AuditLog::log("user logout", "$email,$loginid");
         }
-        BOM::System::AuditLog::log("user logout", "$email,$loginid");
     }
 
     # Invalidates token, but we can only do this if we have a session token
