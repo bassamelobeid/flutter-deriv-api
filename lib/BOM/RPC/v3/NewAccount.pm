1a79372153339238b68c8b126bbbb7f9bd93f6f5
Arun Venkataraman <arun@binary.com>
2016-06-08 08:41:03 +0000

    Added login history logging for new accounts

--- lib/BOM/RPC/v3/NewAccount.pm
+++ lib/BOM/RPC/v3/NewAccount.pm
@@ -66,6 +66,15 @@ sub new_account_virtual {
 
     my $client  = $acc->{client};
     my $account = $client->default_account->load;
+    my $user    = $acc->{user};
+    $user->add_login_history({
+        action      => 'login',
+        environment => BOM::RPC::v3::Utility::login_env($params),
+        successful  => 't'
+    });
+    $user->save;
+
+    BOM::System::AuditLog::log("successful login", "$email");
     return {
         client_id   => $client->loginid,
         email       => $email,
@@ -185,6 +194,16 @@ sub new_account_real {
 
     my $new_client      = $acc->{client};
     my $landing_company = $new_client->landing_company;
+    my $user            = $acc->{user};
+
+    $user->add_login_history({
+        action      => 'login',
+        environment => BOM::RPC::v3::Utility::login_env($params),
+        successful  => 't'
+    });
+    $user->save;
+
+    BOM::System::AuditLog::log("successful login", "$client->email");
     return {
         client_id                 => $new_client->loginid,
         landing_company           => $landing_company->name,
@@ -233,6 +252,16 @@ sub new_account_maltainvest {
 
     my $new_client      = $acc->{client};
     my $landing_company = $new_client->landing_company;
+    my $user            = $acc->{user};
+
+    $user->add_login_history({
+        action      => 'login',
+        environment => BOM::RPC::v3::Utility::login_env($params),
+        successful  => 't'
+    });
+    $user->save;
+
+    BOM::System::AuditLog::log("successful login", "$client->email");
     return {
         client_id                 => $new_client->loginid,
         landing_company           => $landing_company->name,
@@ -287,6 +316,16 @@ sub new_account_japan {
 
     my $new_client      = $acc->{client};
     my $landing_company = $new_client->landing_company;
+    my $user            = $acc->{user};
+
+    $user->add_login_history({
+        action      => 'login',
+        environment => BOM::RPC::v3::Utility::login_env($params),
+        successful  => 't'
+    });
+    $user->save;
+
+    BOM::System::AuditLog::log("successful login", "$client->email");
     return {
         client_id                 => $new_client->loginid,
         landing_company           => $landing_company->name,
