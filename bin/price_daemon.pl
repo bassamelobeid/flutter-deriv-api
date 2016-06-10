de116161e84f2774ceeb38d28df82df2126fcb30
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-06-10 12:45:05 +0800

    tidy

--- bin/price_daemon.pl
+++ bin/price_daemon.pl
@@ -10,7 +10,9 @@ use BOM::RPC::v3::Contract;
 use sigtrap qw/handler signal_handler normal-signals/;
 
 my $workers = 4;
-GetOptions ("workers=i" => \$workers,) ;
+GetOptions(
+    "workers=i" => \$workers,
+);
 
 my $pm = new Parallel::ForkManager($workers);
 
@@ -22,18 +24,17 @@ $pm->run_on_start(
         push @running_forks, $pid;
         DataDog::DogStatsd::Helper::stats_gauge('pricer_daemon.forks.count', (scalar @running_forks));
         warn "Started a new fork [$pid]\n";
-    }
-);
+    });
 $pm->run_on_finish(
     sub {
         my ($pid, $exit_code) = @_;
-        @running_forks = grep {$_ != $pid } @running_forks;
+        @running_forks = grep { $_ != $pid } @running_forks;
         DataDog::DogStatsd::Helper::stats_gauge('pricer_daemon.forks.count', (scalar @running_forks));
         warn "Fork [$pid] ended with exit code [$exit_code]\n";
-    }
-);
+    });
+
 sub signal_handler {
-    kill KILL=>@running_forks;
+    kill KILL => @running_forks;
     exit 0;
 }
 
@@ -49,15 +50,15 @@ while (1) {
 
         my $next = $key->[1];
         $next =~ s/^PRICER_KEYS:://;
-        my $payload  = JSON::XS::decode_json($next);
-        my $params   = {@{$payload}};
-        my $trigger  = $params->{symbol};
+        my $payload = JSON::XS::decode_json($next);
+        my $params  = {@{$payload}};
+        my $trigger = $params->{symbol};
 
-        my $current_time = time;
+        my $current_time    = time;
         my $current_spot_ts = BOM::Market::Underlying->new($params->{symbol})->spot_tick->epoch;
-        my $last_price_ts = $redis->get($next) || 0;
+        my $last_price_ts   = $redis->get($next) || 0;
 
-        next if ($current_spot_ts==$last_price_ts and $current_time - $last_price_ts<=10 );
+        next if ($current_spot_ts == $last_price_ts and $current_time - $last_price_ts <= 10);
 
         $redis->set($next, $current_time);
         my $response = BOM::RPC::v3::Contract::send_ask({args => $params}, 1);
