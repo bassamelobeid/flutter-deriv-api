06a3d3721ecf8a0f3e4b73349f78a84608b6e0b1
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-06-17 07:34:44 +0000

    fix binary.com app for unit test

--- config/sql/authdb/schema_5_up.sql
+++ config/sql/authdb/schema_5_up.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 ALTER TABLE oauth.apps ADD COLUMN redirect_uri VARCHAR(255);
 
-UPDATE oauth.apps SET redirect_uri='https://www.binary.com/en/logged_inws.html' WHERE id='1';
+UPDATE oauth.apps SET redirect_uri='https://www.binary.com/en/logged_inws.html', homepage='', github='', appstore='', googleplay='' WHERE id='binarycom';
 
 DROP TABLE oauth.app_redirect_uri;
 
