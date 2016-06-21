20d13065f13eb29c63ed2938e688aa6027030248
Mahdi Mohammadi <mm@binary.com>
2016-06-21 04:52:04 +0000

    S0P is invalid for asian tick trades

--- lib/BOM/Product/ContractFactory.pm
+++ lib/BOM/Product/ContractFactory.pm
@@ -218,8 +218,8 @@ sub produce_contract {
             my $possible = $input_params{$barrier_name};
 
             #if this is an asisn tick expiry contract and also a barrier is specified, reject the request
-            if (defined $input_params{tick_expiry} and defined $possible and $input_params{bet_type} =~ /^ASIAN/ and $possible ne 'S0P') {
-                die 'Tick expiry contracts cannot have barrier values';
+            if (defined $input_params{tick_expiry} and defined $possible and $input_params{bet_type} =~ /^ASIAN/) {
+                die 'Asian tick-expiry contracts cannot have barrier values';
             }
 
             if (ref($possible) !~ /BOM::Product::Contract::Strike/) {
