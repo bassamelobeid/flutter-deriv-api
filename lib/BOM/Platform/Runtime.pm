7a4a90acfa22b57b396267a8a2a3c3e94eb03856
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-06-23 10:33:09 +0800

    02

--- lib/BOM/Platform/Runtime.pm
+++ lib/BOM/Platform/Runtime.pm
@@ -12,56 +12,23 @@ use YAML::XS;
 use Locale::Country::Extra;
 use Locale::Country;
 
-=head1 NAME
-
-BOM::Platform::Runtime
-
-=head1 SYNOPSIS
-
-  use BOM::Platform::Runtime;
-
-  my $website = BOM::Platform::Runtime->instance->website_list->get('Binary') #Gets the Binary website.
-
-=head1 ATTRIBUTES
-
-=head2 app_config
-
-Returns an reference to an BOM::Platform::Runtime::AppConfig object.
-
-=cut
 
 has 'app_config' => (
     is         => 'ro',
     lazy_build => 1,
 );
 
-=head2 broker_codes
-
-Returns an reference to an BOM::Platform::Runtime::Broker::Codes object.
-
-=cut
 
 has 'broker_codes' => (
     is         => 'ro',
     lazy_build => 1,
 );
 
-=head2 landing_companies
-
-Returns an reference to an BOM::Platform::Runtime::LandingCompany::Registry
-
-=cut
 
 has 'landing_companies' => (
     is         => 'ro',
     lazy_build => 1,
 );
-
-=head2 countries
-
-Returns an reference to a Locale::Country::Extra object.
-
-=cut
 
 has 'countries' => (
     is         => 'ro',
@@ -72,18 +39,6 @@ has 'countries_list' => (
     is         => 'ro',
     lazy_build => 1,
 );
-
-=head1 METHODS
-
-=head2 instance
-
-Returns an active instance of the object.
-
-It actively maintains instance of object using state functionality of perl.
-This means this class is not a singleton but has capabilities to maintain a single
-copy across the system's execution environment.
-
-=cut
 
 my $instance;
 
@@ -185,13 +140,3 @@ sub _build_landing_companies {
 
 __PACKAGE__->meta->make_immutable;
 1;
-
-=head1 AUTHOR
-
-Nick Marden, C<< <nick at regentmarkets.com> >>
-
-=head1 COPYRIGHT
-
-(c) 2011-, RMG Tech (Malaysia) Sdn Bhd
-
-=cut
