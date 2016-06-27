aab8ad9dcf8e7dac406d1cad0207d6caaffaff1e
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-06-27 18:15:28 +0800

    using name

--- lib/BOM/Platform/Account/Virtual.pm
+++ lib/BOM/Platform/Account/Virtual.pm
@@ -36,11 +36,9 @@ sub create_account {
 
     my ($client, $error);
     try {
-        my $countries_list = YAML::XS::LoadFile('/home/git/regentmarkets/bom-platform/config/countries.yml');
-
         $client = BOM::Platform::Client->register_and_return_new_client({
             broker_code =>
-                BOM::Platform::Runtime::LandingCompany::Registry::get($countries_list->{$residence}->{virtual_company})->broker_codes->[0],
+                BOM::Platform::Runtime::LandingCompany::Registry::get('fog')->broker_codes->[0],
             client_password               => $password,
             salutation                    => '',
             last_name                     => '',
