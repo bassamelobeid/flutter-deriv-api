b74c171e0be87c3808bab0d7b7044ce72e152f5c
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-06-28 10:05:07 +0800

    no chronicle

--- config/sql/chronicledb/initialize_db.sql
+++
@@ -1,6 +0,0 @@
--- This will create Database, schema & Roles for chronicle db
-CREATE DATABASE chronicle
-  ENCODING='UTF8'
-  LC_COLLATE='en_US.UTF-8'
-  LC_CTYPE='en_US.UTF-8';
-CREATE ROLE write LOGIN password 'mRX1E3Mi00oS8LG';
