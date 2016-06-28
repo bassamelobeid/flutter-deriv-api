a9006ea9f0546d071c3874b9f78a72fd62f11f2e
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-06-28 10:10:20 +0800

    no auth

--- config/sql/authdb/schema_5_up.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-ALTER TABLE oauth.apps ADD COLUMN redirect_uri VARCHAR(255);
-
-UPDATE oauth.apps SET redirect_uri='https://www.binary.com/en/logged_inws.html', homepage='', github='', appstore='', googleplay='' WHERE id='binarycom';
-
-DROP TABLE oauth.app_redirect_uri;
-
-COMMIT;
