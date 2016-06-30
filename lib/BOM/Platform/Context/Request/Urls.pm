1f3b0e5527392fcb157d9e0ecc6d10bb48f56ceb
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-06-30 11:53:14 +0800

    more

--- lib/BOM/Platform/Context/Request/Urls.pm
+++ lib/BOM/Platform/Context/Request/Urls.pm
@@ -1,80 +1,17 @@
 package BOM::Platform::Context::Request::Urls;
 
-use Memoize;
-use Moose::Role;
 use Mojo::URL;
-
-use BOM::Platform::Runtime;
-use BOM::Platform::Context;
-use BOM::Platform::Static::Config;
-use BOM::System::Config;
-
-sub domain {
-    my $self   = shift;
-    my $domain = $self->domain_name;
-
-    if ($domain eq 'localhost') {
-        $domain = $self->_build_domain_name;
-    }
-
-    $domain =~ s/[a-zA-Z0-9\-]+\.([a-zA-Z0-9\-]+\.[a-zA-Z0-9\-]+)/$1/;
-    return $domain;
-}
 
 sub url_for {
     my ($self, @args) = @_;
 
     my $url = Mojo::URL->new($args[0] || '');
     my $query = $args[1] || {};
-    my $domain_type = _get_domain_type($url->path, (($args[2] ? %{$args[2]} : ())));
-    my $internal = $args[3] || {};
 
-    if ($domain_type->{static}) {
-        my $path = $url->path;
-        $path =~ s/^\///;
-        my $complete_path;
-
-        if ($internal->{internal_static}) {
-            $complete_path = Mojo::URL->new(BOM::Platform::Runtime->instance->app_config->cgi->backoffice->static_url);
-        } else {
-            $complete_path = Mojo::URL->new(BOM::Platform::Static::Config::get_static_url());
-        }
-        $complete_path = $complete_path->to_string . $path;
-        $url           = $url->parse($complete_path);
-    } else {
-        $url->query($query);
-
-        if ($domain_type->{cgi} or $domain_type->{bo}) {
-            my $path = $url->path;
-
-            if ($domain_type->{bo} and $path !~ /backoffice/) {
-                $path = 'backoffice/' . $path;
-            }
-            $path = '/d/' . $path;
-            $path =~ s/\/\//\//g;
-            $url->path($path);
-        } elsif ($url->path !~ /^\//) {
-            my $path = $url->path;
-            $url->path("/$path");
-        }
-
-        if (not exists $domain_type->{no_host}) {
-            my $domain = $self->domain_for($domain_type);
-            $url->host($domain);
-        }
-
-        #Add Language to URL
-        if ($query->{l}) {
-            $url->query([l => $query->{l}]);
-        } elsif (not $domain_type->{no_lang}) {
-            $url->query([l => $self->language]);
-        }
-    }
-
-    #Force https, only if we are building a full url
-    if ($url->host) {
-        $url->scheme('https');
-    }
+    $url->query($query);
+    $url->path('/d/backoffice/' . $url->path);
+    $url->host($self->domain_for());
+    $url->scheme('https');
 
     return $url;
 }
@@ -91,41 +28,6 @@ sub domain_for {
     return $server_name . '.binary.com';
 }
 
-memoize('_get_domain_type');
-
-sub _get_domain_type {
-    my ($path, %defaults) = @_;
-    my $domain_type = {%defaults};
-
-    #Select domain_type base on path
-    if ($path) {
-        if ($path =~ /^(xsl|temp)/) {
-            $domain_type->{localhost} = 1;
-            $domain_type->{no_lang}   = 1;
-        } elsif ($path =~ /^errors/) {
-            $domain_type->{direct}  = 1;
-            $domain_type->{no_lang} = 1;
-        } elsif ($path =~ /\.(?!cgi)/) {
-            $domain_type->{static} = 1;
-        } elsif ($path =~ /backoffice/) {
-            $domain_type->{bo}      = 1;
-            $domain_type->{cgi}     = 1;
-            $domain_type->{no_lang} = 1;
-        } elsif ($path =~ 'f_onlineid') {
-            $domain_type->{dealing} = 1;
-        } elsif ($path =~ /\.cgi/) {
-            $domain_type->{cgi} = 1;
-        }
-    } else {
-        $domain_type->{frontend} = 1;
-    }
-
-    if ($domain_type->{bo}) {
-        $domain_type->{no_lang} = 1;
-    }
-
-    return $domain_type;
-}
 
 1;
 
