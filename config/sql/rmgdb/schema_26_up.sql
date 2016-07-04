8975368b0ebe2a5d5fd7faad34cd32a6d34d54a4
Raunak Kathuria <raunakkathuria@gmail.com>
2016-07-04 17:32:48 +0800

    add sub_account_of column to betonmarkets.client,reference loginid

--- config/sql/rmgdb/schema_26_up.sql
+++ config/sql/rmgdb/schema_26_up.sql
@@ -3,4 +3,7 @@ BEGIN;
 ALTER TABLE betonmarkets.client ADD COLUMN allow_omnibus BOOLEAN;
 ALTER TABLE audit.client ADD COLUMN allow_omnibus BOOLEAN;
 
+ALTER TABLE betonmarkets.client ADD COLUMN sub_account_of VARCHAR(12);
+ALTER TABLE betonmarkets.client ADD CONSTRAINT fk_sub_account_loginid FOREIGN KEY (sub_account_of) REFERENCES betonmarkets.client(loginid) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
 COMMIT;
