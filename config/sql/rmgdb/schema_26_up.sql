4e021ee8e4cf90b1a6ab002fb1a899494bef4131
Raunak Kathuria <raunakkathuria@gmail.com>
2016-07-05 09:47:13 +0800

    Add sub_account_of to audit table also

--- config/sql/rmgdb/schema_26_up.sql
+++ config/sql/rmgdb/schema_26_up.sql
@@ -6,4 +6,6 @@ ALTER TABLE audit.client ADD COLUMN allow_omnibus BOOLEAN;
 ALTER TABLE betonmarkets.client ADD COLUMN sub_account_of VARCHAR(12);
 ALTER TABLE betonmarkets.client ADD CONSTRAINT fk_sub_account_loginid FOREIGN KEY (sub_account_of) REFERENCES betonmarkets.client(loginid) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
+ALTER TABLE audit.client ADD COLUMN sub_account_of VARCHAR(12);
+
 COMMIT;
