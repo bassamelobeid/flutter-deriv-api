cb6a51fb00034d50216cb6d53f438dcc5709225d
Evgeniy Vostrov <evgeniy@binary.com>
2016-07-07 08:16:39 +0000

    Minor fix

--- lib/BOM/WebSocketAPI/v3/Wrapper/Pricer.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Pricer.pm
@@ -139,9 +139,13 @@ sub process_pricing_events {
             $results = $err;
         } else {
             delete $response->{longcode};
-            my $adjusted_results =
-                _price_stream_results_adjustment($pricing_channel->{$serialized_args}->{$amount}->{contract_parameters}, $response,
-                $theo_probability);
+            my $adjusted_results = _price_stream_results_adjustment({
+                    %{$pricing_channel->{$serialized_args}->{$amount}->{contract_parameters}},
+                    app_markup_percentage => $pricing_channel->{$serialized_args}->{$amount}->{args}->{app_markup_percentage}
+                },
+                $response,
+                $theo_probability
+            );
 
             if (my $ref = $adjusted_results->{error}) {
                 my $err = $c->new_error('proposal', $ref->{code}, $ref->{message_to_client});
@@ -197,8 +201,7 @@ sub _price_stream_results_adjustment {
         minimum     => 0,
         maximum     => 1,
     });
-    $contract_parameters->{theo_probability}      = $theo_probability;
-    $contract_parameters->{app_markup_percentage} = $contract_parameters->{app_markup_percentage};
+    $contract_parameters->{theo_probability} = $theo_probability;
     my $contract = BOM::RPC::v3::Contract::create_contract($contract_parameters);
 
     if (my $error = $contract->validate_price) {
