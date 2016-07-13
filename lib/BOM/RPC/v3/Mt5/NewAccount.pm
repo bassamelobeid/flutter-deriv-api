afabdec52142939267641cb1361bb91c74c4ac79
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-07-13 10:32:35 +0000

    mt5 a/c create

--- lib/BOM/RPC/v3/Mt5/NewAccount.pm
+++ lib/BOM/RPC/v3/Mt5/NewAccount.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use BOM::RPC::v3::Utility;
 use BOM::Platform::Context qw (localize);
+use BOM::Platform::User;
 use BOM::Mt5::User;
 
 sub new_account_mt5 {
@@ -26,24 +27,26 @@ sub new_account_mt5 {
                 message_to_client => localize('Invalid account type.')
             });
     }
-
     $args->{group} = $group;
-    my $user = BOM::Mt5::User::create_user($args);
 
-    if ($user->{error}) {
+    my $status = BOM::Mt5::User::create_user($args);
+    if ($status->{error}) {
         return BOM::RPC::v3::Utility::create_error({
                 code              => 'Mt5CreateUserError',
-                message_to_client => $user->{error}
+                message_to_client => $status->{error}
             });
     }
+    my $mt5_login = $status->{login};
 
-    my $login = $user->{login};
+    my $user = BOM::Platform::User->new({email => $client->email});
+    $user->add_loginid({loginid => $mt5_login});
+    $user->save;
+
     my $balance = 0;
-
     # funds in Virtual money
     if ($account_type eq 'demo') {
         $balance = 5000;
-        my $status = BOM::Mt5::User::virtual_deposit($login, $balance);
+        $status = BOM::Mt5::User::virtual_deposit($mt5_login, $balance);
 
         # deposit failed
         if ($status->{error}) {
@@ -52,7 +55,7 @@ sub new_account_mt5 {
     }
 
     return {
-        login           => $user->{login},
+        login           => $mt5_login,
         account_type    => $account_type,
         balance         => $balance
     };
