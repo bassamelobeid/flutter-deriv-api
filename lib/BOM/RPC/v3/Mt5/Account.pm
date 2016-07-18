94a4625d513c4ef228f975c73e667cc5fb2e72c4
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-07-18 09:08:32 +0000

    MT5 deposit / withdrawal

--- lib/BOM/RPC/v3/Mt5/Account.pm
+++ lib/BOM/RPC/v3/Mt5/Account.pm
@@ -46,7 +46,11 @@ sub new_account_mt5 {
     # funds in Virtual money
     if ($account_type eq 'demo') {
         $balance = 5000;
-        $status = BOM::Mt5::User::virtual_deposit($mt5_login, $balance);
+        $status = BOM::Mt5::User::deposit({
+                login   => $mt5_login,
+                amount  => $balance,
+                comment => 'Binary MT5 Virtual Money deposit.'
+            });
 
         # deposit failed
         if ($status->{error}) {
@@ -146,6 +150,143 @@ sub password_change_mt5 {
                 message_to_client => $status->{error}
             });
     }
+    return 1;
+}
+
+sub deposit {
+    my $params = shift;
+    my $client = $params->{client};
+    my $args   = $params->{args};
+    my $source = $params->{source};
+
+    my $from_binary = $args->{from_binary};
+    my $to_mt5      = $args->{to_mt5};
+    my $amount      = $args->{amount};
+
+    if ($amount <= 0) {
+        return BOM::RPC::v3::Utility::create_error({
+                code              => 'Mt5DepositError',
+                message_to_client => localize("Amount must be greater than zero."),
+            });
+    }
+
+    # MT5 login or binary loginid not belongs to user
+    my $user = BOM::Platform::User->new({email => $client->email});
+    if (not grep { 'MT'.$to_mt5 eq $_->loginid } ($user->loginid)) {
+        return BOM::RPC::v3::Utility::permission_error();
+    }
+    if (not grep { $from_binary eq $_->loginid } ($user->loginid)) {
+        return BOM::RPC::v3::Utility::permission_error();
+    }
+
+    my $comment = "Transfer from $from_binary to MT5 account $to_mt5.";
+
+    # withdraw from Binary a/c
+    my $fmClient = BOM::Platform::Client->new({ loginid => $from_binary });
+    my $fmAccount = $fmClient->set_default_account('USD');
+
+    my ($fmPayment) = $fmAccount->add_payment({
+        amount               => -$amount,
+        payment_gateway_code => 'account_transfer',
+        payment_type_code    => 'internal_transfer',
+        status               => 'OK',
+        staff_loginid        => $from_binary,
+        remark               => $comment,
+    });
+    my ($fmTrx) = $fmPayment->add_transaction({
+        account_id    => $fmAccount->id,
+        amount        => -$amount,
+        staff_loginid => $from_binary,
+        referrer_type => 'payment',
+        action_type   => 'withdrawal',
+        quantity      => 1,
+        source        => $source,
+    });
+    $fmAccount->save(cascade => 1);
+    $fmPayment->save(cascade => 1);
+
+    # deposit to MT5 a/c
+    my $status = BOM::Mt5::User::deposit({
+            login   => $to_mt5,
+            amount  => $amount,
+            comment => $comment
+        });
+
+    if ($status->{error}) {
+        return BOM::RPC::v3::Utility::create_error({
+                code              => 'Mt5DepositError',
+                message_to_client => $status->{error}
+            });
+    }
+    return 1;
+}
+
+sub withdrawal {
+    my $params = shift;
+    my $client = $params->{client};
+    my $args   = $params->{args};
+    my $source = $params->{source};
+
+    my $from_mt5  = $args->{from_mt5};
+    my $to_binary = $args->{to_binary};
+    my $amount    = $args->{amount};
+
+    if ($amount <= 0) {
+        return BOM::RPC::v3::Utility::create_error({
+                code              => 'Mt5WithdrawalError',
+                message_to_client => localize("Amount must be greater than zero."),
+            });
+    }
+
+    # MT5 login or binary loginid not belongs to user
+    my $user = BOM::Platform::User->new({email => $client->email});
+    if (not grep { 'MT'.$from_mt5 eq $_->loginid } ($user->loginid)) {
+        return BOM::RPC::v3::Utility::permission_error();
+    }
+    if (not grep { $to_binary eq $_->loginid } ($user->loginid)) {
+        return BOM::RPC::v3::Utility::permission_error();
+    }
+
+    my $comment = "Transfer from MT5 account $from_mt5 to $to_binary.";
+
+    # withdraw from MT5 a/c
+    my $status = BOM::Mt5::User::withdrawal({
+            login   => $from_mt5,
+            amount  => $amount,
+            comment => $comment
+        });
+
+    if ($status->{error}) {
+        return BOM::RPC::v3::Utility::create_error({
+                code              => 'Mt5WithdrawalError',
+                message_to_client => $status->{error}
+            });
+    }
+
+    # deposit to Binary a/c
+    my $toClient = BOM::Platform::Client->new({ loginid => $to_binary });
+    my $toAccount = $toClient->set_default_account('USD');
+
+    my ($toPayment) = $toAccount->add_payment({
+        amount               => $amount,
+        payment_gateway_code => 'account_transfer',
+        payment_type_code    => 'internal_transfer',
+        status               => 'OK',
+        staff_loginid        => $to_binary,
+        remark               => $comment,
+    });
+    my ($toTrx) = $toPayment->add_transaction({
+        account_id    => $toAccount->id,
+        amount        => $amount,
+        staff_loginid => $to_binary,
+        referrer_type => 'payment',
+        action_type   => 'deposit',
+        quantity      => 1,
+        source        => $source,
+    });
+    $toAccount->save(cascade => 1);
+    $toPayment->save(cascade => 1);
+
     return 1;
 }
 
