5b1e66aa19bfb1b3ff28226964f5b7e87e2fcccb
Chylli <chylli@binary.com>
2016-08-04 09:28:38 +0800

    replace AppConfig with App::Config

--- lib/BOM/Platform/Runtime.pm
+++ lib/BOM/Platform/Runtime.pm
@@ -3,7 +3,7 @@ package BOM::Platform::Runtime;
 use Moose;
 use feature 'state';
 
-use BOM::Platform::Runtime::AppConfig;
+use App::Config;
 
 has 'app_config' => (
     is         => 'ro',
@@ -25,7 +25,10 @@ sub instance {
 
 sub _build_app_config {
     my $self = shift;
-    return BOM::Platform::Runtime::AppConfig->new();
+    return App::Config->new(definition_yml => /home/git/regentmarkets/bom-platform/config/app_config_definitions.yml,
+                            chronicle_reader => BOM::System::Chronicle::get_chronicle_reader(),
+                            chronicle_writer => BOM::System::Chronicle::get_chronicle_writer(),
+                           );
 }
 
 __PACKAGE__->meta->make_immutable;
