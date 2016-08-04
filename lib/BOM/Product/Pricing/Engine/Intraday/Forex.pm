13f091f77aba77136f198e0d187d478d74792efd
Evgeniy Vostrov <evgeniy@binary.com>
2016-08-04 05:23:27 +0000

    Minor fix

--- lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
+++ lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
@@ -291,12 +291,12 @@ sub calculate_intraday_bounceback {
 
     my $calibration_coef = $self->coefficients->{$self->bet->underlying->symbol};
     my $max_abs_trend    = max(abs($calibration_coef->{trend_min}), $calibration_coef->{trend_max});
-    my $slope            = $self->slope || 1;
+    my $slope            = $self->slope;
 
     my $bounceback_base_intraday_trend = $self->calculate_bounceback_base($t_mins, $st_or_lt, $self->intraday_trend->amount);
 
     my $bounceback_base_max_trend = $self->calculate_bounceback_base($t_mins, $st_or_lt, (-$max_abs_trend));
-    my $bounceback_base_max_trend_with_slope = $self->calculate_bounceback_base($t_mins, $st_or_lt, $slope * (-$max_abs_trend));
+    my $bounceback_base_max_trend_with_slope = $self->calculate_bounceback_base($t_mins, $st_or_lt, (-1 * $slope * $max_abs_trend));
 
     my $bounceback_safety = $bounceback_base_max_trend - $bounceback_base_max_trend_with_slope;
 
