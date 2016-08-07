aed80b20e73bcd9ae172b8b33d77114c7a96df74
Chylli <chylli@binary.com>
2016-08-08 07:42:47 +0800

    set settings_name

--- lib/BOM/Platform/Runtime.pm
+++ lib/BOM/Platform/Runtime.pm
@@ -30,6 +30,7 @@ sub _build_app_config {
         definition_yml   => '/home/git/regentmarkets/bom-platform/config/app_config_definitions.yml',
         chronicle_reader => BOM::System::Chronicle::get_chronicle_reader(),
         chronicle_writer => BOM::System::Chronicle::get_chronicle_writer(),
+        setting_name     => 'binary',
     );
 }
 
