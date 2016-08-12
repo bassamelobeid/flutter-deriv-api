dc167a9d4aa97fd47167f0b7c9e80435ed8534cd
Sasha Murzin <sasha@binary.com>
2016-08-12 08:20:44 +0000

    Rename fetchall_arrayref to getall_arrayref

--- lib/BOM/RPC/v3/PortfolioManagement.pm
+++ lib/BOM/RPC/v3/PortfolioManagement.pm
@@ -54,7 +54,7 @@ sub __get_open_contracts {
         operation      => 'replica',
     });
 
-    return $clientdb->fetchall_arrayref('select * from bet.get_open_bets_of_account(?,?,?)', [$client->loginid, $client->currency, 'false']);
+    return $clientdb->getall_arrayref('select * from bet.get_open_bets_of_account(?,?,?)', [$client->loginid, $client->currency, 'false']);
 
 }
 
