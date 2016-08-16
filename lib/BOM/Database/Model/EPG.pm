8f3f6eb0d636e120825275aaeb518766a4d0d558
Fayland Lam <fayland@gmail.com>
2016-08-16 21:42:20 +0800

    for complete

--- lib/BOM/Database/Model/EPG.pm
+++ lib/BOM/Database/Model/EPG.pm
@@ -14,7 +14,7 @@ sub _build_dbh {
     return (shift)->client->db->dbh;
 }
 
-sub prepare_pending {
+sub prepare {
     my $self = shift;
     my %params = @_ % 2 ? %{$_[0]} : @_;
 
@@ -37,6 +37,75 @@ sub prepare_pending {
     );
 
     return $id;
+}
+
+sub complete {
+    my ($self, @success_data) = @_;
+
+    return 0 if @success_data > 1; # require a FIX
+
+    my $data = $success_data[0];
+    my $id = $data->{id};
+    my ($epg_request) = $self->dbh->selectrow_array("
+        SELECT * FROM payment.epg_request WHERE id = ?
+    ", undef, $id);
+
+    return 0 unless $epg_request;
+
+    # compare currency and amount
+    if ($data->{amount} != $epg_request->{amount}) {
+        $self->dbh->do("
+            UPDATE payment.epg_request SET status = ?, remark = ? WHERE id = ?
+        ", undef, 'AMOUNT_NOT_MATCH', $data->{data}, $id);
+        return 0;
+    }
+    if ($data->{currency} != $epg_request->{payment_currency}) {
+        $self->dbh->do("
+            UPDATE payment.epg_request SET status = ?, remark = ? WHERE id = ?
+        ", undef, 'CURRENCY_NOT_MATCH', $data->{data}, $id);
+        return 0;
+    }
+
+    my $client  = $self->client;
+    my $account = $client->default_account || die "no account";
+    if ($account->id != $epg_request->{account_id}) {
+        $self->dbh->do("
+            UPDATE payment.epg_request SET status = ?, remark = ? WHERE id = ?
+        ", undef, 'ACCOUNT_NOT_MATCH', $account->id, $id);
+        return 0;
+    }
+
+    my %payment_args = (
+        currency          => $data->{currency},
+        amount            => $data->{amount},
+        remark            => $data->{data},
+        staff             => $client->loginid,
+        created_by        => '',
+        trace_id          => 0,
+        payment_processor => $payment_processor,
+        # transaction_id    => $transaction_id,
+        # ip_address        => $ip_address, # FIXME
+    );
+
+    my $fee = 0; # FIXME
+
+    # Write the payment transaction
+    my $trx;
+
+    if ($data->{type} eq 'deposit') {
+        $trx = $client->payment_epg(%payment_args);
+    } elsif ($data->{type} eq 'withdrawal') {
+        # Don't allow balances to ever go negative! Include any fee in this test.
+        my $balance = $client->default_account->load->balance;
+        if ($amount + $fee > $balance) {
+            my $plusfee = $fee ? " plus fee $fee" : '';
+            return 0;
+            # "Requested withdrawal amount $currency_code $amount$plusfee exceeds client balance $currency_code $balance"
+        }
+        $trx = $client->payment_epg(%payment_args, amount => sprintf("%0.2f", -$data->{amount}));
+    }
+
+    return $trx->id;
 }
 
 no Moose;
