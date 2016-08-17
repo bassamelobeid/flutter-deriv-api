4888ae987c472250f1642fb6742f1e17d1a0e8b6
Torsten Förtsch <torsten@binary.com>
2016-08-17 11:35:03 +0200

    Revert "Removing warn about InvalidContractProposal error reason"

--- lib/BOM/WebSocketAPI/v3/Wrapper/Transaction.pm
+++ lib/BOM/WebSocketAPI/v3/Wrapper/Transaction.pm
@@ -31,6 +31,14 @@ sub buy_get_contract_params {
             BOM::WebSocketAPI::v3::Wrapper::System::_forget_pricing_subscription($c, $proposal_id);
             return;
         }
+        my $app_id           = $c->stash('source')       // 'unknown';
+        my $validated_app_id = $c->stash('valid_source') // 'unknown';
+        warn "WSAPI 'buy' buy_get_contract_params error (App Id: $app_id / $validated_app_id ) : "
+            . (
+              !$ch         ? "stash does not contains 'pricing_channel'"
+            : !$ch->{uuid} ? "'pricing_channel' does not contains {uuid} branch"
+            :                "'pricing_channel' does not contains provided proposal uuid (double click?)"
+            );
     }
     return $c->new_error(($args->{buy_contract_for_multiple_accounts} ? 'buy_contract_for_multiple_accounts' : 'buy'),
         'InvalidContractProposal', $c->l("Unknown contract proposal"));
