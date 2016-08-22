64de055fd0beb4ae7d2c2bdbabaaf0e2f78b0f51
Raunak Kathuria <raunakkathuria@gmail.com>
2016-08-22 02:23:09 +0000

    fix perl critic complain about precedence

--- lib/BOM/RPC/v3/Accounts.pm
+++ lib/BOM/RPC/v3/Accounts.pm
@@ -462,7 +462,7 @@ sub reset_password {
     # clients are ordered by reals-first, then by loginid.  So the first is the 'default'
     my $client = $clients[0];
 
-    if ($client && not $client->is_virtual) {
+    if ($client and not $client->is_virtual) {
         unless ($args->{date_of_birth}) {
             return BOM::RPC::v3::Utility::create_error({
                     code              => "DateOfBirthMissing",
