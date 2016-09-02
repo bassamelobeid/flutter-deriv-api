2ae28d7c3e9509741080ec206e06015757ac4556
Yng Shan <yngshan@binary.com>
2016-09-02 07:33:09 +0000

    add comment for is_expired

--- lib/BOM/Product/Role/ExpireAtEnd.pm
+++ lib/BOM/Product/Role/ExpireAtEnd.pm
@@ -8,7 +8,9 @@ use BOM::Platform::Context qw(localize);
 sub _build_is_expired {
     my $self = shift;
 
-    return 0 unless ($self->is_after_settlement and $self->exit_tick);
+    # As long as it already pass the expiry time and have exit tick, it is consider expired.
+    # is_valid_to_sell will stop it from sell as it has not pass the settlement time
+    return 0 unless ($self->is_after_expiry and $self->exit_tick);
 
     $self->check_expiry_conditions;
 
