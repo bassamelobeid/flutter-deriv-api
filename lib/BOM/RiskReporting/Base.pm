4c2855e7c3092aa684699743ea7c7daea58060fe
Chylli <chylli@binary.com>
2016-09-13 09:03:53 +0800

    no print result

--- lib/BOM/RiskReporting/Base.pm
+++ lib/BOM/RiskReporting/Base.pm
@@ -83,10 +83,7 @@ has live_open_bets => (
 
 sub _build_live_open_bets {
     my $self = shift;
-    my $result = $self->_db->dbh->selectall_hashref(qq{ SELECT * FROM accounting.get_live_open_bets() }, 'id');
-    use Data::Dumper;
-    print "live open bets: " . Dumper($result);
-    return $result;
+    return $self->_db->dbh->selectall_hashref(qq{ SELECT * FROM accounting.get_live_open_bets() }, 'id');
 }
 
 before generate => sub {
