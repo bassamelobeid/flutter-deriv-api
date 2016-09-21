3593566c04e5b3435d37420aa678cd8c29f6d588
Hui Wang <hui@binary.com>
2016-09-21 22:30:10 +0800

    fix perlcritic blame

--- lib/BOM/RPC/v3/PortfolioManagement.pm
+++ lib/BOM/RPC/v3/PortfolioManagement.pm
@@ -96,7 +96,7 @@ sub proposal_open_contract {
     if (my $contract_id = $params->{args}->{contract_id}) {
         @fmbs = @{__get_contract_details_by_id($client, $contract_id)};
         if (!$client->default_account
-               || scalar @fmbs and $fmbs[0]->{account_id} ne $client->default_account->id) {
+                or scalar @fmbs and $fmbs[0]->{account_id} ne $client->default_account->id) {
             @fmbs = ();
         }
     } else {
