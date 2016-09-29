e509dfb5ab51428f22c6a6dfd6be48c3c4b07633
Tom Molesworth <tom@binary.com>
2016-09-29 04:38:49 +0100

    Apply category-based API changes from BOM::Database

--- lib/BOM/System/Chronicle.pm
+++ lib/BOM/System/Chronicle.pm
@@ -274,7 +274,7 @@ sub _dbh {
     # Note that switching DBIx::Connector's ->ping behaviour would not be much help for Data::Chronicle::*
     # handles, we'd need to update Data::Chronicle itself to use DBIx::Connector to get that benefit.
     return $dbh if $dbh and $$ == $pid;
-    BOM::Database::release_dbh($dbh) if $dbh;
+    BOM::Database::release_dbh(chronicle => $dbh) if $dbh;
     $pid = $$;
     $dbh = DBI->connect(
         '' . _dbh_dsn(),
@@ -284,7 +284,7 @@ sub _dbh {
             RaiseError        => 1,
             pg_server_prepare => 0,
         });
-    BOM::Database::register_dbh($dbh);
+    BOM::Database::register_dbh(chronicle => $dbh);
     _dbh_changed();
     return $dbh;
 }
