fa6afd303e58c2d67c3bb5f1b8665cdcd18f3163
Mahdi Mohammadi <mm@binary.com>
2016-09-30 08:11:13 +0000

    cleanup types and remove surface_type

--- lib/BOM/Market/Types.pm
+++ lib/BOM/Market/Types.pm
@@ -5,41 +5,6 @@ use warnings;
 
 use Moose;
 
-=head1 NAME
-
-BOM::Market::Types - validated Moose types with BetOnMarkets-specific semantics
-
-=head1 VERSION
-
-Version 0.01
-
-=head1 SYNOPSIS
-
-This module provides validated definition of various datatypes that are prevalent through the BetOnMarkets system. By convention, these types are all prefixed with 'bom_' in order to avoid namespace collisions.
-
-    package MyClass;
-
-    use Moose;
-
-    use BOM::Market::Types qw( bom_financial_market );
-
-    has 'financial_market' => (
-        is  => 'rw',
-        isa => 'bom_financial_market',
-    );
-
-    package main;
-
-    my $good = new MyClass( financial_market => 'CR1234' ); # works
-    my $bad = new MyClass( financial_market => 'fribitz' ); # dies with an explanation
-
-
-=cut
-
-use POSIX qw( );
-use DateTime;
-use Data::Validate::IP qw( );
-use Math::BigInt;
 use Time::Duration::Concise;
 
 use MooseX::Types::Moose qw(Int Num Str);
@@ -51,27 +16,17 @@ use MooseX::Types -declare => [
         contract_type
         market_markups
         market_feed
-        surface_type
         date_object
         time_interval
         )];
 
 use Moose::Util::TypeConstraints;
-use Try::Tiny;
 
 subtype 'bom_time_interval', as 'Time::Duration::Concise';
 coerce 'bom_time_interval', from 'Str', via { Time::Duration::Concise->new(interval => $_) };
 
 subtype 'bom_date_object', as 'Date::Utility';
 coerce 'bom_date_object', from 'Str', via { Date::Utility->new($_) };
-
-my @surface_types = qw(delta flat moneyness);
-subtype 'bom_surface_type', as Str, where {
-    my $regex = '(' . join('|', @surface_types) . ')';
-    /^$regex$/;
-}, message {
-    "Invalid surface type $_. Must be one of: " . join(', ', @surface_types);
-};
 
 subtype 'bom_financial_market', as 'BOM::Market';
 coerce 'bom_financial_market', from 'Str', via { return BOM::Market::Registry->instance->get($_); };
@@ -84,16 +39,6 @@ coerce 'bom_market_markups', from 'HashRef' => via { BOM::Market::Markups->new($
 
 subtype 'bom_underlying_object', as 'BOM::Market::Underlying';
 coerce 'bom_underlying_object', from 'Str', via { BOM::Market::Underlying->new($_) };
-
-=head1 AUTHOR
-
-Nick Marden, C<< <nick at regentmarkets dot com> >>
-
-=head1 COPYRIGHT
-
-(c) 2010 RMG Technology (Malaysia) Sdn Bhd
-
-=cut
 
 no Moose;
 no Moose::Util::TypeConstraints;
