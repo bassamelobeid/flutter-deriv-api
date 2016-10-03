95ab16ec4679994cb99d56abac044a094d4b5fa3
Sasha Murzin <sasha@binary.com>
2016-10-03 03:35:48 +0000

    More updaters from bom-market

---
+++ bin/bom_update_economic_events.pl
@@ -0,0 +1,76 @@
+#!/etc/rmg/bin/perl
+
+package BOM::System::Script::UpdateEconomicEvents;
+
+use Moose;
+with 'App::Base::Script';
+
+use ForexFactory;
+use Quant::Framework::EconomicEventCalendar;
+use BOM::Platform::Runtime;
+use Date::Utility;
+use DataDog::DogStatsd::Helper qw(stats_gauge);
+use JSON;
+use Path::Tiny;
+use BOM::System::RedisReplicated;
+use Try::Tiny;
+use List::Util qw(first);
+
+sub documentation { return 'This script runs economic events update from forex factory at 00:00 GMT'; }
+
+sub script_run {
+    my $self = shift;
+
+    my @messages;
+    my $parser = ForexFactory->new();
+
+    #read economic events for one week (7-days) starting from 4 days back, so in case of a Monday which
+    #has its last Friday as a holiday, we will still have some events in the cache.
+    my $events_received = $parser->extract_economic_events(0, Date::Utility->new()->minus_time_interval('4d'));
+
+    stats_gauge('economic_events_updates', scalar(@$events_received));
+
+    my $file_timestamp = Date::Utility->new->date_yyyymmdd;
+
+    #this will be an array of all extracted economic events. Later we will store
+
+    foreach my $event_param (@$events_received) {
+        $event_param->{recorded_date} = Date::Utility->new->epoch;
+        Path::Tiny::path("/feed/economic_events/$file_timestamp")->append(time . ' ' . JSON::to_json($event_param) . "\n");
+    }
+
+    try {
+
+        my $tentative_count = grep { $_->{is_tentative} } @$events_received;
+
+        Quant::Framework::EconomicEventCalendar->new({
+                events           => $events_received,
+                recorded_date    => Date::Utility->new(),
+                chronicle_reader => BOM::System::Chronicle::get_chronicle_reader(),
+                chronicle_writer => BOM::System::Chronicle::get_chronicle_writer(),
+            })->save;
+
+        print "stored " . (scalar @$events_received) . " events ($tentative_count are tentative events) in chronicle...\n";
+    }
+    catch {
+        print 'Error occured while saving events: ' . $_;
+    };
+
+    my $num_events_saved = scalar(@$events_received);
+
+    stats_gauge('economic_events_saved', $num_events_saved);
+
+    if (not $num_events_saved > 0) {
+        print 'No economic event is saved on chronicle today. Please check';
+    }
+
+    return 0;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+
+package main;
+
+exit BOM::System::Script::UpdateEconomicEvents->new()->run();
---
+++ bin/update_smartfx_rate.pl
@@ -0,0 +1,85 @@
+#!/etc/rmg/bin/perl
+package BOM::System::Script::UpdateSmartFxRates;
+
+=head1 NAME
+
+BOM::System::Script::UpdateVol;
+
+=head1 DESCRIPTION
+
+Updates our vols with the latest quotes we have received from Bloomberg.
+
+=cut
+
+use Moose;
+with 'App::Base::Script';
+
+use List::Util qw(first);
+use BOM::Market::Underlying;
+use Quant::Framework::InterestRate;
+use Date::Utility;
+
+use JSON qw(from_json);
+
+has world_symbols => (
+    is => 'ro',
+    default => sub {{
+        WLDUSD => {
+            source => [qw(frxEURUSD frxUSDJPY frxGBPUSD frxUSDCAD frxAUDUSD)],
+            negative => [qw(frxGBPUSD frxEURUSD)]},
+        WLDAUD => {
+            source => [qw(frxAUDCAD frxAUDJPY frxAUDUSD frxEURAUD frxGBPAUD)],
+            negative => [qw(frxEURAUD frxGBPAUD)],},
+        WLDEUR => {
+            source => [qw(frxEURAUD frxEURCAD frxEURGBP frxEURJPY frxEURUSD)],
+            negative => [qw()],
+            },
+        WLDGBP => {
+            source => [qw(frxEURGBP frxGBPAUD frxGBPCAD frxGBPJPY frxGBPUSD)],
+            negative => [qw(EURGBP)],
+        },
+    }},
+);
+
+sub documentation {
+    return 'updates smart fx interest rates.';
+}
+
+sub script_run {
+    my $self = shift;
+
+    my $world = $self->world_symbols;
+    my @smart_symbols = keys %$world;
+    foreach my $symbol (@smart_symbols) {
+        my %neg = map {$_ => 1} @{$world->{$symbol}->{negative}};
+        my $date = Date::Utility->new();
+        next if $date->is_a_weekend;
+        my @source = map {BOM::Market::Underlying->new($_)} @{$self->world_symbols->{$symbol}->{source}};
+        my %world_rate;
+        for my $term (sort {$a <=> $b} keys %{Quant::Framework::InterestRate->new({
+            symbol => 'USD',
+            chronicle_reader => BOM::System::Chronicle::get_chronicle_reader(),
+            chronicle_writer => BOM::System::Chronicle::get_chronicle_writer(),
+            })->rates}) {
+	    my %rates = map {$_->symbol => $_->interest_rate_for($term/365) - $_->dividend_rate_for($term/365)} @source;
+	    my @rates_array = map {$neg{$_->symbol} ? -$rates{$_->symbol} : $rates{$_->symbol}} @source;
+	    $world_rate{$term} = ($rates_array[0] + $rates_array[1] + $rates_array[2] + $rates_array[3] + $rates_array[4])/5;
+        }
+        my $ir      = Quant::Framework::InterestRate->new({
+          symbol          => $symbol,
+          rates             => \%world_rate,
+          recorded_date       => $date,
+          chronicle_reader => BOM::System::Chronicle::get_chronicle_reader(),
+          chronicle_writer => BOM::System::Chronicle::get_chronicle_writer(),
+        });
+        $ir->save;
+    }
+
+    return $self->return_value();
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+package main;
+exit BOM::System::Script::UpdateSmartFxRates->new->run;
+1;
