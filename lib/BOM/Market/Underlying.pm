575a7b4dbd798e76dbb8391f53845fc9535852d4
Mahdi Mohammadi <mm@binary.com>
2016-10-03 07:18:00 +0000

    also add default dividend for random numbers

--- lib/BOM/Market/Underlying.pm
+++ lib/BOM/Market/Underlying.pm
@@ -303,6 +303,9 @@ sub _build_config {
     $default_dividend_rate = 0 if $self->symbol eq 'RDMARS';
     $default_dividend_rate = 0 if $self->symbol eq 'RDVENUS';
 
+    $default_dividend_rate = 0 if $self->symbol eq 'R_100' or $self->symbol eq 'R_75';
+    $default_dividend_rate = 0 if $self->symbol eq 'R_50'  or $self->symbol eq 'R_25';
+
     my $build_args = {underlying => $self->system_symbol};
 
     $build_args->{use_official_ohlc} = 1 if ($self->use_official_ohlc);
