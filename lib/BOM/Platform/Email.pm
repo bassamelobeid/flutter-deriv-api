e16995251eb1a84810a8fd2f020164bda1d75db3
Arun Venkataraman <arun@binary.com>
2016-10-05 02:15:24 +0000

    tidy

--- lib/BOM/Platform/Email.pm
+++ lib/BOM/Platform/Email.pm
@@ -59,8 +59,8 @@ sub send_email {
 
     # DON'T send email on devbox except to RMG emails
     return 1
-        if ((!BOM::System::Config::on_production()
-        and $email !~ /(?:binary|regentmarkets|betonmarkets)\.com$/) || $ENV{SKIP_EMAIL});
+        if ((!BOM::System::Config::on_production() and $email !~ /(?:binary|regentmarkets|betonmarkets)\.com$/)
+        || $ENV{SKIP_EMAIL});
 
     my @toemails = split(/\s*\,\s*/, $email);
     foreach my $toemail (@toemails) {
