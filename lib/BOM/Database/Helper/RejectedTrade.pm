06e2ba65ac35f5693fc4fd0e802351bc56ced842
Shahrizal Shaari <shahrizal@binary.com>
2016-10-06 06:37:48 +0000

    debug

--- lib/BOM/Database/Helper/RejectedTrade.pm
+++ lib/BOM/Database/Helper/RejectedTrade.pm
@@ -2,12 +2,7 @@ package BOM::Database::Helper::RejectedTrade;
 
 use Moose;
 use Rose::DB;
-use JSON::XS ();
 use Carp;
-
-has 'id' => (
-    is => 'rw',
-);
 
 has 'login_id' => (
     is => 'rw',
@@ -42,8 +37,9 @@ sub record_fail_txn {
     my $self = shift;
 
     $self->db->dbh->do(
-        'INSERT INTO data_collection.rejected_trades (id,login_id,financial_market_bet_id,shortcode,action_type,reason,details) values(?,?,?,?,?,?,?)',
-        undef, $self->id, $self->login_id, $self->financial_market_bet_id, $self->short_code, $self->action_type, $self->reason, $self->details
+        'INSERT INTO data_collection.rejected_trades (login_id,financial_market_bet_id,shortcode,action_type,reason,details) values(?,?,?,?,?,?)',
+        undef, $self->login_id, $self->financial_market_bet_id,
+        $self->shortcode, $self->action_type, $self->reason, $self->details
     );
 
     return 1;
