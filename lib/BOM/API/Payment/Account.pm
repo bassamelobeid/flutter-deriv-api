c71e69067ea5d3dda343e62d08d9935fa3bb14f6
Ivan Baidakou <ivan@binary.com>
2016-10-08 13:00:16 +0000

    Tidy

--- lib/BOM/API/Payment/Account.pm
+++ lib/BOM/API/Payment/Account.pm
@@ -10,7 +10,6 @@ use BOM::Database::DataMapper::Account;
 use Postgres::FeedDB::CurrencyConverter qw(amount_from_to_currency);
 use Try::Tiny;
 
-
 sub account_GET {
     my $c      = shift;
     my $env    = $c->env;
