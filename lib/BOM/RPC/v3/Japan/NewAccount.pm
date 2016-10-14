efcfdd8eb4a2006b6f1574cb132c0e948806488d
Shahrizal Shaari <shahrizal@binary.com>
2016-10-14 03:46:30 +0000

    Change unless to several lines

--- lib/BOM/RPC/v3/Japan/NewAccount.pm
+++ lib/BOM/RPC/v3/Japan/NewAccount.pm
@@ -264,7 +264,8 @@ sub set_jp_settings {
     my ($client, $website_name, $client_ip, $user_agent, $language, $args) =
         @{$params}{qw/client website_name client_ip user_agent language args/};
 
-    return BOM::RPC::v3::Utility::permission_error() unless ($client->residence eq 'jp' and ($args->{jp_settings} or $args->{email_consent}));
+    return BOM::RPC::v3::Utility::permission_error() unless ($client->residence eq 'jp'
+        and ($args->{jp_settings} or $args->{email_consent}));
     $args = $args->{jp_settings};
 
     my $text = {
