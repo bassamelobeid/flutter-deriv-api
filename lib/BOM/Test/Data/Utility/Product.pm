0d73f39e55c6e154c2acb3655d8e666362b6ee35
Mahdi Mohammadi <mm@binary.com>
2016-10-17 06:30:26 +0000

    remove client-buy-bet

--- lib/BOM/Test/Data/Utility/Product.pm
+++ lib/BOM/Test/Data/Utility/Product.pm
@@ -11,34 +11,6 @@ use Date::Utility;
 use Postgres::FeedDB;
 use BOM::Test::Data::Utility::UnitTestRedis qw(initialize_realtime_ticks_db);
 
-sub client_buy_bet {
-    my ($client, $currency, $amount) = @_;
-
-    my $now        = Date::Utility->new;
-    my $underlying = create_underlying('R_50');
-
-    my $account     = $client->default_account;
-    my $pre_balance = $account->load->balance;
-
-    my $contract = produce_contract({
-        underlying  => $underlying,
-        bet_type    => 'FLASHU',
-        currency    => $currency,
-        payout      => 2 * $amount,
-        date_start  => $now,
-        date_expiry => $now->epoch + 300,
-    });
-
-    local $ENV{REQUEST_STARTTIME} = $now->epoch;
-    my $txn = BOM::Product::Transaction->new({
-        client   => $client,
-        contract => $contract,
-        price    => $amount,
-        staff    => 'system'
-    });
-    return $txn->buy(skip_validation => 1);
-}
-
 sub buy_bet {
     my ($sc, $curr, $client, $price, $start) = @_;
 
