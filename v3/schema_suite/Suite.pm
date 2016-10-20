457636ab0d6cbcbf1d500db716fbaee5f3d92784
Ivan Baidakou <ivan@binary.com>
2016-10-20 08:57:59 +0000

    Do not make it local

--- v3/schema_suite/Suite.pm
+++ v3/schema_suite/Suite.pm
@@ -32,7 +32,8 @@ my $response;
 my $global_test_iteration = 0;
 
 # We don't want to fail due to hiting limits
-local $ENV{BOM_TEST_RATE_LIMITATIONS} = '/home/git/regentmarkets/bom-websocket-tests/v3/schema_suite/rate_limitations.yml';
+## no critic (Variables::RequireLocalizedPunctuationVars)
+$ENV{BOM_TEST_RATE_LIMITATIONS} = '/home/git/regentmarkets/bom-websocket-tests/v3/schema_suite/rate_limitations.yml';
 
 # Return entire contents of file as string
 sub read_file {
