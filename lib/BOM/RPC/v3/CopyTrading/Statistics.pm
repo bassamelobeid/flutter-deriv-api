1de5ea9e6c64232729853b6ed9d9ad81695127be
Evgeniy Vostrov <evgeniy@binary.com>
2016-11-10 09:43:11 +0000

    Added last 12 months profitable trades

--- lib/BOM/RPC/v3/CopyTrading/Statistics.pm
+++ lib/BOM/RPC/v3/CopyTrading/Statistics.pm
@@ -13,82 +13,91 @@ use Data::Dumper;
 sub trader_statistics {
     my $params = shift->{args};
 
-    my $trader             = BOM::Platform::Client->new({loginid => $params->{trader_id}});
+    my $trader = BOM::Platform::Client->new({loginid => $params->{trader_id}});
     my $trader_date_joined = Date::Utility->new($trader->date_joined);
-    my $trader_acc         = $trader->default_account;
 
     my $db = BOM::Database::ClientDB->new({
             client_loginid => $trader->loginid,
         })->db;
-    my $txn_dm = BOM::Database::DataMapper::Transaction->new({
-        client_loginid => $trader->loginid,
-        currency_code  => 'USD',              # TODO is it neccessary?
-        db             => $db,
-    });
 
-    # profitable
-    my $now                = Date::Utility->new();
-    my $monthly_profitable = {};
-    my $yearly_profitable  = {};
-    for (my $year = $trader_date_joined->year; $year <= $now->year; $year++) {
-        for (my $month = 1; $month <= 12; $month++) {
-            $month = sprintf("%.2u", $month);
-            my $date = Date::Utility->new("$year-$month-01");
-            next if $date->month < $trader_date_joined->month and $date->year == $trader_date_joined->year;
-            next if $date->month > $now->month and $date->year == $now->year;
+    my $now                       = Date::Utility->new();
+    my $monthly_profitable_trades = {};
+    my $yearly_profitable_trades  = {};
+    my $trades_breakdown          = {};
+    my $trades_statistic          = {};
+    my ($total_trades, $avg_duration, $trades_profitable, $last_12months_profitable_trades);
 
-            my $first_day_in_current_month = Date::Utility->new($date->year . $date->month . '01000000')->datetime_yyyymmdd_hhmmss;
-            my $last_day_in_current_month =
-                Date::Utility->new($date->year . $date->month . $date->days_in_month . '235959')->datetime_yyyymmdd_hhmmss;
+    for my $account ($trader->account) {
+        my $txn_dm = BOM::Database::DataMapper::Transaction->new({
+            client_loginid => $trader->loginid,
+            currency_code  => $account->currency_code,
+            db             => $db,
+        });
 
-            my $W = $txn_dm->get_monthly_payments_sum($date, $trader_acc, 'withdrawal');
-            my $D = $txn_dm->get_monthly_payments_sum($date, $trader_acc, 'deposit');
-            my $E1 = $txn_dm->get_balance_before_date($last_day_in_current_month,  $trader_acc);    # is the equity at the end of the month
-            my $E0 = $txn_dm->get_balance_before_date($first_day_in_current_month, $trader_acc);    # is the equity at the beginning of the month
+        # trader performance
+        my @sorted_monthly_profits;
+        for (my $year = $trader_date_joined->year; $year <= $now->year; $year++) {
+            for (my $month = 1; $month <= 12; $month++) {
+                $month = sprintf("%.2u", $month);
+                my $date = Date::Utility->new("$year-$month-01");
+                next if $date->month < $trader_date_joined->month and $date->year == $trader_date_joined->year;
+                next if $date->month > $now->month and $date->year == $now->year;
 
-            $monthly_profitable->{$year . '-' . $month} = sprintf("%.4f", ((($E1 + $W) - ($E0 + $D)) / ($E0 + $D)));
+                my $first_day_in_current_month = Date::Utility->new($date->year . $date->month . '01000000')->datetime_yyyymmdd_hhmmss;
+                my $last_day_in_current_month =
+                    Date::Utility->new($date->year . $date->month . $date->days_in_month . '235959')->datetime_yyyymmdd_hhmmss;
+
+                my $W = $txn_dm->get_monthly_payments_sum($date, 'withdrawal');
+                my $D = $txn_dm->get_monthly_payments_sum($date, 'deposit');
+                my $E1 = $txn_dm->get_balance_before_date($last_day_in_current_month);     # is the equity at the end of the month
+                my $E0 = $txn_dm->get_balance_before_date($first_day_in_current_month);    # is the equity at the beginning of the month
+
+                my $current_month_profit = sprintf("%.4f", ((($E1 + $W) - ($E0 + $D)) / ($E0 + $D)));
+                $monthly_profitable_trades->{$year . '-' . $month} = $current_month_profit;
+                push @sorted_monthly_profits, $current_month_profit;
+            }
+
+            my @monthly_profits =
+                map  { $monthly_profitable_trades->{$_} }
+                grep { /^$year/ }
+                keys %$monthly_profitable_trades;
+            $yearly_profitable_trades->{$year} = _year_performance(@monthly_profits);
         }
 
-        my @profitables =
-            map  { $monthly_profitable->{$_} }
-            grep { /^$year/ }
-            keys %$monthly_profitable;
-        my $mult = 1;
-        $mult *= 1 + $_ for @profitables;
-        $yearly_profitable->{$year} = sprintf("%.4f", $mult - 1);
-    }
+        # last 12 months profitable
+        my $last_month_idx = scalar(@sorted_monthly_profits) < 12 ? scalar(@sorted_monthly_profits) : 12;
+        $last_12months_profitable_trades = _year_performance(@sorted_monthly_profits[-$last_month_idx .. -1]);
 
-    # trades_cnt
-    my $total_trades = $txn_dm->get_transactions_cnt({
+        # trades_cnt
+        $total_trades = $txn_dm->get_transactions_cnt({
             action_type => 'buy',
-        },
-        $trader_acc,
-    );
+        });
 
-    # trades average duration
-    my $avg_duration = $txn_dm->get_trades_avg_duration($trader_acc);
+        # trades average duration
+        $avg_duration = $txn_dm->get_trades_avg_duration();
 
-    # trades profitable
-    my $trades_statistic = $txn_dm->get_trades_profitable($trader_acc);
-    my $trades_profitable = $trades_statistic->{'win'}->{count} / ($trades_statistic->{'win'}->{count} + $trades_statistic->{'loss'}->{count});
+        # trades profitable
+        $trades_statistic = $txn_dm->get_trades_profitable();
+        $trades_profitable = $trades_statistic->{'win'}->{count} / ($trades_statistic->{'win'}->{count} + $trades_statistic->{'loss'}->{count});
 
-    # trades_breakdown
-    my $trades_breakdown  = {};
-    my $symbols_breakdown = $txn_dm->get_symbols_breakdown($trader_acc);
-    for my $symbol_data (@$symbols_breakdown) {
-        my $symbol = $symbol_data->[0];
-        my $trades = $symbol_data->[1];
-        $trades_breakdown->{create_underlying($symbol)->market->name} += $trades;
-    }
-    for my $market (keys %$trades_breakdown) {
-        $trades_breakdown->{$market} = sprintf("%.4f", $trades_breakdown->{$market} / $total_trades);
+        # trades_breakdown
+        my $symbols_breakdown = $txn_dm->get_symbols_breakdown();
+        for my $symbol_data (@$symbols_breakdown) {
+            my $symbol = $symbol_data->[0];
+            my $trades = $symbol_data->[1];
+            $trades_breakdown->{create_underlying($symbol)->market->name} += $trades;
+        }
+        for my $market (keys %$trades_breakdown) {
+            $trades_breakdown->{$market} = sprintf("%.4f", $trades_breakdown->{$market} / $total_trades);
+        }
     }
 
     return {
-        active_since => $trader->date_joined,
+        active_since => $trader_date_joined->date,
         # performance
-        monthly_profitable => $monthly_profitable,
-        yearly_profitable  => $yearly_profitable,
+        monthly_profitable_trades       => $monthly_profitable_trades,
+        yearly_profitable_trades        => $yearly_profitable_trades,
+        last_12months_profitable_trades => $last_12months_profitable_trades,
         # trading
         total_trades      => $total_trades,
         trades_profitable => sprintf("%.4f", $trades_profitable),
@@ -99,6 +108,13 @@ sub trader_statistics {
         # copiers
         copiers => 0,    # TODO
     };
+}
+
+sub _year_performance {
+    my (@months) = @_;
+    my $profits_mult = 1;
+    $profits_mult *= 1 + $_ for @months;
+    return sprintf("%.4f", $profits_mult - 1);
 }
 
 1;
