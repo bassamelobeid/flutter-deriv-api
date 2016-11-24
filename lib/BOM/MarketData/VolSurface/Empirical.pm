a854e54dbb8a6dec22da64e62eba8e605fe63851
Shahrizal Shaari <shahrizal@binary.com>
2016-11-24 02:03:13 +0000

    Pls ignore. checking some issue

--- lib/BOM/MarketData/VolSurface/Empirical.pm
+++ lib/BOM/MarketData/VolSurface/Empirical.pm
@@ -17,6 +17,7 @@ use BOM::MarketData::Fetcher::VolSurface;
 use BOM::MarketData::Types;
 
 use Data::Resample::ResampleCache;
+use Data::Resample::TicksCache;
 
 my $news_categories = LoadFile('/home/git/regentmarkets/bom-market/config/files/economic_events_categories.yml');
 my $coefficients    = LoadFile('/home/git/regentmarkets/bom-market/config/files/volatility_calibration_coefficients.yml');
@@ -43,14 +44,22 @@ sub get_volatility {
     my $interval = Time::Duration::Concise->new(interval => max(900, $args->{seconds_to_expiration}) . 's');
     my $fill_cache = $args->{fill_cache} // 1;
 
-    my $resample_cache = Data::Resample::ResampleCache->new({
-        redis => Cache::RedisDB->redis,
+    my $at    = BOM::Market::AggTicks->new;
+    my $ticks = $at->retrieve({
+        underlying   => $underlying,
+        interval     => $interval,
+        ending_epoch => $args->{current_epoch},
+        fill_cache   => $fill_cache,
     });
-    my $ticks = $resample_cache->resample_cache_get({
-        symbol      => $underlying->symbol,
-        start_epoch => $args->{current_epoch} - $interval->seconds,
-        end_epoch   => $args->{current_epoch},
-    });
+
+    #my $resample_cache = Data::Resample::ResampleCache->new({
+    #    redis => Cache::RedisDB->redis,
+    #});
+    #my $ticks = $resample_cache->resample_cache_get({
+    #    symbol      => $underlying->symbol,
+    #    start_epoch => $args->{current_epoch} - $interval->seconds,
+    #    end_epoch   => $args->{current_epoch},
+    #});
 
     # minimum of 1 second to avoid division by zero error.
     my $requested_interval = Time::Duration::Concise->new(interval => max(1, $args->{seconds_to_expiration}));
