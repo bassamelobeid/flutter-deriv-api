58178bd274216fa651c788d047bd82b5aa6c5baf
Torsten Förtsch <torsten@binary.com>
2016-11-24 14:44:46 +0000

    dev

--- bin/benchmark-access_token.pl
+++ bin/benchmark-access_token.pl
@@ -7,16 +7,39 @@ use Time::HiRes ();
 
 my ($token, $req, $fork) = @ARGV;
 
-$req //= 1_000;
-$fork //= 3;
+$req //= 50_000;
+$fork //= 10;
+
+sub hist {
+    my @times = sort {$a<=>$b} @_;
+    my $dist = ($times[-1] - $times[0]) / 10;
+
+    my @hist;
+    my $curr = 0;
+    my $lim = $dist;
+    for (@times) {
+	$lim += $dist, $curr++ while $_ > $lim;
+	$hist[$curr]++;
+    }
+    return sprintf 'min=%.3f max=%.3f hist=(%s)', $times[0], $times[-1], join(',', @hist);
+}
 
 sub doit {
     my $m=BOM::Database::Model::OAuth->new;
     my $stmp = Time::HiRes::time;
+    my $start = $stmp;
+
+    my @times;
+
     while ($req--) {
 	$m->get_loginid_by_access_token($token);
+	my $now = Time::HiRes::time;
+	push @times, ($now-$stmp)*1000;
+	$stmp = $now;
     }
-    printf "Child $$: %.3f sec\n", Time::HiRes::time - $stmp;
+    my $end = Time::HiRes::time;
+
+    printf "Child $$: %.3f sec %s\n", $end - $start, hist(@times);
 }
 
 my (@pids, $pid);
