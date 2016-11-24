ace8226b3c07718419eefd3ddecf3699f3b90307
Torsten Förtsch <torsten@binary.com>
2016-11-24 13:57:50 +0000

    add bin/benchmark-access_token.pl

---
+++ bin/benchmark-access_token.pl
@@ -0,0 +1,37 @@
+#!/etc/rmg/bin/perl
+
+use strict;
+use warnings;
+use BOM::Database::Model::OAuth;
+use Time::HiRes ();
+
+my ($token, $req, $fork) = @ARGV;
+
+$req //= 1_000;
+$fork //= 3;
+
+sub doit {
+    my $m=BOM::Database::Model::OAuth->new;
+    my $stmp = Time::HiRes::time;
+    while ($req--) {
+	$m->get_loginid_by_access_token($token);
+    }
+    printf "Child $$: %.3f sec\n", Time::HiRes::time - $stmp;
+}
+
+my @pids;
+
+for (1..$fork) {
+    select undef, undef, undef, 0.1 until (defined ($pid=fork));
+    if ($pid) {			# parent
+	push @pids, $pid;
+    } else {			# child
+	@pids=();
+	doit;
+	exit 0;
+    }
+}
+
+my $stmp = Time::HiRes::time;
+waitpid $_ for @pids;
+printf "Parent: %.3f sec\n", Time::HiRes::time - $stmp;
