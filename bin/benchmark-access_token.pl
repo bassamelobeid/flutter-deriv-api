2ca31d4c81c0f566c65aeeaf39a38f90c49279f1
Torsten Förtsch <torsten@binary.com>
2016-11-24 17:30:06 +0000

    print limits

--- bin/benchmark-access_token.pl
+++ bin/benchmark-access_token.pl
@@ -22,7 +22,7 @@ sub hist {
 	$lim += $dist, push(@lim, $lim), $dist*=2, $hist[++$curr] = 0 while $_ > $lim;
 	$hist[$curr]++;
     }
-    return sprintf 'min=%.3f max=%.3f hist=(%s)', $times[0], $times[-1], join(',', @hist);
+    return sprintf 'min=%.3f max=%.3f hist=(%s) lim=(%s)', $times[0], $times[-1], join(',', @hist), join(',', @lim);
 }
 
 sub doit {
