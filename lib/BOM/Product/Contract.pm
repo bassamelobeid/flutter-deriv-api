54c88b2f1019fe1e1d72016ab859c99b1967ee35
Mahdi Mohammadi <mm@binary.com>
2016-11-29 06:13:33 +0000

    tidy + fix tests

--- lib/BOM/Product/Contract.pm
+++ lib/BOM/Product/Contract.pm
@@ -1557,7 +1557,7 @@ sub _build_vol_at_strike {
     my $self = shift;
 
     #If surface is flat, don't bother calculating all those arguments
-    return $self->volsurface->get_volatility if ( $self->underlying->volatility_surface_type eq 'flat' );
+    return $self->volsurface->get_volatility if ($self->underlying->volatility_surface_type eq 'flat');
 
     my $pricing_spot = $self->pricing_spot;
     my $vol_args     = {
