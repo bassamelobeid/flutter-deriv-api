aa99d7aa359c106c715c65aa444450de172ee983
Evgeniy Vostrov <evgeniy@binary.com>
2016-11-29 10:15:30 +0000

    Get statistics only for default account

--- lib/BOM/RPC/v3/CopyTrading/Statistics.pm
+++ lib/BOM/RPC/v3/CopyTrading/Statistics.pm
@@ -49,10 +49,10 @@ sub copytrading_statistics {
         copiers => 0,    # TODO
     };
 
-    my $trader_accounts = [$trader->account];
+    my $account = $trader->default_account;
 
     # Check that client has accounts
-    unless (@$trader_accounts) {
+    unless ($account) {
         return $result_hash;
     }
 
@@ -61,99 +61,84 @@ sub copytrading_statistics {
         })->db;
 
     # Calculate average performance for multiple accounts
-    my $now = Date::Utility->new();
-    my (@last_12months_profitable_trades, @performance_probability);
-    for my $account (@{$trader_accounts}) {
-        my $txn_dm = BOM::Database::DataMapper::Transaction->new({
-            client_loginid => $trader->loginid,
-            currency_code  => $account->currency_code,
-            db             => $db,
-        });
+    my $now    = Date::Utility->new();
+    my $txn_dm = BOM::Database::DataMapper::Transaction->new({
+        client_loginid => $trader->loginid,
+        currency_code  => $account->currency_code,
+        db             => $db,
+    });
 
-        # trader performance
-        my @sorted_monthly_profits;
-        for (my $year = $trader_date_joined->year; $year <= $now->year; $year++) {
-            my @monthly_profits_of_current_year;
-            for (my $month = 1; $month <= 12; $month++) {
-                $month = sprintf("%.2u", $month);
-                my $date = Date::Utility->new("$year-$month-01");
-                next if $date->month < $trader_date_joined->month and $date->year == $trader_date_joined->year;
-                next if $date->month > $now->month and $date->year == $now->year;
+    # trader performance
+    my @sorted_monthly_profits;
+    for (my $year = $trader_date_joined->year; $year <= $now->year; $year++) {
+        my @monthly_profits_of_current_year;
+        for (my $month = 1; $month <= 12; $month++) {
+            $month = sprintf("%.2u", $month);
+            my $date = Date::Utility->new("$year-$month-01");
+            next if $date->month < $trader_date_joined->month and $date->year == $trader_date_joined->year;
+            next if $date->month > $now->month and $date->year == $now->year;
 
-                my $current_month = $date->datetime_yyyymmdd_hhmmss;
-                my $next_month    = $date->plus_time_interval('31d')->datetime_yyyymmdd_hhmmss;
+            my $current_month = $date->datetime_yyyymmdd_hhmmss;
+            my $next_month    = $date->plus_time_interval('31d')->datetime_yyyymmdd_hhmmss;
 
-                my ($D, $W) = @{$txn_dm->get_monthly_payments_sum($date)};
-                my $E1 = $txn_dm->get_balance_before_date($next_month);       # it's the equity at the end of the month
-                my $E0 = $txn_dm->get_balance_before_date($current_month);    # it's the equity at the beginning of the month
+            my ($D, $W) = @{$txn_dm->get_monthly_payments_sum($date)};
+            my $E1 = $txn_dm->get_balance_before_date($next_month);       # it's the equity at the end of the month
+            my $E0 = $txn_dm->get_balance_before_date($current_month);    # it's the equity at the beginning of the month
 
-                my $current_month_profit = sprintf("%.4f", ((($E1 + $W) - ($E0 + $D)) / ($E0 + $D)));
-                push @{$result_hash->{monthly_profitable_trades}->{$year . '-' . $month}}, $current_month_profit;
-                push @sorted_monthly_profits,          $current_month_profit;
-                push @monthly_profits_of_current_year, $current_month_profit;
-            }
-
-            push @{$result_hash->{yearly_profitable_trades}->{$year}}, _year_performance(@monthly_profits_of_current_year);
+            my $current_month_profit = sprintf("%.4f", ((($E1 + $W) - ($E0 + $D)) / ($E0 + $D)));
+            $result_hash->{monthly_profitable_trades}->{$year . '-' . $month} = $current_month_profit;
+            push @sorted_monthly_profits,          $current_month_profit;
+            push @monthly_profits_of_current_year, $current_month_profit;
         }
 
-        # last 12 months profitable
-        my $last_month_idx = scalar(@sorted_monthly_profits) < 12 ? scalar(@sorted_monthly_profits) : 12;
-        push @last_12months_profitable_trades, _year_performance(@sorted_monthly_profits[-$last_month_idx .. -1]);
-
-        # Performance Probability
-        my $fmb_dm = BOM::Database::DataMapper::FinancialMarketBet->new({
-            client_loginid => $trader->loginid,
-            currency_code  => $account->currency_code,
-            db             => $db,
-        });
-
-        my $sold_contracts = $fmb_dm->get_sold({
-            limit => 9e5,
-        });
-
-        my $cumulative_pnl      = 0;
-        my $contract_parameters = {};
-        foreach my $contract (@{$sold_contracts}) {
-            my $start_epoch = Date::Utility->new($contract->{start_time})->epoch;
-            my $sell_epoch  = Date::Utility->new($contract->{sell_time})->epoch;
-
-            if ($contract->{bet_type} eq 'CALL' or $contract->{bet_type} eq 'PUT') {
-                push @{$contract_parameters->{start_time}},        $start_epoch;
-                push @{$contract_parameters->{sell_time}},         $sell_epoch;
-                push @{$contract_parameters->{buy_price}},         $contract->{buy_price};
-                push @{$contract_parameters->{payout_price}},      $contract->{payout_price};
-                push @{$contract_parameters->{bet_type}},          $contract->{bet_type};
-                push @{$contract_parameters->{underlying_symbol}}, $contract->{underlying_symbol};
-
-                $cumulative_pnl = $cumulative_pnl + ($contract->{sell_price} - $contract->{buy_price});
-            }
-        }
-        if (grep { $_->{bet_type} =~ /^(call|put)$/i } @{$sold_contracts}) {
-            push @performance_probability,
-                sprintf(
-                "%.4f",
-                1 - Performance::Probability::get_performance_probability({
-                        pnl          => $cumulative_pnl,
-                        payout       => $contract_parameters->{payout_price},
-                        bought_price => $contract_parameters->{buy_price},
-                        types        => $contract_parameters->{bet_type},
-                        underlying   => $contract_parameters->{underlying_symbol},
-                        start_time   => $contract_parameters->{start_time},
-                        sell_time    => $contract_parameters->{sell_time},
-                    }));
-        }
+        $result_hash->{yearly_profitable_trades}->{$year} = _year_performance(@monthly_profits_of_current_year);
     }
 
-    # Average for multiply accounts
-    $result_hash->{monthly_profitable_trades}->{$_} = _mean(@{$result_hash->{monthly_profitable_trades}->{$_}})
-        for keys %{$result_hash->{monthly_profitable_trades}};
-    $result_hash->{yearly_profitable_trades}->{$_} = _mean(@{$result_hash->{yearly_profitable_trades}->{$_}})
-        for keys %{$result_hash->{yearly_profitable_trades}};
-    $result_hash->{last_12months_profitable_trades} = _mean(@last_12months_profitable_trades);
-    $result_hash->{performance_probability}         = _mean(@performance_probability);
+    # last 12 months profitable
+    my $last_month_idx = scalar(@sorted_monthly_profits) < 12 ? scalar(@sorted_monthly_profits) : 12;
+    $result_hash->{last_12months_profitable_trades} = _year_performance(@sorted_monthly_profits[-$last_month_idx .. -1]);
 
-    # Calculate common trading statistics for multiple accounts
-    my $txn_dm = BOM::Database::DataMapper::Transaction->new({db => $db});
+    # Performance Probability
+    my $fmb_dm = BOM::Database::DataMapper::FinancialMarketBet->new({
+        client_loginid => $trader->loginid,
+        currency_code  => $account->currency_code,
+        db             => $db,
+    });
+
+    my $sold_contracts = $fmb_dm->get_sold({
+        limit => 9e5,
+    });
+
+    my $cumulative_pnl      = 0;
+    my $contract_parameters = {};
+    foreach my $contract (@{$sold_contracts}) {
+        my $start_epoch = Date::Utility->new($contract->{start_time})->epoch;
+        my $sell_epoch  = Date::Utility->new($contract->{sell_time})->epoch;
+
+        if ($contract->{bet_type} eq 'CALL' or $contract->{bet_type} eq 'PUT') {
+            push @{$contract_parameters->{start_time}},        $start_epoch;
+            push @{$contract_parameters->{sell_time}},         $sell_epoch;
+            push @{$contract_parameters->{buy_price}},         $contract->{buy_price};
+            push @{$contract_parameters->{payout_price}},      $contract->{payout_price};
+            push @{$contract_parameters->{bet_type}},          $contract->{bet_type};
+            push @{$contract_parameters->{underlying_symbol}}, $contract->{underlying_symbol};
+
+            $cumulative_pnl = $cumulative_pnl + ($contract->{sell_price} - $contract->{buy_price});
+        }
+    }
+    if (grep { $_->{bet_type} =~ /^(call|put)$/i } @{$sold_contracts}) {
+        $result_hash->{performance_probability} = sprintf(
+            "%.4f",
+            1 - Performance::Probability::get_performance_probability({
+                    pnl          => $cumulative_pnl,
+                    payout       => $contract_parameters->{payout_price},
+                    bought_price => $contract_parameters->{buy_price},
+                    types        => $contract_parameters->{bet_type},
+                    underlying   => $contract_parameters->{underlying_symbol},
+                    start_time   => $contract_parameters->{start_time},
+                    sell_time    => $contract_parameters->{sell_time},
+                }));
+    }
 
     # trades average duration
     $result_hash->{avg_duration} = sprintf("%u", BOM::System::RedisReplicated::redis_read->get("COPY_TRADING_AVG_DURATION:$trader_id") || 0);
