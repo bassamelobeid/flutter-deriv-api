2f56f4d56d63ccceda1e825069f6c2a772fbf306
Yng Shan <yngshan@binary.com>
2016-11-30 09:13:15 +0000

    add the new column

--- lib/BOM/Database/AutoGenerated/Rose/QuantsBetVariable.pm
+++ lib/BOM/Database/AutoGenerated/Rose/QuantsBetVariable.pm
@@ -37,9 +37,10 @@ __PACKAGE__->meta->setup(
         exit_spot               => { type => 'numeric' },
         exit_spot_epoch         => { type => 'bigint' },
         price_slippage          => { type => 'numeric' },
-        requested_price         => { type => 'numeric'},
-        recomputed_price        => { type => 'numeric'},
+        requested_price         => { type => 'numeric' },
+        recomputed_price        => { type => 'numeric' },
         trading_period_start    => { type => 'timestamp' },
+        iv_2                    => { type => 'numeric' },
     ],
 
     primary_key_columns => [ 'transaction_id' ],
