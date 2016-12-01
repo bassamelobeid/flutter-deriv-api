dd95cc8131e20d4d3d59729d91093f9ca2b46c9d
Shahrizal Shaari <shahrizal@binary.com>
2016-12-01 07:41:21 +0000

    wrapper to support backtest

---
+++ lib/BOM/Market/DataResample.pm
@@ -0,0 +1,105 @@
+package BOM::Market::DataResample;
+
+=head1 NAME
+
+BOM::Market::DataResample
+
+=head1 SYNOPSYS
+
+    use BOM::Market::DataResample;
+
+=head1 DESCRIPTION
+
+A wrapper to let us use Redis SortedSets to get aggregated tick data.
+
+=cut
+
+use 5.010;
+use Moose;
+
+use Quant::Framework::Underlying;
+use BOM::System::RedisReplicated;
+use Data::Resample::ResampleCache;
+use Data::Resample::TicksCache;
+
+has ticks_cache => (
+    is      => 'ro',
+    default => sub {
+        Data::Resample::TicksCache->new({
+            redis_read  => BOM::System::RedisReplicated::redis_read(),
+            redis_write => BOM::System::RedisReplicated::redis_write(),
+        });
+    },
+);
+
+has resample_cache => (
+    is      => 'ro',
+    default => sub {
+        Data::Resample::ResampleCache->new({
+            redis_read  => BOM::System::RedisReplicated::redis_read(),
+            redis_write => BOM::System::RedisReplicated::redis_write(),
+        });
+    },
+);
+
+sub resample_cache_get {
+    my ($self, $args) = @_;
+
+    my $underlying = $args->{underlying};
+    my $start_time = $args->{start_epoch};
+    my $end_time   = $args->{end_epoch};
+    my $backtest   = $args->{backtest} // 0;
+
+    my $ticks;
+    if ($backtest) {
+        my $raw_ticks = $underlying->ticks_in_between_start_end({
+            start_time => $start_time,
+            end_time   => $end_time,
+        });
+
+        my @rev_ticks = reverse @$raw_ticks;
+        $ticks = $self->resample_cache->resample_cache_backfill({
+            symbol   => $underlying->symbol,
+            ticks    => \@rev_ticks,
+            backtest => $backtest,
+        });
+    } else {
+
+        $ticks = $self->resample_cache->resample_cache_get({
+            symbol      => $underlying->symbol,
+            start_epoch => $start_time,
+            end_epoch   => $end_time,
+        });
+    }
+    return $ticks;
+}
+
+sub tick_cache_get {
+    my ($self, $args) = @_;
+
+    my $underlying = $args->{underlying};
+    my $start_time = $args->{start_epoch};
+    my $end_time   = $args->{end_epoch};
+    my $backtest   = $args->{backtest} // 0;
+
+    my $ticks;
+    if ($backtest) {
+        my $ticks = $underlying->ticks_in_between_start_end({
+            start_time => $start_time,
+            end_time   => $end_time,
+        });
+    } else {
+        $ticks = $self->ticks_cache->tick_cache_get({
+            symbol      => $underlying->symbol,
+            start_epoch => $start_time,
+            end_epoch   => $end_time,
+        });
+    }
+
+    return $ticks;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
