a88ce47c98fa9e1db4de34735198af65ac44fe04
Jun Bon <junbon@binary.com>
2016-12-01 02:29:45 +0000

    remove App::Base::Daemon

--- bin/generate_trading_periods.pl
+++ bin/generate_trading_periods.pl
@@ -1,54 +1,28 @@
-#!/etc/rmg/bin/perl -w
-package BOM::Product::PredefinedOfferings;
+#!/etc/rmg/bin/perl
+package BOM::Product::TradingPeriod;
 
-use Moose;
-with 'App::Base::Daemon';
+use strict;
+use warnings;
 
 use BOM::Product::Contract::PredefinedParameters qw(generate_trading_periods next_generation_epoch);
 use Parallel::ForkManager;
-use Time::HiRes;
+use Time::HiRes qw(clock_nanosleep TIMER_ABSTIME);
 use Date::Utility;
 
-sub documentation {
-    return qq/This daemon generates predefined trading periods for selected underlying symbols at XX:45 and XX:00./;
-}
+#This daemon generates predefined trading periods for selected underlying symbols at XX:45 and XX:00
 
-sub daemon_run {
-    my $self = shift;
+my @selected_symbols = BOM::Product::Contract::PredefinedParameters::supported_symbols;
+my $fm               = Parallel::ForkManager->new(scalar(@selected_symbols));
 
-    my @selected_symbols = BOM::Product::Contract::PredefinedParameters::supported_symbols;
-    my $fm               = Parallel::ForkManager->new(scalar(@selected_symbols));
-
-    while (1) {
-        foreach my $symbol (@selected_symbols) {
-            $fm->start and next;
-            generate_trading_periods($symbol);
-            $fm->finish;
-        }
-        $fm->wait_all_children;
-
-        my $next = next_generation_epoch(Date::Utility->new);
-        my $sleep_interval = $next - Time::HiRes::time;
-
-        Time::HiRes::sleep($sleep_interval);
+while (1) {
+    foreach my $symbol (@selected_symbols) {
+        $fm->start and next;
+        generate_trading_periods($symbol);
+        $fm->finish;
     }
+    $fm->wait_all_children;
 
-    $self->_daemon_run;
+    my $next = next_generation_epoch(Date::Utility->new);
+
+    clock_nanosleep(CLOCK_REALTIME, $next * 1e9, TIMER_ABSTIME);
 }
-
-sub handle_shutdown {
-    my $self = shift;
-    warn('Shutting down');
-    return 0;
-}
-
-no Moose;
-__PACKAGE__->meta->make_immutable;
-
-package main;
-use strict;
-
-exit BOM::Product::PredefinedOfferings->new({
-        user  => 'nobody',
-        group => 'nogroup',
-    })->run;
