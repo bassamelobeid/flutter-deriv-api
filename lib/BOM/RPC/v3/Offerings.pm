938998012ae0f8ec4e07a221ff6fbc58e1022c2a
Jun Bon <junbon@binary.com>
2016-12-02 08:15:32 +0000

    use BOM::Product::Finder

--- lib/BOM/RPC/v3/Offerings.pm
+++ lib/BOM/RPC/v3/Offerings.pm
@@ -7,22 +7,20 @@ use BOM::RPC::v3::Utility;
 use BOM::MarketData qw(create_underlying);
 use BOM::MarketData::Types;
 use BOM::Platform::Context qw (localize request);
-use BOM::Product::Contract::Finder;
-use BOM::Product::Contract::Finder::Japan;
+use BOM::Product::Finder qw(get_contracts_for);
 
 sub contracts_for {
     my $params = shift;
 
-    my $args     = $params->{args};
-    my $symbol   = $args->{contracts_for};
-    my $currency = $args->{currency} || 'USD';
+    my $args         = $params->{args};
+    my $symbol       = $args->{contracts_for};
+    my $currency     = $args->{currency} || 'USD';
+    my $product_type = $args->{product_type} // 'common';
 
-    my $contracts_for;
-    if ($args->{region} and $args->{region} eq 'japan') {
-        $contracts_for = BOM::Product::Contract::Finder::Japan::available_contracts_for_symbol({symbol => $symbol});
-    } else {
-        $contracts_for = BOM::Product::Contract::Finder::available_contracts_for_symbol({symbol => $symbol});
-    }
+    my $contracts_for = get_contracts_for({
+        symbol       => $symbol,
+        product_type => $product_type
+    });
 
     my $i = 0;
     foreach my $contract (@{$contracts_for->{available}}) {
