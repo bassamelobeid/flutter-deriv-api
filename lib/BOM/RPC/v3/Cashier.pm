2e70da83829d57303cc4e937b172d3fd0d09b796
Jun Bon <junbon@binary.com>
2016-12-14 07:51:34 +0000

    added payout_per_symbol_and_contract_type to get_limits

--- lib/BOM/RPC/v3/Cashier.pm
+++ lib/BOM/RPC/v3/Cashier.pm
@@ -353,6 +353,7 @@ sub get_limits {
     my $limit = +{
         account_balance => $client->get_limit_for_account_balance,
         payout          => $client->get_limit_for_payout,
+        payout_per_symbol_and_contract_type => BOM::System::Config::quants->{bet_limits}->{open_positions_payout_per_symbol_and_bet_type_limit}->{$client->currency},
         open_positions  => $client->get_limit_for_open_positions,
     };
 
