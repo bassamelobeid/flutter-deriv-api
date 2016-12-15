5f2fb267e26df68fe9e6925c5b3d3cd19c8160b3
Jun Bon <junbon@binary.com>
2016-12-15 06:46:09 +0000

    fixed script to update economic event seasonality

--- bin/bom_update_economic_events.pl
+++ bin/bom_update_economic_events.pl
@@ -7,8 +7,8 @@ with 'App::Base::Script';
 
 use ForexFactory;
 use Volatility::Seasonality;
-use BOM::MarketData qw(create_underlying_db);
 use Quant::Framework::EconomicEventCalendar;
+use LandingCompany::Offerings qw(get_offerings_flyby);
 use Volatility::Seasonality;
 use BOM::Platform::Runtime;
 use Date::Utility;
@@ -17,7 +17,7 @@ use JSON;
 use Path::Tiny;
 use BOM::System::RedisReplicated;
 use Try::Tiny;
-use List::Util qw(first);
+use List::Util qw(first uniq);
 
 sub documentation { return 'This script runs economic events update from forex factory at 00:00 GMT'; }
 
@@ -55,8 +55,15 @@ sub script_run {
 
         print "stored " . (scalar @$events_received) . " events ($tentative_count are tentative events) in chronicle...\n";
 
-        my @underlying_symbols = create_underlying_db->symbols_for_intraday_fx();
-        my $qfs                = Volatility::Seasonality->new(
+        my @underlying_symbols = uniq(
+            map { $_->{underlying_symbol} } grep { $_->{max_historical_pricer_duration} } get_offerings_flyby()->query({
+                    expiry_type       => 'intraday',
+                    start_type        => 'spot',
+                    market            => ['forex', 'commodities'],
+                    contract_category => 'callput',
+                    barrier_category  => 'euro_non_atm',
+                }));
+        my $qfs = Volatility::Seasonality->new(
             chronicle_reader => BOM::System::RedisReplicated::redis_read,
             chronicle_writer => BOM::System::RedisReplicated::redis_write
         );
