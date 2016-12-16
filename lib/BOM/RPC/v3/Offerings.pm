b5967c8c47a3fa8d2a4b7616fdd26db5642fd9a3
Jun Bon <junbon@binary.com>
2016-12-16 09:20:50 +0000

    specify multi_barrier trading with 'product_type'

--- lib/BOM/RPC/v3/Offerings.pm
+++ lib/BOM/RPC/v3/Offerings.pm
@@ -7,7 +7,8 @@ use BOM::RPC::v3::Utility;
 use BOM::MarketData qw(create_underlying);
 use BOM::MarketData::Types;
 use BOM::Platform::Context qw (localize request);
-use BOM::Product::Finder qw(get_contracts_for);
+use BOM::Product::Contract::Finder;
+use BOM::Product::Contract::Finder::Japan;
 
 sub contracts_for {
     my $params = shift;
@@ -15,12 +16,14 @@ sub contracts_for {
     my $args         = $params->{args};
     my $symbol       = $args->{contracts_for};
     my $currency     = $args->{currency} || 'USD';
-    my $product_type = $args->{product_type};
+    my $product_type = $args->{product_type} // 'basic';
 
-    my $contracts_for = get_contracts_for({
-        symbol => $symbol,
-        ($product_type) ? (product_type => $product_type) : (),
-    });
+    my $contracts_for;
+    if (($args->{region} and $args->{region} eq 'japan') or $product_type eq 'multi_barrier') {
+        $contracts_for = BOM::Product::Contract::Finder::Japan::available_contracts_for_symbol({symbol => $symbol});
+    } else {
+        $contracts_for = BOM::Product::Contract::Finder::available_contracts_for_symbol({symbol => $symbol});
+    }
 
     my $i = 0;
     foreach my $contract (@{$contracts_for->{available}}) {
