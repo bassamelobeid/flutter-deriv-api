42f4494f1fdf4254849da3d64822d61a6bd81508
Evgeniy Vostrov <evgeniy@binary.com>
2016-12-23 05:04:22 +0000

    Review fixes

--- lib/BOM/Database/DataMapper/Transaction.pm
+++ lib/BOM/Database/DataMapper/Transaction.pm
@@ -367,8 +367,7 @@ sub get_monthly_payments_sum {
                @ sum(CASE WHEN amount < 0 THEN amount ELSE 0 END) withdrawal
           FROM payment.payment
          WHERE account_id = $1
-           AND extract(month from payment_time) > 5
-           AND extract(year from payment_time) >= 2016
+           AND payment_time >= '2016-06-01'::TIMESTAMP
       GROUP BY 1, 2
       ORDER BY 1, 2
     };
@@ -399,8 +398,7 @@ sub get_monthly_balance {
                        min(transaction_time) over (partition by extract(year from transaction_time), extract(month from transaction_time)) AS min_time
                 FROM   transaction.transaction
                 WHERE  account_id = $1
-                  AND  extract(month from transaction_time) > 5
-                  AND  extract(year from transaction_time) >= 2016
+                  AND  transaction_time >= '2016-06-01'::TIMESTAMP
                 ORDER BY transaction_time
             ) as t1
             WHERE t1.transaction_time = t1.max_time OR t1.transaction_time = t1.min_time
@@ -437,8 +435,7 @@ sub unprocessed_bets {
         WHERE
             account_id = ?
             AND (id > ? $where_unsold_ids)
-            AND  extract(month from purchase_time) > 5
-            AND  extract(year from purchase_time) >= 2016
+            AND  purchase_time >= '2016-06-01'::TIMESTAMP
         ORDER BY id ASC
     };
 
