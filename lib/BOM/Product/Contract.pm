eaa00503f13708ac7eb40da458052b98e540e329
Mahdi Mohammadi <mm@binary.com>
2016-12-27 02:26:21 +0000

    remove base commission_min and max

--- lib/BOM/Product/Contract.pm
+++ lib/BOM/Product/Contract.pm
@@ -1062,8 +1062,6 @@ sub _build_price_calculator {
             currency                => $self->currency,
             deep_otm_threshold      => $self->otm_threshold,
             maximum_total_markup    => BOM::System::Config::quants->{commission}->{maximum_total_markup},
-            base_commission_min     => BOM::System::Config::quants->{commission}->{adjustment}->{minimum},
-            base_commission_max     => BOM::System::Config::quants->{commission}->{adjustment}->{maximum},
             base_commission_scaling => $base_commission_scaling,
             app_markup_percentage   => $self->app_markup_percentage,
             ($self->has_base_commission)
