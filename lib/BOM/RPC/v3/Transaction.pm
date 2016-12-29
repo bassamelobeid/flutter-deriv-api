0289badca73350ae6bf4be8a7ce5dce8fd114901
Chylli <chylli.email@gmail.com>
2016-12-29 02:08:40 +0000

    perltidy

--- lib/BOM/RPC/v3/Transaction.pm
+++ lib/BOM/RPC/v3/Transaction.pm
@@ -19,61 +19,84 @@ use BOM::Database::ClientDB;
 sub buy {
     my $params = shift;
 
-    my $client              = $params->{client} // die "client should be authed when get here";
-    my $source              = $params->{source};
-    my $contract_parameters = $params->{contract_parameters};
-    my $args                = $params->{args};
+    my $client = $params->{client}
+      // die "client should be authed when get here";
+    my $source               = $params->{source};
+    my $contract_parameters  = $params->{contract_parameters};
+    my $args                 = $params->{args};
     my $payout               = $params->{payout};
     my $trading_period_start = $contract_parameters->{trading_period_start};
-    my $purchase_date        = time;                                           # Purchase is considered to have happened at the point of request.
-    $contract_parameters = BOM::RPC::v3::Contract::prepare_ask($contract_parameters);
+    my $purchase_date =
+      time;   # Purchase is considered to have happened at the point of request.
+    $contract_parameters =
+      BOM::RPC::v3::Contract::prepare_ask($contract_parameters);
     $contract_parameters->{landing_company} = $client->landing_company->short;
     my $amount_type = $contract_parameters->{amount_type};
 
-    my ($contract, $response);
+    my ( $contract, $response );
 
     try {
-        die unless BOM::RPC::v3::Contract::pre_validate_start_expire_dates($contract_parameters);
+        die
+          unless BOM::RPC::v3::Contract::pre_validate_start_expire_dates(
+            $contract_parameters);
     }
     catch {
-        warn __PACKAGE__ . " buy pre_validate_start_expire_dates failed, parameters: " . Dumper($contract_parameters);
-        $response = BOM::RPC::v3::Utility::create_error({
-                code              => 'ContractCreationFailure',
-                message_to_client => BOM::Platform::Context::localize('Cannot create contract')});
+        warn __PACKAGE__
+          . " buy pre_validate_start_expire_dates failed, parameters: "
+          . Dumper($contract_parameters);
+        $response = BOM::RPC::v3::Utility::create_error(
+            {
+                code => 'ContractCreationFailure',
+                message_to_client =>
+                  BOM::Platform::Context::localize('Cannot create contract')
+            }
+        );
     };
     return $response if $response;
     try {
         $contract = produce_contract($contract_parameters);
     }
     catch {
-        warn __PACKAGE__ . " buy produce_contract failed, parameters: " . Dumper($contract_parameters);
-        $response = BOM::RPC::v3::Utility::create_error({
-                code              => 'ContractCreationFailure',
-                message_to_client => BOM::Platform::Context::localize('Cannot create contract')});
+        warn __PACKAGE__
+          . " buy produce_contract failed, parameters: "
+          . Dumper($contract_parameters);
+        $response = BOM::RPC::v3::Utility::create_error(
+            {
+                code => 'ContractCreationFailure',
+                message_to_client =>
+                  BOM::Platform::Context::localize('Cannot create contract')
+            }
+        );
     };
     return $response if $response;
-    my $trx = BOM::Product::Transaction->new({
-        client   => $client,
-        contract => $contract,
-        price    => ($args->{price} || 0),
-        (defined $payout)      ? (payout      => $payout)      : (),
-        (defined $amount_type) ? (amount_type => $amount_type) : (),
-        purchase_date => $purchase_date,
-        source        => $source,
-        (defined $trading_period_start) ? (trading_period_start => $trading_period_start) : (),
-    });
+    my $trx = BOM::Product::Transaction->new(
+        {
+            client   => $client,
+            contract => $contract,
+            price    => ( $args->{price} || 0 ),
+            ( defined $payout )      ? ( payout      => $payout )      : (),
+            ( defined $amount_type ) ? ( amount_type => $amount_type ) : (),
+            purchase_date => $purchase_date,
+            source        => $source,
+            ( defined $trading_period_start )
+            ? ( trading_period_start => $trading_period_start )
+            : (),
+        }
+    );
 
-    if (my $err = $trx->buy) {
-        return BOM::RPC::v3::Utility::create_error({
-            code              => $err->get_type,
-            message_to_client => $err->{-message_to_client},
-        });
+    if ( my $err = $trx->buy ) {
+        return BOM::RPC::v3::Utility::create_error(
+            {
+                code              => $err->get_type,
+                message_to_client => $err->{-message_to_client},
+            }
+        );
     }
 
     $response = {
         transaction_id => $trx->transaction_id,
         contract_id    => $trx->contract_id,
-        balance_after  => sprintf('%.2f', $trx->balance_after),
+        balance_after  => sprintf( '%.2f', $trx->balance_after ),
         purchase_time  => $trx->purchase_date->epoch,
         buy_price      => $trx->price,
         start_time     => $contract->date_start->epoch,
@@ -82,7 +105,7 @@ sub buy {
         payout         => $contract->payout
     };
 
-    if ($contract->is_spread) {
+    if ( $contract->is_spread ) {
         $response->{stop_loss_level}   = $contract->stop_loss_level;
         $response->{stop_profit_level} = $contract->stop_profit_level;
         $response->{amount_per_point}  = $contract->amount_per_point;
@@ -94,58 +117,68 @@ sub buy {
 sub buy_contract_for_multiple_accounts {
     my $params = shift;
 
-    my $client = $params->{client} // die "client should be authed when get here";
+    my $client = $params->{client}
+      // die "client should be authed when get here";
     my @result;
     my $found_at_least_one;
 
     my $msg1 = BOM::Platform::Context::localize('Invalid token');
 
-    # $msg2 should match the message generated by BOM : : WebSocketAPI : : Websocket_v3
-    # for the same case. Hence, 'trade' is passed as parameter.
-    my $msg2 = BOM::Platform::Context::localize('Permission denied, requires [_1] scope.', 'trade');
+# $msg2 should match the message generated by BOM : : WebSocketAPI : : Websocket_v3
+# for the same case. Hence, 'trade' is passed as parameter.
+    my $msg2 = BOM::Platform::Context::localize(
+        'Permission denied, requires [_1] scope.', 'trade' );
 
     $params->{args}->{tokens} //= [];
 
-    return BOM::RPC::v3::Utility::create_error({
+    return BOM::RPC::v3::Utility::create_error(
+        {
             code              => 'TooManyTokens',
-            message_to_client => localize('Up to 100 tokens are allowed.')}) if @{$params->{args}->{tokens}} > 100;
+            message_to_client => localize('Up to 100 tokens are allowed.')
+        }
+    ) if @{ $params->{args}->{tokens} } > 100;
 
-    for my $t (@{$params->{args}->{tokens}}) {
+    for my $t ( @{ $params->{args}->{tokens} } ) {
         my $token_details = BOM::RPC::v3::Utility::get_token_details($t);
         my $loginid;
 
-        if ($token_details and $loginid = $token_details->{loginid} and grep({ /^trade$/ } @{$token_details->{scopes}})) {
+        if (    $token_details
+            and $loginid = $token_details->{loginid}
+            and grep( { /^trade$/ } @{ $token_details->{scopes} } ) )
+        {
             push @result,
-                +{
+              +{
                 token   => $t,
                 loginid => $loginid,
-                };
+              };
             $found_at_least_one = 1;
             next;
         }
 
         if ($loginid) {
+
             # here we got a valid token but with insufficient privileges
             push @result,
-                +{
+              +{
                 token => $t,
                 code  => 'PermissionDenied',
                 error => $msg2,
-                };
+              };
             next;
 
         }
 
         push @result,
-            +{
+          +{
             token => $t,
             code  => 'InvalidToken',
             error => $msg1,
-            };
+          };
     }
 
-    my ($contract, $response);
+    my ( $contract, $response );
     if ($found_at_least_one) {
+
         # NOTE: we rely here on BOM::Product::Transaction to perform all the
         #       client validations like client_status and self_exclusion.
 
@@ -154,21 +187,30 @@ sub buy_contract_for_multiple_accounts {
         my $args                = $params->{args};
         my $payout              = $params->{payout};
 
-        my $purchase_date = time;    # Purchase is considered to have happened at the point of request.
-        $contract_parameters = BOM::RPC::v3::Contract::prepare_ask($contract_parameters);
-        $contract_parameters->{landing_company} = $client->landing_company->short;
+        my $purchase_date = time
+          ;   # Purchase is considered to have happened at the point of request.
+        $contract_parameters =
+          BOM::RPC::v3::Contract::prepare_ask($contract_parameters);
+        $contract_parameters->{landing_company} =
+          $client->landing_company->short;
         my $amount_type = $contract_parameters->{amount_type};
 
         try {
-            die unless BOM::RPC::v3::Contract::pre_validate_start_expire_dates($contract_parameters);
+            die
+              unless BOM::RPC::v3::Contract::pre_validate_start_expire_dates(
+                $contract_parameters);
         }
         catch {
             warn __PACKAGE__
-                . " buy_contract_for_multiple_accounts pre_validate_start_expire_dates failed, parameters: "
-                . Dumper($contract_parameters);
-            $response = BOM::RPC::v3::Utility::create_error({
+              . " buy_contract_for_multiple_accounts pre_validate_start_expire_dates failed, parameters: "
+              . Dumper($contract_parameters);
+            $response = BOM::RPC::v3::Utility::create_error(
+                {
                     code              => 'ContractCreationFailure',
-                    message_to_client => BOM::Platform::Context::localize('Cannot create contract')});
+                    message_to_client => BOM::Platform::Context::localize(
+                        'Cannot create contract')
+                }
+            );
         };
         return $response if $response;
 
@@ -176,48 +218,62 @@ sub buy_contract_for_multiple_accounts {
             $contract = produce_contract($contract_parameters);
         }
         catch {
-            warn __PACKAGE__ . " buy_contract_for_multiple_accounts produce_contract failed, parameters: " . Dumper($contract_parameters);
-            $response = BOM::RPC::v3::Utility::create_error({
+            warn __PACKAGE__
+              . " buy_contract_for_multiple_accounts produce_contract failed, parameters: "
+              . Dumper($contract_parameters);
+            $response = BOM::RPC::v3::Utility::create_error(
+                {
                     code              => 'ContractCreationFailure',
-                    message_to_client => BOM::Platform::Context::localize('Cannot create contract')});
+                    message_to_client => BOM::Platform::Context::localize(
+                        'Cannot create contract')
+                }
+            );
         };
         return $response if $response;
 
-        my $trx = BOM::Product::Transaction->new({
-            client   => $client,
-            multiple => \@result,
-            contract => $contract,
-            price    => ($args->{price} || 0),
-            (defined $payout)      ? (payout      => $payout)      : (),
-            (defined $amount_type) ? (amount_type => $amount_type) : (),
-            purchase_date => $purchase_date,
-            source        => $source,
-        });
+        my $trx = BOM::Product::Transaction->new(
+            {
+                client   => $client,
+                multiple => \@result,
+                contract => $contract,
+                price    => ( $args->{price} || 0 ),
+                ( defined $payout )      ? ( payout      => $payout )      : (),
+                ( defined $amount_type ) ? ( amount_type => $amount_type ) : (),
+                purchase_date => $purchase_date,
+                source        => $source,
+            }
+        );
 
-        if (my $err = $trx->batch_buy) {
-            return BOM::RPC::v3::Utility::create_error({
-                code              => $err->get_type,
-                message_to_client => $err->{-message_to_client},
-            });
+        if ( my $err = $trx->batch_buy ) {
+            return BOM::RPC::v3::Utility::create_error(
+                {
+                    code              => $err->get_type,
+                    message_to_client => $err->{-message_to_client},
+                }
+            );
         }
     }
 
     for my $el (@result) {
         my $new = {};
-        if (exists $el->{code}) {
-            @{$new}{qw/token code message_to_client/} = @{$el}{qw/token code error/};
-        } else {
+        if ( exists $el->{code} ) {
+            @{$new}{qw/token code message_to_client/} =
+              @{$el}{qw/token code error/};
+        }
+        else {
             $new->{token}          = $el->{token};
             $new->{transaction_id} = $el->{txn}->{id};
             $new->{contract_id}    = $el->{fmb}->{id};
-            $new->{purchase_time}  = Date::Utility->new($el->{fmb}->{purchase_time})->epoch;
-            $new->{buy_price}      = $el->{fmb}->{buy_price};
-            $new->{start_time}     = Date::Utility->new($el->{fmb}->{start_time})->epoch;
-            $new->{longcode}       = $contract->longcode;
-            $new->{shortcode}      = $el->{fmb}->{short_code};
-            $new->{payout}         = $el->{fmb}->{payout_price};
+            $new->{purchase_time} =
+              Date::Utility->new( $el->{fmb}->{purchase_time} )->epoch;
+            $new->{buy_price} = $el->{fmb}->{buy_price};
+            $new->{start_time} =
+              Date::Utility->new( $el->{fmb}->{start_time} )->epoch;
+            $new->{longcode}  = $contract->longcode;
+            $new->{shortcode} = $el->{fmb}->{short_code};
+            $new->{payout}    = $el->{fmb}->{payout_price};
 
-            if ($contract->is_spread) {
+            if ( $contract->is_spread ) {
                 $new->{stop_loss_level}   = $contract->stop_loss_level;
                 $new->{stop_profit_level} = $contract->stop_profit_level;
                 $new->{amount_per_point}  = $contract->amount_per_point;
@@ -226,47 +282,64 @@ sub buy_contract_for_multiple_accounts {
         $el = $new;
     }
 
-    return +{result => \@result};
+    return +{ result => \@result };
 }
 
 sub sell {
     my $params = shift;
 
-    my $client = $params->{client} // die "client should be authed when get here";
+    my $client = $params->{client}
+      // die "client should be authed when get here";
 
-    my ($source, $args) = ($params->{source}, $params->{args});
+    my ( $source, $args ) = ( $params->{source}, $params->{args} );
     my $id = $args->{sell};
 
-    my $clientdb = BOM::Database::ClientDB->new({
-        client_loginid => $client->loginid,
-        operation      => 'replica',
-    });
+    my $clientdb = BOM::Database::ClientDB->new(
+        {
+            client_loginid => $client->loginid,
+            operation      => 'replica',
+        }
+    );
 
-    my @fmbs = @{$clientdb->getall_arrayref('select * from bet_v1.get_open_contract_by_id(?)', [$id])};
+    my @fmbs = @{
+        $clientdb->getall_arrayref(
+            'select * from bet_v1.get_open_contract_by_id(?)', [$id] )
+    };
 
-    return BOM::RPC::v3::Utility::create_error({
+    return BOM::RPC::v3::Utility::create_error(
+        {
             code              => 'InvalidSellContractProposal',
-            message_to_client => BOM::Platform::Context::localize('Unknown contract sell proposal')}) unless @fmbs;
+            message_to_client => BOM::Platform::Context::localize(
+                'Unknown contract sell proposal')
+        }
+    ) unless @fmbs;
 
-    my $contract_parameters = shortcode_to_parameters($fmbs[0]->{short_code}, $client->currency);
+    my $contract_parameters =
+      shortcode_to_parameters( $fmbs[0]->{short_code}, $client->currency );
     $contract_parameters->{landing_company} = $client->landing_company->short;
     my $amount_type = $contract_parameters->{amount_type};
     my $contract    = produce_contract($contract_parameters);
-    my $trx         = BOM::Product::Transaction->new({
-        client   => $client,
-        contract => $contract,
-        (defined $amount_type) ? (amount_type => $amount_type) : (),
-        contract_id => $id,
-        price       => ($args->{price} || 0),
-        source      => $source,
-    });
+    my $trx         = BOM::Product::Transaction->new(
+        {
+            client   => $client,
+            contract => $contract,
+            ( defined $amount_type ) ? ( amount_type => $amount_type ) : (),
+            contract_id => $id,
+            price       => ( $args->{price} || 0 ),
+            source      => $source,
+        }
+    );
 
-    if (my $err = $trx->sell) {
-        return BOM::RPC::v3::Utility::create_error({
-            code              => $err->get_type,
-            message_to_client => $err->{-message_to_client},
-            message           => "Contract-Sell Fail: " . $err->get_type . " $err->{-message_to_client}: $err->{-mesg}"
-        });
+    if ( my $err = $trx->sell ) {
+        return BOM::RPC::v3::Utility::create_error(
+            {
+                code              => $err->get_type,
+                message_to_client => $err->{-message_to_client},
+                message           => "Contract-Sell Fail: "
+                  . $err->get_type
+                  . " $err->{-message_to_client}: $err->{-mesg}"
+            }
+        );
     }
 
     $trx = $trx->transaction_record;
@@ -274,8 +347,8 @@ sub sell {
     return {
         transaction_id => $trx->id,
         contract_id    => $id,
-        balance_after  => sprintf('%.2f', $trx->balance_after),
-        sold_for       => abs($trx->amount),
+        balance_after  => sprintf( '%.2f', $trx->balance_after ),
+        sold_for       => abs( $trx->amount ),
     };
 }
 
