85a1ffa22e2b1a4545e48b1673d33b96f06df5ae
Torsten Foertsch <torsten@binary.com>
2017-01-22 15:54:54 +0000

    fix

--- lib/BOM/RPC/v3/Accounts.pm
+++ lib/BOM/RPC/v3/Accounts.pm
@@ -551,7 +551,7 @@ sub get_settings {
                 address_state                  => $client->state,
                 address_postcode               => $client->postcode,
                 phone                          => $client->phone,
-                allow_copiers                  => $client->allow_copiers,
+                allow_copiers                  => $client->allow_copiers // 0,
                 is_authenticated_payment_agent => ($client->payment_agent and $client->payment_agent->is_authenticated) ? 1 : 0,
                 $client_tnc_status ? (client_tnc_status => $client_tnc_status->reason) : (),
             )
