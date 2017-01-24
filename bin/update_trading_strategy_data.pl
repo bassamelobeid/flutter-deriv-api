7da024c0e8b4ded00ce830271c05b18c48b7ccd6
Tom Molesworth <tom@binary.com>
2017-01-24 14:13:36 +0800

    Use forks for parallel processing

--- bin/update_trading_strategy_data.pl
+++ bin/update_trading_strategy_data.pl
@@ -13,9 +13,13 @@ use YAML qw(LoadFile);
 use Path::Tiny;
 use Data::Dumper;
 use Fcntl qw(:flock);
+use Parallel::ForkManager;
 
 # How many ticks to request at a time
 use constant TICK_CHUNK_SIZE => 86400;
+
+# Number of forks to create
+use constant WORKERS => 4;
 
 ++$|;
 
@@ -42,85 +46,90 @@ try {
     my $output_base = '/var/lib/binary/trading_strategy_data/' . Date::Utility->new($start)->date;
     path($output_base)->mkpath;
 
+    # Gather data and create jobs
+    my @jobs;
     for my $symbol (@{$config->{underlyings}}) {
-        print "Symbol $symbol\n";
+        for (@{$config->{durations}}) {
+            for my $bet_type (@{$config->{types}}) {
+                push @jobs, join "\0", $symbol, $duration, $bet_type;
+            }
+        }
+    }
+    @jobs = shuffle @jobs;
+
+    my $pm = Parallel::ForkManager->new(WORKERS);
+    JOB:
+    for my $job (@jobs) {
+        my $pid = $pm->start and next JOB;
+        my ($symbol, $duration_step, $bet_type) = split "\0", $job;
+        print "$pid working on " . ($job =~ s/\0/ /gr) . " from $start..$end\n";
+        my ($duration, %duration_options) = split ' ', $duration_step;
+
         my $api = Postgres::FeedDB::Spot::DatabaseAPI->new(
             db_handle  => Postgres::FeedDB::read_dbh(),
             underlying => $symbol
         );
 
-        my %fh;
-        print "Getting ticks from $start to $end...\n";
-        my $current = $start;
-        BATCH:
-        while ($current < $end) {
-            my @ticks = reverse @{
-                $api->ticks_start_end_with_limit_for_charting({
-                        start_time => $current,
-                        end_time   => $current + TICK_CHUNK_SIZE,
-                        limit      => TICK_CHUNK_SIZE,
-                    })}
-                or last BATCH;    # if we had no ticks, then we're done for this symbol
-            for (@{$config->{durations}}) {
-                my ($duration, %duration_options) = split ' ', $_;
-                $duration_options{step} //= '1t';
-                my ($step_amount, $step_unit) = $duration_options{step} =~ /(\d+)([tmhs])/ or die "unknown step type " . $duration_options{step};
-                if ($step_unit eq 'm') {
-                    $step_amount *= 60;
-                    $step_unit = 's';
-                } elsif ($step_unit eq 'h') {
-                    $step_amount *= 3600;
-                    $step_unit = 's';
-                }
-                print "Duration $duration\n";
-                for my $bet_type (@{$config->{types}}) {
-                    print "Bet type $bet_type\n";
-                    my $key = join '_', $symbol, $duration, $duration_options{step}, $bet_type;
-                    unless (exists $fh{$key}) {
-                        open $fh{$key}, '>:encoding(UTF-8)', $output_base . '/' . $key . '.csv' or die $!;
-                        $fh{$key}->autoflush(1);
-                    }
-                    my $idx = 0;
-                    while ($idx <= $#ticks) {
-                        my $tick = $ticks[$idx];
+        if(my @ticks = reverse @{
+            $api->ticks_start_end_with_limit_for_charting({
+                start_time => $start,
+                end_time   => $end,
+                limit      => ($end - $start),
+            })}) {
 
-                        my $args = {
-                            underlying   => $symbol,
-                            bet_type     => $bet_type,
-                            date_start   => $tick->{epoch},
-                            date_pricing => $tick->{epoch},
-                            duration     => $duration,
-                            currency     => 'USD',
-                            payout       => 10,
-                            barrier      => 'S0P',
-                        };
-                        try {
-                            my $contract         = produce_contract($args);
-                            my $contract_expired = produce_contract({
-                                %$args,
-                                date_pricing => $now,
-                            });
-                            if ($contract_expired->is_expired) {
-                                my $ask_price = $contract->ask_price;
-                                my $value     = $contract_expired->value;
-                                $fh{$key}->print(join(",", (map $tick->{$_}, qw(epoch quote)), $ask_price, $value, $contract->theo_price) . "\n");
-                            }
-                        }
-                        catch {
-                            warn "Failed to price with parameters " . Dumper($args) . " - $_\n";
-                        };
-                        if ($step_unit eq 't') {
-                            $idx += $step_amount;
-                        } elsif ($step_unit eq 's') {
-                            ++$idx while $idx <= $#ticks && $step_amount >= $ticks[$idx]->{epoch} - $tick->{epoch};
-                        } else {
-                            die "Invalid step unit $step_unit";
-                        }
+            $duration_options{step} //= '1t';
+            my ($step_amount, $step_unit) = $duration_options{step} =~ /(\d+)([tmhs])/ or die "unknown step type " . $duration_options{step};
+            if ($step_unit eq 'm') {
+                $step_amount *= 60;
+                $step_unit = 's';
+            } elsif ($step_unit eq 'h') {
+                $step_amount *= 3600;
+                $step_unit = 's';
+            }
+
+            my $key = join '_', $symbol, $duration, $duration_options{step}, $bet_type;
+            open my $fh, '>:encoding(UTF-8)', $output_base . '/' . $key . '.csv' or die $!;
+            $fh->autoflush(1);
+
+            my $idx = 0;
+            while ($idx <= $#ticks) {
+                my $tick = $ticks[$idx];
+
+                my $args = {
+                    underlying   => $symbol,
+                    bet_type     => $bet_type,
+                    date_start   => $tick->{epoch},
+                    date_pricing => $tick->{epoch},
+                    duration     => $duration,
+                    currency     => 'USD',
+                    payout       => 10,
+                    barrier      => 'S0P',
+                };
+                try {
+                    my $contract         = produce_contract($args);
+                    my $contract_expired = produce_contract({
+                            %$args,
+                            date_pricing => $now,
+                        });
+                    if ($contract_expired->is_expired) {
+                        my $ask_price = $contract->ask_price;
+                        my $value     = $contract_expired->value;
+                        $fh{$key}->print(join(",", (map $tick->{$_}, qw(epoch quote)), $ask_price, $value, $contract->theo_price) . "\n");
                     }
+                }
+                catch {
+                    warn "Failed to price with parameters " . Dumper($args) . " - $_\n";
+                };
+                if ($step_unit eq 't') {
+                    $idx += $step_amount;
+                } elsif ($step_unit eq 's') {
+                    ++$idx while $idx <= $#ticks && $step_amount >= $ticks[$idx]->{epoch} - $tick->{epoch};
+                } else {
+                    die "Invalid step unit $step_unit";
                 }
             }
-            $current = 1 + $ticks[-1]{epoch};
         }
+        $pm->finish;
     }
 }
 catch {
