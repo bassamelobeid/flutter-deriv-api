1b5a754791eea46761091eb94d7ed1ce21b65793
Ivan Baidakou <ivan@binary.com>
2017-02-09 16:12:53 +0000

    Remove debug code

--- lib/Binary/WebSocketAPI.pm
+++ lib/Binary/WebSocketAPI.pm
@@ -490,7 +490,6 @@ sub startup {
             my $c    = shift;
             my $key  = $c->rate_limitations_key;
             my $hits = $c->stash->{rate_limitations_hits};
-            return;
             $c->ws_redis_master->set(
                 $key => encode_json($hits),
                 EX   => 3600,
