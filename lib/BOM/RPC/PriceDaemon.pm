10617a4c8015988798b97bf48ed0df7136868896
Tom Molesworth <tom@binary.com>
2017-02-10 13:34:53 +0800

    Log the pricing key rather than Dumper output on invalid data

--- lib/BOM/RPC/PriceDaemon.pm
+++ lib/BOM/RPC/PriceDaemon.pm
@@ -90,7 +90,7 @@ sub run {
             # If incomplete or invalid keys somehow got into pricer,
             # delete them here.
             unless (_validate_params($params)) {
-                warn "Invalid parameters: " . Data::Dumper->Dumper($params);
+                warn "Invalid parameters: $next";
                 $redis->del($key->[1], $next);
                 next;
             }
