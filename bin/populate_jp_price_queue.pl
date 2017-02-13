a5f5349d661cf97c74313bd6c424100abf423df5
Tom Molesworth <tom@binary.com>
2017-02-13 06:37:52 +0000

    Clean up sccript

--- bin/populate_jp_price_queue.pl
+++ bin/populate_jp_price_queue.pl
@@ -15,6 +15,9 @@ use BOM::MarketData qw(create_underlying);
 use JSON::XS qw(encode_json);
 use List::UtilsBy qw(rev_nsort_by);
 use Pricing::Engine::EuropeanDigitalSlope;
+use Date::Utility;
+use POSIX qw(floor);
+use Time::HiRes qw(clock_nanosleep CLOCK_REALTIME TIMER_ABSTIME);
 
 use Log::Any qw($log);
 use Log::Any::Adapter qw(Stderr);
@@ -30,15 +33,16 @@ while(1) {
         }
     );
     my $now = Time::HiRes::time;
-    $log->infof("Retrieved symbols - %.2fms", 1000 * ($now - $start));
+    $log->debugf("Retrieved symbols - %.2fms", 1000 * ($now - $start));
 
     my @jobs;
+    my $skipped = 0;
     for my $symbol (@symbols) {
         my $contracts_for = BOM::Product::Contract::Finder::Japan::available_contracts_for_symbol({
             symbol => $symbol
         });
         $now = Time::HiRes::time;
-        $log->infof("Retrieved contracts for %s - %.2fms", $symbol, 1000 * ($now - $start));
+        $log->debugf("Retrieved contracts for %s - %.2fms", $symbol, 1000 * ($now - $start));
         $start = $now;
 
         for my $contract_parameters (@{$contracts_for->{available}}) {
@@ -51,9 +55,13 @@ while(1) {
                 if(exists $expired{$barrier_desc}) {
                     $skipped++;
                 } else {
-                    my %pricing_queue_args = (
+                    my @pricing_queue_args = (
+                        amount               => 1000,
                         basis                => 'payout',
                         currency             => 'JPY',
+                        contract_type        => $contract_parameters->{contract_type},
+                        price_daemon_cmd     => 'price',
+                        skips_price_validation => 1,
                         landing_company      => 'japan',
                         date_expiry          => $contract_parameters->{trading_period}{date_expiry}{epoch},
                         trading_period_start => $contract_parameters->{trading_period}{date_start}{epoch},
@@ -69,28 +77,23 @@ while(1) {
                            )
                         )
                     );
-                    my @contract_parameters = (
-                        bet_type             => $contract_parameters->{contract_type},
-                        fixed_expiry         => 1,
-                        payout               => 1000,
-                        %pricing_queue_args,
-                    );
-                    $pricing_queue_args{proposal} = 1;
-                    $pricing_queue_args{contract_type} = $contract_parameters->{contract_type};
-                    my $contract = produce_contract(@contract_parameters);
-                    push @jobs, 'PRICER_KEYS::' . encode_json([
-                        short_code => $contract->shortcode,
-                        amount => 1000,
-                        map {; $_ => $pricing_queue_args{$_} } sort keys %pricing_queue_args
-                    ]);
+                    $log->debugf("Contract parameters will be %s", \@pricing_queue_args);
+                    # my $contract = produce_contract(@contract_parameters);
+                    push @jobs, \@pricing_queue_args;
                 }
             }
         }
     }
-    $log->infof("Total of %d jobs to process, %d skipped", 0 + @jobs, $skipped);
+    $log->debugf("Total of %d jobs to process, %d skipped", 0 + @jobs, $skipped);
     my $redis = BOM::System::RedisReplicated::redis_pricer;
-    $redis->set("PRICER_KEYS::$_", EX => 60) for @jobs;
+warn "redis = $redis\n";
+    $redis->setex("PRICER_KEYS::" . encode_json($_), 10, 1) for @jobs;
     # Sleep to start of next minute
-    clock_nanosleep(CLOCK_REALTIME, 60e9 * (1 + floor(time / 60)), TIMER_ABSTIME);
+    {
+        my $now = Time::HiRes::time;
+        my $target = 60e9 * (1 + floor($now / 60));
+        $log->debugf("Will sleep until %s (current time %s)", map $_->iso8601, Date::Utility->new($target / 1e9), Date::Utility->new);
+        clock_nanosleep(CLOCK_REALTIME, $target, TIMER_ABSTIME);
+    }
 }
 
