231b87c7448d9188da1f35f3fb95d0b148d12f96
Jun Bon <junbon@binary.com>
2017-02-17 01:59:40 +0000

    syntax

--- lib/BOM/JapanContractDetails.pm
+++ lib/BOM/JapanContractDetails.pm
@@ -128,7 +128,7 @@ sub verify_with_shortcode {
     my $contract          = produce_contract($pricing_args);
     my @contracts         = ([$contract, $verify_ask]);
     # due to complexity in $action_type, this is a hacky fix.
-    push @contracts, [$contract->opposite_contract, $contract->discounted_probability->amount * $contract->payout - $verify_bid]) if defined $verify_bid;
+    push @contracts, [$contract->opposite_contract, $contract->discounted_probability->amount * $contract->payout - $verify_bid] if defined $verify_bid;
 
     my $parameters;
     foreach my $ind (0 .. $#contracts) {
