355031bf237687404cbea3d306820a47006a5d6e
Sasha Murzin <sasha@binary.com>
2017-03-02 02:30:21 +0000

    tidy

--- lib/BOM/RPC/v3/Contract.pm
+++ lib/BOM/RPC/v3/Contract.pm
@@ -212,7 +212,8 @@ sub _get_ask {
             # but later could produce valid contract dependant on volatility moves
             # so we need to store contract_parameters and longcode to use them later
             if ($code eq 'ContractBuyValidationError') {
-                my $longcode = eval { $contract->longcode } || ''; # if we can't get the longcode that's fine, we still want to return the original error
+                my $longcode =
+                    eval { $contract->longcode } || '';    # if we can't get the longcode that's fine, we still want to return the original error
                 $response->{contract_parameters} = $contract_parameters;
                 $response->{longcode}            = $longcode;
             }
