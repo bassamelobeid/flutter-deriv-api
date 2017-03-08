f4cacc88d0583105cab108981d903ea769621b52
Raunak Kathuria <raunakkathuria@gmail.com>
2017-03-08 09:56:36 +0000

    add amount check in withdraw not in transfer

--- lib/BOM/RPC/v3/Cashier.pm
+++ lib/BOM/RPC/v3/Cashier.pm
@@ -541,15 +541,6 @@ sub paymentagent_transfer {
         return $error_sub->($error_msg);
     }
 
-    my ($max_withdraw, $min_withdraw) = ($payment_agent->max_withdraw, $payment_agent->min_withdraw);
-    if ($max_withdraw and $amount > $max_withdraw) {
-        return $error_sub->(localize("Invalid amount. Maximum withdrawal allowed is [_1].", $max_withdraw));
-    }
-
-    if ($min_withdraw and $amount < $min_withdraw) {
-        return $error_sub->(localize("Invalid amount. Minimum withdrawal allowed is [_1].", $min_withdraw));
-    }
-
     ## validate amount
     if ($amount < 10 || $amount > 2000) {
         return $error_sub->(localize('Invalid amount. minimum is 10, maximum is 2000.'));
@@ -806,6 +797,15 @@ sub paymentagent_withdraw {
 
     if (not $client->residence or scalar keys %{$authenticated_pa} == 0) {
         return $error_sub->(localize('The Payment Agent facility is currently not available in your country.'));
+    }
+
+    my ($max_withdraw, $min_withdraw) = ($payment_agent->max_withdraw, $payment_agent->min_withdraw);
+    if ($max_withdraw and $amount > $max_withdraw) {
+        return $error_sub->(localize("Invalid amount. Maximum withdrawal allowed is [_1].", $max_withdraw));
+    }
+
+    if ($min_withdraw and $amount < $min_withdraw) {
+        return $error_sub->(localize("Invalid amount. Minimum withdrawal allowed is [_1].", $min_withdraw));
     }
 
     ## validate amount
