356d8a836c9f845ee33198d13a5f9e6c3f6fbaa0
Chylli <chylli@binary.com>
2017-03-16 08:37:16 +0800

    rewrite 2

--- bin/update_highlow_windows.pl
+++ bin/update_highlow_windows.pl
@@ -2,20 +2,7 @@
 
 use strict;
 use warnings;
-
-use BOM::Product::Contract::PredefinedParameters qw(update_predefined_highlow);
-use Cache::RedisDB;
-use JSON qw(from_json);
+use UpdateHighLowWindows;
 
 #Update high and low of symbols for predefined periods.
-
-my @symbols = BOM::Product::Contract::PredefinedParameters::supported_symbols;
-my $redis = Cache::RedisDB->redis;
-
-$redis->subscription_loop(
-    subscribe        => [map { 'FEED_LATEST_TICK::' . $_ } @symbols],
-    default_callback => sub {
-        my $tick_data = from_json($_[3]);
-        update_predefined_highlow($tick_data);
-    },
-);
+exit UpdateHighLowWindows->run;
