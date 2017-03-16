2edf714e9cbed2517a1b993261cbaec90a4dae08
Chylli <chylli@binary.com>
2017-03-16 10:32:18 +0800

    rewrite

--- bin/risk_scenario_analysis.pl
+++ bin/risk_scenario_analysis.pl
@@ -1,47 +1,8 @@
 #!/etc/rmg/bin/perl
 
-package BOM::Backoffice::RiskScenarioAnalysis;
-
-=head1 NAME
-
-BOM::Backoffice::RiskScenarioAnalysis
-
-=head1 DESCRIPTION
-
-Determine a value for our curent open positions at risk.
-
-=cut
-
-use Moose;
-use BOM::Platform::Runtime;
-use lib qw(/home/git/regentmarkets/bom-backoffice/lib/ /home/git/regentmarkets/bom-market/lib/);
-use BOM::RiskReporting::ScenarioAnalysis;
-with 'App::Base::Script';
-
-sub script_run {
-    my $self = shift;
-
-    BOM::RiskReporting::ScenarioAnalysis->new->generate;
-
-    return 0;
-}
-
-sub documentation {
-    return qq{
-This script creates  risk analysis scenarios for open contracts.
-    };
-}
-
-sub cli_template {
-    return $0;
-}
-
-no Moose;
-__PACKAGE__->meta->make_immutable;
-1;
-
 package main;
 use strict;
+use BOM::Script::RiskScenarioAnalysis;
 
-exit BOM::Backoffice::RiskScenarioAnalysis->new->run;
+exit BOM::Script::RiskScenarioAnalysis->new->run;
 
---
+++ lib/BOM/Script/Riskd.pm
@@ -0,0 +1,71 @@
+package BOM::Script::Riskd;
+
+use Moose;
+with 'App::Base::Daemon';
+
+use lib qw(/home/git/regentmarkets/bom-backoffice/lib/);
+use Time::Duration::Concise::Localize;
+use BOM::Platform::Runtime;
+
+use BOM::RiskReporting::Dashboard;
+use BOM::RiskReporting::MarkedToModel;
+no indirect;
+use Try::Tiny;
+
+has rest_period => (
+    is         => 'ro',
+    isa        => 'Time::Duration::Concise::Localize',
+    lazy_build => 1,
+);
+
+sub _build_rest_period {
+    return Time::Duration::Concise::Localize->new(interval => '37s');
+}
+
+sub documentation {
+    return qq/This daemon generates live risk reports./;
+}
+
+sub daemon_run {
+    my $self = shift;
+
+    while (1) {
+        try {
+            BOM::RiskReporting::MarkedToModel->new->generate;
+        }
+        catch {
+            warn "Failure in BOM::RiskReporting::MarkedToModel: $_\n";
+        };
+        $self->rest;
+        try {
+            BOM::RiskReporting::Dashboard->new->generate;
+        }
+        catch {
+            warn "Failure in BOM::RiskReporting::Dashboard: $_\n";
+        };
+        $self->rest;
+    }
+
+    return 255;
+}
+
+sub rest {
+    my $self     = shift;
+    my $how_long = $self->rest_period;
+
+    BOM::Platform::Runtime->instance->app_config->check_for_update;    # We're a long-running process. See if config changed underneath us.
+    sleep($how_long->seconds);
+
+    return;
+}
+
+sub handle_shutdown {
+    my $self = shift;
+    warn('Shutting down.');
+    return 0;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
