16ff5e73d7fd1a60beac7e27daf1b36653949a0d
Chylli <chylli@binary.com>
2017-03-20 08:47:10 +0800

    rerwrite interest_rats

--- bin/update_implied_interest_rates.pl
+++ bin/update_implied_interest_rates.pl
@@ -1,20 +1,6 @@
 #!/etc/rmg/bin/perl
-package BOM::MarketDataAutoUpdater::UpdateImpliedInterestRates;
-
-use Moose;
-with 'App::Base::Script';
-
-use BOM::MarketDataAutoUpdater::ImpliedInterestRates;
-
-sub documentation { return 'This is a cron that updates interest rates info from Bloomberg to Chronicle.'; }
-
-sub script_run {
-    my $self = shift;
-    BOM::MarketDataAutoUpdater::ImpliedInterestRates->new->run;
-    return $self->return_value();
-}
-
-no Moose;
-__PACKAGE__->meta->make_immutable;
 package main;
-exit BOM::MarketDataAutoUpdater::UpdateImpliedInterestRates->new()->run();
+use strict;
+use warnings;
+use BOM::MarketDataAutoUpdater::Script::UpdateImpliedInterestRates;
+exit BOM::MarketDataAutoUpdater::Script::UpdateImpliedInterestRates->new()->run();
