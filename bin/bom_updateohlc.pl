60b1ab3dab05ef138275ee67e5642c04da146189
Chylli <chylli@binary.com>
2017-03-20 08:16:07 +0800

    update updateohlc.pl

--- bin/bom_updateohlc.pl
+++ bin/bom_updateohlc.pl
@@ -1,40 +1,5 @@
 #!/etc/rmg/bin/perl
-package BOM::MarketDataAutoUpdater::UpdateOhlc;
-
-use Moose;
-with 'App::Base::Script';
-
-use BOM::MarketDataAutoUpdater::OHLC;
-sub documentation {
-    return 'Updates ohlc we have received from Bloomberg.';
-}
-
-sub options {
-    return [{
-             name          => 'action',
-             documentation => 'update or verify ohlc',
-             option_type   => 'string',
-             default       => 'update',
-        },
-    ];
-}
-
-sub script_run {
-    my $self = shift;
-
-    my $action  = $self->getOption('action');
-
-    if ($action eq 'update') {
-         BOM::MarketDataAutoUpdater::OHLC->new->run;
-    } elsif ($action eq 'verify') {
-         BOM::MarketDataAutoUpdater::OHLC->new->verify_ohlc_update;
-    }
-
-    return $self->return_value();
-}
-
-no Moose;
-__PACKAGE__->meta->make_immutable;
-1;
-package main;
-exit BOM::MarketDataAutoUpdater::UpdateOhlc->new->run;
+use strict;
+use warnings;
+use BOM::MarketDataAutoUpdater::Script::UpdateOhlc;
+exit BOM::MarketDataAutoUpdater::Script::UpdateOhlc->new->run;
