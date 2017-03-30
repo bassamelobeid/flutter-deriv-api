4a5e82bf7139be66e438f90e32f7fb3186a5e630
Jun Bon <junbon@binary.com>
2017-03-30 04:00:08 +0000

    bug fixes

--- lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
+++ lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
@@ -687,7 +687,7 @@ sub _build_risk_markup {
     }
     my $open_at_start = $bet->underlying->calendar->is_open_at($bet->underlying->exchange, $bet->date_start);
 
-    if ($open_at_start and $bet->underlying->is_in_quiet_period($bet->date_pricing)) {
+    if ($open_at_start and $bet->underlying->calendar->is_in_quiet_period($bet->underlying->symbol, $bet->date_pricing)) {
         my $quiet_period_markup = Math::Util::CalculatedValue::Validatable->new({
             name        => 'quiet_period_markup',
             description => 'Intraday::Forex markup factor for underlyings in the quiet period',
