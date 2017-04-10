5c22baf7f087296d7562f873e432c458b4b9ea1b
Ivan Baidakou <ivan@binary.com>
2017-04-10 15:17:30 +0000

    Silence warnings like:
    pricer-green pricer-daemon Use of uninitialized value $contract_type in concatenation (.) or string at .. PriceDaemon.pm line 117.

--- lib/BOM/Pricing/PriceDaemon.pm
+++ lib/BOM/Pricing/PriceDaemon.pm
@@ -111,12 +111,12 @@ sub run {
         }
         qw(feed chronicle) or next;
 
-        {
+        if (($response->{rpc_time} // 0) > 1000) {
             my $contract_type = $params->{contract_type};
             $contract_type = join '_', @$contract_type if ref $contract_type;
+            $contract_type //= 'unknown';
             stats_timing('pricer_daemon.rpc_time', $response->{rpc_time},
-                {tags => $self->tags('contract_type:' . $contract_type, 'currency:' . $params->{currency})})
-                if (($response->{rpc_time} // 0) > 1000);
+                {tags => $self->tags('contract_type:' . $contract_type, 'currency:' . $params->{currency})});
         }
 
         my $subscribers_count = $redis->publish($key->[1], encode_json($response));
