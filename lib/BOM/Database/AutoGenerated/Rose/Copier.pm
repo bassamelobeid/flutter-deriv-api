f9328b3da951167148c12ac41692926728272946
Vladimir Melnichenko <vladimir.melnichenko@binary.com>
2017-04-11 01:52:32 +0000

    Rose

--- lib/BOM/Database/AutoGenerated/Rose/Copier.pm
+++ lib/BOM/Database/AutoGenerated/Rose/Copier.pm
@@ -15,6 +15,8 @@ __PACKAGE__->meta->setup(
         trade_type      => { type => 'varchar', length => 50 },
         min_trade_stake => { type => 'numeric' },
         max_trade_stake => { type => 'numeric' },
+        autolink        => { type => 'boolean', default => 'false' },
+        linkcount       => { type => 'integer', default => 1 },
     ],
 
     primary_key_columns => [ 'copier_id' ],
