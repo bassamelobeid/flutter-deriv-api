226f6fcbe91c8565e7ae1408bf362688b9cf680e
Raunak Kathuria <raunakkathuria@gmail.com>
2017-04-11 14:18:34 +0000

    minor change for consistency

--- lib/BOM/Product/Contract/Batch.pm
+++ lib/BOM/Product/Contract/Batch.pm
@@ -147,13 +147,13 @@ sub ask_prices {
                     ? $contract->payout
                     : $contract->ask_price;
                 $contract_info->{error}{details} = {
-                    display_value => (sprintf('%.2f', $display_value)),
-                    payout        => sprintf('%.2f',  $display_value),
+                    display_value => sprintf('%.2f', $display_value),
+                    payout        => sprintf('%.2f', $display_value),
                 };
             } else {
                 $contract_info->{error}{details} = {
-                    display_value => (sprintf('%.2f', $contract->ask_price)),
-                    payout        => sprintf('%.2f',  $contract->payout),
+                    display_value => sprintf('%.2f', $contract->ask_price),
+                    payout        => sprintf('%.2f', $contract->payout),
                 };
             }
             if ($contract->two_barriers) {
