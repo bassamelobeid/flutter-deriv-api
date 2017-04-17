10e558f496d594bfdc74f161faa3d74731aa76ae
Vladimir Melnichenko <vladimir.melnichenko@binary.com>
2017-04-17 02:51:06 +0000

    Long live TryCatch!

--- lib/Binary/WebSocketAPI/v3/Instance/Redis.pm
+++ lib/Binary/WebSocketAPI/v3/Instance/Redis.pm
@@ -1,10 +1,11 @@
 package Binary::WebSocketAPI::v3::Instance::Redis;
 use strict;
 use warnings;
+no indirect;
 use YAML::XS qw| LoadFile |;
 use Exporter qw| import   |;
 use DataDog::DogStatsd::Helper qw| stats_inc stats_dec |;
-
+use Try::Tiny;
 use Mojo::Redis2;
 
 my $config = {
@@ -35,20 +36,21 @@ sub create {
 }
 
 sub check_connections {
-    local $@;
-
-    foreach my $server_name (keys %$config) {
-        my $server;
-        eval {
+    my ($server, $server_name);
+    try {
+        for $server_name (keys %$config) {
+            undef $server;
             $server = __PACKAGE__->$server_name();
-            1;
-        } or do {
-            die "$server_name is not available:" . $@;
-        };
-        eval { $server->ping(); 1; } or do {
-            die "Redis server $server_name does not work! Host: " . $server->url->host . ", port: " . $server->url->port . "\nREASON: " . $@;
-            }
+            $server->ping();
+        }
     }
+    catch {
+        if ($server) {
+            die "Redis server $server_name does not work! Host: " . $server->url->host . ", port: " . $server->url->port . "\nREASON: " . $_;
+        } else {
+            die "$server_name is not available:" . $_;
+        }
+    };
     return 1;
 }
 
@@ -62,10 +64,9 @@ sub pricer_write {
             my ($self, $msg, $channel) = @_;
             if ($self->{shared_info}{$channel}) {
                 foreach my $c_key (keys %{$self->{shared_info}{$channel}}) {
-                    delete $self->{shared_info}{$channel}{$c_key} and next
-                        unless $self->{shared_info}{$channel}{$c_key};
-                    my $c = $self->{shared_info}{$channel}{$c_key};
-                    Binary::WebSocketAPI::v3::Wrapper::Pricer::process_pricing_events($c, $msg, $channel) if ref $c;
+                    delete $self->{shared_info}{$channel}{$c_key}, next
+                        unless $self->{shared_info}{$channel}{$c_key} && ref $self->{shared_info}{$channel}{$c_key};
+                    Binary::WebSocketAPI::v3::Wrapper::Pricer::process_pricing_events($self->{shared_info}{$channel}{$c_key}, $msg, $channel);
                 }
             }
         });
