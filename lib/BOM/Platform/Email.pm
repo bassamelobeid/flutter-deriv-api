60cf75908ceb0ef022c06b4004fda28920a335f9
Chylli <chylli@binary.com>
2017-04-19 06:34:48 +0800

    remove try

--- lib/BOM/Platform/Email.pm
+++ lib/BOM/Platform/Email.pm
@@ -5,7 +5,6 @@ use warnings;
 
 use URL::Encode;
 use Email::Stuffer;
-use Try::Tiny;
 use Encode;
 
 use Brands;
@@ -27,7 +26,7 @@ sub send_email {
     my $fromemail          = $args_ref->{'from'} // '';
     my $email              = $args_ref->{'to'} // '';
     my $subject            = $args_ref->{'subject'} // '';
-    my @message            = @{ $args_ref->{'message'} // [] };
+    my @message            = @{$args_ref->{'message'} // []};
     my $use_email_template = $args_ref->{'use_email_template'};
     my $attachment         = $args_ref->{'attachment'};
     my $skip_text2html     = $args_ref->{'skip_text2html'};
@@ -36,10 +35,8 @@ sub send_email {
     my $request = request();
     my $language = $request ? $request->language : 'EN';
 
-    unless ( $email && $fromemail && $subject ) {
-        warn(
-"from, to, or subject missed - [from: $fromemail, to: $email, subject: $subject]"
-        );
+    unless ($email && $fromemail && $subject) {
+        warn("from, to, or subject missed - [from: $fromemail, to: $email, subject: $subject]");
         return 0;
     }
 
@@ -48,23 +45,22 @@ sub send_email {
 
     return 1 if $ENV{SKIP_EMAIL};
 
-    my @toemails = split( /\s*\,\s*/, $email );
+    my @toemails = split(/\s*\,\s*/, $email);
     foreach my $toemail (@toemails) {
         if (    $toemail
-            and $toemail !~
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/ )
+            and $toemail !~ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)
         {
             warn("erroneous email address $toemail");
             return 0;
         }
     }
 
-    my $brand = Brands->new( name => request()->brand );
-    if ( $fromemail eq $brand->emails('support') ) {
+    my $brand = Brands->new(name => request()->brand);
+    if ($fromemail eq $brand->emails('support')) {
         $fromemail = "\"" . $brand->website_name . "\" <$fromemail>";
     }
 
-    my $message = join( "\n", @message );
+    my $message = join("\n", @message);
     my $mail_message = $message;
     if ($use_email_template) {
         my $vars = {
@@ -74,34 +70,25 @@ sub send_email {
             skip_text2html        => $skip_text2html,
             content               => $message,
         };
-        $vars->{text_email_template_loginid} =
-          localize( 'Your Login ID: [_1]', $template_loginid )
-          if $template_loginid;
-        if ( $language eq 'JA' ) {
-            $vars->{japan_footer_text} =
-              localize('{JAPAN ONLY}footer text of email template for Japan');
+        $vars->{text_email_template_loginid} = localize('Your Login ID: [_1]', $template_loginid)
+            if $template_loginid;
+        if ($language eq 'JA') {
+            $vars->{japan_footer_text} = localize('{JAPAN ONLY}footer text of email template for Japan');
         }
-        BOM::Platform::Context::template->process( 'common_email.html.tt',
-            $vars, \$mail_message )
-          || die BOM::Platform::Context::template->error();
+        BOM::Platform::Context::template->process('common_email.html.tt', $vars, \$mail_message)
+            || die BOM::Platform::Context::template->error();
     }
 
-    my $email_stuffer =
-      Email::Stuffer->from($fromemail)->to($email)->subject($subject)
-      ->text_body($mail_message);
+    my $email_stuffer = Email::Stuffer->from($fromemail)->to($email)->subject($subject)->text_body($mail_message);
     if ($attachment) {
         $email_stuffer->attach_file($attachment);
     }
 
-    try {
-        $email_stuffer->send;
-        1;
+    unless ($email_stuffer->send) {
+        warn("Error sending mail [$subject]: ", $_)
+            unless $ENV{BOM_SUPPRESS_WARNINGS};
+        return 0;
     }
-    catch {
-        warn( "Error sending mail [$subject]: ", $_ )
-          unless $ENV{BOM_SUPPRESS_WARNINGS};
-        0;
-    } or return 0;
 
     return 1;
 }
