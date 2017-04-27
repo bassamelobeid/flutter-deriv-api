a7265f7fd9fe1ec9b25d7ac1554094c535656ac3
Yng Shan <yngshan@binary.com>
2017-04-27 06:36:10 +0000

    remove original barrier

--- lib/BOM/Product/Role/DoubleBarrier.pm
+++ lib/BOM/Product/Role/DoubleBarrier.pm
@@ -46,11 +46,6 @@ sub BUILD {
 
 has [qw(supplied_high_barrier supplied_low_barrier)] => (is => 'ro');
 
-has [qw(original_high_barrier original_low_barrier)] => (
-    is  => 'rw',
-    isa => 'Maybe[BOM::Product::Contract::Strike]'
-);
-
 has high_barrier => (
     is      => 'rw',
     isa     => 'Maybe[BOM::Product::Contract::Strike]',
@@ -62,7 +57,6 @@ sub _build_high_barrier {
     my $self = shift;
 
     my $high_barrier = $self->make_barrier($self->supplied_high_barrier);
-    $self->original_high_barrier($self->initial_barrier) if defined $self->initial_barrier;
     return $high_barrier;
 }
 
@@ -77,7 +71,6 @@ sub _build_low_barrier {
     my $self = shift;
 
     my $low_barrier = $self->make_barrier($self->supplied_low_barrier);
-    $self->original_low_barrier($self->initial_barrier) if defined $self->initial_barrier;
     return $low_barrier;
 }
 has barriers_for_pricing => (
