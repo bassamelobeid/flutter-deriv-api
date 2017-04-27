d2506821cd7b15157e4bc6d1522b5a0d36446a14
Yng Shan <yngshan@binary.com>
2017-04-27 06:38:42 +0000

    remove original barrier

--- lib/BOM/Product/Role/SingleBarrier.pm
+++ lib/BOM/Product/Role/SingleBarrier.pm
@@ -18,15 +18,10 @@ has barrier => (
     builder => '_build_barrier',
 );
 
-has original_barrier => (
-    is  => 'rw',
-    isa => 'Maybe[BOM::Product::Contract::Strike]',
-);
 
 sub _build_barrier {
     my $self    = shift;
     my $barrier = $self->make_barrier($self->supplied_barrier);
-    $self->original_barrier($self->initial_barrier) if defined $self->initial_barrier;
     return $barrier;
 }
 has barriers_for_pricing => (
