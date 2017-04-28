aa16e084ae459471ebd249ebfb364582bc790fad
Tom Molesworth <tom@binary.com>
2017-04-28 13:08:35 +0800

    Fix typo in ->_pricing_args lookup

--- lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
+++ lib/BOM/Product/Pricing/Engine/Intraday/Forex.pm
@@ -278,7 +278,7 @@ sub _tentative_events_markup {
             }
         }
 
-        my %args = (map { $_ => $bet->pricing_args->{$_} } qw(spot t payouttime_code));
+        my %args = (map { $_ => $bet->_pricing_args->{$_} } qw(spot t payouttime_code));
 
         my $engine = Pricing::Engine::Intraday::Forex::Base->new(
             ticks                => $self->ticks_for_trend,
