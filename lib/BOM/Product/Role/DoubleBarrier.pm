426819ffb484f5d974d39b33602d3b81f0ce560e
Yng Shan <yngshan@binary.com>
2017-05-04 03:51:36 +0000

    fix the identical barrier issue

--- lib/BOM/Product/Role/DoubleBarrier.pm
+++ lib/BOM/Product/Role/DoubleBarrier.pm
@@ -26,6 +26,11 @@ sub BUILD {
                 message           => 'High and low barriers must be different',
                 message_to_client => [$ERROR_MAPPING->{SameBarriersNotAllowed}],
             });
+            # these are dummy barriers that so that the calculation will not die
+            my $new_low_barrier  = $self->make_barrier($barrier1->as_absolute - $self->pip_size);
+            my $new_high_barrier = $self->make_barrier($barrier1->as_absolute + $self->pip_size);
+            $self->low_barrier($new_low_barrier);
+            $self->high_barrier($new_high_barrier);
         }
     }
 
