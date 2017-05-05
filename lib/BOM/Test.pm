d3a853c5fe5bd552de41379959ed320659fb0543
Chylli <chylli@binary.com>
2017-05-05 09:08:19 +0800

    add pricing url

--- lib/BOM/Test.pm
+++ lib/BOM/Test.pm
@@ -73,6 +73,7 @@ BEGIN {
 
         $ENV{DB_POSTFIX} = '_test';
         $ENV{RPC_URL}    = 'http://127.0.0.1:15005/';
+        $ENV{PRICING_URL} = 'http://127.0.0.1:15006/';
     }
     $ENV{TEST_DATABASE} = 1;    ## no critic (RequireLocalizedPunctuationVars)
 }
