c3bf44b2489ee90a3b4f788425c8ab5504e156c1
Chylli <chylli@binary.com>
2017-05-08 08:53:27 +0800

    no name

--- lib/BOM/Test/RPC/PricingRpc.pm
+++ lib/BOM/Test/RPC/PricingRpc.pm
@@ -8,7 +8,6 @@ my $service;
 BEGIN {
     if ($ENV{PRICING_RPC_URL}) {
         $service = BOM::Test::RPC::Service->new({
-            name   => 'pricing-rpc',
             url    => $ENV{PRICING_RPC_URL},
             script => '/home/git/regentmarkets/cpan/local/bin/hypnotoad /home/git/regentmarkets/bom-pricing/bin/binary_pricing_rpc.pl'
         });
