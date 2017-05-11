10d7cd5be4f9f99fcf16aae6d191ddb98c38cb09
Boris Blinov <boris.blinov@binary.com>
2017-05-11 10:14:52 +0000

    move to ENV{MOCKTIME}

--- bin/binary_pricing_rpc.pl
+++ bin/binary_pricing_rpc.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
+use if $ENV{MOCKTIME}, 'BOM::Test::Time';
 
 # Start command line interface for application
 require Mojolicious::Commands;
