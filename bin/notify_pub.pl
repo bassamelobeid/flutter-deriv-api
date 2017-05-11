62980a92096d13f4ed4f721025ca50cd5edb5076
Chylli <chylli@binary.com>
2017-05-11 08:30:45 +0800

    add pid file

--- bin/notify_pub.pl
+++ bin/notify_pub.pl
@@ -2,5 +2,13 @@
 use strict;
 use warnings;
 use BOM::Platform::Script::NotifyPub;
+use Getopt::Long;
+use Path::Tiny;
 
+my $pid_file;
+GetOptions("pid-file=s" => \$pid_file) || die "Bad options";
+if ($pid_file) {
+    $pid_file = Path::Tiny($pid_file);
+    $pid_file->spew($$);
+}
 exit BOM::Platform::Script::NotifyPub::run();
