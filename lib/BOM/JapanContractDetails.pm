d3651157e093e1ffc96e5ab68a6fb8c3f94c05b4
Yng Shan <yngshan@binary.com>
2017-05-15 07:05:57 +0000

    tidy code

--- lib/BOM/JapanContractDetails.pm
+++ lib/BOM/JapanContractDetails.pm
@@ -272,12 +272,10 @@ sub _get_pricing_parameter_from_IH_pricer {
 
     my $intraday_delta_correction = $pe->base_probability->peek_amount('intraday_delta_correction');
     $pricing_parameters->{intraday_delta_correction} = {
-        short_term_delta_correction => $contract->get_time_to_expiry->minutes < 10
-        ? $intraday_delta_correction
+          short_term_delta_correction => $contract->get_time_to_expiry->minutes < 10 ? $intraday_delta_correction
         : $contract->get_time_to_expiry->minutes > 20 ? 0
         : $pe->base_probability->peek_amount('delta_correction_short_term_value'),
-        long_term_delta_correction => $contract->get_time_to_expiry->minutes > 20
-        ? $intraday_delta_correction
+        long_term_delta_correction => $contract->get_time_to_expiry->minutes > 20 ? $intraday_delta_correction
         : $contract->get_time_to_expiry->minutes < 10 ? 0
         :                                               $pe->base_probability->peek_amount('delta_correction_long_term_value'),
     };
