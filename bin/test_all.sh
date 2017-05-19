6d3b48777145e6a25758612738876e2a119117fe
Chylli <chylli@binary.com>
2017-05-19 15:15:41 +0800

    fix typo

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -2,7 +2,8 @@
 MAIN_DIR=/home/git/regentmarkets
 for d in `ls $MAIN_DIR`
 do
-    if [ -f $MAIN_DIR/$d/Makefile && grep '^test:$']
+    MAKEFILE=$MAIN_DIR/$d/Makefile
+    if [ -f $MAKEFILE ] && grep '^test:$' $MAKEFILE
     then
         (cd $d; make test)
     fi
