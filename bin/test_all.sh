626df13d933e0a8ce8d87c8bb03d14c50db35e57
Chylli <chylli@binary.com>
2017-05-19 15:22:22 +0800

    workdir

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -2,9 +2,10 @@
 MAIN_DIR=/home/git/regentmarkets
 for d in `ls $MAIN_DIR`
 do
-    MAKEFILE=$MAIN_DIR/$d/Makefile
+    WORKDIR=$MAIN_DIR/$d
+    MAKEFILE=$WORKDIR/Makefile
     if [ -f $MAKEFILE ] && grep '^test:$' $MAKEFILE
     then
-        (cd $d; make test)
+        (cd $WORKDIR && make test)
     fi
 done
