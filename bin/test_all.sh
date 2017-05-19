f0868137552390bf152b42eee7fdbe19458e78c8
Chylli <chylli@binary.com>
2017-05-19 15:32:19 +0800

    -q

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -4,7 +4,7 @@ for d in `ls $MAIN_DIR`
 do
     WORKDIR=$MAIN_DIR/$d
     MAKEFILE=$WORKDIR/Makefile
-    if [ -f $MAKEFILE ] && grep '^test:$' $MAKEFILE
+    if [ -f $MAKEFILE ] && grep -q '^test:$' $MAKEFILE
     then
         (cd $WORKDIR && make test)
     fi
