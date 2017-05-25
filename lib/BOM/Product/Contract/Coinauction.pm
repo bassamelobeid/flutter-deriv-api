d4dc74216ef53cd916aaa898301b2566831adede
Yng Shan <yngshan@binary.com>
2017-05-25 08:20:52 +0000

    fix bug

--- lib/BOM/Product/Contract/Coinauction.pm
+++ lib/BOM/Product/Contract/Coinauction.pm
@@ -27,10 +27,11 @@ has [qw(trading_period_start number_of_tokens token_type coin_address ask_price)
     is => 'rw',
 );
 
-has [qw(app_markup_dollar_amount payout)]=> {
-    is   => 'ro',
+has [qw(app_markup_dollar_amount payout)] => (
+    is      => 'ro',
+    isa     => 'Num',
     default => 0,
-};
+);
 
 has underlying => (
     is       => 'ro',
