7a79eb56c72f8f3b2785908e413f1a06d75be148
Shahrizal Shaari <shahrizal@binary.com>
2017-05-29 02:01:22 +0000

    Refactoring into Binary NonBinary

---
+++ lib/BOM/Product/Role/Binary.pm
@@ -0,0 +1,11 @@
+package BOM::Product::Role::Binary;
+
+use Moose::Role;
+use Time::Duration::Concise;
+
+sub is_atm_bet {
+
+    return 1;
+}
+
+1;
---
+++ lib/BOM/Product/Role/NonBinary.pm
@@ -0,0 +1,6 @@
+package BOM::Product::Role::NonBinary;
+
+use Moose::Role;
+use Time::Duration::Concise;
+
+1;
