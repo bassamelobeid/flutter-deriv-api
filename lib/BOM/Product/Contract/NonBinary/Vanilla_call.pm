f32cca57663784b62c9d333e4bdf3255633ae92a
Shahrizal Shaari <shahrizal@binary.com>
2017-05-29 05:03:42 +0000

    move vanilla call as non binary

---
+++ lib/BOM/Product/Contract/NonBinary/Vanilla_call.pm
@@ -0,0 +1,23 @@
+package BOM::Product::Contract::Vanilla_call;
+
+use Moose;
+extends 'BOM::Product::Contract';
+with 'BOM::Product::Role::NonBinary', 'BOM::Product::Role::SingleBarrier';
+
+use BOM::Product::Pricing::Engine::BlackScholes;
+
+sub ticks_to_expiry {
+    die 'no ticks_to_expiry on a VANILLA_CALL contract';
+}
+
+sub _build_pricing_engine_name {
+    return 'BOM::Product::Pricing::Engine::BlackScholes';
+}
+
+sub _build_pricing_engine {
+    return BOM::Product::Pricing::Engine::BlackScholes->new({bet => shift});
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
