24e14a59d55fb08f209d0ab7542f25fc15385e52
Raunak Kathuria <raunakkathuria@gmail.com>
2017-05-29 07:46:44 +0000

    format limits as per standard

--- lib/BOM/RPC/v3/Cashier.pm
+++ lib/BOM/RPC/v3/Cashier.pm
@@ -383,10 +383,12 @@ sub get_limits {
     my $wl_config       = $payment_limits->{withdrawal_limits}->{$landing_company};
 
     my $limit = +{
-        account_balance => $client->get_limit_for_account_balance,
-        payout          => $client->get_limit_for_payout,
-        payout_per_symbol_and_contract_type =>
-            BOM::Platform::Config::quants->{bet_limits}->{open_positions_payout_per_symbol_and_bet_type_limit}->{$client->currency},
+        account_balance                     => sprintf('%' . get_amount_precision($client->currency) . 'f', $client->get_limit_for_account_balance),
+        payout                              => sprintf('%' . get_amount_precision($client->currency) . 'f', $client->get_limit_for_payout),
+        payout_per_symbol_and_contract_type => sprintf(
+            '%' . get_amount_precision($client->currency) . 'f',
+            BOM::Platform::Config::quants->{bet_limits}->{open_positions_payout_per_symbol_and_bet_type_limit}->{$client->currency}
+        ),
         open_positions => $client->get_limit_for_open_positions,
     };
 
@@ -411,7 +413,7 @@ sub get_limits {
 
     $limit->{num_of_days}       = $numdays;
     $limit->{num_of_days_limit} = $numdayslimit;
-    $limit->{lifetime_limit}    = $lifetimelimit;
+    $limit->{lifetime_limit}    = sprintf('%' . get_amount_precision($client->currency) . 'f', $lifetimelimit);
 
     if (not $client->client_fully_authenticated) {
         # withdrawal since $numdays
@@ -420,27 +422,20 @@ sub get_limits {
             start_time => Date::Utility->new(Date::Utility->new->epoch - 86400 * $numdays),
             exclude    => ['currency_conversion_transfer'],
         });
-        $withdrawal_for_x_days = sprintf(
-            '%' . get_amount_precision($withdrawal_limit_curr) . 'f',
-            amount_from_to_currency($withdrawal_for_x_days, $client->currency, $withdrawal_limit_curr));
+        $withdrawal_for_x_days = amount_from_to_currency($withdrawal_for_x_days, $client->currency, $withdrawal_limit_curr);
 
         # withdrawal since inception
-        my $withdrawal_since_inception = $payment_mapper->get_total_withdrawal({exclude => ['currency_conversion_transfer']});
-        $withdrawal_since_inception = sprintf(
-            '%' . get_amount_precision($withdrawal_limit_curr) . 'f',
-            amount_from_to_currency($withdrawal_since_inception, $client->currency, $withdrawal_limit_curr));
+        my $withdrawal_since_inception = amount_from_to_currency($payment_mapper->get_total_withdrawal({exclude => ['currency_conversion_transfer']}),
+            $client->currency, $withdrawal_limit_curr);
 
-        $limit->{withdrawal_since_inception_monetary} = $withdrawal_since_inception;
-        $limit->{withdrawal_for_x_days_monetary}      = $withdrawal_for_x_days;
-
-        my $remainder = sprintf(
-            '%' . get_amount_precision($withdrawal_limit_curr) . 'f',
-            min(($numdayslimit - $withdrawal_for_x_days), ($lifetimelimit - $withdrawal_since_inception)));
+        my $remainder = min(($numdayslimit - $withdrawal_for_x_days), ($lifetimelimit - $withdrawal_since_inception));
         if ($remainder < 0) {
             $remainder = 0;
         }
 
-        $limit->{remainder} = $remainder;
+        $limit->{withdrawal_since_inception_monetary} = sprintf('%' . get_amount_precision($client->currency) . 'f', $withdrawal_since_inception);
+        $limit->{withdrawal_for_x_days_monetary}      = sprintf('%' . get_amount_precision($client->currency) . 'f', $withdrawal_for_x_days);
+        $limit->{remainder}                           = sprintf('%' . get_amount_precision($client->currency) . 'f', $remainder);
     }
 
     return $limit;
