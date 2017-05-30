b3a3a1eb3353e65c2b22a446e82e8761971fda2a
Shahrizal Shaari <shahrizal@binary.com>
2017-05-30 06:43:26 +0000

    build tick for spot min and max

--- lib/BOM/Product/Role/NonBinary.pm
+++ lib/BOM/Product/Role/NonBinary.pm
@@ -3,4 +3,25 @@ package BOM::Product::Role::NonBinary;
 use Moose::Role;
 use Time::Duration::Concise;
 
+has ticks_for_lookbacks => (
+    is         => 'ro',
+    lazy_build => 1,
+);
+
+sub _build_ticks_for_lookbacks {
+
+    my $self      = shift;
+    my $now       = $self->date_pricing->epoch;
+    my $end_epoch = $now > $self->date_expiry->epoch ? $self->date_expiry->epoch : $now;
+
+    my @ticks_since_start = @{
+        $self->underlying->ticks_in_between_start_end({
+                start_time => $self->date_start->epoch,
+                end_time   => $end_epoch,
+            })};
+
+    return \@ticks_since_start;
+
+}
+
 1;
