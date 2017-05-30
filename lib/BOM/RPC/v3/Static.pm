1a950d70fb4df7200f7b18953d2c9563431cd139
Raunak Kathuria <raunakkathuria@gmail.com>
2017-05-30 03:35:47 +0000

    pass as hashref not hash

--- lib/BOM/RPC/v3/Static.pm
+++ lib/BOM/RPC/v3/Static.pm
@@ -61,7 +61,7 @@ sub website_status {
         api_call_limits          => BOM::RPC::v3::Utility::site_limits,
         clients_country          => $params->{country_code},
         supported_languages      => BOM::Platform::Runtime->instance->app_config->cgi->supported_languages,
-        currencies_config        => map { $_ => substr $_, -1 } Price::Calculator::get_precision_config()->{amount},
+        currencies_config        => [map { $_ => substr($_, -1) } Price::Calculator::get_precision_config()->{amount}],
     };
 }
 
