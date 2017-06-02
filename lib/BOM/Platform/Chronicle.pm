c50477cf61a5a80041b0b15928ff487bbed55ff4
Chylli <chylli@binary.com>
2017-06-02 09:10:51 +0800

    [ci skip] fix error

--- lib/BOM/Platform/Chronicle.pm
+++ lib/BOM/Platform/Chronicle.pm
@@ -271,7 +271,7 @@ sub _dbic {
     # Silently ignore if there is not configuration for Pg chronicle (e.g. in Travis)
     return undef if not defined _config()->{chronicle};
     state $dbh_addr;
-    $dbic //= DBI->connect(
+    $dbic //= DBIx::Connector->new(
         '' . _dbh_dsn(),
         # User and password are part of the DSN
         '', '',
