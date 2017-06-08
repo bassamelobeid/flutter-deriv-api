8f501d272504776e6e58a18e72a9b51c803a566b
Nael Alolwani <nael@binary.com>
2017-06-08 19:40:23 +0800

    removed datadog tag

--- bin/clear_no_expiry_limits.pl
+++ bin/clear_no_expiry_limits.pl
@@ -8,8 +8,8 @@ my @keys = @{$redis->keys("*")};
 for (grep /rate_limits::/, @keys) {
     my $exp = $redis->ttl($_);
     if($exp == -1) {
-        DataDog::DogStatsd::Helper::stats_inc('bom_websocket_api.v_3.bad_expiry.count',
-                {tags => ['tag:'   ]}); #what tag to use.
+        #what tag to use, I removed tag as i think that we only need to know how many times this is happening.
+        DataDog::DogStatsd::Helper::stats_inc('bom_websocket_api.v_3.bad_expiry.count');
         $redis->del($_);
     }
 }
