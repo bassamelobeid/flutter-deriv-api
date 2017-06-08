a6bd67b17ad57d21268b1469f9a0e7e18e6af049
Chylli <chylli@binary.com>
2017-06-08 09:42:03 +0800

    replace

--- lib/BOM/Database/Model/AccessToken.pm
+++ lib/BOM/Database/Model/AccessToken.pm
@@ -3,13 +3,13 @@ package BOM::Database::Model::AccessToken;
 use Moose;
 use BOM::Database::AuthDB;
 
-has 'dbh' => (
+has 'dbic' => (
     is         => 'ro',
     lazy_build => 1,
 );
 
-sub _build_dbh {
-    return BOM::Database::AuthDB::rose_db->dbh;
+sub _build_dbic {
+    return BOM::Database::AuthDB::rose_db->dbic;
 }
 
 sub __parse_array {
@@ -21,6 +21,7 @@ sub __parse_array {
 
 my @token_scopes = ('read', 'trade', 'payments', 'admin');
 my %available_scopes = map { $_ => 1 } @token_scopes;
+
 sub __filter_valid_scopes {
     my $s = shift;
     return [grep { $available_scopes{$_} } @$s];
@@ -31,8 +32,8 @@ sub create_token {
 
     $scopes = __filter_valid_scopes($scopes);
 
-    my $dbh = $self->dbh;
-    my ($token) = $dbh->selectrow_array("SELECT auth.create_token(15, ?, ?, ?, ?)", undef, $loginid, $display_name, $scopes, $ip);
+    my $dbic = $self->dbic;
+    my ($token) = $dbic->run(sub { $_->selectrow_array("SELECT auth.create_token(15, ?, ?, ?, ?)", undef, $loginid, $display_name, $scopes, $ip) });
 
     return $token;
 }
@@ -40,7 +41,8 @@ sub create_token {
 sub get_token_details {
     my ($self, $token) = @_;
 
-    my $details = $self->dbh->selectrow_hashref(<<'SQL', undef, $token);
+    my $details = $self->dbic->run(sub {
+            ->selectrow_hashref(<<'SQL', undef, $token) });
 SELECT loginid, creation_time, scopes, display_name, last_used, valid_for_ip
   FROM auth.get_token_details($1)
 SQL
@@ -52,12 +54,15 @@ SQL
 sub get_scopes_by_access_token {
     my ($self, $access_token) = @_;
 
-    my $sth = $self->dbh->prepare("
+    my $scopes = $self->dbic->run(
+        sub {
+            my $sth = $_->prepare("
         SELECT scopes FROM auth.access_token
         WHERE token = ?
     ");
-    $sth->execute($access_token);
-    my $scopes = $sth->fetchrow_array;
+            $sth->execute($access_token);
+            $sth->fetchrow_array;
+        });
     $scopes = __parse_array($scopes);
     return @$scopes;
 }
@@ -66,57 +71,66 @@ sub get_tokens_by_loginid {
     my ($self, $loginid) = @_;
 
     my @tokens;
-    my $sth = $self->dbh->prepare("
+    $self->dbic->run(
+        sub {
+
+            my $sth = $_->prepare("
         SELECT
             token, display_name, scopes, last_used::timestamp(0), valid_for_ip
         FROM auth.access_token WHERE client_loginid = ? ORDER BY display_name
     ");
-    $sth->execute($loginid);
-    while (my $r = $sth->fetchrow_hashref) {
-        $r->{scopes} = __parse_array($r->{scopes});
-        push @tokens, $r;
-    }
-
+            $sth->execute($loginid);
+            while (my $r = $sth->fetchrow_hashref) {
+                $r->{scopes} = __parse_array($r->{scopes});
+                push @tokens, $r;
+            }
+        });
     return wantarray ? @tokens : \@tokens;
 }
 
 sub get_token_count_by_loginid {
     my ($self, $loginid) = @_;
 
-    return $self->dbh->selectrow_array(
-        "SELECT COUNT(*) FROM auth.access_token WHERE client_loginid = ?", undef, $loginid
-    );
+    return $self->dbic->run(
+        sub {
+            $_->selectrow_array("SELECT COUNT(*) FROM auth.access_token WHERE client_loginid = ?", undef, $loginid);
+        });
 }
 
 sub is_name_taken {
     my ($self, $loginid, $display_name) = @_;
 
-    return $self->dbh->selectrow_array(
-        "SELECT 1 FROM auth.access_token WHERE client_loginid = ? AND display_name = ?", undef, $loginid, $display_name
-    );
+    return $self->dbic->run(
+        sub {
+            $_->selectrow_array("SELECT 1 FROM auth.access_token WHERE client_loginid = ? AND display_name = ?", undef, $loginid, $display_name);
+        });
 }
 
 sub remove_by_loginid {
     my ($self, $client_loginid) = @_;
 
-    return $self->dbh->do(
-        "DELETE FROM auth.access_token WHERE client_loginid = ?", undef, $client_loginid
-    );
+    return $self->dbic->run(
+        sub {
+            $_->do("DELETE FROM auth.access_token WHERE client_loginid = ?", undef, $client_loginid);
+        });
 }
 
 sub remove_by_token {
     my ($self, $token, $loginid) = @_;
 
-    return $self->dbh->do(
-        "DELETE FROM auth.access_token WHERE token = ? and client_loginid = ?", undef, $token, $loginid
-    );
+    return $self->dbic->run(
+        sub {
+            $_->do("DELETE FROM auth.access_token WHERE token = ? and client_loginid = ?", undef, $token, $loginid);
+        });
 }
 
 sub get_all_tokens_by_loginid {
     my ($self, $loginid) = @_;
 
     my @tokens;
-    my $sth = $self->dbh->prepare('
+    $self->dbic->run(
+        sub {
+            my $sth = $_->prepare('
         SELECT
             access_token
         FROM
@@ -131,10 +145,11 @@ sub get_all_tokens_by_loginid {
         WHERE
             client_loginid = $1;
     ');
-    $sth->execute($loginid);
-    while (my $r = $sth->fetchrow_arrayref) {
-        push @tokens, $r->[0];
-    }
+            $sth->execute($loginid);
+            while (my $r = $sth->fetchrow_arrayref) {
+                push @tokens, $r->[0];
+            }
+        });
 
     return wantarray ? @tokens : \@tokens;
 }
