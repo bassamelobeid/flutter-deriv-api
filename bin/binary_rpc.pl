b1fdc573c7551fa14d7bfc5fe90853d219116faf
Boris Blinov <boris.blinov@binary.com>
2017-06-08 08:27:44 +0000

    travis update

--- bin/binary_rpc.pl
+++ bin/binary_rpc.pl
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-use if $ENV{MOCKTIME}, 'BOM::Test::Time';
 
 # Start command line interface for application
 require Mojolicious::Commands;
