a5d8b662ec829415ce499289a3ead215a37fad60
Chylli <chylli@binary.com>
2017-06-08 08:58:24 +0800

    replace dbh with dbic

--- lib/BOM/Database/Model/UserConnect.pm
+++ lib/BOM/Database/Model/UserConnect.pm
@@ -4,13 +4,13 @@ use Moose;
 use BOM::Database::UserDB;
 use JSON;
 
-has 'dbh' => (
+has 'dbic' => (
     is         => 'ro',
     lazy_build => 1,
 );
 
-sub _build_dbh {
-    return BOM::Database::UserDB::rose_db->dbh;
+sub _build_dbic {
+    return BOM::Database::UserDB::rose_db->dbic;
 }
 
 sub insert_connect {
@@ -25,18 +25,24 @@ sub insert_connect {
         if ($connected_user_id != $user_id) {
             return {error => 'CONNECTED_BY_OTHER'};
         }
-        $self->dbh->do("
+        $self->dbic->run(
+            sub {
+                $_->do("
             UPDATE users.binary_user_connects
             SET provider_data = ?, date=NOW()
             WHERE binary_user_id = ? AND provider = ?
         ", undef, encode_json($provider_data), $user_id, $provider);
+            });
     } else {
-        $self->dbh->do("
+        $self->dbic->run(
+            sub {
+                $_->do("
             INSERT INTO users.binary_user_connects
                 (binary_user_id, provider, provider_identity_uid, provider_data)
             VALUES
                 (?, ?, ?, ?)
         ", undef, $user_id, $provider, $provider_identity_uid, encode_json($provider_data));
+            });
     }
 
     return {success => 1};
@@ -48,20 +54,26 @@ sub get_user_id_by_connect {
     my $provider              = $provider_data->{user}->{identity}->{provider};
     my $provider_identity_uid = $provider_data->{user}->{identity}->{provider_identity_uid};
 
-    return $self->dbh->selectrow_array("
+    return $self->dbic->run(
+        sub {
+            $_->selectrow_array("
         SELECT binary_user_id FROM users.binary_user_connects WHERE provider = ? AND provider_identity_uid = ?
     ", undef, $provider, $provider_identity_uid);
+        });
 }
 
 sub get_connects_by_user_id {
     my ($self, $user_id) = @_;
 
     my @providers;
-    my $sth = $self->dbh->prepare("SELECT provider FROM users.binary_user_connects WHERE binary_user_id = ?");
-    $sth->execute($user_id);
-    while (my ($p) = $sth->fetchrow_array) {
-        push @providers, $p;
-    }
+    $self->dbic->run(
+        sub {
+            my $sth = $_->prepare("SELECT provider FROM users.binary_user_connects WHERE binary_user_id = ?");
+            $sth->execute($user_id);
+            while (my ($p) = $sth->fetchrow_array) {
+                push @providers, $p;
+            }
+        });
 
     return wantarray ? @providers : [@providers];
 }
@@ -69,9 +81,12 @@ sub get_connects_by_user_id {
 sub remove_connect {
     my ($self, $user_id, $provider) = @_;
 
-    return $self->dbh->do("
+    return $self->dbic->run(
+        sub {
+            $_->do("
         DELETE FROM users.binary_user_connects WHERE binary_user_id = ? AND provider = ?
     ", undef, $user_id, $provider);
+        });
 }
 
 no Moose;
