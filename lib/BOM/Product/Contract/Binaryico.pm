3fc83fdd2a5c5dd8b0a59afed83fabd5630ab5f5
Yng Shan <yngshan@binary.com>
2017-06-09 04:19:18 +0000

    do not pass in date start and date expiry for ICO as there is none

--- lib/BOM/Product/Contract/Binaryico.pm
+++ lib/BOM/Product/Contract/Binaryico.pm
@@ -3,7 +3,7 @@ package BOM::Product::Contract::Binaryico;
 use Moose;
 extends 'BOM::Product::Contract::Coinauction';
 
-sub code { return 'BinaryICO' }
+sub code { return 'Binaryico' }
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
