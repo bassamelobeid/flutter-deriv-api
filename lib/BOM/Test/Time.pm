72511e2a36a948fcd0fa9324abf3e63d162ef1a5
Boris Blinov <boris.blinov@binary.com>
2017-06-09 08:31:50 +0000

    comment added

--- lib/BOM/Test/Time.pm
+++ lib/BOM/Test/Time.pm
@@ -9,9 +9,25 @@ use Date::Utility;
 use Exporter qw( import );
 our @EXPORT_OK = qw( set_date set_date_from_file );
 
-#
-# simple module to provide mocked time
-#
+=head
+
+The logic on mocking time in RPC and PricingRPC follows.
+
+RPC.pm code can be called in two ways:
+
+- through normal way - via bin/binary_rpc.pl or binary_pricing_rpc.pl. In theese scripts we're checking
+   if there is $ENV{MOCKTIME} defined. If it is - we load BOM::Test::Time and have mocked time.
+   MOCKTIME is set only when services are started via BOM::Test::Service::BomRpc or PricingRpc.
+
+- in-process from bom-rpc tests. Here we already have BOM::Test::Time module loaded and don't have $ENV{MOCKTIME}
+
+
+During handling RPC calls in bom-rpc and bom-pricing code we check if BOM::Test::Time is loaded though checking %INC
+and if yes, then set time to the requested one via set_date_from_file().
+
+=cut
+
+
 
 our $mocked_time_file = '/tmp/mocked_time';
 
