3aa09681ec240c0a7d6f3ba9a4662fa91ef2544a
Nael Alolwani <nael@binary.com>
2017-06-12 14:51:15 +0800

    enhancements

--- bin/clear_no_expiry_limits.pl
+++ bin/clear_no_expiry_limits.pl
@@ -1,5 +1,5 @@
 #---------------------------------------------------------
-# Purpose: This script will delete all the rate limit keys that have TTL -1 (dont have expiry time)
+# Purpose: Since we set retelimit keys in two atomic operation(one to set key and other to set TTL), under heavy load it might result of having rate_limit keys without TTL, This script will delete all the rate limit keys that doesnt have TTL set (dont have expiry time)
 # Usage:   This script should be set to run as a cron job
 #---------------------------------------------------------
 use strict;
@@ -8,8 +8,8 @@ use DataDog::DogStatsd::Helper;
 use Binary::WebSocketAPI::v3::Instance::Redis qw(ws_redis_master);
 
 my $redis = ws_redis_master();
-my @keys = @{$redis->keys("*")};
-for (grep /rate_limits::/, @keys) {
+
+for (@{$redis->scan_all(MATCH => 'rate_limits::*', COUNT => 20000)}) {
     my $exp = $redis->ttl($_);
     if($exp == -1) {
         #no need to use any tag here
