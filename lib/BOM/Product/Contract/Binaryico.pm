838f54439fb6073945b5104cee83c75c120f044d
Yng Shan <yngshan@binary.com>
2017-06-16 06:21:31 +0000

    the code should be in capital letter

--- lib/BOM/Product/Contract/Binaryico.pm
+++ lib/BOM/Product/Contract/Binaryico.pm
@@ -3,7 +3,7 @@ package BOM::Product::Contract::Binaryico;
 use Moose;
 extends 'BOM::Product::Contract::Coinauction';
 
-sub code { return 'Binaryico' }
+sub code { return 'BINARYICO' }
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
