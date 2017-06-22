8d2c0a1d03c0a72ba9923adfc6fde806523eabca
Yng Shan <yngshan@binary.com>
2017-06-22 05:44:20 +0000

    fix typo

--- lib/BOM/Database/DataMapper/FinancialMarketBet.pm
+++ lib/BOM/Database/DataMapper/FinancialMarketBet.pm
@@ -270,7 +270,7 @@ sub _fmb_rose_to_fmb_model {
         $param->{'spread_bet_record'} = $rose_object->spread_bet;
         $model_class = 'BOM::Database::Model::FinancialMarketBet::SpreadBet';
     } elsif ($rose_object->bet_class eq $BOM::Database::Model::Constants::BET_CLASS_COINAUCTION_BET) {
-        $param->{'spread_bet_record'} = $rose_object->coinauction_bet;
+        $param->{'coinauction_bet_record'} = $rose_object->coinauction_bet;
         $model_class = 'BOM::Database::Model::FinancialMarketBet::CoinauctionBet';
     }else {
         Carp::croak('UNSUPPORTED rose_object class [' . $rose_object->bet_class . ']');
