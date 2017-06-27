4aebb559652e3205bd49abff4420019dacfd43e7
Sasha Murzin <sasha@binary.com>
2017-06-27 09:46:02 +0000

    test all variants from contracts_for

--- v3/109_proposal_array.pl
+++ v3/109_proposal_array.pl
@@ -5,6 +5,7 @@ use Date::Utility;
 use FindBin qw/$Bin/;
 use lib "$Bin/../lib";
 use Net::EmptyPort qw(empty_port);
+use Data::Dumper;
 
 use Test::More;
 use Test::Deep;
@@ -37,6 +38,15 @@ my $pt       = 'multi_barrier';
 my $symbol   = "frxUSDJPY";
 
 my $now = Date::Utility->new;
+
+my $contract_type_pairs = {
+    "PUT"           => "CALLE",
+    "ONETOUCH"      => "NOTOUCH",
+    "EXPIRYMISS"    => "EXPIRYRANGEE",
+    "RANGE"         => "UPORDOWN",
+};
+
+my $trading_frames = {};
 
 #build_test_frxUSDJPY_data();
 generate_trading_periods('frxUSDJPY');
@@ -82,7 +92,6 @@ BOM::Test::Data::Utility::FeedTestDatabase::create_tick({
 my $t = build_wsapi_test();
 
 
-
 my $contracts_for = $t->await::contracts_for( {
         "contracts_for"     => $symbol,
         "currency"          => $currency,
@@ -90,9 +99,35 @@ my $contracts_for = $t->await::contracts_for( {
         "product_type"      => $pt,
 });
 
+
+
+my $contract_types = {};
+@{$contract_types}{keys %$contract_type_pairs, values %$contract_type_pairs} = undef;
+note explain $contract_types;
+my $proposal_array_variants = {};
+for my $i (0 .. $#{$contracts_for->{contracts_for}{available}}) {
+    my $tf = $contracts_for->{contracts_for}{available}[$i]{trading_period}{duration};
+    $trading_frames->{$tf} = 1;
+    my $ct = $contracts_for->{contracts_for}{available}[$i]{contract_type};
+    next unless exists $contract_type_pairs->{$ct};
+    my $key = $ct.'_'.$contract_type_pairs->{$ct}.'_'.$tf;
+    next if exists $proposal_array_variants->{$key};
+    $proposal_array_variants->{$key} = {
+        available_barriers      => $contracts_for->{contracts_for}{available}[$i]{available_barriers},
+        barriers                => $contracts_for->{contracts_for}{available}[$i]{barriers},
+        date_expiry             => $contracts_for->{contracts_for}{available}[$i]{trading_period}{date_expiry}{epoch},
+        trading_period_start    => $contracts_for->{contracts_for}{available}[$i]{trading_period}{date_start}{epoch},
+    };
+    #$contracts_for->{contracts_for}{available}[$i] = undef if exists $contract_types->{$contracts_for->{contracts_for}{available}[$i]{contract_type}};
+}
+
+#note explain $contracts_for;
+note explain $trading_frames;
+note "#############" x 3;
+note explain $proposal_array_variants;
+
 my $put = [grep { $_->{contract_type} eq 'PUT' and $_->{trading_period}{duration} eq '2h15m'} @{$contracts_for->{contracts_for}{available}}]->[0];
 
-note explain $put;
 
 my $barriers = $put->{available_barriers};
 my $fixed_bars= [map {{barrier=>$_}} @$barriers];
@@ -130,6 +165,45 @@ my $proposal_array_req = {
 my $response = $t->await::proposal_array($proposal_array_req);
 
 note explain $response;
+
+test_schema('proposal_array', $response);
+
+my $proposal_array_req_tpl = {
+    'symbol' => 'frxUSDJPY',
+    'req_id' => '1',
+    'subscribe' => '1',
+    'barriers' => undef,
+    'date_expiry' => undef,
+    'currency' => 'JPY',
+    'amount' => '100',
+    'trading_period_start' => undef,
+    'basis' => 'payout',
+    'proposal_array' => '1',
+    'contract_type' => undef,
+    'passthrough' => {}
+};
+
+for my $key (keys %$proposal_array_variants) {
+    my ($ct1, $ct2) = split '_', $key;
+    my $data = $proposal_array_variants->{$key};
+    my $fbarriers = [];
+    if ($data->{barriers} == 2) {
+        $fbarriers = [map { {barrier2 => $_->[0], barrier => $_->[1]} } @{$data->{available_barriers}}]
+    } else {
+        $fbarriers = [map { {barrier=>$_} } @{$data->{available_barriers}}];
+    }
+    note "key: $key";
+    $proposal_array_req_tpl->{barriers} = $fbarriers;
+    $proposal_array_req_tpl->{date_expiry} = $data->{date_expiry};
+    $proposal_array_req_tpl->{trading_period_start} = $data->{trading_period_start};
+    $proposal_array_req_tpl->{contract_type} = [$ct1, $ct2];
+    note explain $proposal_array_req_tpl;
+    note "==================================";
+    my $response = $t->await::proposal_array($proposal_array_req);
+    test_schema('proposal_array', $response);
+    note "===++++" x 5;
+}
+
 
 
 $t->finish_ok;
