d82448d5cb8a09d5b3be06e735d5d67cec8600ca
Chylli <chylli@binary.com>
2017-07-04 14:24:05 +0800

    perltiyd

--- t/run_quant_benchmark_test.pl
+++ t/run_quant_benchmark_test.pl
@@ -33,10 +33,10 @@ sub _benchmark_testing_setup {
     $Quant::Framework::Underlying::interest_rates_source = 'market';
 
     my $file_path = '/home/git/regentmarkets/bom-quant-benchmark/t/csv/interest_rates.csv';
-    my $csv = Text::CSV->new({ sep_char => ',' });
+    my $csv = Text::CSV->new({sep_char => ','});
 
     open(my $data, '<', $file_path) or die "Could not open '$file_path' $!\n";
-    my $dummy_line = <$data>;
+    my $dummy_line       = <$data>;
     my $chronicle_writer = BOM::Platform::Chronicle::get_chronicle_writer;
     while (my $line = <$data>) {
         chomp $line;
@@ -47,9 +47,9 @@ sub _benchmark_testing_setup {
             my $symbol = $fields[0];
             my %rates;
 
-            for(my $i=1;$i<scalar @fields;$i+=2) {
+            for (my $i = 1; $i < scalar @fields; $i += 2) {
                 my $tenor = $fields[$i];
-                my $rate = $fields[$i+1];
+                my $rate  = $fields[$i + 1];
 
                 $rates{$tenor} = $rate;
             }
@@ -58,13 +58,12 @@ sub _benchmark_testing_setup {
                 'interest_rates',
                 $symbol,
                 {
-                    symbol  => $symbol,
-                    type    => 'market',
-                    rates   => \%rates,
-                    date    => '2010-01-01T00:00:00Z'
+                    symbol => $symbol,
+                    type   => 'market',
+                    rates  => \%rates,
+                    date   => '2010-01-01T00:00:00Z'
                 },
-                                   Date::Utility->new("2010-01-01")
-                                  );
+                Date::Utility->new("2010-01-01"));
 
         } else {
             warn "Line could not be parsed: $line\n";
@@ -73,9 +72,7 @@ sub _benchmark_testing_setup {
 
     close $data;
 
-
     $chronicle_writer->set('partial_trading', 'late_opens', {}, Date::Utility->new("2010-01-01"));
-
 
     return 1;
 }
@@ -107,10 +104,10 @@ has test_suite_mapper => (
     isa     => 'HashRef',
     default => sub {
         {
-            merlin              => 'Runner::Merlin',
-            sdfx                => 'Runner::Superderivatives_FX',
-            sdeq                => 'Runner::Superderivatives_EQ',
-            ovra                => 'Runner::Bloomberg',
+            merlin => 'Runner::Merlin',
+            sdfx   => 'Runner::Superderivatives_FX',
+            sdeq   => 'Runner::Superderivatives_EQ',
+            ovra   => 'Runner::Bloomberg',
         };
     },
 );
@@ -156,26 +153,26 @@ sub script_run {
 sub analyse_report {
     my ($self, $report, $test) = @_;
 
-    my $benchmark = LoadFile('/home/git/regentmarkets/bom-quant-benchmark/t/benchmark.yml');
+    my $benchmark      = LoadFile('/home/git/regentmarkets/bom-quant-benchmark/t/benchmark.yml');
     my $test_benchmark = $benchmark->{$self->getOption('suite')}->{$test};
     foreach my $base_or_num (keys %$report) {
         foreach my $bet_type (keys %{$report->{$base_or_num}}) {
             subtest "$test benchmark" => sub {
                 cmp_ok(
-                    roundnear(0.0001,$report->{$base_or_num}->{$bet_type}->{avg}),
+                    roundnear(0.0001, $report->{$base_or_num}->{$bet_type}->{avg}),
                     '<=',
-                        roundnear(0.0001,$test_benchmark->{$base_or_num}->{$bet_type}->{avg}),
-                        "Avg mid diff of bet_type[$bet_type] for [$base_or_num] is within benchmark"
-                    );
-                    cmp_ok(
-                        roundnear(0.0001,$report->{$base_or_num}->{$bet_type}->{max}),
-                        '<=',
-                        roundnear(0.0001,$test_benchmark->{$base_or_num}->{$bet_type}->{max}),
-                        "Max mid diff of bet_type[$bet_type] for [$base_or_num] is within benchmark"
-                    );
-                };
-            }
+                    roundnear(0.0001, $test_benchmark->{$base_or_num}->{$bet_type}->{avg}),
+                    "Avg mid diff of bet_type[$bet_type] for [$base_or_num] is within benchmark"
+                );
+                cmp_ok(
+                    roundnear(0.0001, $report->{$base_or_num}->{$bet_type}->{max}),
+                    '<=',
+                    roundnear(0.0001, $test_benchmark->{$base_or_num}->{$bet_type}->{max}),
+                    "Max mid diff of bet_type[$bet_type] for [$base_or_num] is within benchmark"
+                );
+            };
         }
+    }
 }
 no Moose;
 __PACKAGE__->meta->make_immutable;
