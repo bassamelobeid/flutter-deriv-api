3ca00a842c9d614197ab6ebaba4ac85a277fae32
Dmitry Sirotkin <contact@dieser.me>
2017-07-10 03:50:51 +0000

    resolved merge conflict

--- lib/BOM/RPC/v3/Cashier.pm
+++ lib/BOM/RPC/v3/Cashier.pm
@@ -435,7 +435,7 @@ sub get_limits {
 
     $limit->{num_of_days}       = $numdays;
     $limit->{num_of_days_limit} = $numdayslimit;
-    $limit->{lifetime_limit}    = formatnumber('amount', $currency, $lifetimelimit) * 1;
+    $limit->{lifetime_limit}    = formatnumber('amount', $currency, $lifetimelimit);
 
     # withdrawal since $numdays
     my $payment_mapper = BOM::Database::DataMapper::Payment->new({client_loginid => $client->loginid});
