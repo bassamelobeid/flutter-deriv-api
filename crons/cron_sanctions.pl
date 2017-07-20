706bf3ab7ffe32dac69d81fac9a42835dac15012
Boris Blinov <boris.blinov@binary.com>
2017-07-20 09:20:56 +0000

    updated as per review

--- crons/cron_sanctions.pl
+++ crons/cron_sanctions.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 
+use Path::Tiny;
+
 use Brands;
 use Client::Account;
 use BOM::Database::ClientDB;
@@ -18,32 +20,30 @@ use BOM::Platform::Email qw(send_email);
 
 my $file_flag    = '/tmp/last_cron_sanctions_check_run';
 my $reports_path = '/home/nobody/compliance-sanctions-lists';
-my $brand        = Brands->new(name => 'binary');
+my @brokers      = qw/CR MF MLT MX/;
+
+my $brand = Brands->new(name => 'binary');
 
 my $last_run = (stat $file_flag)[9] // 0;
 $BOM::Platform::Client::Sanctions::sanctions->update_data();
 { open my $fh, '>', $file_flag; close $fh };
 exit if $last_run > $BOM::Platform::Client::Sanctions::sanctions->last_updated();
 
-my $brokers = ['CR', 'MF', 'MLT', 'MX'];
-my $matched;
-$matched->{$_} = do_broker($_) for @$brokers;
+my $matched = {map { $_ => get_matched_clients_by_broker($_) } @brokers};
 do_report($matched);
 
 sub do_report {
     my $matched = shift;
     my $headers = 'List Name,Database,LoginID,First Name,Last Name,Email,Phone,Gender,DateOfBirth,DateJoined,Residence,Citizen,Status,Reason';
     my $r       = '';
-    foreach my $k (keys %$matched) {
-        $r .= map_client_data($_) . "\n" for @{$matched->{$k}};
+    foreach my $k (sort keys %$matched) {
+        $r .= make_client_csv_line($_) . "\n" for sort { $a->[0]->loginid cmp $b->[0]->loginid } @{$matched->{$k}};
     }
     print $r;
 
-    my $csv_filename = $reports_path . '/sanctions-run-' . Date::Utility->new()->date . '.csv';
-    open my $fh, '>', $csv_filename;
-    print $fh $headers, "\n";
-    print $fh $r;
-    close $fh;
+    my $csv_file = path($reports_path . '/sanctions-run-' . Date::Utility->new()->date . '.csv');
+    $csv_file->spew_utf8($headers . "\n");
+    $csv_file->spew_utf8($r . "\n");
 
     send_email({
         from       => $brand->emails('support'),
@@ -51,21 +51,22 @@ sub do_report {
         cc         => 'sysadmin@binary.com',
         subject    => 'Sanction list checked',
         message    => ["Here is a list of clients against sanctions:\n$r"],
-        attachment => $csv_filename,
+        attachment => $csv_file->canonpath,
     });
     return;
 }
 
-sub map_client_data {
+sub make_client_csv_line {
     my ($c, $list) = @{+shift};
-    my $r = '';
-    $r .= $list . ',';
-    $r .= $c->$_ // '' . ',' foreach qw(broker loginid first_name last_name email phone gender date_of_birth date_joined residence citizen);
-    $r .= $_->status_code . ',' . $_->reason . ',' for ($c->client_status->[0]);    #use only last status
-    return $r;
+    my @fields = (
+        $list,
+        (map { $c->$_ // '' } qw(broker loginid first_name last_name email phone gender date_of_birth date_joined residence citizen)),
+        (map { $_->status_code, $_->reason } ($c->client_status->[0])),    #use only last status
+    );
+    return join(',', @fields);
 }
 
-sub do_broker {
+sub get_matched_clients_by_broker {
     my $broker = shift;
     my @matched;
     my $clients = BOM::Database::ClientDB->new({
@@ -83,14 +84,12 @@ sub do_broker {
 
     foreach my $c (@$clients) {
         my $client = Client::Account->new({loginid => $c});
-        my $list;
-        push @matched,
-            [$client, $list]
-            if $list = BOM::Platform::Client::Sanctions->new({
+        my $list = BOM::Platform::Client::Sanctions->new({
                 client     => $client,
                 brand      => $brand,
                 skip_email => 1,
             })->check();
+        push @matched, [$client, $list] if $list;
     }
     return \@matched;
 }
