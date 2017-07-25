c3721ec12f6b1d6cf374b812d88461f15550c070
Boris Blinov <boris.blinov@binary.com>
2017-07-25 07:47:13 +0000

    update

--- lib/BOM/Test/Helper/FinancialAssessment.pm
+++ lib/BOM/Test/Helper/FinancialAssessment.pm
@@ -5,17 +5,43 @@ use strict;
 
 use BOM::Platform::Account::Real::default;
 
-=head2 get_fulfilled_hash
+=head2 get_fulfilled_h
 
-    returns hash of all questions inside financial assesements with answers. Answers are selected by getting first in sorted list.
+    returns hash of all questions inside financial assesements with answers.
+    Answer selection is controlled by optional argument:
+    - first (default) - first from sorted list of names
+    - min - element with minimum weight, first from sorted list
+    - max - element with maximum weight, last in sorted list
 
 =cut
 
+sub _get_by_index {
+    my $h = shift;
+    return (sort { $h->{$a} <=> $h->{$b} || $a cmp $b } keys %$h)[shift];
+
+}
+my $first = sub {
+    return (sort keys %{$_[0]})[0];
+};
+my $min = sub { return _get_by_index($_[0], 0) };
+my $max = sub { return _get_by_index($_[0], -1) };
+my $types = {
+    'first' => $first,
+    'min'   => $min,
+    'max'   => $max,
+};
+
 sub get_fulfilled_hash {
-    my $h = BOM::Platform::Account::Real::default::get_financial_input_mapping();
-    my %r = map { $_ => (sort keys %{$h->{$_}->{possible_answer}})[0] } keys %$h;
+    my $type = shift // 'first';
+    die "Types can be: ", join ', ', (keys %$types) unless defined $types->{$type};
+    return _get_with_selector($types->{$type});
+}
+
+sub _get_with_selector {
+    my $func = shift;
+    my $h    = BOM::Platform::Account::Real::default::get_financial_input_mapping();
+    my %r    = map { $_ => $func->($h->{$_}->{possible_answer}) } keys %$h;
     return \%r;
 }
 
 1;
-
