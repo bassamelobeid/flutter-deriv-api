4d9b0728fbd60bacbc3c646ae4af28f42e3461da
Yng Shan <yngshan@binary.com>
2017-07-26 05:59:21 +0000

    fix critic warning

--- lib/BOM/Backoffice/Sysinit.pm
+++ lib/BOM/Backoffice/Sysinit.pm
@@ -18,7 +18,7 @@ use Try::Tiny::Except ();    # should be preloaded as early as possible
 sub init {
     $ENV{REQUEST_STARTTIME} = Time::HiRes::time;                           ## no critic (RequireLocalizedPunctuationVars)
     $^T                     = time;                                        ## no critic (RequireLocalizedPunctuationVars)
-    $ENV{PGSERVICEFILE}     = '/home/nobody/.pg_service_backprice.conf';
+    $ENV{PGSERVICEFILE}     = '/home/nobody/.pg_service_backprice.conf';   ## no critic (RequireLocalizedPunctuationVars)
     # Turn off any outstanding alarm, perhaps from a previous request in this mod_perl process,
     # while we figure out how we might want to alarm on this particular request.
     alarm(0);
