7ea147319c8dbb1ddc9f4a42afc1dec03226e08f
Boris Blinov <boris.blinov@binary.com>
2017-08-10 05:16:08 +0000

    parallel processing

--- crons/cron_sanctions.pl
+++ crons/cron_sanctions.pl
@@ -5,6 +5,7 @@ use warnings;
 use Date::Utility;
 use Data::Validate::Sanctions;
 use Path::Tiny;
+use Parallel::ForkManager;
 
 use Brands;
 use Client::Account;
@@ -23,27 +24,42 @@ use BOM::Platform::Email qw(send_email);
 my $file_flag    = '/tmp/last_cron_sanctions_check_run';
 my $reports_path = shift or die "Provide path for storing files as an argument";
 my @brokers      = qw/CR MF MLT MX/;
+my $verbose      = 1;
+my $childs       = 2;
 
 my $brand = Brands->new(name => 'binary');
 my $sanctions = Data::Validate::Sanctions->new(sanction_file => BOM::Platform::Config::sanction_file);
 
 my $last_run = (stat $file_flag)[9] // 0;
-#$sanctions->update_data();
-#{ open my $fh, '>', $file_flag; close $fh };
-#exit if $last_run > $sanctions->last_updated();
+$sanctions->update_data();
+{ open my $fh, '>', $file_flag; close $fh };
+exit if $last_run > $sanctions->last_updated();
 
-my $matched = {map { $_ => get_matched_clients_by_broker($_) } @brokers};
-do_report($matched);
+do_report();
 
 sub do_report {
-    my $matched = shift;
+    my $pm = new Parallel::ForkManager($childs);
+    my $r  = '';
+    $pm->run_on_finish(
+        sub {
+            my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $data_structure_reference) = @_;
+            if (defined($data_structure_reference)) {    # children are not forced to send anything
+                $r .= $$data_structure_reference;
+            }
+        });
+    for (@brokers) {
+        $pm->start and next;
+        warn time, " $_ starting" if $verbose;
+        my $result = get_matched_clients_info_by_broker($_);
+        warn time, " $_ finished" if $verbose;
+        $pm->finish(0, \$result);
+    }
+    $pm->wait_all_children;
+
+    print $r;
+
     my $headers =
         'List Name,List Updated,Database,LoginID,First Name,Last Name,Email,Phone,Gender,DateOfBirth,DateJoined,Residence,Citizen,Status,Reason';
-    my $r = '';
-    foreach my $k (sort keys %$matched) {
-        $r .= make_client_csv_line($_) . "\n" for sort { $a->[0]->loginid cmp $b->[0]->loginid } @{$matched->{$k}};
-    }
-    print $r;
 
     my $csv_file = path($reports_path . '/sanctions-run-' . Date::Utility->new()->date . '.csv');
     $csv_file->spew_utf8($headers . "\n");
@@ -70,7 +86,7 @@ sub make_client_csv_line {
     return join(',', @fields);
 }
 
-sub get_matched_clients_by_broker {
+sub get_matched_clients_info_by_broker {
     my $broker = shift;
     my @matched;
     my $dbh = BOM::Database::ClientDB->new({
@@ -95,7 +111,8 @@ sub get_matched_clients_by_broker {
         my $list = $sanctions->is_sanctioned($client->first_name, $client->last_name);
         push @matched, [$client, $list] if $list;
     }
-    return [] unless @matched;
+    return '' unless @matched;
+
     my $values = join ",", ('(?,?)') x scalar @matched;
     $dbh->do(
         qq{
@@ -108,6 +125,9 @@ sub get_matched_clients_by_broker {
             }, undef, map { $_->[1] => $_->[0]->loginid } @matched
     ) or warn $DBI::errstr;
 
-    return \@matched;
+    my $result = '';
+    $result .= make_client_csv_line($_) . "\n" for sort { $a->[0]->loginid cmp $b->[0]->loginid } @matched;
+
+    return $result;
 }
 
