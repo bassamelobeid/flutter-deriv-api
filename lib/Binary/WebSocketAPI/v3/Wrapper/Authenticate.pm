841b400743a6b88c362bbbae5b7f644d5d56743c
Amin Marashi <marashi.s.amin@gmail.com>
2017-08-15 03:08:26 +0000

    Tidy

--- lib/Binary/WebSocketAPI/v3/Wrapper/Authenticate.pm
+++ lib/Binary/WebSocketAPI/v3/Wrapper/Authenticate.pm
@@ -23,22 +23,21 @@ sub generate_upload_id {
     return $last_upload_id = ($last_upload_id + 1) % 1 << 32;
 }
 
-
 sub save_upload_info {
     my ($c, $rpc_response, $req_storage) = @_;
 
     my $params = {
-	file_id        => $rpc_response->{file_id},
+        file_id        => $rpc_response->{file_id},
         call_type      => $rpc_response->{call_type},
-	upload_id      => generate_upload_id(),
+        upload_id      => generate_upload_id(),
         sha1           => Digest::SHA1->new,
-	received_bytes => 0,
-	req_id         => $req_storage->{req_id},
+        received_bytes => 0,
+        req_id         => $req_storage->{req_id},
         passthrough    => $req_storage->{passthrough},
     };
 
     $params->{uploader} = uploader($params);
-    
+
     $c->stash(document_uploads => $params);
 }
 
