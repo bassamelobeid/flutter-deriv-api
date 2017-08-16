024d0a053dfd267b792af7e3675627edcecf575f
Chylli <chylli@binary.com>
2017-08-17 07:29:44 +0800

    update name and fix warning

--- lib/BOM/RPC/v3/PortfolioManagement.pm
+++ lib/BOM/RPC/v3/PortfolioManagement.pm
@@ -141,7 +141,7 @@ sub proposal_open_contract {
                 buy_price             => $fmb->{buy_price},
                 app_markup_percentage => $params->{app_markup_percentage},
                 landing_company       => $lc_name,
-                source                => $fmb->{source},
+                sell_source                => $fmb->{sell_source},
             });
         if (exists $bid->{error}) {
             $response->{$id} = $bid;
