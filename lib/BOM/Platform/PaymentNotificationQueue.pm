f0d2fed8a5cac0cdf4ed52a49eac183a75e0135c
Tom Molesworth <tom@binary.com>
2017-08-18 15:54:48 +0800

    Queue sender

---
+++ lib/BOM/Platform/PaymentNotificationQueue.pm
@@ -0,0 +1,40 @@
+package BOM::Platform::PaymentNotificationQueue;
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+BOM::Platform::PaymentNotificationQueue
+
+=head1 DESCRIPTION
+
+Pushes information about deposits and withdrawals to a queue so we can
+send information to adwords/analytics/facebook.
+
+=cut
+
+use Mojo::Redis2;
+use Future;
+use JSON::XS qw(encode_json);
+
+use constant MAX_QUEUE_LENGTH => 1000;
+
+# TODO This must be in config, so we should add to chef
+my $redis_url = 'redis://localhost:6359';
+
+sub redis {
+    state $redis = Mojo::Redis2->new(url => $redis_url);
+    return $redis;
+}
+
+sub add {
+    my ($class, %args) = @_;
+    my $redis = $class->redis;
+    return Future->done if $redis->llen('payment_notification_queue') > MAX_QUEUE_LENGTH;
+    my $data = encode_json(\%args);
+    $redis->push('payment_notification_queue', $data);
+    return Future->done;
+}
+
+1;
