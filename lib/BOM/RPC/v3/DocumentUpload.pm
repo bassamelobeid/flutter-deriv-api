0c3d73b0f626f51f6b3b1de1cdcc42747bc577fb
Apoorv Joshi <apoorv@binary.com>
2017-08-21 07:45:22 +0000

    Finish up changes

--- lib/BOM/RPC/v3/DocumentUpload.pm
+++ lib/BOM/RPC/v3/DocumentUpload.pm
@@ -3,14 +3,63 @@ package BOM::RPC::v3::DocumentUpload;
 use strict;
 use warnings;
 use BOM::Platform::Context qw (localize);
+use Try::Tiny;
+use Date::Utility;
 
 sub upload {
     my $params = shift;
-    my ($client, $upload_id) = @{$params}{qw/client upload_id document_type document_id document_format expiration_date/};
+    my ($client, $upload_id, $document_type, $document_id, $document_format, $expiration_date, $status) =
+        @{$params}{qw/client upload_id document_type document_id document_format expiration_date status/};
+
+    # Early return for virtual accounts.
     return BOM::RPC::v3::Utility::create_error({
             code              => 'UploadDenied',
-            message_to_client => localize("Virtual accounts don't require uploads.")}) if $client->is_virtual;
-    
+            message_to_client => localize("Virtual accounts don't require document uploads.")}) if $client->is_virtual;
+
+    if ($expiration_date ne '') {
+        my ($current_date, $parsed_date, $error);
+        $current_date = Date::Utility->new();
+        try {
+            $parsed_date = Date::Utility->new($expiration_date);
+        }
+        catch {
+            $error = $_;
+        };
+        if ($error) {
+            # warn $error;
+            return BOM::RPC::v3::Utility::create_error({
+                    code              => 'UploadDenied',
+                    message_to_client => localize("Invalid expiration_date.")});
+        } elsif ($parsed_date->is_before($current_date) || $parsed_date->is_same_as($current_date)) {
+            return BOM::RPC::v3::Utility::create_error({
+                    code              => 'UploadDenied',
+                    message_to_client => localize("expiration_date cannot be less than or equal to current date.")});
+        }
+    }
+
+    if ($document_type && $document_id && $document_format && $expiration_date) {
+        my $newfilename = join('.', $document_id, time(), $document_format);
+        my $upload = {
+            document_type              => $document_type,
+            document_format            => $document_format,
+            document_path              => $newfilename,
+            authentication_method_code => 'ID_DOCUMENT',
+            expiration_date            => $expiration_date,
+            document_id                => $document_id,
+            status                     => 'uploading',
+        };
+        $client->add_client_authentication_document($upload);
+        $client->save();
+
+        return {
+            success   => 1,
+            file_name => $newfilename
+        };
+    }
+
+    return BOM::RPC::v3::Utility::create_error({
+            code              => 'UploadDenied',
+            message_to_client => localize("Missing parameter.")});
 }
 
 1;
--- t/BOM/RPC/90_document_upload.t
+++ t/BOM/RPC/90_document_upload.t
@@ -6,7 +6,6 @@ use Test::Mojo;
 use BOM::Test::Data::Utility::UnitTestDatabase qw(:init);
 use BOM::Database::Model::OAuth;
 use utf8;
-use Data::Dumper;
 
 my $email       = 'dummy@binary.com';
 my $test_client = BOM::Test::Data::Utility::UnitTestDatabase::create_client({
@@ -30,7 +29,8 @@ $c->call_ok($method, $params)->has_error->error_message_is('The token is invalid
 
 $params->{token} = $token;
 # Valid token but virtual account.
-$c->call_ok($method, $params)->has_error->error_message_is("Virtual accounts don't require uploads.", "don't allow virtual accounts to upload");
+$c->call_ok($method, $params)
+    ->has_error->error_message_is("Virtual accounts don't require document uploads.", "don't allow virtual accounts to upload");
 
 # Creating new real account.
 $test_client = BOM::Test::Data::Utility::UnitTestDatabase::create_client({
@@ -39,10 +39,26 @@ $test_client = BOM::Test::Data::Utility::UnitTestDatabase::create_client({
 ($token) = BOM::Database::Model::OAuth->new->store_access_token_only(1, $test_client->loginid);
 
 # For CR accounts.
-$params->{token} = $token;
-$params->{upload_id} = "some_id";
-my $result = $c->call_ok($method, $params)->result;
+$params->{token}  = $token;
+$params->{upload} = "some_id";
 
-print Dumper($result);
+$params->{expiration_date} = "asdaas-asd-asd";    # Invalid date
+$c->call_ok($method, $params)->has_error->error_message_is('Invalid expiration_date.', 'check invalid expiration_date');
+
+$params->{expiration_date} = "2017-08-09";        # Expired documents.
+$c->call_ok($method, $params)
+    ->has_error->error_message_is('expiration_date cannot be less than or equal to current date.', 'check expiration_date is before current date');
+
+# Missing parameters
+$params->{expiration_date} = "2117-08-11";        # 100 years is all I give you, humanity!
+$c->call_ok($method, $params)->has_error->error_message_is('Missing parameter.', 'check if missing parameters');
+
+$params->{document_type}   = "passport";
+$params->{document_id}     = "ABCD1234";
+$params->{document_format} = "jpg";
+my $result = $c->call_ok($method, $params)->result;
+my @docs = $test_client->find_client_authentication_document(query => [document_path => $result->{file_name}]);
+# Succesfully retrieved object from database.
+is($docs[0]->document_id, $params->{document_id});
 
 done_testing();
