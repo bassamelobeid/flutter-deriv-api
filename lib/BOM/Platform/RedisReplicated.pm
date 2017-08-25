b8b30c51ca6d7405674667d620b0b4ed739e75d7
Boris Blinov <boris.blinov@binary.com>
2017-08-25 02:09:38 +0000

    polishing

--- lib/BOM/Platform/RedisReplicated.pm
+++ lib/BOM/Platform/RedisReplicated.pm
@@ -4,6 +4,14 @@ package BOM::Platform::RedisReplicated;
 
 BOM::Platform::RedisReplicated - Provides read/write pair of redis client
 
+=head1 DESCRIPTION
+
+This module has functions to return RedisDB object, connected to appropriate Redis.
+
+Please note:
+Don't cache returned object for a long term. All needed caching is done inside
+here, so better always call needed function to get working connection.
+
 =cut
 
 use strict;
@@ -30,10 +38,14 @@ sub _connect {
     my $connection_config = $config->{$redis_type}->{$access_type};
     if ($connections->{$key}) {
         try {
-            $connections->{$key}->ping();
+            # I was not able to reproduce an error for the last 24h
+            # that's why I'm not sure if separate cases for read and write needed
+            # if not - we can keep only ping() regardless of $access_type
+            $connections->{$key}->set('test','test') if $access_type eq 'write';
+            $connections->{$key}->ping() if $access_type eq 'read'
         }
         catch {
-            warn "RedisReplicated::_connect existing connection died: $_, reconnecting";
+            warn "RedisReplicated::_connect $key died: $_, reconnecting";
             $connections->{$key} = undef;
         };
     }
@@ -41,7 +53,7 @@ sub _connect {
         timeout => $timeout,
         host    => $connection_config->{host},
         port    => $connection_config->{port},
-        ($connection_config->{password} ? ('password', $connection_config->{password}) : ()));
+        ($connection_config->{password} ? ('password' => $connection_config->{password}) : ()));
 
     return $connections->{$key};
 }
