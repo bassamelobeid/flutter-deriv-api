cd65248fae6b016872b88ae96cb88651622f1cad
Hui Wang <hui@binary.com>
2017-08-25 07:03:21 +0000

    Add file name

--- lib/BOM/RPC/v3/DocumentUpload.pm
+++ lib/BOM/RPC/v3/DocumentUpload.pm
@@ -9,29 +9,15 @@ use Date::Utility;
 sub upload {
     my $params = shift;
     my $client = $params->{client};
-    my ($document_type, $document_id, $document_format, $expiration_date, $status, $file_name) =
-        @{$params->{args}}{qw/document_type document_id document_format expiration_date status file_name/};
+    my ($document_type, $document_id, $document_format, $expiration_date, $document_path, $status, $file_name) =
+        @{$params->{args}}{qw/document_type document_id document_format expiration_date document_path status file_name/};
 
     # Early return for virtual accounts.
     return BOM::RPC::v3::Utility::create_error({
             code              => 'UploadDenied',
             message_to_client => localize("Virtual accounts don't require document uploads.")}) if $client->is_virtual;
 
-    if (defined($status) && $status eq "success") {
-        my ($doc) = $client->find_client_authentication_document(query => [document_path => $file_name]);
-
-        # Return if document is not present in db.
-        return BOM::RPC::v3::Utility::create_error({
-                code              => 'UploadDenied',
-                message_to_client => localize("Document not found.")}) unless defined($doc);
-
-        $doc->{status} = "uploaded";
-        $doc->save();
-
-        return $params->{args};
-    }
-
-    if ($expiration_date ne '') {
+    if (defined $expiration_date && $expiration_date ne '') {
         my ($current_date, $parsed_date, $error);
         $current_date = Date::Utility->new();
         try {
@@ -41,7 +27,6 @@ sub upload {
             $error = $_;
         };
         if ($error) {
-            # warn $error;
             return BOM::RPC::v3::Utility::create_error({
                     code              => 'UploadDenied',
                     message_to_client => localize("Invalid expiration_date.")});
@@ -50,17 +35,21 @@ sub upload {
                     code              => 'UploadDenied',
                     message_to_client => localize("expiration_date cannot be less than or equal to current date.")});
         }
+    } else {
+        $expiration_date = '';
     }
 
-    if ($document_type && $document_id && $document_format && $expiration_date) {
-        my $newfilename = join('.', $document_id, time(), $document_format);
+    # Add new entry to database.
+    if ($document_type && $document_id && $document_format) {
+        my $newfilename = join '.', $document_id, time(), $document_format;
         my $upload = {
             document_type              => $document_type,
             document_format            => $document_format,
-            document_path              => $newfilename,
+            document_path              => '',
             authentication_method_code => 'ID_DOCUMENT',
             expiration_date            => $expiration_date,
             document_id                => $document_id,
+            file_name                  => $file_name,
             status                     => 'uploading',
         };
         $client->add_client_authentication_document($upload);
@@ -70,6 +59,22 @@ sub upload {
             file_name => $newfilename,
             call_type => 1,
         };
+    }
+    
+    # On success update the status of file to uploaded.
+    if (defined $status && $status eq "success") {
+        my ($doc) = $client->find_client_authentication_document(query => [file_name => $file_name]);
+
+        # Return if document is not present in db.
+        return BOM::RPC::v3::Utility::create_error({
+                code              => 'UploadDenied',
+                message_to_client => localize("Document not found.")}) unless defined($doc);
+
+        $doc->{status} = "uploaded";
+        $doc->{document_path} = $document_path;
+        $doc->save();
+
+        return $params->{args};
     }
 
     return BOM::RPC::v3::Utility::create_error({
