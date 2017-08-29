8f0cc8994802b9d9a077b32b5bcc2291c8b3e4d7
Boris Blinov <boris.blinov@binary.com>
2017-08-29 04:31:04 +0000

    top_up helper added

--- lib/BOM/Test/Helper/Client.pm
+++ lib/BOM/Test/Helper/Client.pm
@@ -5,7 +5,13 @@ use warnings;
 
 use Exporter qw( import );
 
-our @EXPORT_OK = qw(create_client);
+use Client::Account;
+use BOM::Platform::Client::IDAuthentication;
+use BOM::Platform::Client::Utility;
+use BOM::Platform::Password;
+use Test::More;
+
+our @EXPORT_OK = qw( create_client top_up );
 
 sub create_client {
     my $broker   = shift || 'CR';
@@ -36,4 +42,72 @@ sub create_client {
     return $client;
 }
 
+sub top_up {
+    my ($c, $cur, $amount, $payment_type) = @_;
+
+    $payment_type //= 'ewallet';
+
+    my $fdp = $c->is_first_deposit_pending;
+    my @acc = $c->account;
+
+# two behaviours:
+#_home_git_regentmarkets_bom-cryptocurrency_t_BOM_002_helper_b.t
+#_home_git_regentmarkets_bom-platform_t_BOM_Platform_financial_market_bet_02.t
+#_home_git_regentmarkets_bom-rpc_t_BOM_RPC_Transaction_12_copiers.t
+#    return if (@acc && $acc[0]->currency_code ne $cur);
+#
+#    if (not @acc) {
+#       @acc = $c->add_account({
+#          currency_code => $cur,
+#          is_default    => 1
+#       });
+#    }
+#_home_git_regentmarkets_bom-transaction_t_BOM_transaction.t
+#_home_git_regentmarkets_bom-transaction_t_BOM_transaction2.t
+    if (@acc) {
+        @acc = grep { $_->currency_code eq $cur } @acc;
+        @acc = $c->add_account({
+                currency_code => $cur,
+                is_default    => 0
+            }) unless @acc;
+    } else {
+        @acc = $c->add_account({
+            currency_code => $cur,
+            is_default    => 1
+        });
+    }
+
+    my $acc = $acc[0];
+    unless (defined $acc->id) {
+        $acc->save;
+        note 'Created account ' . $acc->id . ' for ' . $c->loginid . ' segment ' . $cur;
+    }
+
+    my ($pm) = $acc->add_payment({
+        amount               => $amount,
+        payment_gateway_code => "legacy_payment",
+        payment_type_code    => $payment_type,
+        status               => "OK",
+        staff_loginid        => "test",
+        remark               => __FILE__ . ':' . __LINE__,
+    });
+    $pm->legacy_payment({legacy_type => $payment_type});
+    my ($trx) = $pm->add_transaction({
+        account_id    => $acc->id,
+        amount        => $amount,
+        staff_loginid => "test",
+        remark        => __FILE__ . ':' . __LINE__,
+        referrer_type => "payment",
+        action_type   => ($amount > 0 ? "deposit" : "withdrawal"),
+        quantity      => 1,
+    });
+    $acc->save(cascade => 1);
+    $trx->load;    # to re-read (get balance_after)
+
+    BOM::Platform::Client::IDAuthentication->new(client => $c)->run_authentication
+        if $fdp;
+
+    note $c->loginid . "'s balance is now $cur " . $trx->balance_after . "\n";
+    return;
+}
 1;
