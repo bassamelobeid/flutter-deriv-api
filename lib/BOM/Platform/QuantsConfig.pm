cb867f11026c22934bf32677a3a6bb820a299fa9
Jun Bon <junbon@binary.com>
2017-09-07 08:26:34 +0000

    fixes

--- lib/BOM/Platform/QuantsConfig.pm
+++ lib/BOM/Platform/QuantsConfig.pm
@@ -74,11 +74,12 @@ sub get_config {
 
     my @match;
     foreach my $key (keys %$existing_config) {
-        my $config       = $existing_config->{$key};
-        my $matched_ct   = (!$config->{contract_type} || first { $_ eq $args->{contract_type} } @{$config->{contract_type}});
-        my $matched_ul   = (!$config->{underlying_symbol} || first { $_ eq $args->{underlying_symbol} } @{$config->{underlying_symbol}});
-        my $matched_curr = (!$config->{currency_symbol} || first { $args->{underlying_symbol} =~ /$_/ } @{$config->{currency_symbol}});
-        if ($matched_ct && ($matched_ul || $matched_curr)) {
+        my $config         = $existing_config->{$key};
+        my $matched_ct     = (!$config->{contract_type} || first { $_ eq $args->{contract_type} } @{$config->{contract_type}});
+        my $matched_all_ul = (!$config->{underlying_symbol} && !$config->{currency_symbol});
+        my $matched_ul     = ($config->{underlying_symbol} && first { $_ eq $args->{underlying_symbol} } @{$config->{underlying_symbol}});
+        my $matched_curr   = ($config->{currency_symbol} && first { $args->{underlying_symbol} =~ /$_/ } @{$config->{currency_symbol}});
+        if ($matched_ct && ($matched_all_ul || $matched_ul || $matched_curr)) {
             push @match, $config;
         }
     }
