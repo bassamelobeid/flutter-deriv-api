255e54ed14b8bb254ea5fdb680653fa0a23e67da
Boris Blinov <boris.blinov@binary.com>
2017-09-08 01:36:37 +0000

    fix

--- lib/BOM/Pricing/v3/Contract.pm
+++ lib/BOM/Pricing/v3/Contract.pm
@@ -339,7 +339,7 @@ sub get_bid {
             date_settlement     => $contract->date_settlement->epoch,
             currency            => $contract->currency,
             longcode            => localize($contract->longcode),
-            shortcode           => $shortcode,
+            shortcode           => $short_code,
             payout              => $contract->payout,
             contract_type       => $contract->code
         };
