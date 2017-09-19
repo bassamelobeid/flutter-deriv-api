cdce33280d9eb3bf82586ce40b5e5bb1ebaa6ab9
Kelvin Ting <kelvin@binary.com>
2017-09-19 06:02:50 +0000

    Removed variable

--- lib/BOM/RPC/v3/Cashier.pm
+++ lib/BOM/RPC/v3/Cashier.pm
@@ -97,8 +97,7 @@ sub cashier {
     }
 
     ## if currency is a cryptocurrency, use cryptocurrency cashier
-    my $legal_currencies = LandingCompany::Registry::get('costarica')->legal_allowed_currencies;
-    if ($legal_currencies->{$currency} eq 'crypto') {
+    if (LandingCompany::Registry::get('costarica')->legal_allowed_currencies->{$currency) eq 'crypto') {
         return _get_cryptocurrency_cashier_url($client->loginid, $params->{website_name}, $currency, $action, $params->{language}, $brand->name);
     }
 
