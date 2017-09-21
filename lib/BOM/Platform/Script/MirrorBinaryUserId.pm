a9b8d5c3383a1c725cb06874636adf24f7da2ed4
Torsten Förtsch <torsten@binary.com>
2017-09-21 07:09:33 +0000

    logging and some minor bugs

--- lib/BOM/Platform/Script/MirrorBinaryUserId.pm
+++ lib/BOM/Platform/Script/MirrorBinaryUserId.pm
@@ -7,11 +7,17 @@ use warnings;
 use BOM::Database::ClientDB;
 
 use DBI;
-# use DBD::Pg;
 use IO::Select;
 use Try::Tiny;
 
+our $DEBUG = 1;
 use constant TMOUT => 10;
+
+select +(select(STDERR), $| = 1)[0];
+sub log_msg {
+    my ($level, $msg) = @_;
+    print STDERR strftime('%F %T: ', localtime), $msg, "\n" if $DEBUG >= $level;
+}
 
 sub userdb {
     my $ip = shift;
@@ -30,14 +36,14 @@ sub update_clientdb {
     my $row = shift;
     my ($binary_user_id, $loginid) = @$row;
 
+    log_msg 2, "setting binary_user_id=$binary_user_id for $loginid";
     return try {
         my $dbh = BOM::Database::ClientDB->new({
-            loginid => $loginid,
+            client_loginid => $loginid,
         })->db->dbh;
         my @res = @{$dbh->selectcol_arrayref(<<'SQL', undef, $loginid, $binary_user_id)};
 SELECT betonmarkets.update_binary_user_id(?::VARCHAR(12), ?::BIGINT)
 SQL
-        warn "res = @res";
         unless ($res[0]) {
             warn "loginid $loginid has binary_user_id $binary_user_id but does not exist in clientdb\n";
         }
@@ -52,24 +58,34 @@ sub do_one {
     my $userdb = shift;
 
     my @brokers = @{$userdb->selectcol_arrayref('SELECT * FROM q.loginid_brokers()')};
+    unless (@brokers) {
+        log_msg 2, "queue is empty";
+        return 0;
+    }
+
+    log_msg 2, "need to process these broker codes: @brokers";
 
     my $rows_removed = 0;
     for my $broker (@brokers) {
         # q.loginid_next() returns one row at a time
         $userdb->begin_work;
-        my @rows = $userdb->selectall_array('SELECT * FROM q.loginid_next(?)', undef, $broker);
-        unless (@rows) {
+        my $rows = $userdb->selectall_arrayref('SELECT * FROM q.loginid_next(?)', undef, $broker);
+        unless (@$rows) {
             $userdb->commit;
+            log_msg 2, " ==> done with $broker";
             next;
         }
 
-        if (update_clientdb $rows[0]) {
+        if (update_clientdb $rows->[0]) {
+            log_msg 2, " ==> clientdb updated";
             $rows_removed++;
             $userdb->commit;
         } else {
+            log_msg 2, " ==> clientdb for broker $broker is not available -- rolling back";
             $userdb->rollback;
         }
     }
+    log_msg 2, "$rows_removed rows removed from queue";
     return $rows_removed;
 }
 
@@ -80,8 +96,10 @@ sub run {
             my $sel = IO::Select->new;
             $sel->add($dbh->{pg_socket});
 
-            $dbh->do("LISTEN q.add_loginid");
-            $dbh->do("NOTIFY q.add_loginid"); # trigger first round
+            $dbh->do('LISTEN "q.add_loginid"');
+            $dbh->do('NOTIFY "q.add_loginid"'); # trigger first round
+
+            log_msg 1, "started";
 
             while (1) {
                 # check if there is at least one notification
