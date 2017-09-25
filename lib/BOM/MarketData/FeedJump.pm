4ff8d4ef65d698b97ccf5d3a18600a518ce67567
Jun Bon <junbon@binary.com>
2017-09-25 01:50:38 +0000

    fixes hard-coded underlying

--- lib/BOM/MarketData/FeedJump.pm
+++ lib/BOM/MarketData/FeedJump.pm
@@ -9,6 +9,7 @@ use Mojo::Redis2;
 use BOM::Platform::QuantsConfig;
 use BOM::Platform::Chronicle;
 use Quant::Framework::EconomicEventCalendar;
+use LandingCompany::Offerings qw(get_offerings_flyby);
 
 use Try::Tiny;
 use namespace::autoclean;
@@ -33,7 +34,9 @@ sub BUILD {
     );
     $self->_eec($eec);
 
-    my %symbols = map { $_ => 1 } qw(frxAUDJPY frxAUDUSD frxEURGBP frxEURJPY frxEURUSD frxGBPJPY frxGBPUSD frxUSDCAD frxUSDJPY);
+    # we are only concern about the 9 forex pairs where we offer multi-barrier trading on.
+    my $fb = get_offerings_flyby({}, 'japan');
+    my %symbols = map { $_ => 1 } $fb->values_for_key('underlying_symbol');
     $self->_symbols_to_perform_check(\%symbols);
 
     return;
