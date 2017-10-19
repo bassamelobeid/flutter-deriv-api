b7750e42a966e2baa92a131e600c2fb8aa10bf75
Ivan Baidakou <ivan@binary.com>
2017-10-19 07:55:56 +0000

    Simplify logic

--- lib/Binary/WebSocketAPI/Hooks.pm
+++ lib/Binary/WebSocketAPI/Hooks.pm
@@ -326,11 +326,7 @@ sub check_useragent {
 
 sub _on_sanity_failed {
     my ($c) = @_;
-    my $client_ip =
-        ($c->tx && $c->tx->req && $c->tx->req->headers->header('REMOTE_ADDR'))
-        ? $c->tx->req->headers->header('REMOTE_ADDR')
-        : $c->client_ip;
-
+    my $client_ip = $_->stash->{client_ip};
     my $tags = ["client_ip:$client_ip", "app_name:" . ($c->stash('app_name') || ''), "app_id:" . ($c->stash('source') || ''),];
     DataDog::DogStatsd::Helper::stats_inc('bom_websocket_api.sanity_check_failed.count', {tags => $tags});
 
