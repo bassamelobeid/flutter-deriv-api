3ee5ec693a4c0332abbfe56a32abc866d641f13a
Ivan Baidakou <ivan@binary.com>
2017-11-07 14:03:30 +0000

    Silence warning like:
    07 Nov 2017 19:54:11.736api-green01 websocketapi: Use of uninitialized value in hash element at /home/git/regentmarkets/binary-websocket-api/bin/../lib/Binary/WebSocketAPI/Hooks.pm line 223, <$RAND> line 1601.
    Production_green (+1)/binary-websocket-api_trace.log

--- lib/Binary/WebSocketAPI/Hooks.pm
+++ lib/Binary/WebSocketAPI/Hooks.pm
@@ -220,7 +220,8 @@ sub before_forward {
 sub _rpc_suffix {
     my ($c) = @_;
 
-    my $suffix = $Binary::WebSocketAPI::DIVERT_APP_IDS{$c->app_id} ? '_' . $Binary::WebSocketAPI::DIVERT_APP_IDS{$c->app_id} : '';
+    my $processor = $Binary::WebSocketAPI::DIVERT_APP_IDS{$c->app_id // ''};
+    my $suffix = $processor ? '_' . $processor : '';
     unless (exists $c->app->config->{"rpc_url" . $suffix}) {
         warn "Suffix $suffix not found in config for app ID " . $c->app_id . "\n";
         $suffix = '';
