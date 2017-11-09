eaefe8bc9eeaeeed5b7e498f50a85df7f1cad477
Kelvin Ting <kelvin@binary.com>
2017-11-09 05:41:54 +0000

    Tidy

--- lib/BOM/Test/Helper/ExchangeRates.pm
+++ lib/BOM/Test/Helper/ExchangeRates.pm
@@ -8,16 +8,16 @@ our @EXPORT_OK = qw( populate_exchange_rates );
 # Mock exchange rates and populate in redis
 sub populate_exchange_rates {
     my $rates = {
-                USD => 1,
-                EUR => 1.1888,
-                GBP => 1.3333,
-                JPY => 0.0089,
-                BTC => 5500,
-                BCH => 320,
-                LTC => 50,
-            };
-            
-    Cache::RedisDB->set('QUOTE', "frx${_}USD", {quote => $rates->{$_}}) for keys %$rates;   
+        USD => 1,
+        EUR => 1.1888,
+        GBP => 1.3333,
+        JPY => 0.0089,
+        BTC => 5500,
+        BCH => 320,
+        LTC => 50,
+    };
+
+    Cache::RedisDB->set('QUOTE', "frx${_}USD", {quote => $rates->{$_}}) for keys %$rates;
 }
 
 1;
