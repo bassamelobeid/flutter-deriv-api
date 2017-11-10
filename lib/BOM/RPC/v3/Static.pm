36de4258abb94cb31f98a6a414d834171b3af6ef
Tom Molesworth <tom@binary.com>
2017-11-10 08:57:06 +0000

    Include ico_status flag in output

--- lib/BOM/RPC/v3/Static.pm
+++ lib/BOM/RPC/v3/Static.pm
@@ -170,6 +170,10 @@ sub ico_status {
     my $ico_info   = live_open_ico_bids($currency);
     $ico_info->{final_price_usd} = $app_config->system->suspend->ico_final_price;
     $ico_info->{final_price} = amount_from_to_currency($ico_info->{final_price_usd}, USD => $currency);
+    $ico_info->{ico_status} = (
+            $app_config->system->suspend->is_auction_ended
+                or not $app_config->system->suspend->is_auction_started
+        ) ? 'closed' : 'open';
 
     return $ico_info;
 }
