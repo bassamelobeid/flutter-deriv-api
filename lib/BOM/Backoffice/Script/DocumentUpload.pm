30c4f729d380eca96ce906092412823f5a3117c4
Amin Marashi <marashi.s.amin@gmail.com>
2017-11-16 08:35:57 +0000

    Use Amazon::S3::SignedURLGenerator for generating s3 urls

--- lib/BOM/Backoffice/Script/DocumentUpload.pm
+++ lib/BOM/Backoffice/Script/DocumentUpload.pm
@@ -3,10 +3,8 @@ package BOM::Backoffice::Script::DocumentUpload;
 use warnings;
 use strict;
 
-use Digest::SHA qw/hmac_sha1_base64 sha1_hex/;
-use URI::Escape;
 use File::Slurp;
-
+use Amazon::S3::SignedURLGenerator;
 use Amazon::S3;
 
 use BOM::Backoffice::Config;
@@ -18,26 +16,19 @@ my $secret_key = $document_auth_s3->{secret_key};
 my $region     = $document_auth_s3->{region};
 my $bucket     = $document_auth_s3->{bucket};
 
+my $generator = Amazon::S3::SignedURLGenerator->new(
+    aws_access_key_id     => $access_key,
+    aws_secret_access_key => $secret_key,
+    prefix => "https://s3-$region.amazonaws.com/",
+    expires => 600, # 10 minutes
+);
+
 sub get_s3_url {
-    my $file_path = shift;
+    my $file_name = shift;
 
-    die 'Cannot get s3 url for the document because the file_name is missing' unless $file_path;
+    die 'Cannot get s3 url for the document because the file_name is missing' unless $file_name;
 
-    my $expires_in = time + 60 * 5;
-    my $method     = 'GET';
-
-    my $signature = hmac_sha1_base64("$method\n\n\n$expires_in\n/$bucket/$file_path", $secret_key);
-
-    while (length($signature) % 4) {
-        $signature .= '=';
-    }
-
-    $access_key = uri_escape($access_key);
-    $signature  = uri_escape($signature);
-
-    my $query = "AWSAccessKeyId=$access_key&Expires=$expires_in&Signature=$signature";
-
-    return "https://s3-$region.amazonaws.com/$bucket/$file_path?$query";
+    return $generator->generate_url('GET', "$bucket/$file_name", {});
 }
 
 sub upload {
