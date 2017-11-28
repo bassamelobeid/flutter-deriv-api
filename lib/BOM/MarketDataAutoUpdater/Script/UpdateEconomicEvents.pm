3fbace640f98e11400b32963ff5d46b7a7a9da90
Chylli <chylli@binary.com>
2017-11-28 08:28:26 +0800

    revert back to master to debug test failure of bom-backoffice

--- lib/BOM/MarketDataAutoUpdater/Script/UpdateEconomicEvents.pm
+++ lib/BOM/MarketDataAutoUpdater/Script/UpdateEconomicEvents.pm
@@ -8,7 +8,7 @@ use BOM::MarketData qw(create_underlying create_underlying_db);
 use BOM::Platform::Runtime;
 use Date::Utility;
 use DataDog::DogStatsd::Helper qw(stats_gauge);
-use JSON::MaybeXS;
+use JSON;
 use Path::Tiny;
 use BOM::Platform::Chronicle;
 use Try::Tiny;
@@ -37,7 +37,7 @@ sub script_run {
 
     foreach my $event_param (@$events_received) {
         $event_param->{recorded_date} = Date::Utility->new->epoch;
-        Path::Tiny::path("/feed/economic_events/$file_timestamp")->append(time . ' ' . JSON::MaybeXS->new->encode($event_param) . "\n");
+        Path::Tiny::path("/feed/economic_events/$file_timestamp")->append(time . ' ' . JSON::to_json($event_param) . "\n");
     }
 
     try {
