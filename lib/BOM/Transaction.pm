08ac252711500c8279cb45a83f0cfa84e9f48d61
Shahrizal Shaari <shahrizal@binary.com>
2017-12-04 08:07:15 +0000

    typo

--- lib/BOM/Transaction.pm
+++ lib/BOM/Transaction.pm
@@ -376,7 +376,7 @@ sub calculate_limits {
 
         my @limits_arr = map { $_->{non_binary_contract_limit} } grep { exists $_->{non_binary_contract_limit}; } @{$non_binary_custom_limits[0]};
         my $custom_limit = min(@limits_arr);
-        $limits{lookback_open_position_limit} = $custom_limit if defined $min_limit;
+        $limits{lookback_open_position_limit} = $custom_limit if defined $custom_limit;
     }
 
     if (not $contract->tick_expiry) {
