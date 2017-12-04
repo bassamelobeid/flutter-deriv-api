9058351889a68a31d87ad5d7a4495cb3a526819a
junbon <junbon@binary.com>
2017-12-04 10:27:56 +0800

    junbon/offerings_validation (#1009)
    https://trello.com/c/0EHR2pG2/461-junbon-offeringsvalidation#
    
    * remove LandingCompany::Offerings
    
    * fixes for LC::Offerings
    
    * fixes readability

--- lib/BOM/Backoffice/EconomicEventTool.pm
+++ lib/BOM/Backoffice/EconomicEventTool.pm
@@ -5,11 +5,11 @@ use warnings;
 
 use Date::Utility;
 use JSON qw(to_json);
-use LandingCompany::Offerings qw(get_offerings_flyby);
 use List::Util qw(first);
 use Quant::Framework::EconomicEventCalendar;
 use Try::Tiny;
 use Volatility::Seasonality;
+use LandingCompany::Offerings;
 
 use BOM::Backoffice::Request;
 use BOM::MarketData qw(create_underlying_db);
@@ -189,8 +189,9 @@ my @symbols;
 sub _get_affected_underlying_symbols {
     return @symbols if @symbols;
 
-    my $fb = get_offerings_flyby(BOM::Platform::Runtime->instance->get_offerings_config);
-    @symbols = $fb->query({submarket => 'major_pairs'}, ['underlying_symbol']);
+    # default to costarica since it does not matter
+    my $offerings_obj = LandingCompany::Offerings->get('costarica', BOM::Platform::Runtime->instance->get_offerings_config);
+    @symbols = $offerings_obj->query({submarket => 'major_pairs'}, ['underlying_symbol']);
     return @symbols;
 }
 
