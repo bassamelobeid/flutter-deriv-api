ba7d9c3396ab8c40365fcec004fb5d4ca112c9f6
Jun Bon <junbon@binary.com>
2017-12-06 09:24:14 +0000

    Include barrier generation in GenerateTradingPeriods

--- lib/BOM/Product/Script/GenerateTradingPeriods.pm
+++ lib/BOM/Product/Script/GenerateTradingPeriods.pm
@@ -3,7 +3,8 @@ use strict;
 use warnings;
 
 use LandingCompany::Registry;
-use BOM::Product::Contract::PredefinedParameters qw(generate_trading_periods next_generation_epoch);
+use BOM::Product::ContractFinder;
+use BOM::Product::Contract::PredefinedParameters qw(generate_trading_periods next_generation_epoch generate_barriers_for_window);
 use BOM::Platform::Runtime;
 use Time::HiRes qw(clock_nanosleep TIMER_ABSTIME CLOCK_REALTIME);
 use Date::Utility;
@@ -15,6 +16,8 @@ sub run {
 
     my $offerings_obj    = LandingCompany::Registry::get('japan')->multi_barrier_offerings(BOM::Platform::Runtime->instance->get_offerings_config);
     my @selected_symbols = $offerings_obj->values_for_key('underlying_symbol');
+    my $chronicle_writer = BOM::Platform::Chronicle::get_chronicle_writer();
+    my $finder           = BOM::Product::ContractFinder->new;
 
     while (1) {
         my $now  = Date::Utility->new;
@@ -26,7 +29,22 @@ sub run {
             my @redis_key = BOM::Product::Contract::PredefinedParameters::trading_period_key($symbol, $now);
             my $ttl = max(1, $next - $now->epoch);
             # 1 - to save to chronicle database
-            BOM::Platform::Chronicle::get_chronicle_writer()->set(@redis_key, [grep { defined } @$tp], $now, 1, $ttl);
+            $chronicle_writer->set(@redis_key, [grep { defined } @$tp], $now, 1, $ttl);
+
+            # generate predefined barriers for a specific trading window
+            foreach my $trading_period (@$tp) {
+                my $barriers = generate_barriers_for_window($symbol, $trading_period);
+                # for barriers that's already generated, skip them here.
+                next unless $barriers;
+                my @redis_barrier_key = BOM::Product::Contract::PredefinedParameters::predefined_barrier_key($symbol, $trading_period);
+                my $ttl = max(1, $trading_period->{date_expiry}->{epoch} - $trading_period->{date_start}->{epoch});
+                $chronicle_writer->set(@redis_barrier_key, $barriers, $now, 1, $ttl);
+            }
+
+            # store by contract category expiry and barriers.
+            my @barrier_key = BOM::Product::Contract::PredefinedParameters::barrier_by_category_key($symbol);
+            my $by_category = $finder->multi_barrier_contracts_by_category_for({symbol => $symbol});
+            $chronicle_writer->set(@barrier_key, $by_category, $now, 1, $ttl);
         }
 
         clock_nanosleep(CLOCK_REALTIME, $next * 1e9, TIMER_ABSTIME);
