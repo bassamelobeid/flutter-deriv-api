18827a3e15ba24d00c819ab5ca6cdc5395f7a1fe
Amin Marashi <marashi.s.amin@gmail.com>
2017-12-06 01:14:57 +0000

    Remove extra warn

--- lib/BOM/Test/Initializations.pm
+++ lib/BOM/Test/Initializations.pm
@@ -3,8 +3,6 @@ package BOM::Test::Initializations;
 use strict;
 use warnings;
 
-warn '============**********';
-
 use BOM::Test::Data::Utility::FeedTestDatabase qw(:init);
 use BOM::Test::Data::Utility::UnitTestMarketData qw(:init);
 use BOM::Test::Data::Utility::UnitTestDatabase qw(:init);
