bbb705f95366202580fea2ea94b86913fbf6eadf
Chylli <chylli@binary.com>
2017-12-07 18:12:24 +0800

    update method

--- lib/BOM/Platform/User.pm
+++ lib/BOM/Platform/User.pm
@@ -181,7 +181,7 @@ sub clients_for_lc_short {
     my $lc_short  = shift // die 'need landing_company';
     my @login_ids = keys %{$self->loginid_details};
     return map { Client::Account->new({loginid => $_, db_operation => 'replica'}) }
-        grep { LandingCompany::Registry::get_by_loginid($_)->short eq $lc_short } @login_ids;
+        grep { LandingCompany::Registry->get_by_loginid($_)->short eq $lc_short } @login_ids;
 }
 
 sub loginid_details {
