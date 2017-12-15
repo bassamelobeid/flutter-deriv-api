300e9101c0f93bdc74cf57bc3c13acd7c12d0234
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2017-12-15 18:22:58 +0000

    Ensure ->has_no_error can cope with non-HASH returning RPCs e.g. mt5_password_check

--- lib/BOM/Test/RPC/Client.pm
+++ lib/BOM/Test/RPC/Client.pm
@@ -65,7 +65,10 @@ sub has_no_error {
     $description ||= "response for /$method has no error";
 
     my $result = $self->result;
-    $self->_test('ok', $result && !$result->{error}, $description) or
+    # Most RPCs return a HASH ref with an 'error' key on failure.
+    # A few RPCs (e.g. mt5_password_check) return a boolean truth on success
+    my $failed = ref($result) && $result->{error};
+    $self->_test('ok', !$failed, $description) or
         Test::More::diag("Expected no error, got\n", Data::Dumper->Dump([$result], [qw(result)]));
     return $self;
 }
