013c6d107e844fb93034e32465adc24d150598c2
Jun Bon <junbon@binary.com>
2017-12-20 05:49:03 +0000

    increased commission

--- lib/BOM/Product/ContractPricer.pm
+++ lib/BOM/Product/ContractPricer.pm
@@ -494,6 +494,10 @@ sub _build_base_commission {
     }
     my $underlying_base = get_underlying_base_commission($args);
 
+    if (not $self->for_sale and $self->market->name eq 'volidx' and not $self->is_atm_bet and $self->remaining_time->seconds <= 60) {
+        $underlying_base = 0.02;
+    }
+
     return $underlying_base * $per_market_scaling / 100;
 }
 
