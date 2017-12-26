c6bd24aa8a4631b736e47f87b278cf353bb52a38
Jun Bon <junbon@binary.com>
2017-12-26 06:44:31 +0000

    fixes typo

--- lib/BOM/Pricing/QueuePopulator/Japan.pm
+++ lib/BOM/Pricing/QueuePopulator/Japan.pm
@@ -107,7 +107,7 @@ sub process {    ## no critic qw(Subroutines::RequireArgUnpacking)
     # Get a full list of symbols since some may have been updated/disabled
     # since the last time
     my @symbols =
-        LandingCompany::Regsitry::get('japan')->multi_barrier_offerings(BOM::Platform::Runtime->instance->get_offerings_config)
+        LandingCompany::Registry::get('japan')->multi_barrier_offerings(BOM::Platform::Runtime->instance->get_offerings_config)
         ->values_for_key('underlying_symbol');
     my $now = Time::HiRes::time;
     $log->debugf("Retrieved symbols - %.2fms", 1000 * ($now - $start));
