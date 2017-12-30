c3af8bcd497d767da65019ef6f81649f8c7a67cf
Chylli <chylli@binary.com>
2017-12-30 22:29:42 +0800

    print error

--- lib/BOM/Test/RPC/Client.pm
+++ lib/BOM/Test/RPC/Client.pm
@@ -65,7 +65,7 @@ sub has_no_error {
     $description ||= "response for /$method has no error";
 
     my $result = $self->result;
-    $self->_test('ok', $result && !$result->{error}, $description);
+    $self->_test('ok', $result && !$result->{error}, $description) or diag("error: $result->{error}");
     return $self;
 }
 
