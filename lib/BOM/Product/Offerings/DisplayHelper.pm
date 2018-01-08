f394e64a2e3db07ba9039c25543bdaa81fbcb5a3
Yng Shan <yngshan@binary.com>
2018-01-08 07:45:58 +0000

    tidy code

--- lib/BOM/Product/Offerings/DisplayHelper.pm
+++ lib/BOM/Product/Offerings/DisplayHelper.pm
@@ -97,8 +97,7 @@ has trading_calendar => (
 );
 
 sub _build_trading_calendar {
-    return Quant::Framework->new
-      ->trading_calendar(BOM::Platform::Chronicle::get_chronicle_reader);
+    return Quant::Framework->new->trading_calendar(BOM::Platform::Chronicle::get_chronicle_reader);
 }
 
 my %known_decorations = (
@@ -106,11 +105,11 @@ my %known_decorations = (
     name => sub { return $_->display_name },
 
     times => sub {
-        my ( $parent_obj, $self ) = @_;
+        my ($parent_obj, $self) = @_;
         my $trading_calendar = $self->trading_calendar;
         my $exchange         = $_->exchange;
 
-        if ( my $cached = $self->times_cache->{ $exchange->symbol } ) {
+        if (my $cached = $self->times_cache->{$exchange->symbol}) {
             return $cached;
         }
 
@@ -122,104 +121,71 @@ my %known_decorations = (
             close      => [],
             settlement => $no_data
         };
-        if ( my $open =
-            $trading_calendar->opening_on( $exchange, $self->date ) )
-        {
-            push @{ $times->{open} }, $open->$display_method;
+        if (my $open = $trading_calendar->opening_on($exchange, $self->date)) {
+            push @{$times->{open}}, $open->$display_method;
             my @closes;
-            push @closes,
-              $trading_calendar->closing_on( $exchange, $self->date );
-            $times->{settlement} =
-              $trading_calendar->get_exchange_open_times( $exchange,
-                $self->date, 'daily_settlement' )->$display_method;
-            if ( my $breaks =
-                $trading_calendar->trading_breaks( $exchange, $self->date ) )
-            {
+            push @closes, $trading_calendar->closing_on($exchange, $self->date);
+            $times->{settlement} = $trading_calendar->get_exchange_open_times($exchange, $self->date, 'daily_settlement')->$display_method;
+            if (my $breaks = $trading_calendar->trading_breaks($exchange, $self->date)) {
                 for my $break (@$breaks) {
-                    push @{ $times->{open} }, $break->[-1]->$display_method;
+                    push @{$times->{open}}, $break->[-1]->$display_method;
                     push @closes, $break->[0];
                 }
             }
-            @{ $times->{close} } =
-              map  { $_->$display_method }
-              sort { $a->epoch <=> $b->epoch } @closes;
+            @{$times->{close}} =
+                map  { $_->$display_method }
+                sort { $a->epoch <=> $b->epoch } @closes;
         }
-        push @{ $times->{open} },  $no_data if not @{ $times->{open} };
-        push @{ $times->{close} }, $no_data if not @{ $times->{close} };
-        $self->times_cache->{ $exchange->symbol } = $times;
+        push @{$times->{open}},  $no_data if not @{$times->{open}};
+        push @{$times->{close}}, $no_data if not @{$times->{close}};
+        $self->times_cache->{$exchange->symbol} = $times;
 
         return $times;
     },
 
     events => sub {
-        my ( $parent_obj, $self ) = @_;
+        my ($parent_obj, $self) = @_;
         my $trading_calendar = $self->trading_calendar;
         my $exchange         = $_->exchange;
         my @events;
-        if ( my $cached = $self->holidays_cache->{ $exchange->symbol } ) {
+        if (my $cached = $self->holidays_cache->{$exchange->symbol}) {
             @events = @$cached;
-        }
-        else {
-            my $today = Date::Utility->today;
-            my $trading_day =
-              $trading_calendar->trading_date_for( $exchange, $self->date );
+        } else {
+            my $today               = Date::Utility->today;
+            my $trading_day         = $trading_calendar->trading_date_for($exchange, $self->date);
             my $how_long            = $today->days_in_month;
             my $date_display_method = 'date';
             my %seen_rules;
-            foreach my $day ( 0 .. $how_long ) {
-                my $when = $trading_day->plus_time_interval( $day . 'd' );
+            foreach my $day (0 .. $how_long) {
+                my $when = $trading_day->plus_time_interval($day . 'd');
 
-         # Assumption is these are all mutually exclusive.
-         # If you would both open late and close early, you'd make it a holiday.
-         # If you have a holiday you wouldn't open or close at all.
-         # Put a note here when you discover the exception.
-                my ( $rule, $message );
-                my $change_rules =
-                  $trading_calendar->regularly_adjusts_trading_hours_on(
-                    $exchange, $when );
-                my $early_closes =
-                  $trading_calendar->closes_early_on( $exchange, $when );
+                # Assumption is these are all mutually exclusive.
+                # If you would both open late and close early, you'd make it a holiday.
+                # If you have a holiday you wouldn't open or close at all.
+                # Put a note here when you discover the exception.
+                my ($rule, $message);
+                my $change_rules = $trading_calendar->regularly_adjusts_trading_hours_on($exchange, $when);
+                my $early_closes = $trading_calendar->closes_early_on($exchange, $when);
                 if ($early_closes) {
 
 # Finance::Calendar does not have access to our localization methods, so we localize here instead
 # Only set the rule as Friday if it is early close due to Friday.
                     $rule = $change_rules->{daily_close}->{rule}
-                      if ( defined $change_rules->{daily_close}->{rule}
-                        and $early_closes->hour
-                        . 'h' eq $change_rules->{daily_close}->{to} );
+                        if (defined $change_rules->{daily_close}->{rule}
+                        and $early_closes->hour . 'h' eq $change_rules->{daily_close}->{to});
                     $message =
-                      $self->c
-                      ? $self->c->l(
-                        'Closes early (at [_1])',
-                        $trading_calendar->closing_on( $exchange, $when )
-                          ->time_hhmm
-                      )
-                      : 'Closes early (at '
-                      . $trading_calendar->closing_on( $exchange, $when )
-                      ->time_hhmm . ')';
-                }
-                elsif ( $trading_calendar->opens_late_on( $exchange, $when ) ) {
+                          $self->c
+                        ? $self->c->l('Closes early (at [_1])', $trading_calendar->closing_on($exchange, $when)->time_hhmm)
+                        : 'Closes early (at ' . $trading_calendar->closing_on($exchange, $when)->time_hhmm . ')';
+                } elsif ($trading_calendar->opens_late_on($exchange, $when)) {
                     $rule = $change_rules->{daily_open}->{rule}
-                      if ( defined $change_rules->{daily_open}->{rule}
-                        and $trading_calendar->opens_late_on( $exchange, $when )
-                        ->hour . 'h' eq $change_rules->{daily_open}->{to} );
+                        if (defined $change_rules->{daily_open}->{rule}
+                        and $trading_calendar->opens_late_on($exchange, $when)->hour . 'h' eq $change_rules->{daily_open}->{to});
                     $message =
-                      $self->c
-                      ? $self->c->l(
-                        'Opens late (at [_1])',
-                        $trading_calendar->opening_on( $exchange, $when )
-                          ->time_hhmm
-                      )
-                      : 'Opens late (at '
-                      . $trading_calendar->opening_on( $exchange, $when )
-                      ->time_hhmm . ')';
-                }
-                elsif (
-                    my $holiday_desc = $trading_calendar->is_holiday_for(
-                        $exchange->symbol, $when
-                    )
-                  )
-                {
+                          $self->c
+                        ? $self->c->l('Opens late (at [_1])', $trading_calendar->opening_on($exchange, $when)->time_hhmm)
+                        : 'Opens late (at ' . $trading_calendar->opening_on($exchange, $when)->time_hhmm . ')';
+                } elsif (my $holiday_desc = $trading_calendar->is_holiday_for($exchange->symbol, $when)) {
                     $message = $holiday_desc;
                 }
                 if ($message) {
@@ -227,33 +193,30 @@ my %known_decorations = (
 # This would be easier here with a hash, but then they might end up out of order.
 # I'd rather deal with that here than in TT.
 
-                    my $where =
-                      first_index { $_->{descrip} eq $message } @events;
+                    my $where = first_index { $_->{descrip} eq $message } @events;
 
                     # first_index returns -1 for not found.  Idiots.
                     my $explain = $rule // $when->$date_display_method;
-                    if ( $where != -1 ) {
+                    if ($where != -1) {
                         $events[$where]->{dates} .= ', ' . $explain
-                          unless ( $rule
+                            unless ($rule
                             && $explain eq $rule
-                            && $seen_rules{$rule} );
-                    }
-                    else {
+                            && $seen_rules{$rule});
+                    } else {
                         if (    $when->is_same_as($trading_day)
-                            and $trading_day->is_same_as($today) )
+                            and $trading_day->is_same_as($today))
                         {
-                            $explain =
-                              $self->c ? $self->c->l('today') : 'today';
+                            $explain = $self->c ? $self->c->l('today') : 'today';
                         }
                         push @events,
-                          {
+                            {
                             descrip => $message,
                             dates   => $explain,
-                          };
+                            };
                     }
-                    $seen_rules{$rule} = 1 if ( $rule and $explain eq $rule );
+                    $seen_rules{$rule} = 1 if ($rule and $explain eq $rule);
                 }
-                $self->holidays_cache->{ $exchange->symbol } = \@events;
+                $self->holidays_cache->{$exchange->symbol} = \@events;
             }
         }
         return \@events;
@@ -274,9 +237,7 @@ sub _build_tree {
 
     foreach my $market (
         sort { $a->display_order <=> $b->display_order }
-        map  { Finance::Asset::Market::Registry->instance->get($_) }
-        $offerings_obj->values_for_key('market')
-      )
+        map  { Finance::Asset::Market::Registry->instance->get($_) } $offerings_obj->values_for_key('market'))
     {
         my $children    = [];
         my $market_info = {
@@ -286,9 +247,7 @@ sub _build_tree {
         };
         foreach my $submarket (
             sort { $a->display_order <=> $b->display_order }
-            map  { Finance::Asset::SubMarket::Registry->instance->get($_) }
-            $offerings_obj->query( { market => $market->name }, ['submarket'] )
-          )
+            map { Finance::Asset::SubMarket::Registry->instance->get($_) } $offerings_obj->query({market => $market->name}, ['submarket']))
         {
             my $children       = [];
             my $submarket_info = {
@@ -299,15 +258,12 @@ sub _build_tree {
                 parent      => $market_info,
             };
             foreach my $ul (
-                sort { ncmp( $a->display_name, $b->display_name ) }
-                map { create_underlying($_) } $offerings_obj->query(
-                    {
+                sort { ncmp($a->display_name, $b->display_name) }
+                map { create_underlying($_) } $offerings_obj->query({
                         market    => $market->name,
                         submarket => $submarket->name
                     },
-                    ['underlying_symbol']
-                )
-              )
+                    ['underlying_symbol']))
             {
                 my $children        = [];
                 my $underlying_info = {
@@ -319,12 +275,7 @@ sub _build_tree {
                 };
                 foreach my $bc (
                     sort { $a->display_order <=> $b->display_order }
-                    map  { Finance::Contract::Category->new($_) }
-                    $offerings_obj->query(
-                        { underlying_symbol => $ul->symbol },
-                        ['contract_category']
-                    )
-                  )
+                    map { Finance::Contract::Category->new($_) } $offerings_obj->query({underlying_symbol => $ul->symbol}, ['contract_category']))
                 {
                     my $children      = [];
                     my $category_info = {
@@ -333,12 +284,11 @@ sub _build_tree {
                         children   => $children,
                         parent     => $underlying_info
                     };
-                    push @{ $underlying_info->{contract_categories} },
-                      $category_info;
+                    push @{$underlying_info->{contract_categories}}, $category_info;
                 }
-                push @{ $submarket_info->{underlyings} }, $underlying_info;
+                push @{$submarket_info->{underlyings}}, $underlying_info;
             }
-            push @{ $market_info->{submarkets} }, $submarket_info;
+            push @{$market_info->{submarkets}}, $submarket_info;
         }
         push @$tree, $market_info;
     }
@@ -362,16 +312,15 @@ Returns the tree with decorations applied.
 =cut
 
 sub decorate_tree {
-    my ( $self, %decorations ) = @_;
+    my ($self, %decorations) = @_;
 
-    foreach my $level ( keys %decorations ) {
-        foreach my $item ( @{ $self->get_items_on_level($level) } ) {
-            foreach my $as ( keys %{ $decorations{$level} } ) {
+    foreach my $level (keys %decorations) {
+        foreach my $item (@{$self->get_items_on_level($level)}) {
+            foreach my $as (keys %{$decorations{$level}}) {
                 my $func = $decorations{$level}->{$as};
-                my $sub =
-                  ( ref $func ? $func : $known_decorations{$func} ) || next;
+                my $sub = (ref $func ? $func : $known_decorations{$func}) || next;
                 local $_ = $item->{obj};
-                $item->{$as} = $sub->( $item->{parent_obj}, $self );
+                $item->{$as} = $sub->($item->{parent_obj}, $self);
             }
         }
     }
@@ -380,15 +329,15 @@ sub decorate_tree {
 }
 
 sub get_items_on_level {
-    my ( $self, $level ) = @_;
-    my @levels = @{ $self->levels };
+    my ($self, $level) = @_;
+    my @levels = @{$self->levels};
     my $chosen_level = first_index { $_ eq $level } @levels;
-    die "Level $level must match one of those defined: " . join( ', ', @levels )
-      if ( $chosen_level == -1 );
+    die "Level $level must match one of those defined: " . join(', ', @levels)
+        if ($chosen_level == -1);
 
     my $list = $self->tree;    # Start at the top of the list;
-    for ( my $i = 1 ; $i <= $chosen_level ; $i++ ) {
-        $list = [ map { @{ $_->{ $levels[$i] } } } @$list ];
+    for (my $i = 1; $i <= $chosen_level; $i++) {
+        $list = [map { @{$_->{$levels[$i]}} } @$list];
     }
 
     return $list;
