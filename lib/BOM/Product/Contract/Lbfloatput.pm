07cf8ca3c671ee6e89692b4321e29dee526e68db
Jun Bon <junbon@binary.com>
2018-02-26 06:51:17 +0000

    need the exit_tick to be valid before finalizing the value

--- lib/BOM/Product/Contract/Lbfloatput.pm
+++ lib/BOM/Product/Contract/Lbfloatput.pm
@@ -7,7 +7,7 @@ with 'BOM::Product::Role::Lookback', 'BOM::Product::Role::SingleBarrier', 'BOM::
 sub check_expiry_conditions {
     my $self = shift;
 
-    if ($self->exit_tick) {
+    if ($self->exit_tick and $self->is_valid_exit_tick) {
         my ($high) = @{$self->get_ohlc_for_period()}{qw(high)};
         if (defined $high) {
             my $value = ($high - $self->exit_tick->quote) * $self->multiplier;
