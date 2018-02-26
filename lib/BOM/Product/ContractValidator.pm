90a3c3c8497cbc87d1074e3f9442cb17f96cc4ac
Shahrizal Shaari <shahrizal@binary.com>
2018-02-26 02:18:22 +0000

    remove previously added amount type check

--- lib/BOM/Product/ContractValidator.pm
+++ lib/BOM/Product/ContractValidator.pm
@@ -405,14 +405,6 @@ sub _validate_input_parameters {
                 message_to_client => [$ERROR_MAPPING->{MultiplierDecimalPlace}],
             };
         }
-
-        my $params = $self->build_parameters;
-        if (defined $params->{amount_type}) {
-            return {
-                message           => 'Amount type is only used for binary options.',
-                message_to_client => [$ERROR_MAPPING->{LookbackBasisUsage}],
-            };
-        }
     }
 
     return;
