c9407d1cc10868eb70a833ba9b4c3ac1fe7a67fd
Shahrizal Shaari <shahrizal@binary.com>
2018-03-15 03:31:14 +0000

    update reset tick part of the code

--- lib/BOM/Product/Contract/Resetput.pm
+++ lib/BOM/Product/Contract/Resetput.pm
@@ -14,7 +14,7 @@ sub _build_barrier {
     my $barrier = $self->make_barrier($self->supplied_barrier);
 
     if ($self->date_pricing->epoch >= $self->date_start->epoch + $self->reset_time->seconds) {
-        my $reset_spot = $self->underlying->tick_at($self->date_start->epoch + $self->reset_time->seconds, {allow_inconsistent => 1});
+        my $reset_spot = $self->underlying->tick_at($self->date_start->epoch + $self->reset_time->seconds - 1);
         if ($reset_spot and $reset_spot->quote > $barrier->as_absolute) {
             #If it is OTM, reset to a new barrier
             $barrier = $self->make_barrier($reset_spot->quote);
