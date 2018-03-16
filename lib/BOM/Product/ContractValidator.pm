d71b26a00fa643296c8c22ea7addebc829ba41b2
Shahrizal Shaari <shahrizal@binary.com>
2018-03-16 02:08:00 +0000

    update supplied barrier validation for reset

--- lib/BOM/Product/ContractValidator.pm
+++ lib/BOM/Product/ContractValidator.pm
@@ -407,8 +407,8 @@ sub _validate_input_parameters {
         }
     }
 
-    if ($self->category_code eq 'reset') {
-        if (defined $self->supplied_barrier and $self->supplied_barrier ne 'S0P') {
+    if ($self->category_code eq 'reset' and not $self->for_sale) {
+        if ($self->supplied_barrier ne 'S0P') {
             return {
                 message           => 'Non atm barrier for reset contract is not allowed.',
                 message_to_client => [$ERROR_MAPPING->{ResetBarrierError}],
