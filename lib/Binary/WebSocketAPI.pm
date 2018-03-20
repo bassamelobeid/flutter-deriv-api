6056a3bf6274da2663ec2cd2dc9a39584e5bf0d0
Suyash Mohan <suyash@binary.com>
2018-03-20 07:48:38 +0000

    define API account_authentication

--- lib/Binary/WebSocketAPI.pm
+++ lib/Binary/WebSocketAPI.pm
@@ -449,9 +449,10 @@ sub startup {
                 rpc_response_cb => \&Binary::WebSocketAPI::v3::Wrapper::DocumentUpload::add_upload_info,
             }
         ],
-        ['copy_start',         {require_auth => 'trade'}],
-        ['copy_stop',          {require_auth => 'trade'}],
-        ['app_markup_details', {require_auth => 'admin'}]];
+        ['copy_start',             {require_auth => 'trade'}],
+        ['copy_stop',              {require_auth => 'trade'}],
+        ['app_markup_details',     {require_auth => 'admin'}],
+        ['account_authentication', {require_auth => 'admin'}]];
 
     for my $action (@$actions) {
         my $f             = '/home/git/regentmarkets/binary-websocket-api/config/v3/' . $action->[0];
