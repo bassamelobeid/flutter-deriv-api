a7fed8f3bf8e9a1613b466b8d3f29a88a2c8e315
CraigLawrence <craig@binary.com>
2018-03-28 09:24:02 +0000

    fixed test and added some for filtering

---
+++ t/lib/mock_binary_mt5.pl
@@ -0,0 +1,78 @@
+# This script contains a mocked MT5 wrapper command used by the tests in t/BOM/RPC/30_mt5.t
+
+use strict;
+use warnings;
+
+use open ':encoding(UTF-8)';
+use List::Util qw(pairgrep);
+use JSON::MaybeXS;
+
+use constant {
+    MT_RET_OK => 0,
+};
+
+# Mocked account details
+# This hash shared between two files, and should be kept in-sync to avoid test failures
+#   t/BOM/user.t
+#   t/lib/mock_binary_mt5.pl
+my %DETAILS_REAL = (
+    login    => '1000',
+    group   => 'real\something',
+);
+
+my %DETAILS_DEMO = (
+    login    => '2000',
+    group   => 'demo\something',
+);
+
+my $json = JSON::MaybeXS->new;
+
+my $cmd   = shift @ARGV;
+my $input = $json->decode(
+    do { local $/; <STDIN> }
+);
+
+if (my $code = main->can("cmd_$cmd")) {
+    my $output = $code->($input);
+    print $json->encode($output) . "\n";
+    exit 0;
+} else {
+    print STDERR "Unrecognised command $cmd\n";
+    exit 1;
+}
+
+sub cmd_UserAdd {
+    # Not used by any bom-user tests
+}
+
+sub cmd_UserDepositChange {
+    # Not used by any bom-user tests
+}
+
+sub cmd_UserGet {
+    my ($input) = @_;
+    
+    return {
+        ret_code => MT_RET_OK,
+        user     => {pairgrep { $a ne "password" } %DETAILS_REAL},
+    } if $input->{login} eq $DETAILS_REAL{login};
+    
+    return {
+        ret_code => MT_RET_OK,
+        user     => {pairgrep { $a ne "password" } %DETAILS_DEMO},
+    } if $input->{login} eq $DETAILS_DEMO{login};
+
+    die "TODO: mock UserGet on unknown login\n";
+}
+
+sub cmd_UserUpdate {
+    # Not used by any bom-user tests
+}
+
+sub cmd_UserPasswordChange {
+    # Not used by any bom-user tests
+}
+
+sub cmd_UserPasswordCheck {
+    # Not used by any bom-user tests
+}
