daf9b1367aa17a635305b2f2e25b5bc7bca88a39
Chylli <chylli@binary.com>
2018-03-29 14:47:21 +0800

    perltidy

--- lib/BOM/User/Static.pm
+++ lib/BOM/User/Static.pm
@@ -16,12 +16,12 @@ This class provides static configurations like error mapping and generic message
 =cut
 
 my $config = {
-              errors => {
-                         # kept camel case because RPC/WS/Pricing follow this convention
-                         # it will be consistent in case in future we want to send
-                         # these as error codes to RPC/Pricing
-                        },
-             };
+    errors => {
+        # kept camel case because RPC/WS/Pricing follow this convention
+        # it will be consistent in case in future we want to send
+        # these as error codes to RPC/Pricing
+    },
+};
 
 =head2 get_error_mapping
 
@@ -30,7 +30,7 @@ Return error mapping for all the error message related to Contract
 =cut
 
 sub get_error_mapping {
-  return $config->{errors};
+    return $config->{errors};
 }
 
 1;
