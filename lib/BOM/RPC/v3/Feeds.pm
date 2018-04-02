95998a30b612bd0142de593b117d96d9e9009183
Mat Sepahi <mat@firstsource.io>
2018-04-02 18:31:56 +0000

    RPC call tested successfully on API playground.

--- lib/BOM/RPC/v3/Feeds.pm
+++ lib/BOM/RPC/v3/Feeds.pm
@@ -37,23 +37,23 @@ The return value is an anonymous hash contains the following items:
 =cut
 
 rpc exchange_rates => sub {
+    my $base = "USD";
     # Get available currencies
-    my $payout_currencies = BOM::RPC::v3::Utility::filter_out_suspended_cryptocurrencies('costarica');
-    
-    my %exchange_rates;
-    foreach $currency (@$payout_currencies)){
-        try{
-            if ($currency eq "USD")
-                continue;
-            $ex_rate = in_USD($currency);
-            %exchange_rates[$currency] = 1/$rate if (looks_like_number($ex_rate) && $ex_rate != 0);
-        }
+    my $supported_currencies = BOM::RPC::v3::Utility::filter_out_suspended_cryptocurrencies('costarica');
+
+    my %rates_map;
+    foreach my $currency (@$supported_currencies) {
+            next if $currency eq $base;
+            try{
+                my $ex_rate = in_USD(1, $currency);
+                $rates_map{$currency} = 1/$ex_rate if looks_like_number($ex_rate) && $ex_rate != 0;
+            };
     }
     
     return {
         date => time,
-        base => "USD",
-        rates => \%exchange_rates,
+        base => $base,
+        rates => \%rates_map,
     };
 };
 
