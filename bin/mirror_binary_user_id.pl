0c7ec2960fa29aac2a26dd8c5639b90dd6dace4e
Chylli <chylli@binary.com>
2018-04-16 12:50:25 +0800

    add description

--- bin/mirror_binary_user_id.pl
+++ bin/mirror_binary_user_id.pl
@@ -6,8 +6,8 @@ use BOM::User::Script::MirrorBinaryUserId;
 $BOM::User::Script::MirrorBinaryUserId::DEBUG = $ENV{DBG};
 BOM::User::Script::MirrorBinaryUserId::run;
 
-=Desciption
+=head1 DESCIPTION
 
-This script will copy user id from table user  to binary_user_id in table client . So that we can get user directly by binary_user_id in table client.
+This script will copy user id from table user to binary_user_id in table client . So that we can get user directly by binary_user_id in table client.
 
 =cut
