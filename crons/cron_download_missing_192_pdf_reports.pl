8f8aea54cde5b64d4414d4dee0d78de7b9ff93e2
Amin Marashi <marashi.s.amin@gmail.com>
2018-04-24 05:01:12 +0000

    Refactor cron to check for the last 2 hours and minor issues

--- crons/cron_download_missing_192_pdf_reports.pl
+++ crons/cron_download_missing_192_pdf_reports.pl
@@ -7,6 +7,7 @@ use Path::Tiny;
 
 use BOM::Platform::ProveID;
 use BOM::Platform::Runtime;
+use BOM::Platform::Client::IDAuthentication;
 use BOM::User::Client;
 
 my $accounts_dir = BOM::Platform::Runtime->instance->app_config->system->directory->db . "/f_accounts";
@@ -16,11 +17,11 @@ for my $broker (qw/MF MX/) {
     my $pending_loginids = find_loginids_with_pending_experian($broker);
 
     for my $loginid (@$pending_loginids) {
-        my ($xml_exists, $pdf_exists) = -e get_filename($broker, $loginid), -e get_filename($broker, $loginid, '.pdf');
+        my ($xml_exists, $pdf_exists) = (-e get_filename($broker, $loginid), -e get_filename($broker, $loginid, 1));
 
         if ($pdf_exists && $xml_exists) {
             # Files exists but status is not set, set the status and skip
-            set_proveid_received($loginid);
+            remove_pending_status($loginid);
             next;
         }
 
@@ -45,21 +46,23 @@ sub find_loginids_with_pending_experian {
     my $broker = shift;
     my $dbic   = get_db_for_broker($broker)->dbic;
 
-    return $dbic->run(
+    my $result = $dbic->run(
         fixup => sub {
-            $_->selectall_arrayref(<<'SQL') });
-select client_loginid from betonmarkets.client_status
-where status_code = 'proveid_pending' and last_modified_date >= (current_date - '1 month'::interval);
+            $_->selectall_arrayref(<<'SQL', {Slice => {}}) });
+SELECT client_loginid FROM betonmarkets.client_status
+WHERE status_code = 'proveid_pending' AND last_modified_date >= NOW() - INTERVAL '2 hour';
 SQL
 
+    return [grep { $_ =~ /$broker/ } map { $_->{client_loginid} } @$result];
 }
 
 sub get_filename {
-    my ($broker, $loginid, $extension) = @_;
+    my ($broker, $loginid, $is_pdf) = @_;
 
-    $extension //= "";
+    my $extension = $is_pdf ? '.pdf' : '';
+    my $type      = $is_pdf ? 'pdf'  : 'xml';
 
-    return "$accounts_dir/$broker/192com_authentication/pdf/$loginid.$so$extension";
+    return "$accounts_dir/$broker/192com_authentication/$type/$loginid.$so$extension";
 }
 
 sub request_pdf {
@@ -69,12 +72,10 @@ sub request_pdf {
 
     my $client = get_client($loginid);
 
-    BOM::Platform::ProveID->new(
-        client        => $client,
-        result_as_xml => $result_as_xml,
-        search_option => $so,
-        )->save_pdf_result
-        or die("Failed to save Experian result for $client");
+    eval { BOM::Platform::ProveID->new(client => $client, result_as_xml => $result_as_xml, search_option => $so,)->save_pdf_result }
+        or die("Failed to save Experian pdf for " . $client->loginid . ": $@");
+
+    remove_pending_status($loginid);
 }
 
 sub request_proveid {
@@ -88,14 +89,13 @@ sub request_proveid {
     )->_do_proveid;
 }
 
-sub set_proveid_received {
+sub remove_pending_status {
     my $loginid = shift;
 
     my $client = get_client($loginid);
 
     $client->clr_status('proveid_pending');
-    $client->set_status('proveid_received');
-    $client->save or die 'Unable to save proveid_received status';
+    $client->save or die 'Unable to clear proveid_pending status';
 }
 
 sub get_client {
