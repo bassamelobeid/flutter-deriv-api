f3a3dcb65ea7b732337dfcb7e3379994542bad2d
Reginaldo Costa <regcostajr@gmail.com>
2018-04-30 08:47:59 +0000

    test for DAI

--- lib/BOM/Test/Helper/CTC.pm
+++ lib/BOM/Test/Helper/CTC.pm
@@ -2,15 +2,17 @@ package BOM::Test::Helper::CTC;
 
 use strict;
 use warnings;
+no warnings qw/redefine/;
 
-use Moose;
-extends 'BOM::CTC::Helper';
+use BOM::CTC::Helper;
 
-sub currency_code {
-    my ($self) = @_;
+BEGIN {
+    *BOM::CTC::Helper::currency_code = sub {
+        my ($self) = @_;
 
-    my $currency_code = $self->client->default_account->currency_code;
-    return $currency_code;
-}
+        my $currency_code = $self->client->default_account->currency_code;
+        return $currency_code;
+    };
+};
 
 1;
