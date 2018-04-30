2357067933438dac716a9b6d9eb1db91a8354f3f
Jun Bon <junbon@binary.com>
2018-04-30 02:27:11 +0000

    missed rose generation for global limits

--- lib/BOM/Database/AutoGenerated/Rose/CurrencyConfig.pm
+++ lib/BOM/Database/AutoGenerated/Rose/CurrencyConfig.pm
@@ -11,7 +11,6 @@ __PACKAGE__->meta->setup(
     columns => [
         currency_code => { type => 'varchar', length => 3, not_null => 1 },
         precision     => { type => 'integer' },
-        crypto        => { type => 'boolean', default => 'false' },
     ],
 
     primary_key_columns => [ 'currency_code' ],
