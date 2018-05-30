0cd687d71948464138cfcdaddc7a8178631b8fed
Shahrizal Shaari <shahrizal@binary.com>
2018-05-30 01:49:23 +0000

    feedback

--- lib/Binary/WebSocketAPI/v3/Wrapper/Pricer.pm
+++ lib/Binary/WebSocketAPI/v3/Wrapper/Pricer.pm
@@ -640,7 +640,6 @@ sub process_bid_event {
             if ($response->{buy_price} and $response->{bid_price}) {
                 $response->{profit}            = $response->{bid_price} - $response->{buy_price};
                 $response->{profit_percentage} = $response->{profit} / $response->{buy_price} * 100;
-                $response->{profit_percentage} = $response->{profit_percentage} . '%';
             }
             Binary::WebSocketAPI::v3::Wrapper::System::forget_one($c, $stash_data->{uuid})
                 if $response->{is_sold};
