c1e5e372b16f7c4b955f71bb32be9ee012092e5a
Dmitry <dmitry@binary.com>
2018-06-21 11:22:31 +0000

    implement getters for FI and TE data

--- lib/BOM/User/Client.pm
+++ lib/BOM/User/Client.pm
@@ -410,6 +410,30 @@ sub is_financial_information_complete {
     return $self->_is_fa_section_complete('financial_information');
 }
 
+sub trading_experience {
+    my $self = shift;
+
+    return $self->_decode_fa_section('trading_experience');
+}
+
+sub financial_information {
+    my $self = shift;
+
+    return $self->_decode_fa_section('financial_information');
+}
+
+sub _decode_fa_section {
+    my $self = shift;
+    my $key  = shift;
+
+    my $fa = $self->_decode_financial_assessment();
+    return undef unless $fa;
+
+    my $im = BOM::Platform::Account::Real::default::get_financial_input_mapping();
+
+    return grep { $fa->{$_} } keys %{$im->{$key}};
+}
+
 sub _is_fa_section_complete {
     my $self = shift;
     my $key  = shift;
