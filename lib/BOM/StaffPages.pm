064514982577a3779cf2e11b844195590d4bfcae
Zak B. Elep <zak@binary.com>
2018-07-12 14:01:25 +0800

    Update Auth0 Lock to v11
    cf. https://auth0.com/docs/migrations/guides/legacy-lock-api-deprecation

--- lib/BOM/StaffPages.pm
+++ lib/BOM/StaffPages.pm
@@ -31,7 +31,7 @@ sub login {
     <form id='auth-form' name='second_step_auth' action='$params->{submit}'>
     <input type='hidden' id='auth0-token' name='token' />
     </form>
-    <script src="https://cdn.auth0.com/js/lock-9.1.min.js"></script>
+    <script src="https://cdn.auth0.com/js/lock/11.7.2/lock.min.js"></script>
     <script>
     var lock = new Auth0Lock('$clientId', 'binary.auth0.com');
       lock.show(function onLogin(err, profile, id_token, access_token) {
