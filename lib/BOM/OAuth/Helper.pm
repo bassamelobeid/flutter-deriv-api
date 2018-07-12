7a878e8a5aea72240d4427b4dd4fcd38e02166d5
Zak B. Elep <zakame@zakame.net>
2018-07-13 07:28:52 +0800

    BOM::OAuth::Helper: properly extract brand from params (#181)
    Typo here is causing `Experimental keys on scalar is now forbidden` in
    Perl 5.26 (not to mention it is an outright bug.)

--- lib/BOM/OAuth/Helper.pm
+++ lib/BOM/OAuth/Helper.pm
@@ -7,7 +7,7 @@ sub extract_brand_from_params {
     my ($self, $params) = @_;
     # extract encoded brand name from parameters curried from oneall
 
-    my $brand = (grep { /(?:amp;)brand/ } keys $params)[0];
+    my $brand = (grep { /(?:amp;)brand/ } keys %$params)[0];
     # if brand contains empty string return undef
     return undef unless $brand;
     return $params->{$brand};
