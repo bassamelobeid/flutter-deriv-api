8cfe3fd4894e00dbc13ba5faf0de15e973c3e845
Bill MacArthur <bill@binary.com>
2018-07-26 03:27:23 -0400

    bill/mfid_reporting (#974)
    * mfid_reporting script
    
    * tidy
    
    * accept date argument to script
    
    * add .csv file suffix
    
    * more changes regarding file naming
    
    * add SFTP uploading of files
    
    * tidy
    
    * more tidy
    
    * rm whitespace in SQL
    
    * use warnings
    
    * employ yml config for all sftp connection details
    
    * bring report script up to current state
    
    * tidy
    
    * add accounting to the recipient list
    
    * tidy
    
    * specify local file path
    
    * mfid_reporting.pl closer to the final product
    
    * tidy
    
    * tweak mfid_reporting.pl to not bail out on weekends when there are no trades to report

---
+++ crons/mfid_reporting.pl
@@ -0,0 +1,76 @@
+#!/etc/rmg/bin/perl
+use strict;
+use warnings;
+
+use Date::Utility;
+use Brands;
+use BOM::Platform::Email qw(send_email);
+use BOM::MapFintech;
+
+=head2
+
+ This script:
+ - creates our two daily MFID reporting files
+
+=cut
+
+my $specified_rptDate = $ARGV[0];
+my $brand = Brands->new(name => 'binary');
+
+my $report_recipients = join(',', $brand->emails('compliance'), 'bill@binary.com', 'x-acc@binary.com');
+my $failure_recipients = join(',', $brand->emails('compliance'), 'sysadmin@binary.com');
+
+# If we pass in a date, then we presumably want to report on that date
+# Yesterday is default
+# MAP Fintech originally specified that the filename should include ddmmyyy_hhmmss of the generation time, but should report on the previous day's activity,
+# They subsequently acquiesced to a conventional format of yyyymmdd_hhmmss
+# so we won't specify a strict seed for the default, otherwise we'll always have '000000' for the time component
+my $rd = Date::Utility->new($specified_rptDate);
+
+# our files will be written out for reference
+my $fileTail = join('', $rd->year, sprintf('%02d', $rd->month), sprintf('%02d', $rd->day_of_month), '_', $rd->hour, $rd->minute, $rd->second);
+my $tradesFN = 'BIE001_trades_' . $fileTail . '.csv';
+my $usersFN  = 'BIE001_users_' . $fileTail . '.csv';
+
+# where will they go
+my $reports_path = '/reports/Emir/' . $rd->year;
+
+# automatically maintain our year-named sub-directories
+mkdir $reports_path unless (-d $reports_path);
+
+# now adjust our actual reporting date if we have not specified the same
+$rd = $rd->minus_time_interval('1d') unless $specified_rptDate;
+my $rptDate = $rd->date_yyyymmdd;
+
+# just let PG/psql create the files directly
+my $rz = qx(/usr/bin/psql service=collector01 -v ON_ERROR_STOP=1 -X <<SQL
+    SET SESSION CHARACTERISTICS as TRANSACTION READ ONLY;
+    \\COPY (SELECT * FROM mfid_trades_rpt('$rptDate')) TO '$reports_path/$tradesFN' WITH (FORMAT 'csv', DELIMITER ',', FORCE_QUOTE *, HEADER);
+    \\COPY (SELECT * FROM mfid_users_rpt('$rptDate')) TO '$reports_path/$usersFN' WITH (FORMAT 'csv', DELIMITER ',', FORCE_QUOTE *, HEADER);
+SQL
+);
+
+# that psql call gives a proper result like this
+# COPY 456
+# COPY 7890
+# COPY 0 can happen on the weekend for trades, but we will always produce a complete client list
+if ($rz =~ /COPY [0-9]+.*COPY [1-9]+/s) {
+
+    my $upload_status = BOM::MapFintech::upload("$reports_path", [$tradesFN, $usersFN]);
+    my $message = $upload_status ? "There was a problem uploading files: $upload_status" : 'Files uploaded successfully';
+    $message .= "\n\nSee attached.";
+
+    # break this into two emails with a separate attachment each if necessary
+    send_email({
+            from       => $brand->emails('support'),
+            to         => $report_recipients,
+            subject    => "Emir reporting - $rptDate",
+            message    => [$message],
+            attachment => ["$reports_path/$tradesFN", "$reports_path/$usersFN"]});
+} else {
+    send_email({
+            from    => 'sysadmin@binary.com',
+            to      => $failure_recipients,
+            subject => "Emir reporting failure - $rptDate",
+            message => ["An unexpected response was received while trying to create the report files today: $rz"]});
+}
