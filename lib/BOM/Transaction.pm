019ac9d0df1556c2cdf07a7341f42f9ee416430c
Shahrizal Shaari <shahrizal@binary.com>
2018-07-28 12:50:27 +0800

    Fix DD stats for path dependent (#357)
    * Fix DD stats for path dependent
    
    * update contract exp epoch for path dependent

--- lib/BOM/Transaction.pm
+++ lib/BOM/Transaction.pm
@@ -1418,8 +1418,9 @@ sub sell_expired_contracts {
                 # this a hack that should be removed later when examination is done.
                 if ($collect_stats) {
                     $result->{contract_expiry_epoch} = $contract->exit_tick->epoch if $contract->exit_tick;
-                    $result->{bet_type}              = $bet->bet_type;
-                    $result->{expiry_type}           = $contract->expiry_type;
+                    $result->{contract_expiry_epoch} = $contract->hit_tick->epoch if $contract->is_path_dependent and $contract->hit_tick;
+                    $result->{bet_type}    = $bet->bet_type;
+                    $result->{expiry_type} = $contract->expiry_type;
                 }
 
             } elsif ($client->is_virtual and $now->epoch >= $contract->date_settlement->epoch + 3600) {
