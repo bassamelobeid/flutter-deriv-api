7fcf501b52bd8f4f93b9a62222afcdb5bf22742a
Greg Sabino Mullane <greg@binary.com>
2018-08-23 19:06:10 +0000

    Apply many changes per review suggestions

--- bin/verify_functions.pl
+++ bin/verify_functions.pl
@@ -8,79 +8,90 @@ use Data::Dumper;
 use Path::Tiny;
 use Getopt::Long qw( GetOptions );
 Getopt::Long::Configure(qw/ auto_version /);
+use Log::Any qw($log), default_adapter => 'Stdout';
+
 my (%opt, $verbose, $quiet, $repo, $repodir, $db);
 
-our $VERSION = '1.3';
+our $VERSION = '1.4';
 
 my $USAGE = "Usage: $0 --repo=<repo name> --db=<service file name>";
 
 get_all_options();
 
-my ($taginfo, $git_hashes) = get_git_hashes();
+my ($taginfo, $commitinfo, $git_hashes) = get_git_hashes();
 
 my ($dbinfo, $db_hashes) = get_db_hashes();
 
 if (!$quiet) {
-    printf "Repo: %s at tag %s, created on %s\n (points to commit %s, created on %s)\n",
-        $repo, $taginfo->{name}, $taginfo->{tagdate}, $taginfo->{shortcommit}, $taginfo->{commitdate};
-    printf "Database service: %s (%sport=%s user=%s database=%s)\n",
+    my $message = sprintf 
+"Repo: %s at commit %s, created on %s
+Author: %s Title: %s
+Latest tag: %s, created on %s, points to commit %s
+Database service: %s (%sport=%s user=%s database=%s)",
+
+$repo, $commitinfo->{hash}, $commitinfo->{date},
+    $commitinfo->{author}, $commitinfo->{title},
+    $taginfo->{name}, $taginfo->{tagdate}, $taginfo->{shortcommit},
         $dbinfo->{service}, $dbinfo->{host} ? "host=$dbinfo->{host} " : '',
         $dbinfo->{port}, $dbinfo->{user}, $dbinfo->{dbname};
-}
 
-my @wrong;
+    $log->notice($message);
+
+}
 
 my $prefix = '* ';
 
 ## Any functions that exist in the repo but not the database?
+my @onlyrepo;
 for my $gitfunc (keys %{$git_hashes}) {
     next if exists $db_hashes->{$gitfunc};
-    push @wrong => $gitfunc;
+    push @onlyrepo => $gitfunc;
 }
-if (@wrong) {
-    for my $func (sort @wrong) {
-        print "${prefix}Function in the repo but not in database $db: $func\n";
+if (@onlyrepo) {
+    for my $func (sort @onlyrepo) {
+         $log->notice("${prefix}Function in the repo but not in database $db: $func");
     }
 } elsif ($verbose) {
-    print "${prefix}All functions in the repo are also in database $db\n";
+     $log->notice("${prefix}All functions in the repo are also in database $db");
 }
 
 ## Any functions that exist in the database but not in the repo?
-undef @wrong;
+my @onlydb;
 for my $dbfunc (keys %{$db_hashes}) {
     next if exists $git_hashes->{$dbfunc};
-    push @wrong => $dbfunc;
+    push @onlydb => $dbfunc;
 }
-if (@wrong) {
-    for my $func (sort @wrong) {
-        print "${prefix}Function in database $db but not in the repo: $func\n";
+if (@onlydb) {
+    for my $func (sort @onlydb) {
+        $log->notice("${prefix}Function in database $db but not in the repo: $func");
     }
 } elsif ($verbose) {
-    print "${prefix}All functions in database $db are also in the repo\n";
+    $log->notice("${prefix}All functions in database $db are also in the repo");
 }
 
 ## Any functions with a mismatched checksum?
 my $identical = 0;
-undef @wrong;
+my @mismatch;
 for my $dbfunc (keys %{$db_hashes}) {
     next if !exists $git_hashes->{$dbfunc};
     if ($db_hashes->{$dbfunc} ne $git_hashes->{$dbfunc}) {
-        push @wrong => $dbfunc;
+        push @mismatch => $dbfunc;
     } else {
         $identical++;
     }
 }
-if (@wrong) {
-    for my $func (sort @wrong) {
-        print "${prefix}Function is different in the repo and database $db: $func\n";
+if (@mismatch) {
+    for my $func (sort @mismatch) {
+        $log->notice("${prefix}Function is different in the repo and database $db: $func");
     }
 } elsif ($verbose) {
-    print "${prefix}All functions have identical checksums for database $db\n";
+    $log->notice("${prefix}All functions have identical checksums for database $db");
 }
 
-$verbose and print "Total identical functions for db $db: $identical\n";
+$verbose and $log->notice("Total identical functions for db $db: $identical");
 
-print "\n";
+$log->notice("\n");
+
 
 exit;
 
@@ -89,7 +100,7 @@ sub debug {
     return if $opt{debug} < 1;
     my $message = shift;
     chomp $message;
-    print ">>DEBUG: $message\n";
+    $log->debug(">>DEBUG: $message");
     return;
 }
 
@@ -97,38 +108,36 @@ sub get_git_hashes {
 
     ## Grab the manifest information from the latest git tag for a repo
     ## Assumes a file named "manifest" exists in the root directory of the repo
-    ## Returns two hashrefs:
+    ## Returns three hashrefs:
     ## 1. git tag information (name, date, tagger, commit, commit date)
-    ## 2. function names and their hash values
+    ## 2. latest commit information (hash, author name, date, title)
+    ## 3. function names and their hash values
 
     if (!$repodir->exists) {
-        $quiet or print "Creating $repodir\n";
-        my $TOKEN   = get_token();
-        my $command = "clone https://$TOKEN\@github.com/regentmarkets/$repo $repodir";
-        run_git_command($command);
+	$log->fatal( "Cannot proceed: directory does not exist: $repodir");
+	exit 1;
     }
 
-    ## Just in case, stash away any existing work
-    my $command = 'stash save';
-    run_git_command($command);
+    ## We rely on other people leaving the repo in our desired state
 
-    ## Make sure we are on the latest important tag
-    $command = "tag -l | grep '^V' | sort -k1.2 -g | tail -1";
+    ## Grab the latest "V" tag
+    my $command = "tag -l | grep '^V' | sort -k1.2 -g | tail -1";
     my $latest_tag = run_git_command($command);
     chomp $latest_tag;
-    length $latest_tag or die qq{Could not determine latest tag for $repo\n};
-    $verbose and print "Latest git tag: $latest_tag\n";
+    $verbose and $log->notice( "Latest git tag: $latest_tag");
 
     ## Gather information about this tag
-    my $taginfo = get_tag_info($latest_tag);
+    my $taginfo = get_tag_info( $latest_tag);
 
-    ## Switch to this tag if not already there
-    $command = qq{checkout "$latest_tag"};
-    run_git_command($command);
+    ## Gather information about the latest commit
+    my $commitinfo = get_latest_commit_info();
 
     ## Open the manifest file and read in the checksums
     my $manifest = path($repodir, 'manifest');
-    $manifest->exists or die "Could not find $manifest!\n";
+    if (!$manifest->exists) {
+	$log->fatal("Could not find $manifest!");
+	exit 1;
+    }
     my @lines = $manifest->lines;
     my %git_func_hash;
     for my $line (@lines) {
@@ -136,17 +145,18 @@ sub get_git_hashes {
             $git_func_hash{$2} = $1;
         } else {
             chomp $line;
-            warn "Unknown line inside $manifest: $line\n";
+            warn $log->warning("Unknown line inside $manifest: $line");
         }
     }
 
     if ($verbose) {
         my $count = keys %git_func_hash;
-        print "Entries found in $manifest: $count\n";
+        $log->notice("Entries found in $manifest: $count");
     }
 
-    return $taginfo, \%git_func_hash;
+    return $taginfo, $commitinfo, \%git_func_hash;
 }
+
 
 sub get_tag_info {
 
@@ -155,37 +165,71 @@ sub get_tag_info {
     my $tagname = shift;
 
     my $command = qq{git show "$tagname"};
-    my $result  = run_git_command($command);
+    my $result = run_git_command($command);
 
-    debug substr($result, 0, 400) . "\n\n";
+    debug substr($result,0,400) . "\n\n";
 
     ## Sanity check
     if ($result !~ /^tag $tagname$/m) {
-        die "Invalid tag information returned from $command\n";
+	$log->fatal("Invalid tag information returned from $command");
+	exit 1;
     }
 
     my %taginfo = (
-        name => $tagname,
-    );
+	name => $tagname,
+	);
 
     if ($result =~ /^Tagger:\s+(.+)/m) {
-        $taginfo{tagger} = $1;
+	$taginfo{tagger} = $1;
     }
     if ($result =~ s/^Date:\s+(.+)//m) {
-        $taginfo{tagdate} = $1;
+	$taginfo{tagdate} = $1;
     }
     if ($result =~ /^commit ([a-f0-9]+)/m) {
-        $taginfo{commit} = $1;
-        $taginfo{shortcommit} = substr($1, 0, 8);
+	$taginfo{commit} = $1;
+	$taginfo{shortcommit} = substr($1,0,8);
     }
     if ($result =~ /^Author:\s+(.+)/m) {
-        $taginfo{author} = $1;
+	$taginfo{author} = $1;
     }
     if ($result =~ /^Date:\s+(.+)/m) {
-        $taginfo{commitdate} = $1;
+	$taginfo{commitdate} = $1;
     }
 
     return \%taginfo;
+}
+
+
+sub get_latest_commit_info {
+
+    ## Return information about the latest commit
+
+    my $command = qq{git log -1 --format="Commit: %h%nAuthor: %an%nDate: %aD%nTitle: %s%n"};
+    my $result = run_git_command($command);
+
+    debug $result;
+
+    ## Sanity check
+    if ($result !~ /^Commit: ([a-f0-9]+)/) {
+        $log->fatal("Invalid log information returned from $command");
+	exit 1;
+    }
+
+    my %commitinfo = (
+	hash => $1,
+	);
+
+    if ($result =~ /^Author:\s+(.+)/m) {
+	$commitinfo{author} = $1;
+    }
+    if ($result =~ /^Date:\s+(.+)/m) {
+	$commitinfo{date} = $1;
+    }
+    if ($result =~ /^Title:\s+(.+)/m) {
+	$commitinfo{title} = $1;
+    }
+
+    return \%commitinfo;
 }
 
 sub run_git_command {
@@ -205,6 +249,9 @@ sub run_git_command {
     return $output;
 
 }
+
+
+
 
 sub get_db_hashes {
 
@@ -235,27 +282,30 @@ EOF
         $db_func_hash{$2} = $1;
     }
 
-    keys %db_func_hash or die qq{Could not get database function checksums\n};
+    if (! keys %db_func_hash) {
+	$log->fatal("Could not get database function checksums from $db");
+	exit 1;
+    }
 
     if ($verbose) {
         my $count = keys %db_func_hash;
-        print "Entries found in database $db: $count\n";
+        $log->notice( "Entries found in database $db: $count");
     }
 
-    $SQL     = "SELECT current_database(), setting, user, inet_server_addr() FROM pg_settings WHERE name = 'port'";
+    $SQL = "SELECT current_database(), setting, user, inet_server_addr() FROM pg_settings WHERE name = 'port'";
     $command = qq{psql service="$db" -AX -qt -c "$SQL"};
     debug "Running: $command";
     my $result = qx{ $command };
     chomp $result;
     debug "Result: $result";
-    my ($dbname, $dbport, $dbuser, $dbhost) = split /\|/ => $result;
+    my ($dbname,$dbport,$dbuser,$dbhost) = split /\|/ => $result;
     my $dbinfo = {
-        service => $db,
-        dbname  => $dbname,
-        host    => $dbhost,
-        port    => $dbport,
-        user    => $dbuser,
-    };
+	service => $db,
+	dbname => $dbname,
+	host => $dbhost,
+	port => $dbport,
+	user => $dbuser,
+	};
 
     return $dbinfo, \%db_func_hash;
 
@@ -265,10 +315,11 @@ sub get_token {
 
     ## Attempt to get a token for github
 
-    my $tokenfile = path($opt{tokenfile} // ($ENV{HOME}, '.config', 'git'));
+    my $tokenfile = path( $opt{tokenfile} // ($ENV{HOME}, '.config', 'git') );
 
     if (!$tokenfile->exists) {
-        die "Could not find token file: $tokenfile";
+        $log->fatal("Could not find token file: $tokenfile");
+	exit 1;
     }
 
     my $data = $tokenfile->slurp_utf8;
@@ -279,30 +330,42 @@ sub get_token {
         return $token;
     }
 
-    die "File does not contain a token: $tokenfile\n";
+    $log->fatal("File does not contain a token: $tokenfile");
+    exit 1;
 }
 
 sub get_all_options {
 
     ## Set some default options
     %opt = (
-        verbose => 0,
+	verbose => 0,
         debug   => 0,
-        quiet   => 0,
-        githome => "$ENV{HOME}/repos",
+	quiet => 0,
+        githome => '/home/git/regentmarkets',
     );
 
-    GetOptions(\%opt, 'verbose', 'debug+', 'quiet|q', 'database|db=s', 'repo|repository=s', 'githome=s', 'tokenfile=s', 'help|h',) or die;
+    GetOptions(
+        \%opt,
+        'verbose',
+        'debug+',
+        'quiet|q',
+        'database|db=s',
+        'repo|repository=s',
+        'githome=s',
+	'tokenfile=s',
+        'help|h',
+    ) or die;
 
     if ($opt{help} or !$opt{repo} or !$opt{database}) {    ## no critic
-        die "$USAGE\n";
+        $log->fatal("$USAGE");
+	exit 1;
     }
 
     $verbose = $opt{verbose};
-    $quiet   = $opt{quiet};
-    $repo    = $opt{repo};
-    $repodir = path($opt{githome} || $ENV{HOME}, $repo);
-    $db      = $opt{database};
+    $quiet = $opt{quiet};
+    $repo = $opt{repo};
+    $repodir = path($opt{githome}, $repo);
+    $db = $opt{database};
 
     return \%opt;
 
