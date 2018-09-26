f1211aa4e675ee5e61581b80ef62bf1909143029
Arun Venkataraman <arun@regentmarkets.com>
2018-09-26 15:23:45 +0800

    Using X-Client-Country to get country (#1028)
    https://trello.com/c/J9RiEPWx/7475-oauth-service-doesnt-use-x-client-country-to-get-the-correct-client-country-from-desktop-app
    
    * Using X-Client-Country to get country
    
    * removed condition
    
    * Test for X-Client-Country
    
    * Removed test
    
    * Fix for warning
    
    * Removed extra space

--- lib/BOM/Platform/Context/Request/Builders.pm
+++ lib/BOM/Platform/Context/Request/Builders.pm
@@ -59,7 +59,8 @@ sub from_mojo {
 
     $args->{domain_name} = $request->url->to_abs->host;
 
-    my $client_country = lc($request->headers->header('CF-IPCOUNTRY') || 'aq');
+    my ($custom_header_country) = $request->headers->header('X-Client-Country') || '' =~ m/^([a-z]{2})$/i;
+    my $client_country = lc($custom_header_country || $request->headers->header('CF-IPCOUNTRY') || 'aq');
     $client_country = 'aq' if ($client_country eq 'xx');
     $args->{country_code} = $client_country;
 
