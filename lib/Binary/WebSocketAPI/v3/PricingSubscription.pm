12a3b1b6c5596d74b581492d5c90a88a2baebd8f
Tom Molesworth <tom@binary.com>
2018-09-27 12:45:17 +0800

    tom/revert_subscription_refactor (#2430)
    https://trello.com/c/C3AYp6ah/7491-tom-revertsubscriptionrefactor
    
    * tom/revert_subscription_refactor
    
    * Revert "Chylli/refactor websocket subscriptions part1 (#2388)"
    
    This reverts commit 5399308e59d41f55374b6be92dd4513e7bdbcc7b.

--- lib/Binary/WebSocketAPI/v3/PricingSubscription.pm
+++ lib/Binary/WebSocketAPI/v3/PricingSubscription.pm
@@ -2,13 +2,12 @@ package Binary::WebSocketAPI::v3::PricingSubscription;
 
 use strict;
 use warnings;
+use Binary::WebSocketAPI::v3::Instance::Redis qw| redis_pricer |;
 
 use Moo;
 
-use Scalar::Util qw(weaken);
-use DataDog::DogStatsd::Helper qw(stats_inc stats_dec);
-
-use Binary::WebSocketAPI::v3::Instance::Redis qw(redis_pricer);
+use Scalar::Util qw| weaken |;
+use DataDog::DogStatsd::Helper qw| stats_inc stats_dec |;
 
 has channel_name => (
     is       => 'ro',
@@ -19,16 +18,15 @@ has redis_server => (is => 'lazy');
 sub BUILD {
     my $self = shift;
     stats_inc('bom_websocket_api.v_3.pricing_subscriptions.instances');
-
-    # For pricer_queue daemon
+    ### For pricer_queue daemon
+    $self->redis_server->set($self->channel_name, 1);
     my $channel_name = $self->channel_name;
-    $self->redis_server->set($channel_name, 1);
     $self->redis_server->subscribe(
-        [$channel_name],
+        [$self->channel_name],
         sub {
             my ($redis_self, $err) = @_;
             if ($err) {
-                warn "Failed to start pricing subscription for $channel_name due to error: $err";
+                warn "Pricing subscription was not started. Channel -->> $channel_name  Error -->> $err";
                 stats_inc('bom_websocket_api.v_3.pricing_subscriptions.instances.error');
             } else {
                 stats_inc('bom_websocket_api.v_3.pricing_subscriptions.instances.success');
@@ -39,15 +37,18 @@ sub BUILD {
 }
 
 sub _build_redis_server {
-    return redis_pricer();
+    return redis_pricer;
 }
 
 sub subscribe {
     my ($self, $c) = @_;
 
-    Scalar::Util::weaken($self->redis_server->{shared_info}{$self->channel_name}{$c + 0} = $c);
+    $self->redis_server->{shared_info}{$self->channel_name}{$c + 0} = $c;
 
     stats_inc('bom_websocket_api.v_3.pricing_subscriptions.clients');
+
+    Scalar::Util::weaken($self->redis_server->{shared_info}{$self->channel_name}{$c + 0});
+
     return $self;
 }
 
@@ -56,23 +57,10 @@ sub DEMOLISH {
 
     stats_dec('bom_websocket_api.v_3.pricing_subscriptions.instances');
 
-    my $channel_name = $self->channel_name;
-    delete $self->redis_server->{shared_info}{$channel_name};
-    $self->redis_server->unsubscribe(
-        [$channel_name],
-        sub {
-            my ($redis_self, $err) = @_;
-            if ($err) {
-                warn "Failed to stop pricing subscription for $channel_name due to error: $err";
-                stats_inc('bom_websocket_api.v_3.pricing_subscriptions.unsubscribe.error');
-            } else {
-                stats_inc('bom_websocket_api.v_3.pricing_subscriptions.unsubscribe.success');
-            }
-            return $redis_self;
-        });
+    delete $self->redis_server->{shared_info}{$self->channel_name};
+    $self->redis_server->unsubscribe([$self->channel_name]);
 
     return;
 }
 
 1;
-
