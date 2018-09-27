14edd499c0aa59695250d301e7520a2f81993689
billmarriott <38449124+billmarriott@users.noreply.github.com>
2018-09-27 14:25:36 +0800

    billmarriott/copy_token_optimization (#539)
    https://trello.com/c/W55zbbIY/7399-8-billmarriott-copytokenoptimization-16
    
    * Use new db function to check tokens
    
    * lazy load auth db handle
    
    * Moose way
    
    * Move get_copiers() to db
    
    * Don't concat trader_id in query
    
    * Cast placeholder

--- lib/BOM/Database/DataMapper/Copier.pm
+++ lib/BOM/Database/DataMapper/Copier.pm
@@ -1,113 +1,78 @@
 package BOM::Database::DataMapper::Copier;
 
-use List::Util qw(uniq);
+use List::Util qw(any uniq);
 use BOM::Database::Model::AccessToken;
 use BOM::Database::Model::OAuth;
+use BOM::Database::AuthDB;
 use Moose;
 
 extends 'BOM::Database::DataMapper::Base';
+
+has auth_db => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { BOM::Database::AuthDB::rose_db()->dbic });
 
 sub get_copiers_cnt {
     my ($self, $args) = @_;
 
     my $sql = q{
-        SELECT copier_id, trader_token
+        SELECT copier_id, trader_id, trader_token
           FROM betonmarkets.copiers
          WHERE trader_id = $1
     };
 
     my @binds = ($args->{trader_id});
     my $res = $self->db->dbic->run(fixup => sub { [$_->selectall_arrayref($sql, undef, @binds)]->[0] });
-    return scalar @{_filter_invalid_tokens($res)};
+    return scalar @{$self->_filter_invalid_tokens($res)};
 }
 
 sub get_trade_copiers {
     my ($self, $args) = @_;
-    ### This SQL query can be written in more obvious way but this long query is faster.
-    ### Please, check with DBA if you want to change it
-    my $sql = <<'SQL';
-SELECT DISTINCT copier_id, trader_token from (
-    SELECT copier_id, trader_token
-      FROM betonmarkets.copiers AS copiers
-     WHERE trader_id = $1
-       AND trade_type = $2
-       AND asset = $3
-       AND ($4 IS NULL OR
-            (min_trade_stake is NULL OR $4 >= min_trade_stake) AND
-            (max_trade_stake is NULL OR $4 <= max_trade_stake))
 
-    UNION ALL
-
-    SELECT copier_id, trader_token
-      FROM betonmarkets.copiers AS copiers
-     WHERE trader_id = $1
-       AND trade_type = '*'
-       AND asset = $3
-       AND ($4 IS NULL OR
-            (min_trade_stake is NULL OR $4 >= min_trade_stake) AND
-            (max_trade_stake is NULL OR $4 <= max_trade_stake))
-
-    UNION ALL
-
-    SELECT copier_id, trader_token
-      FROM betonmarkets.copiers AS copiers
-     WHERE trader_id = $1
-       AND trade_type = $2
-       AND asset = '*'
-       AND ($4 IS NULL OR
-            (min_trade_stake is NULL OR $4 >= min_trade_stake) AND
-            (max_trade_stake is NULL OR $4 <= max_trade_stake))
-
-    UNION ALL
-
-    SELECT copier_id, trader_token
-      FROM betonmarkets.copiers AS copiers
-     WHERE trader_id = $1
-       AND trade_type = '*'
-       AND asset = '*'
-       AND ($4 IS NULL OR
-            (min_trade_stake is NULL OR $4 >= min_trade_stake) AND
-            (max_trade_stake is NULL OR $4 <= max_trade_stake))
-) t
-SQL
+    my $sql = 'SELECT copier_id, trader_id, trader_token FROM betonmarkets.get_copiers($1,$2,$3,$4)';
 
     my $res = $self->db->dbic->run(
         fixup => sub {
             $_->selectall_arrayref($sql, undef, @{$args}{qw/trader_id trade_type asset price/});
         }) // [];
-    return _filter_invalid_tokens($res);
+    return $self->_filter_invalid_tokens($res);
 }
 
 sub get_traders {
     my ($self, $args) = @_;
 
     my $sql = q{
-        SELECT trader_id, trader_token
+        SELECT trader_id, trader_id, trader_token
           FROM betonmarkets.copiers
          WHERE copier_id = $1
     };
 
     my @binds = ($args->{copier_id});
     my $res = $self->db->dbic->run(fixup => sub { $_->selectall_arrayref($sql, undef, @binds) });
-    return _filter_invalid_tokens($res);
+    return $self->_filter_invalid_tokens($res);
 }
 
-# Takes pairs of logins and tokens, returns a unique list of logins
-# that had a valid token OR no token at all
-
+# Takes arrayref of [ item, login, token ]
+# Returns a unqiue list of items who had a login with a valid token.
 sub _filter_invalid_tokens {
-    my $list = shift;
+    my ($self, $list) = @_;
+
+    my @logins = uniq map { $_->[1] } @$list;
+
+    my $res = $self->auth_db->run(
+        fixup => sub {
+            $_->selectall_arrayref('SELECT * FROM get_valid_tokens_for_loginids(?::VARCHAR[])', undef, \@logins);
+        });
+
+    # Make a hash of valid tokens per login id
+    my %tokens;
+    push @{$tokens{$_->[0]}}, $_->[1] for @$res;
+
     my @copiers = map { $_->[0] } grep {
-        my $token = $_->[1];
-        if (!$token) {    # because copiers created before this change did not store trader token
-            1;
-        } elsif (length $token == 15) {    # access token
-            BOM::Database::Model::AccessToken->new->get_token_details($token)->{loginid};
-        } elsif (length $token == 32 && $token =~ /^a1-/) {
-            BOM::Database::Model::OAuth->new->get_token_details($token)->{loginid};
-        } else {
-            0;
-        }
+        my $loginid = $_->[1];
+        my $token   = $_->[2];
+        !$token || (exists $tokens{$loginid} && any { $_ eq $token } @{$tokens{$loginid}});
     } @$list;
     return [uniq @copiers];
 }
