0d677051b1d9bd01db480867eb09947f25016829
regcostajr <regcostajr@gmail.com>
2018-10-13 18:46:25 +0000

    add test for suspended currency

--- lib/BOM/Test/Helper/CTC.pm
+++ lib/BOM/Test/Helper/CTC.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use BOM::CTC::Helper;
+use BOM::Platform::Client::CashierValidation;
 
 BEGIN {
     *BOM::CTC::Helper::currency_code = sub {
@@ -12,6 +13,10 @@ BEGIN {
         my $currency_code = $self->client->default_account->currency_code;
         return $currency_code;
     };
+
+    *BOM::Platform::Client::CashierValidation::is_crypto_currency_suspended = sub {
+        return 0;
+    }
 }
 
 1;
