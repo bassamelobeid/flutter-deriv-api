6be296e0befff3e5e06decdc5a0365d4bcd7189a
wongtiongkiat <39819760+wongtiongkiat@users.noreply.github.com>
2018-10-19 18:33:44 +0800

    wongtiongkiat/merge_duplicate_emails (#1392)
    * compiled duplicated clients into a single email in a day instead of sending it seperately
    
    * removed diagnostic lines
    
    * removed diagnostic lines
    
    * updated desk to support
    
    * Remove blank lines

--- crons/cron_check_duplicate_new_client.pl
+++ crons/cron_check_duplicate_new_client.pl
@@ -7,6 +7,12 @@ BEGIN {
     push @INC, "/home/git/regentmarkets/bom-backoffice/lib";
 }
 
+use Email::Address::UseXS;
+use Email::Stuffer;
+use BOM::Platform::Context qw (request);
+use Template;
+use Brands;
+
 use BOM::Backoffice::Sysinit ();
 use BOM::Database::DataMapper::CollectorReporting;
 use BOM::User;
@@ -18,31 +24,34 @@ if ($ENV{REQUEST_METHOD}) {
     die 'REQUEST_METHOD[' . $ENV{REQUEST_METHOD} . '] exists!?';
 }
 
-my $check_date = Date::Utility->new(time - 86400)->date;
+my $check_date    = Date::Utility->new(time - 86400)->date;
+my $support_email = Brands->new(name => request()->brand)->emails('support');
+my $template      = Template->new(ABSOLUTE => 1);
 
-#connect to collector for getting data
+# connect to collector for getting data
 my $report_mapper = BOM::Database::DataMapper::CollectorReporting->new({
     broker_code => 'FOG',
     operation   => 'collector'
 });
+
 my $client_dup_list = $report_mapper->check_clients_duplication(Date::Utility->new($check_date)->truncate_to_day);
 
-# Duplicate new client found
-my $note_header = qq{
-=========================================================================================
-The following client opened an account on $check_date but has the same name and date of birth as other clients.
-=========================================================================================\n\n};
-
+my @client_data;
 my $dup_unique;
 foreach my $client_hash (@{$client_dup_list}) {
+
+    # skip if client_hash is completely empty
+    next if (not $client_hash->{first_name} and not $client_hash->{last_name} and not $client_hash->{date_of_birth});
+
     # avoid sending multiple emails for same client with multiple duplicate loginids
     my $client_str = join(',', $client_hash->{first_name} // '', $client_hash->{last_name} // '', $client_hash->{date_of_birth} // '');
     next if (defined $dup_unique and exists $dup_unique->{$client_str});
     $dup_unique->{$client_str} = 1;
 
     my $loginid = $client_hash->{new_loginid};
-    my $client  = BOM::User::Client::get_instance({loginid => $loginid});
-    my $user    = $client->user;
+    my $client = BOM::User::Client::get_instance({loginid => $loginid});
+
+    my $user = $client->user;
 
     my $siblings = {map { $_->loginid => 1 } $user->clients};
     my @duplicate_clients = map {
@@ -56,10 +65,23 @@ foreach my $client_hash (@{$client_dup_list}) {
 
     next unless @duplicate_clients;
 
-    my $note_content = $note_header;
-    $note_content .= $loginid . '(' . $client_hash->{first_name} . ' ' . $client_hash->{last_name} . ")\n";
-    $note_content .= '   Duplicate login id: ' . join(', ', @duplicate_clients) . "\n\n";
-    $client->add_note("Duplicate clients found for [$loginid] opened on $check_date", $note_content);
+    push @client_data,
+        {
+        loginid            => $loginid,
+        first_name         => $client_hash->{first_name},
+        last_name          => $client_hash->{last_name},
+        duplicated_loginid => join(', ', @duplicate_clients)};
+
+}
+
+if (@{$client_dup_list}) {
+    my $template_data = {
+        clients_data => \@client_data,
+        check_date   => $check_date,
+    };
+    $template->process('/home/git/regentmarkets/bom-backoffice/templates/email/duplicated_clients.html.tt', $template_data, \my $html)
+        or die 'Template error: ' . $template->error;
+    Email::Stuffer->from($support_email)->to($support_email)->subject("Duplicate clients found on $check_date")->html_body($html)->send_or_die;
 }
 
 =head1 NAME
