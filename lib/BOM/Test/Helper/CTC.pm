eaae8472dfb0b974f60164d9ec96d195f97d7b8a
regcostajr <regcostajr@gmail.com>
2018-10-26 07:22:46 +0000

    updating tests

--- lib/BOM/Test/Helper/CTC.pm
+++ lib/BOM/Test/Helper/CTC.pm
@@ -7,16 +7,16 @@ use BOM::CTC::Helper;
 use BOM::Platform::Client::CashierValidation;
 
 BEGIN {
-    *BOM::CTC::Helper::currency_code = sub {
+    *BOM::CTC::Helper::currency = sub {
         my ($self) = @_;
 
         my $currency_code = $self->client->default_account->currency_code;
-        return $currency_code;
+        return { code => $currency_code };
     };
 
     *BOM::Platform::Client::CashierValidation::is_crypto_currency_suspended = sub {
         return 0;
-        }
+    };
 }
 
 1;
