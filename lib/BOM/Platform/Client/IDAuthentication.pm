fc9c603ee3ac23f70d64051ec8ba4af5d6167547
Anthony-Wong <34021543+Anthony-Wong@users.noreply.github.com>
2018-10-30 18:53:21 +0800

    Anthony-Wong/proveid_changes (#1029)
    * init
    
    * experian connection file
    
    * auth file
    
    * more idauth file
    
    * polish
    
    * user not found and part of matching
    
    * finished dob check
    
    * removed ofsi as it is same as boe
    
    * tidy
    
    * syntax error, probably will find more
    
    * syntax error, probably will find even more
    
    * small changes
    
    * syntax
    
    * anonymous sub for valid countries
    
    * cr and virtual tests
    
    * test changes
    
    * mx test barebone
    
    * removed old
    
    * tidy
    
    * filepath
    
    * tidy
    
    * added email test
    
    * tidy
    
    * test
    
    * added last of test cases
    
    * tidy
    
    * changes 8 oct morning
    
    * test chanes 8 oct morning
    
    * xml test files
    
    * fraud xml
    
    * refactored the refactor
    
    * qa testing
    
    * tidy
    
    * mocked xml responses and server
    
    * correct path
    
    * tidy
    
    * tweaks and fixes
    
    * pdfs and cleanup
    
    * tidy
    
    * tidy
    
    * loads of touchup + response
    
    * renamed files
    
    * corrected error handling
    
    * ofsi pdf and creds
    
    * started uat tests
    
    * added xml mock
    
    * server fix
    
    * tweaks
    
    * more auth tests
    
    * ordered adress tag
    
    * tidy
    
    * redid no entry test
    
    * no entry handling
    
    * variable name
    
    * start script on test
    
    * full qual
    
    * other qual
    
    * get to moose
    
    * deleted old test
    
    * moved client and accidental quotes
    
    * removed uselss check
    
    * removed get
    
    * package name
    
    * ITS ALIVE
    
    * fixes
    
    * big push fixing tonnes
    
    * cleanup
    
    * tidy
    
    * repathed
    
    * more test tweaks
    
    * our ref changes
    
    * xml changes
    
    * moved files
    
    * tidy and cleanup
    
    * tidy
    
    * fault test
    
    * syntax
    
    * add 2fa to mock server
    
    * proverc reverted as service running by default now
    
    * travis experian mock script
    
    * tidy
    
    * tests fix
    
    * moved order of travis script
    
    * overhaul
    
    * unidfy pdf
    
    * MOJO UA WORKSSSS
    
    * removed comment
    
    * tidy
    
    * removed TODO
    
    * croak to die
    
    * cr handling
    
    * slight clenaup
    
    * tidy
    
    * cleanup
    
    * tidy
    
    * moar cleanup
    
    * even moar cleanup
    
    * changes with landing company module
    
    * tidy
    
    * MOAR cleanup
    
    * caught a bug
    
    * init
    
    * debugging
    
    * travis test fix
    
    * travis script
    
    * test email overhaul and replace result server test
    
    * cleanup
    
    * major cleanup
    
    * typos
    
    * more cleanup
    
    * tidy
    
    * folder changes
    
    * tidy
    
    * hash ref to hash
    
    * tidy
    
    * deprivatize
    
    * formal doc
    
    * moved to yml
    
    * tidy
    
    * removed wrong comment
    
    * paths
    
    * docs on mock server
    
    * on->with
    
    * moved successful request actions around
    
    * doc for idauth.pm
    
    * tidy
    
    * typo
    
    * removed underscores
    
    * cleanup + action mapping
    
    * tidy
    
    * fully auth one line
    
    * return undefs
    
    * tidy
    
    * cleanup
    
    * ro
    
    * styleeeeeeee
    
    * error var GONE
    
    * tidy
    
    * lazy build
    
    * tidy
    
    * oops
    
    * eyad's comments
    
    * tidy
    
    * util func
    
    * tidy
    
    * ugh
    
    * tidy

--- lib/BOM/Platform/Client/IDAuthentication.pm
+++ lib/BOM/Platform/Client/IDAuthentication.pm
@@ -1,139 +1,228 @@
 package BOM::Platform::Client::IDAuthentication;
 
-use Moose;
-use namespace::autoclean;
+use Moo;
 use Try::Tiny;
 use List::Util qw( first );
 
 use Brands;
 use BOM::User::Client;
 use BOM::Config;
+use XML::LibXML;
 use BOM::Platform::Email qw(send_email);
 use BOM::Platform::Context qw(localize request);
 use BOM::Platform::ProveID;
 
 has client => (
-    is  => 'ro',
-    isa => 'BOM::User::Client'
+    is       => 'ro',
+    required => 1
 );
 
-has force_recheck => (
-    is      => 'ro',
-    isa     => 'Bool',
-    default => 0
-);
+=head2 run_authentication
+
+This is called for validation checks on first time deposits
+
+=cut
 
 sub run_authentication {
-    my $self   = shift;
-    my $client = $self->client;
+    my $self    = shift;
+    my $client  = $self->client;
+    my $loginid = $client->loginid;
 
-    return if $client->is_virtual;
-    # Binary Investment clients should already be fully_authenticated by the time this code runs following an intial deposit.
-    # Binary Investment accounts are set to "unwelcome" when they are first created.  Document
-    # submission is REQUIRED before the account is enabled for use
-    my $landing_company;
-    return if $client->fully_authenticated || ($landing_company = $client->landing_company->short) eq 'maltainvest';
+    return if $client->is_virtual || $client->fully_authenticated;
 
-    # any of these callouts might invoke _request_id_authentication which
-    # will return a structure suitable for passing to a mailer.
-    my $envelope;
-    if ($landing_company eq 'iom') {
-        $envelope = $self->do_proveid;
-    } elsif ($landing_company eq 'malta'
-        && !$client->status->age_verification
-        && !$client->has_valid_documents)
-    {
-        $envelope = $self->_request_id_authentication;
+    my $landing_company = $client->landing_company->short;
+
+    my $requirements = $client->landing_company->first_deposit_requirements;
+
+    my $action_mapping = {
+        age_verified => \&_age_verified,
+        proveid      => \&_proveid,
+        fully_auth   => \&_fully_auth,
+    };
+
+    for my $req (@$requirements) {
+        unless (exists $action_mapping->{$req}) {
+            warn "Invalid requirement";
+            next;
+        }
+        $action_mapping->{$req}->($self);
     }
 
-    send_email($envelope) if $envelope;
     return;
 }
 
-#
-# All logic in do_proveid meet compliance requirements, which can be changed over time
-#
-sub do_proveid {
-    my $self   = shift;
-    my $client = $self->client;
+=head2 _age_verified
 
-    my $set_status = sub {
-        my ($status, $reason, $description) = @_;
-        $description //= $reason;
-        $self->_notify($reason, $description) if $status ne 'age_verification';
-        return $client->status->set($status, 'system', $reason);
-    };
+Checks if client is age verified, if not, set cashier lock on client
 
-    my $prove_id_result = $self->_fetch_proveid;
-    my @matches = @{$prove_id_result->{matches} // []};
-    my $skip_request_for_id;
+=cut
 
-    # Do not send ID Authentication email if Experian request has failed, setting the user to unwelcome status
-    unless ($prove_id_result) {
-        $client->status->set('unwelcome', 'system', 'FailedExperian - Unable to fetch the Experian results');
-        return undef;
+sub _age_verified {
+    my $self    = shift;
+    my $client  = $self->client;
+    my $loginid = $client->loginid;
+
+    if (!$client->status->age_verification && !$client->has_valid_documents) {
+        $client->status->set('cashier_locked', 'system', 'Age verification is needed after first deposit.');
+        $self->_request_id_authentication();
+        $self->_notify_cs("Account $loginid cashier locked until age verification",
+            "An email has been sent out to the client requesting for Proof of Identification.");
     }
-
-    my $unwelcome_status = $client->status->unwelcome;
-
-    $client->status->clear_unwelcome
-        if ($unwelcome_status and $unwelcome_status->{reason} =~ /FailedExperian/);
-
-    # deceased or fraud => disable the client
-    if ($prove_id_result->{deceased} or $prove_id_result->{fraud}) {
-        my $key = $prove_id_result->{deceased} ? "deceased" : "fraud";
-        $set_status->('disabled', 'PROVE ID INDICATES ' . uc($key), "Client was flagged as $key by Experian Prove ID check");
-        $skip_request_for_id = 1;
-    }
-    # we have a match, handle it
-    elsif (@matches) {
-        my $type;
-        # Credit Reference Check
-        # Office of Foreign Assets Control, HM Treasury, Politically Exposed Person, presence in Sanction List => disable the client
-        if ($type = first { /(OFAC|BOE|PEP|OFSI)/ } @matches) {
-            $set_status->('disabled', "$type match");
-        }
-        # Director is ok, no unwelcome, no documents request
-        elsif (grep { /Directors/ } @matches) {
-        }
-        $skip_request_for_id = 1;
-    }
-    # County Court Judgement is ok, no unwelcome, no documents request
-    elsif ($prove_id_result->{CCJ}) {
-        $skip_request_for_id = 1;
-    }
-    # KYC Summary check
-    if ($prove_id_result->{kyc_summary_score} <= 2) {
-        $set_status->('unwelcome', 'INSUFFICIENT EXPERIAN KYC SUMMARY SCORE');
-    } else {
-        $set_status->('age_verification', 'EXPERIAN SUMMARY SCORE ENOUGH FOR AGE VERIFICATION');
-        $skip_request_for_id = 1;
-    }
-
-    # Save the latest changes to client status to avoid multiple calls to the DB
-    $client->save;
-
-    # unwelcome status will be set up there
-    return $self->_request_id_authentication unless $skip_request_for_id;
 
     return undef;
 }
 
+=head2 _fully_auth
+
+Checks if client is fully authenticated, if not, set client as unwelcome
+
+=cut
+
+sub _fully_auth {
+    my $self   = shift;
+    my $client = $self->client;
+
+    $client->status->set("unwelcome", "system", "Client was not fully authenticated before making first deposit") unless $client->fully_authenticated;
+
+    return undef;
+}
+
+=head2 _proveid
+
+Checks the proveid results of the client from Experian
+
+=cut
+
+sub _proveid {
+    my $self    = shift;
+    my $client  = $self->client;
+    my $loginid = $client->loginid;
+
+    my $prove_id_result = $self->_fetch_proveid;
+
+    return undef unless $prove_id_result;
+
+    my $xml = XML::LibXML->new()->parse_string($prove_id_result);
+
+    my ($credit_reference_summary) = $xml->findnodes('/Search/Result/CreditReference/CreditReferenceSummary');
+    my ($kyc_summary)              = $xml->findnodes('/Search/Result/Summary/KYCSummary');
+    my ($report_summary)           = $xml->findnodes('/Search/Result/Summary/ReportSummary/DatablocksSummary');
+
+    my $matches = {};
+
+    $matches->{"Deceased"} = $credit_reference_summary->findnodes('DeceasedMatch')->[0]->textContent() || 0;
+    $matches->{"Fraud"} =
+        $report_summary->findnodes('//DatablockSummary[Name="Fraud"]/Decision')->[0]->textContent() || 0;
+    $matches->{"PEP"}  = $credit_reference_summary->findnodes('PEPMatch')->[0]->textContent()  || 0;
+    $matches->{"BOE"}  = $credit_reference_summary->findnodes('BOEMatch')->[0]->textContent()  || 0;
+    $matches->{"OFAC"} = $credit_reference_summary->findnodes('OFACMatch')->[0]->textContent() || 0;
+
+    my @invalid_matches = grep { $matches->{$_} > 0 } keys %$matches;
+
+    if (@invalid_matches) {
+        my $msg = join(", ", @invalid_matches);
+        $client->status->set('disabled', 'system', "Experian categorizes this client as $msg.");
+        $self->_notify_cs("Account $loginid disabled following Experian results",
+            "Experian results has marked this client as $msg and an email has been sent out to the client requesting for Proof of Identification.");
+        return $self->_request_id_authentication();
+    }
+
+    # Handle DOB match for age verification
+    my $dob_match = ($kyc_summary->findnodes("DateOfBirth/Count"))[0]->textContent();
+
+    if ($dob_match > 0) {
+        return $client->status->set('age_verification', 'system', "Experian results are sufficient to mark client as age verified.");
+    } else {
+        $client->status->set('unwelcome', 'system', "Experian results are insufficient to mark client as age verified.");
+        $self->_notify_cs(
+            "Account $loginid unwelcome following Experian results",
+            "Experian results was insufficient to mark client as age verified and an email has been sent out to the client requesting for Proof of Identification."
+        );
+        return $self->_request_id_authentication();
+    }
+}
+
+=head2 _fetch_proveid
+
+Fetches the proveid result of the client from Experian through BOM::Platform::ProveID
+
+=cut
+
+sub _fetch_proveid {
+    my $self    = shift;
+    my $client  = $self->client;
+    my $loginid = $client->loginid;
+
+    my $result;
+    my $successful_request;
+    try {
+        $client->status->set('proveid_requested', 'system', 'ProveID request has been made for this account.');
+
+        $result = BOM::Platform::ProveID->new(client => $self->client)->get_result;
+
+        $successful_request = 1;
+    }
+    catch {
+        my $error = $_;
+
+        if ($error =~ /ErrorCode: 50\d/
+            ) # ErrorCode 500 and 501 are Search Errors according to Appendix B of https://github.com/regentmarkets/third_party_API_docs/blob/master/AML/20160520%20Experian%20ID%20Search%20XML%20API%20v1.22.pdf
+        {
+            # We don't retry when there is a search error (no entry or otherwise)
+            $client->status->set('unwelcome', 'system', 'No entry for this client found in Experian database.');
+            $self->_notify_cs(
+                "Account $loginid unwelcome due to lack of entry in Experian database",
+                "An email has been sent out to the client requesting for Proof of Identification."
+            );
+            $self->_request_id_authentication();
+            $successful_request = 1;    # Successful request made, even if response is invalid
+        } else {
+            # We set this flag for when the ProveID request fails and "cron_download_missing_192_pdf_reports.pl" will retry ProveID requests for these accounts every 12 hours
+            $client->status->set('proveid_pending', 'system', 'Experian request failed and will be attempted again within 12 hours.');
+            $client->status->set('unwelcome', 'system', 'FailedExperian - Experian request failed and will be attempted again within 12 hours.');
+
+            my $brand   = Brands->new(name => request()->brand);
+            my $loginid = $self->client->loginid;
+            my $message = <<EOM;
+There was an error during Experian request.
+Error is: $error
+Client: $loginid
+EOM
+            send_email({
+                    from    => $brand->emails('compliance'),
+                    to      => $brand->emails('compliance'),
+                    subject => "Experian request error for client $loginid",
+                    message => [$message]});
+            $successful_request = 0;
+        }
+
+        if ($successful_request) {
+            # On successful requests, we clear this status so the cron job will not retry ProveID requests on this account
+            $client->status->clear_proveid_pending;
+
+            # Clear unwelcome status set from failing Experian request
+            my $unwelcome_status = $client->status->unwelcome;
+            $client->status->clear_unwelcome if ($unwelcome_status && $unwelcome_status->{reason} =~ /^FailedExperian/);
+        }
+    };
+    return $result;
+}
+
+=head2 _request_id_authentication
+
+Sends an email to the client requesting for Proof of Identity
+
+=cut
+
 sub _request_id_authentication {
     my $self   = shift;
     my $client = $self->client;
-    my $status = 'cashier_locked';
-
-    # special case for MX: forbid them to trade before age_verified. cashier_locked enables to trade
-    $status = "unwelcome" if $client->landing_company->short eq 'iom';
-
-    $client->status->set($status, 'system', 'Experian id authentication failed on first deposit');
 
     my $client_name = join(' ', $client->salutation, $client->first_name, $client->last_name);
     my $brand         = Brands->new(name => request()->brand);
     my $support_email = $brand->emails('support');
-    my $ce_subject    = localize('Documents are required to verify your identity');
-    my $ce_body       = localize(<<'EOM', $client_name, $brand->website_name, $support_email);
+    my $subject       = localize('Documents are required to verify your identity');
+    my $body          = localize(<<'EOM', $client_name, $brand->website_name, $support_email);
 Dear [_1],
 
 We are writing to you regarding your account with [_2].
@@ -142,7 +231,7 @@ We are legally required to verify that clients are over the age of 18, and so we
 
 - Valid Passport or Driving licence or National ID card
 
-In order to comply with licencing regulations, you will be unable to make further deposits or withdrawals or to trade on the account until we receive this document.
+In order to comply with licencing regulations, you will be unable to make further deposits or withdrawals or to trade with your account until we receive this document.
 
 We look forward to hearing from you soon.
 
@@ -150,71 +239,31 @@ Kind regards,
 
 [_2]
 EOM
-    return ({
+    return send_email({
         from               => $support_email,
         to                 => $client->email,
-        subject            => $ce_subject,
-        message            => [$ce_body],
+        subject            => $subject,
+        message            => [$body],
         use_email_template => 1,
         template_loginid   => $client->loginid,
     });
 }
 
-sub _notify {
-    my ($self, $id, $msg) = @_;
+=head _notify_cs($subject, $body)
 
-    return unless BOM::Config::on_production();
+Adds an entry in desk.com for the client with $subject and $body
+
+=cut
+
+sub _notify_cs {
+    my ($self, $subject, $body) = @_;
+
+    return unless BOM::Config::on_production();    #TODO Remove when desk.com mocked server is ready. Also merge to main sub
 
     my $client = $self->client;
-    $client->add_note($id, $client->loginid . ' ' . $msg);
-    return;
+    $client->add_note($subject, $client->loginid . ' ' . $body);
+
+    return undef;
 }
 
-sub _fetch_proveid {
-    my $self = shift;
-
-    return unless BOM::Config::on_production();
-
-    my $client  = $self->client;
-    my $premise = $self->client->address_1;
-    if ($premise =~ /^(\d+)/) {
-        $premise = $1;
-    }
-    my $result;
-    try {
-        $client->status->multi_set_clear({
-            set => ['proveid_requested', 'proveid_pending'],
-        });
-        $result = BOM::Platform::ProveID->new(
-            client        => $self->client,
-            search_option => 'ProveID_KYC',
-            premise       => $premise,
-            force_recheck => $self->force_recheck
-        )->get_result;
-        # Workaround to distinguish failed search from failed request
-        # Failed search = user not found, failed request = error contacting Experian
-        $result //= {};
-        $client->status->clear_proveid_pending;
-    }
-    catch {
-        my $brand    = Brands->new(name => request()->brand);
-        my $clientid = $self->client->loginid;
-        my $message  = <<EOM;
-There was an error during Experian request.
-Error is: $_
-Client: $clientid
-EOM
-        warn "Experian error in _fetch_proveid: ", $_;
-        send_email({
-            from    => $brand->emails('compliance'),
-            to      => $brand->emails('compliance'),
-            subject => 'Experian request error',
-            message => [$message],
-        });
-    };
-    $client->save;
-    return $result;
-}
-
-__PACKAGE__->meta->make_immutable;
 1;
