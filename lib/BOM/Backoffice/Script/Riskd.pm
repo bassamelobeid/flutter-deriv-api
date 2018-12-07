50b3cd421f184f99c8d90f6b9cdf0dc05b503344
Mohammad Reza Hamedanchi <binary-hamedanchi@users.noreply.github.com>
2018-12-07 13:53:15 +0800

    mohammad/risk_monitoring (#1444)
    * Add monitoring for backoffice risk dashboard
    
    * Add pod for subroutines

--- lib/BOM/Backoffice/Script/Riskd.pm
+++ lib/BOM/Backoffice/Script/Riskd.pm
@@ -8,7 +8,10 @@ use BOM::Config::Runtime;
 
 use BOM::RiskReporting::Dashboard;
 use BOM::RiskReporting::MarkedToModel;
+use DataDog::DogStatsd::Helper qw(stats_gauge);
 use Try::Tiny;
+
+use constant INTERVAL => 37;
 
 has rest_period => (
     is         => 'ro',
@@ -17,7 +20,20 @@ has rest_period => (
 );
 
 sub _build_rest_period {
-    return Time::Duration::Concise::Localize->new(interval => '37s');
+    return Time::Duration::Concise::Localize->new(interval => INTERVAL.'s');
+}
+
+has last_run => (
+    is         => 'rw',
+    isa        => 'HashRef',
+    lazy_build => 1,
+);
+
+sub _build_last_run {
+    return +{
+        mtm       => time - 2 * INTERVAL, # we assume there was a previous successful run at the beginning, to prevent sending 0 as elapsed time
+        dashboard => time - INTERVAL,
+    };
 }
 
 sub run {
@@ -26,13 +42,16 @@ sub run {
     while (1) {
         try {
             BOM::RiskReporting::MarkedToModel->new->generate;
+            $self->send_log('MTM');
         }
         catch {
             warn "Failure in BOM::RiskReporting::MarkedToModel: $_\n";
         };
         $self->rest;
+
         try {
             BOM::RiskReporting::Dashboard->new->generate;
+            $self->send_log('Dashboard');
         }
         catch {
             warn "Failure in BOM::RiskReporting::Dashboard: $_\n";
@@ -42,6 +61,30 @@ sub run {
 
     return 255;
 }
+
+=head1 METHODS
+
+=head2 send_log
+
+Sends the elapsed time since the previous successful run of each process to DataDog
+and updates C<last_run> for the next round.
+
+=cut
+
+sub send_log {
+    my ($self, $type) = @_;
+    my $last_run      = $self->last_run;
+    my $now           = time;
+
+    stats_gauge('risk_reporting.run', $now - $last_run->{lc $type}, { tags => ["tag:$type"] });
+    $last_run->{lc $type} = $now;
+}
+
+=head2 rest
+
+Checks for possible config updates and delays the next process run for 'INTERVAL' seconds.
+
+=cut
 
 sub rest {
     my $self     = shift;
