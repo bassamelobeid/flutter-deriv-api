7b2823937561be6c3e051afa81a367b60e4a2901
chylli <chylli@binary.com>
2018-12-20 17:51:07 +0800

    make warn can emit utf8 messages (#1805)
    * make warn can emit utf8 messages
    
    * trigger

--- bin/binary_rpc.pl
+++ bin/binary_rpc.pl
@@ -8,6 +8,11 @@ use lib "$FindBin::Bin/../lib";
 use Mojo::JSON::MaybeXS;
 use if $ENV{MOCKTIME}, 'BOM::Test::Time';    # check BOM::Test::Time for details
 
+# There does not appear to be any specific handling for
+# output layers in Mojolicious::Commands, so we need to
+# set this ourselves.
+binmode STDERR, ':encoding(UTF-8)';
+
 # Start command line interface for application
 require Mojolicious::Commands;
 Mojolicious::Commands->start_app('BOM::RPC');
