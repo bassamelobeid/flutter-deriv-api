653d981eca228ac4bfda680b1fa8db416d9553b5
Tazeek <tazeek.rakib@gmail.com>
2019-01-10 10:58:52 +0800

    tazeek/refactor_fa (#143)
    * Removed score_keys
    
    * Removed section check
    
    * Updated should_warn
    
    * Removed SRP-related code
    
    * decode_fa changed
    
    * Removed financial assessment file
    
    * config is now global variable
    
    * Tidy
    
    * Updated _build_diffs
    
    * Semi colon
    
    * Further refactored _build_diffs
    
    * build_financial_assessment updated
    
    * Updated key name for email
    
    * Combined high risk check and CR client check
    
    * Entered into array
    
    * Missing dollar added
    
    * Documentation updated
    
    * Documentation updated
    
    * Reverted to previous checks
    
    * Tests fixed
    
    * Code for previous value checking modified
    
    * decode_fa correctly updated
    
    * handle non-existing records
    
    * Missing bracket added
    
    * Tidy
    
    * CircleCI rebuild
    
    * Changed to stable

--- lib/BOM/User/FinancialAssessment.pm
+++ lib/BOM/User/FinancialAssessment.pm
@@ -7,18 +7,19 @@ use BOM::User;
 use BOM::Platform::Account::Real::default;
 use BOM::Platform::Context qw (request);
 use BOM::Platform::Email qw(send_email);
+use BOM::Config;
 use Brands;
 
 use JSON::MaybeUTF8 qw(decode_json_utf8 encode_json_utf8);
-use YAML::XS;
 use List::Util qw/none all any/;
-use Array::Utils qw/array_minus/;
 
 use feature "state";
 
 use base qw (Exporter);
 our @EXPORT_OK =
-    qw(update_financial_assessment get_config build_financial_assessment is_section_complete decode_fa decode_obsolete_data should_warn get_section format_to_new);
+    qw(update_financial_assessment build_financial_assessment is_section_complete decode_fa decode_obsolete_data should_warn get_section format_to_new);
+
+my $config = BOM::Config::financial_assessment_fields();
 
 =head2 update_financial_assessment
 
@@ -32,13 +33,12 @@ sub update_financial_assessment {
     my ($user, $args, %options) = @_;
     my $is_new_mf_client = $options{new_mf_client} // 0;
 
-    # Doesn't matter which client we get as they all share the same financial assessment details. Will be changed when we move financial assessment data from client level to user level
+    # Doesn't matter which client we get as they all share the same financial assessment details
     my @all_clients = $user->clients();
     my $previous    = $all_clients[0]->financial_assessment();
     $previous = decode_fa($previous) if $previous;
 
-    # We need to update Financial Assessment data for each client. This will change when we move Financial Assessment data from client level to user level.
-    my $filtered_args = _filter_relevant_keys($args);
+    my $filtered_args = +{map { $_ => $args->{$_} } grep { $args->{$_} } @{_financial_assessment_keys()}};
 
     my $data_to_be_saved;
     $data_to_be_saved = {%$previous} if $previous;
@@ -47,27 +47,32 @@ sub update_financial_assessment {
         $data_to_be_saved->{$key} = $filtered_args->{$key};
     }
 
-    foreach my $cli (@all_clients) {    #TODO : change to only save this to the user after userdb change
+    # We need to update Financial Assessment data for each client.
+    foreach my $cli (@all_clients) {
         $cli->financial_assessment({data => encode_json_utf8($data_to_be_saved)});
         $cli->save;
     }
+
+    # Emails are sent for:
+    # - Non-CR clients
+    # - High risk CR with MT5 accounts
     my @client_ids = $user->loginids();
     if (my @cr_clients = $user->clients_for_landing_company('costarica')) {
-        if (   (any { $_->aml_risk_level eq 'high' } @cr_clients)
-            && (any { $_ =~ /^MT/ } @client_ids))    # TODO : Change MT5 check to mt5_logins when that is changed not to hit MT5's servers
-        {
-            return _email_diffs_to_compliance($previous, $args, \@client_ids, $is_new_mf_client);
-        }
-    } else {                                         # if the user falls under any other landing company
+
+        return _email_diffs_to_compliance($previous, $args, \@client_ids, $is_new_mf_client)
+            if ((any { $_ =~ /^MT/ } @client_ids) && (any { $_->aml_risk_level eq 'high' } @cr_clients));
+    } else {
         return _email_diffs_to_compliance($previous, $args, \@client_ids, $is_new_mf_client);
     }
+
+    return undef;
 }
 
-# Given the previous and the new financial assessment data, parses the changes made and email said changes to compliance.
+# Email to compliance, based on updates on financial assessment
 sub _email_diffs_to_compliance {
     my ($previous, $new, $client_ids, $is_new_mf_client) = @_;
+
     my $message;
-    my $brand = Brands->new(name => request()->brand);
     $new = build_financial_assessment($new);
     my $subject = join ", ", grep { $_ !~ /^VR|MT/ } @$client_ids;
 
@@ -75,17 +80,23 @@ sub _email_diffs_to_compliance {
         my $diffs = _build_diffs($new, $previous);
 
         foreach my $key (keys %$diffs) {
+
             $message .= "$key : " . $diffs->{$key}->[0] . "  ->  " . $diffs->{$key}->[1] . "\n";
         }
 
         $subject .= ' assessment test details have been updated';
+
     } elsif ($is_new_mf_client) {
+
         foreach my $section (keys %$new) {
-            if ($section ne "scores") {
-                foreach my $key (keys %{$new->{$section}}) {
-                    my $key_obj = $new->{$section}->{$key};
-                    $message .= "$key_obj->{label} : $key_obj->{answer}\n" if $key_obj->{answer};
-                }
+
+            next if $section eq "scores";
+
+            foreach my $key (keys %{$new->{$section}}) {
+                my $key_obj = $new->{$section}->{$key};
+                next unless $key_obj->{answer};
+
+                $message .= "$key_obj->{label} : $key_obj->{answer}\n";
             }
         }
 
@@ -95,9 +106,9 @@ sub _email_diffs_to_compliance {
     return undef unless $message;
 
     $message .= "\nTotal Score :  " . $new->{scores}->{total_score} . "\n";
-    $message .= "Trading Experience Score :  " . $new->{scores}->{trading_score} . "\n";
+    $message .= "Trading Experience Score :  " . $new->{scores}->{trading_experience} . "\n";
     $message .= "CFD Score :  " . $new->{scores}->{cfd_score} . "\n";
-    $message .= "Financial Information Score :  " . $new->{scores}->{financial_information_score};
+    $message .= "Financial Information Score :  " . $new->{scores}->{financial_information};
 
     if ($is_new_mf_client) {
         # If it has gotten to this point with should_warn being true, the client would already have had to accept the risk disclosure
@@ -105,6 +116,7 @@ sub _email_diffs_to_compliance {
         $message .= should_warn($new) ? "shown and client accepted the disclosure." : "not shown.";
     }
 
+    my $brand = Brands->new(name => request()->brand);
     return send_email({
         from    => $brand->emails('support'),
         to      => $brand->emails('compliance'),
@@ -113,78 +125,69 @@ sub _email_diffs_to_compliance {
     });
 }
 
-# Takes in a built FA (using build_financial_assessment) and an unbuilt one and returns their differences in a hash containing arrays
+=head2 _build_diffs
+
+Takes in a built FA (from client-side) and an unbuilt one (from database) and returns their differences in a hash containing arrays
+
+=cut
+
 sub _build_diffs {
     my ($new, $previous) = @_;
     my $diffs;
 
     for my $sect (sort keys %$new) {
-        if ($sect ne 'scores') {    #Score are not used in finding the difference in Financial Assessment data
-            for my $key (sort keys %{$new->{$sect}}) {
-                if ($new->{$sect}->{$key}->{answer}) {
-                    if (!$previous->{$key}) {
-                        $diffs->{$new->{$sect}->{$key}->{label}} = ["N/A", $new->{$sect}->{$key}->{answer}];
-                    } elsif ($previous->{$key} ne $new->{$sect}->{$key}->{answer}) {
-                        $diffs->{$new->{$sect}->{$key}->{label}} = [$previous->{$key}, $new->{$sect}->{$key}->{answer}];
-                    }
-                }
+
+        # Score are not used in finding the difference in Financial Assessment data
+        next if $sect eq 'scores';
+
+        for my $key (sort keys %{$new->{$sect}}) {
+
+            my $new_answer = $new->{$sect}->{$key}->{answer};
+            next unless $new_answer;
+
+            my $previous_answer = $previous->{$key};
+
+            unless ($previous_answer) {
+                $previous_answer = "N/A";
+            } elsif ($previous_answer eq $new_answer) {
+                next;
             }
+
+            $diffs->{$new->{$sect}->{$key}->{label}} = [$previous_answer, $new_answer];
         }
     }
 
     return $diffs;
 }
 
-# This function takes in the old structure of the data stored in the database and formats it to the new structure that is to be applied by an SRP
-sub format_to_new {
-    my $args = shift;
+=head2 build_financial_assessment
 
-    return +{map { $_ => $args->{$_}->{answer} } grep { ref $args->{$_} } keys %$args};
-}
+Takes in raw data (label => answer) and produces evaluated structured financial assessment output
 
-sub get_config {
-    state $config = YAML::XS::LoadFile('/home/git/regentmarkets/bom-user/share/financial_assessment_structure.yml');
-    return $config;
-}
+Two main labels are build: Trading experience and financial information
 
-sub _filter_relevant_keys {
-    my $args = shift;
+=cut
 
-    return +{map { $_ => $args->{$_} } grep { $args->{$_} } @{_financial_assessment_keys()}};
-}
-
-# Takes in raw data (label => answer) and produces evaluated structured financial assessment output
 sub build_financial_assessment {
     my $raw = shift;
 
-    my $config = get_config();
     my $result;
 
-    for my $key (keys %{$config->{financial_information}}) {
-        $result->{financial_information}->{$key}->{label}  = $config->{financial_information}->{$key}->{label};
-        $result->{financial_information}->{$key}->{answer} = $raw->{$key};
+    foreach my $fa_information (keys %$config) {
+        foreach my $key (keys %{$config->{$fa_information}}) {
 
-        my $provided_answer  = $raw->{$key};
-        my $possible_answers = $config->{financial_information}->{$key}->{possible_answer};
+            my $provided_answer  = $raw->{$key};
+            my $possible_answers = $config->{$fa_information}->{$key}->{possible_answer};
+            my $score            = ($provided_answer && defined $possible_answers->{$provided_answer}) ? $possible_answers->{$provided_answer} : 0;
 
-        my $score = ($provided_answer && defined $possible_answers->{$provided_answer}) ? $possible_answers->{$provided_answer} : 0;
-        $result->{financial_information}->{$key}->{score} = $score;
-        $result->{scores}->{financial_information_score} += $score;
-        $result->{scores}->{total_score}                 += $score;
-    }
+            $result->{$fa_information}->{$key}->{label}  = $config->{$fa_information}->{$key}->{label};
+            $result->{$fa_information}->{$key}->{answer} = $provided_answer;
+            $result->{$fa_information}->{$key}->{score}  = $score;
 
-    for my $key (keys %{$config->{trading_experience}}) {
-        $result->{trading_experience}->{$key}->{label}  = $config->{trading_experience}->{$key}->{label};
-        $result->{trading_experience}->{$key}->{answer} = $raw->{$key};
-
-        my $provided_answer  = $raw->{$key};
-        my $possible_answers = $config->{trading_experience}->{$key}->{possible_answer};
-
-        my $score = ($provided_answer && defined $possible_answers->{$provided_answer}) ? $possible_answers->{$provided_answer} : 0;
-        $result->{trading_experience}->{$key}->{score} = $score;
-        $result->{scores}->{trading_score} += $score;
-        $result->{scores}->{cfd_score}     += $score if $key eq 'cfd_trading_frequency' or $key eq 'cfd_trading_experience';
-        $result->{scores}->{total_score}   += $score;
+            $result->{scores}->{$fa_information} += $score;
+            $result->{scores}->{total_score} += $score;
+            $result->{scores}->{cfd_score} += $score if $key eq 'cfd_trading_frequency' or $key eq 'cfd_trading_experience';
+        }
     }
 
     return $result;
@@ -193,19 +196,13 @@ sub build_financial_assessment {
 sub _financial_assessment_keys {
     my $should_split = shift;
 
-    my $config = get_config();
     return +{map { $_ => [keys %{$config->{$_}}] } keys %$config} if $should_split;
     return [map { keys %$_ } values %$config];
-}
-
-sub _score_keys {
-    return ('trading_score', 'financial_information_score', 'cfd_score', 'total_score');
 }
 
 sub is_section_complete {
     my $fa      = shift;
     my $section = shift;
-    my $config  = get_config();
 
     return 0 + all {
         $fa->{$_} && defined $config->{$section}->{$_}->{possible_answer}->{$fa->{$_}}
@@ -213,41 +210,35 @@ sub is_section_complete {
     @{_financial_assessment_keys(1)->{$section}};
 }
 
-# Decodes the raw FA data from the DB. Can take in a section parameter to only return fields relevant to that section (trading_experience or financial_information).
 sub decode_fa {
-    my $fa  = shift;
-    my $key = shift;
-    $fa = $fa ? decode_json_utf8($fa->data || '{}') : undef;
-    return {} unless $fa;
-    # Only the old structure would have an answer object inside a key, will remove when SRP goes through to change all old formats to new
-    if (any { ref($fa->{$_}) } keys %$fa) {
-        $fa = format_to_new($fa);
-    }
+    my $fa = shift;
 
-    return $fa unless $key;
-    return +{map { $_ => $fa->{$_} } grep { $fa->{$_} } @{_financial_assessment_keys(1)->{$key}}};
+    return {} unless $fa;
+
+    $fa = $fa->data ? decode_json_utf8($fa->data) : {};
+
+    return $fa;
 }
 
-# Show the Risk Disclosure warning message when the trading score is less than 8 or CFD score is less than 4
+=head2 should_warn
+
+Show the Risk Disclosure warning message when the trading score is less than 8 or CFD score is less than 4
+
+=cut 
+
 sub should_warn {
     my $fa = shift;
 
     # Only parse if it's not already parsed
     $fa = $fa->{scores} ? $fa : build_financial_assessment($fa);
+    my $scores = $fa->{scores};
 
-    # No warning when CFD score is 4 or more
-    return 0 if $fa->{scores}->{cfd_score} >= 4;
+    my $cfd_score_warn = $scores->{cfd_score} >= 4;
+    my $trading_score_warn = ($scores->{trading_experience} >= 8 && $scores->{trading_experience} <= 16);
 
-    # No warning when trading score is from 8 to 16
-    return 0 if ($fa->{scores}->{trading_score} >= 8 and $fa->{scores}->{trading_score} <= 16);
+    return 0 if $cfd_score_warn || $trading_score_warn;
 
     return 1;
-}
-
-sub get_section {
-    my $client  = shift;
-    my $section = shift;
-    return decode_fa($client->financial_assessment(), $section);
 }
 
 1;
