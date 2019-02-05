b70c979151b8b44cd64b9f8cb62022e370876e97
Raunak Kathuria <raunakkathuria@gmail.com>
2019-02-05 20:31:20 +0800

    raunak/plug_customerio (#21)
    https://trello.com/c/9CZooyXr/8045-8-raunak-plugcustomerio-4
    
    * add sub to delete record for customerio, general refactoring
    
    * fix syntax and test
    
    * update config key name
    
    * map email_consent to unsubscribe
    
    * delete all loginid record for email consent

--- lib/BOM/Event/Actions/Customerio.pm
+++ lib/BOM/Event/Actions/Customerio.pm
@@ -36,29 +36,7 @@ sub register_details {
 
     return undef unless $client;
 
-    my $details = {
-        # mandatory
-        email      => $client->email,
-        created_at => Date::Utility->new($client->date_joined)->epoch,
-        # optional
-
-        company         => $client->landing_company->short,
-        language        => $data->{language} // 'EN',
-        first_name      => $client->first_name // '',
-        last_name       => $client->last_name // '',
-        affiliate_token => $client->myaffiliates_token // '',
-        unsubscribed    => $data->{unsubscribe} ? 'true' : 'false',
-        account_type    => $client->is_virtual ? 'virtual' : 'real',
-        country_code => $client->residence // '',
-        country => $client->residence ? Locale::Country::code2country($client->residence) : '',
-        is_region_eu => $client->landing_company->short =~ /^(?:malta|iom)/ ? 1 : 0,
-
-        # remove these before passing
-        loginid => $loginid,
-
-    };
-
-    return _connect_and_update_customer_io($details);
+    return _connect_and_update_customerio_record(_get_details_structure($client, $data));
 }
 
 =head2 email_consent
@@ -75,7 +53,7 @@ When a user subscribes/unsubscribes to marketing's newsletter
 
 sub email_consent {
     my $data    = shift;
-    my $loginid = $data->{loginid};
+    my $loginid = delete $data->{loginid};
 
     return undef unless $loginid;
 
@@ -83,24 +61,28 @@ sub email_consent {
 
     return undef unless $client;
 
-    my $details = {
-        unsubscribed => $data->{email_consent} ? 'false' : 'true',
-        loginid => $loginid,
-    };
+    # trigger event for each client as customerio save it per loginid
+    foreach my $client_obj ($client->user->clients()) {
+        _connect_and_update_customerio_record(
+            loginid => $client_obj->loginid,
+            details => _get_details_structure(
+                client          => $client_obj,
+                customerio_data => $data
+            ))
+            and next
+            if (exists $data->{email_consent} and $data->{email_consent});
 
-    return _connect_and_update_customer_io($details);
+        _connect_and_delete_customerio_record(loginid => $client_obj->loginid);
+    }
+
+    return 1;
 }
 
-sub _connect_and_update_customer_io {
-    my $details = shift;
+sub _connect_and_delete_customerio_record {
+    my (%args) = @_;
 
-    my $ua = Mojo::UserAgent->new->connect_timeout(TIMEOUT);
-
-    my $config = BOM::Config::third_party()->{customerio};
-    my $url    = Mojo::URL->new($config->{api_uri} . '/customers/' . delete $details->{loginid});
-    $url->query($details);
-    $url->userinfo($config->{api_site_id} . ':' . $config->{api_key});
-    my $tx = $ua->put($url);
+    my $url = _get_base_url($args{loginid});
+    my $tx  = _get_user_agent()->delete($url);
 
     if (my $err = $tx->error) {
         die 'Error - ' . $err->{code} . ' response: ' . $err->{message} if $err->{code};
@@ -108,6 +90,63 @@ sub _connect_and_update_customer_io {
     }
 
     return $tx->success ? 1 : 0;
+}
+
+sub _connect_and_update_customerio_record {
+    my (%args) = @_;
+
+    my $url = _get_base_url($args{loginid});
+    $url->query($args{details});
+    my $tx = _get_user_agent()->put($url);
+
+    if (my $err = $tx->error) {
+        die 'Error - ' . $err->{code} . ' response: ' . $err->{message} if $err->{code};
+        die 'Error - Connection error: ' . $err->{message};
+    }
+
+    return $tx->success ? 1 : 0;
+}
+
+sub _get_user_agent {
+    return Mojo::UserAgent->new->connect_timeout(TIMEOUT);
+}
+
+sub _get_base_url {
+    my $customerio_id = shift;
+
+    my $config = BOM::Config::third_party()->{customerio};
+    my $url    = Mojo::URL->new($config->{api_uri} . "/customers/$customerio_id");
+    $url->userinfo($config->{site_id} . ':' . $config->{api_key});
+
+    return $url;
+}
+
+sub _get_details_structure {
+    my (%details) = @_;
+
+    my $client = $details{client};
+    my $data   = $details{customerio_data};
+
+    return {
+        # mandatory
+        email      => $client->user->email,
+        created_at => Date::Utility->new($client->date_joined)->epoch,
+
+        # optional
+        company         => $client->landing_company->short,
+        language        => $data->{language} // 'EN',
+        first_name      => $client->first_name // '',
+        last_name       => $client->last_name // '',
+        affiliate_token => $client->myaffiliates_token // '',
+        unsubscribed    => $data->{email_consent} ? 'false' : 'true',
+        account_type    => $client->is_virtual ? 'virtual' : 'real',
+        country_code => $client->residence // '',
+        country => $client->residence ? Locale::Country::code2country($client->residence) : '',
+        is_region_eu => $client->landing_company->short =~ /^(?:malta|iom)/ ? 1 : 0,
+
+        # remove these before passing
+        loginid => $client->loginid,
+    };
 }
 
 1;
