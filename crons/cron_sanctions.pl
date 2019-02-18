fd6b060caa312b7dd422819f518098954bcd5953
Tazeek <tazeek.rakib@gmail.com>
2019-02-18 23:35:51 +0800

    tazeek/refactor_cron_sanctions_part_one (#1503)
    https://trello.com/c/FQ2zkDNa/8240-16-tazeek-refactorcronsanctionspartone
    
    * Database checks re-ordered
    
    * Go per broker code
    
    * Remove $r
    
    * Adjustments made
    
    * Added new function
    
    * Database function working
    
    * Using single quotes
    
    * Updated fix
    
    * 0 replaced with empty string
    
    * Use today
    
    Co-Authored-By: tazeek <tazeek.rakib@gmail.com>
    
    * Reverting
    
    * Re-added path
    
    * Complete the comments
    
    * Closing section added

--- crons/cron_sanctions.pl
+++ crons/cron_sanctions.pl
@@ -6,7 +6,7 @@ binmode STDOUT, ':encoding(UTF-8)';
 
 use Date::Utility;
 use Data::Validate::Sanctions;
-use Path::Tiny;
+use Path::Tiny qw(path);
 use Getopt::Long;
 use Text::CSV;
 
@@ -32,15 +32,20 @@ GetOptions(
 ) or die "Invalid argument\n";
 
 Log::Any::Adapter->import(qw(Stdout), log_level => $verbose ? 'info' : 'warning');
-
 my $reports_path = shift or die "Provide path for storing files as an argument";
-my @brokers = qw/CR MF MLT MX/;
+
+# The broker codes are ordered for two reasons:
+# 1. Regulated broker codes (EU) has a higher priority
+# 2. The regulated broker codes have a smaller number of clients, in comparison to CR
+# Thus, it would be better to send the emails one at a time, with regulated ones first
+my @brokers = qw/MF MX MLT CR/;
 
 my $sanctions = Data::Validate::Sanctions->new(sanction_file => BOM::Config::sanction_file);
 
 my $file_flag = path('/tmp/last_cron_sanctions_check_run');
 my $last_run = $file_flag->exists ? $file_flag->stat->mtime : 0;
 $sanctions->update_data();
+
 $file_flag->spew("Created by $0 PID $$");
 if (($last_run > $sanctions->last_updated()) && !$force) {
     $log->infof('Exiting because sanctions appear up to date (file=%s)', $file_flag);
@@ -53,35 +58,52 @@ do_report();
 
 sub do_report {
 
-    my $r = '';
-    for my $broker (@brokers) {
-        $log->infof('Starting %s at %s', $broker, scalar localtime);
-        my $result = get_matched_clients_info_by_broker($broker);
-        $r .= $result // '';
-        $log->infof('Finished %s at %s', $broker, scalar localtime);
-    }
-
-    $log->info($r);
-
-    my $headers =
-        'Matched name,List Name,List Updated,DOB (From list),DOB (Client),Database,LoginID,First Name,Last Name,Gender,Date Joined,Residence,Citizen,Matched Reason';
-
-    my $csv_file = path($reports_path . '/sanctions-run-' . Date::Utility->new()->date . '.csv');
-    $csv_file->append_utf8($headers . "\n");
-    $csv_file->append_utf8($r . "\n");
-
+    my $result;
     my $brand = Brands->new(name => 'binary');
 
-    $log->infof('Wrote CSV file %s', $csv_file);
+    my $today_date = Date::Utility::today()->date;
 
-    send_email({
-        from       => $brand->emails('support'),
-        to         => join(',', $brand->emails('compliance'), 'sysadmin@binary.com'),
-        subject    => 'Sanction list checked',
-        message    => ["Here is a list of clients against sanctions:\n$r"],
-        attachment => $csv_file->canonpath,
+    my @headers =
+        (
+        'Matched name,List Name,List Updated,DOB (From list),DOB (Client),Database,LoginID,First Name,Last Name,Gender,Date Joined,Residence,Citizen,Matched Reason'
+        );
+
+    my $csv = Text::CSV->new({
+        eol        => "\n",
+        quote_char => undef
     });
-    return;
+
+    my $csv_rows;
+
+    for my $broker (@brokers) {
+
+        $log->infof('Starting %s at %s', $broker, scalar localtime);
+
+        $csv_rows = get_matched_clients_info_by_broker($broker);
+
+        $log->infof('Finished %s at %s', $broker, scalar localtime);
+
+        # CSV creation starts here
+        my $filename = path($reports_path . '/' . $broker . '_daily_sanctions_report_' . $today_date . '.csv');
+        my $file     = path($filename)->openw_utf8;
+
+        $csv->print($file, \@headers);
+        $csv->print($file, $_) for @$csv_rows;
+        # CSV creation ends here
+
+        $log->infof('Wrote CSV file %s', $filename);
+
+        close $file;
+
+        send_email({
+            from       => $brand->emails('support'),
+            to         => join(',', $brand->emails('compliance'), 'sysadmin@binary.com'),
+            subject    => 'Sanction list for ' . $broker . ' at ' . $today_date,
+            attachment => $filename->canonpath,
+        });
+    }
+
+    return undef;
 }
 
 sub get_matched_clients_info_by_broker {
@@ -93,45 +115,49 @@ sub get_matched_clients_info_by_broker {
             db_operation => 'write',
         })->db->dbic;
 
-    my $output = '';
-    my $csv    = Text::CSV->new({
-        eol        => "\n",
-        quote_char => undef
-    });
+    my @csv_rows;
+    my $csv_row;
+    my $client;
+    my $client_matched;
+    my $sinfo;
 
-    my $updates = $dbic->run(
+    $dbic->run(
         fixup => sub {
+
             my $update_sanctions_check =
                 $_->prepare(q{UPDATE betonmarkets.sanctions_check SET result=?,tstmp=now(),type='C' WHERE client_loginid = ?});
 
-            my $sth = $_->prepare(
+            my $query_filter = $_->prepare(
                 q{
-                   SELECT broker_code, loginid, first_name, last_name, email, phone, gender, date_of_birth, date_joined, residence, citizen
-                   FROM betonmarkets.client
-                   WHERE loginid ~ ('^' || ? || '\\d')
-                   ORDER BY loginid
-                 }
+                SELECT broker_code, loginid, first_name, last_name, gender, date_of_birth, date_joined, residence, citizen
+                FROM betonmarkets.client
+                WHERE loginid ~ ('^' || ? || '\\d')
+                ORDER BY loginid
+            }
             );
-            $sth->execute($broker);
-            my $client;
-            while ($client = $sth->fetchrow_hashref()) {
 
-                my $sinfo = $sanctions->get_sanctioned_info($client->{first_name}, $client->{last_name}, $client->{date_of_birth});
-                $update_sanctions_check->execute($sinfo->{matched} ? $sinfo->{list} : '0', $client->{loginid});
+            $query_filter->execute($broker);
 
-                if ($sinfo->{matched}) {
-                    my @fields = (
-                        $sinfo->{name},
-                        $sinfo->{list},
-                        $listdate{$sinfo->{list}} //= Date::Utility->new($sanctions->last_updated($sinfo->{list}))->date,
-                        $sinfo->{matched_dob},
-                        (map { $client->{$_} // '' } qw(date_of_birth broker_code loginid first_name last_name gender date_joined residence citizen)),
-                        $sinfo->{reason});
-                    $csv->combine(@fields);
-                    $output .= $csv->string();
-                }
+            while ($client = $query_filter->fetchrow_hashref()) {
+
+                $sinfo = $sanctions->get_sanctioned_info($client->{first_name}, $client->{last_name}, $client->{date_of_birth});
+                $client_matched = $sinfo->{matched} ? $sinfo->{list} : '';
+
+                $update_sanctions_check->execute($client_matched, $client->{loginid});
+
+                next unless $sinfo->{matched};
+
+                $csv_row = [
+                    $sinfo->{name},
+                    $sinfo->{list},
+                    $listdate{$sinfo->{list}} //= Date::Utility->new($sanctions->last_updated($sinfo->{list}))->date,
+                    $sinfo->{matched_dob},
+                    (map { $client->{$_} // '' } qw(date_of_birth broker_code loginid first_name last_name gender date_joined residence citizen)),
+                    $sinfo->{reason}];
+
+                push @csv_rows, $csv_row;
             }
         });
 
-    return $output;
+    return \@csv_rows;
 }
