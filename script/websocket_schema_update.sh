d4f042f6b283b552670e966e565d30a8a0f8f27f
Tom Molesworth <tom@binary.com>
2019-03-27 11:50:54 +0800

    tom/websocket_schema_update (#2707)
    https://trello.com/c/eI98MqMt/8524-2-tom-websocketschemaupdate-05
    
    * tom/websocket_schema_update
    
    * Include doc build step
    
    * Remove unused alias
    
    * Test: show changes but do not update yet
    
    * No need for services
    
    * Enable update step
    
    * Remove test step for building on PRs

---
+++ script/websocket_schema_update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+rm -rf /tmp/websockets
+
+git clone git@github.com:binary-com/websockets /tmp/websockets
+
+cd /tmp/websockets
+git config --local user.email "sysadmin@binary.com"
+git config --local user.name "CircleCI"
+
+rsync /home/git/regentmarkets/binary-websocket-api/config /tmp/websockets/ --delete -a
+rm -fvr /tmp/websockets/config/*/change_password
+rm -fvr /tmp/websockets/config/*/reset_password
+
+# Show what we're changing - anyone can easily check version control for this,
+# but this diff usually is not too long.
+git diff | cat
+
+git add -A
+# Commit fails if there is nothing to commit. We don't want to commit
+# in such cases but we don't want to fail either. So, we catch the
+# output and investigate what went wrong with the commit.
+# The tee command is added to see the message in the log.
+X="$(git commit -m "JSON Schema Update")" ||
+tee /dev/stderr <<<"$X" | grep -q 'nothing to commit'
+git push origin HEAD
+
