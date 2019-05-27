6475cf5686d13a75add3b3276a9d9a8fc497c5e7
Michael.Mueller <michael.mueller@binary.com>
2019-05-27 08:59:35 +0000

    make tidy

--- lib/BOM/Test/WebsocketAPI/Template/Transaction.pm
+++ lib/BOM/Test/WebsocketAPI/Template/Transaction.pm
@@ -17,24 +17,24 @@ rpc_request transaction => sub {
         args => {
             transaction => 1,
         },
-     shortcode => sprintf('%s_%s_20.43_1557198681_1557705599_S0P_0', $_->contract->contract_type, $_->contract->underlying->symbol)
-    };
+        shortcode => sprintf('%s_%s_20.43_1557198681_1557705599_S0P_0', $_->contract->contract_type, $_->contract->underlying->symbol)};
     },
     qw(contract);
 
 rpc_response transaction => sub {
-   
+
     my $contract = $_->contract;
     {
-            'longcode'          => sprintf(
-                'Win payout if %s is strictly %s than entry spot at close on 2019-04-29.',
-                $contract->underlying->display_name,
-                $contract->contract_type eq 'CALL' ? 'higher' : 'lower'
-            ),
-            'display_name' => $contract->underlying->display_name,
-            'date_expiry' => 1557705599,
-            'symbol' => $contract->underlying->symbol,
-            barrier => 'S0P'
-    }; };
+        'longcode' => sprintf(
+            'Win payout if %s is strictly %s than entry spot at close on 2019-04-29.',
+            $contract->underlying->display_name,
+            $contract->contract_type eq 'CALL' ? 'higher' : 'lower'
+        ),
+        'display_name' => $contract->underlying->display_name,
+        'date_expiry'  => 1557705599,
+        'symbol'       => $contract->underlying->symbol,
+        barrier        => 'S0P'
+    };
+};
 
 1;
