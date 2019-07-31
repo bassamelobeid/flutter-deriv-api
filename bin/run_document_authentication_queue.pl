33192f84052a3af4abbf4f5e8122856c3dd0cff7
Nobody User <nobody@qa39.regentmarkets.com>
2019-07-31 03:41:04 +0000

    forgot the change back to info

--- bin/run_document_authentication_queue.pl
+++ bin/run_document_authentication_queue.pl
@@ -3,6 +3,6 @@ use strict;
 use warnings;
 use BOM::Event::Listener;
 
-use Log::Any::Adapter qw(Stderr), log_level => 'debug';
+use Log::Any::Adapter qw(Stderr), log_level => $ENV{BOM_LOG_LEVEL} // 'info';
 
 BOM::Event::Listener->new(queue => 'DOCUMENT_AUTHENTICATION_QUEUE')->run;
