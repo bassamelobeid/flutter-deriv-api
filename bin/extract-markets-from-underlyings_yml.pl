cabf4ce9b5daaf0bc89d09236105457d3f583f55
junbon-binary <junbon@binary.com>
2019-08-01 13:35:33 +0800

    junbon/rename_betdotmarket_first (#654)
    * modify script
    
    * modify script to be more flexible

--- bin/extract-markets-from-underlyings_yml.pl
+++ bin/extract-markets-from-underlyings_yml.pl
@@ -9,7 +9,7 @@
 # The output is a table of symbols that have been changed (inserted or updated).
 #
 # NOTE: bet.open_contract_aggregates and bet.global_aggregates do not have to
-#       be re-recreated because of changes on bet.market.
+#       be re-recreated because of changes on bet.limits_market_mapper.
 
 # NOTE: we should NOT delete symbols unless we are absolutely sure there are
 #       no open contracts using them.
@@ -20,11 +20,11 @@ use YAML qw/LoadFile/;
 use File::ShareDir ();
 
 my $l=LoadFile(File::ShareDir::dist_file('Finance-Underlying', 'underlyings.yml'));
+my ($connection_param, $tablename) = @ARGV;
+open my $psql, '|-', 'psql', '-X1', '-v', 'ON_ERROR_STOP=on', $connection_param;
 
-open my $psql, '|-', 'psql', '-X1', '-v', 'ON_ERROR_STOP=on', @ARGV;
-
-print $psql <<'EOF';
-CREATE TEMP TABLE tt(LIKE bet.market) ON COMMIT DROP;
+print $psql <<"EOF";
+CREATE TEMP TABLE tt(LIKE $tablename) ON COMMIT DROP;
 COPY tt(symbol, market, submarket, market_type) FROM stdin;
 EOF
 
@@ -33,8 +33,8 @@ print $psql "$_\t$l->{$_}->{market}\t$l->{$_}->{submarket}\t".(defined $l->{$_}-
 print $psql "\\.\n";
 
 
-print $psql <<'EOF';
-INSERT INTO bet.market AS m(symbol, market, submarket, market_type)
+print $psql <<"EOF";
+INSERT INTO $tablename AS m(symbol, market, submarket, market_type)
 SELECT symbol, market, submarket, market_type FROM tt
     ON CONFLICT(symbol) DO UPDATE
    SET market=EXCLUDED.market, submarket=EXCLUDED.submarket, market_type=EXCLUDED.market_type
