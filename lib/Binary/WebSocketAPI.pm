952e243930bfccbfc2cea3d35e411d85b323b639
chengyang-binary <45479072+chengyang-binary@users.noreply.github.com>
2019-08-19 14:30:31 +0800

    limchengyang/notification_event_api (#2930)
    * initial version
    
    * rerun
    
    * rerun
    
    * small change in schema
    
    * added enum
    
    * Update config/v3/notification_event/receive.json
    
    Co-Authored-By: Mohammad Reza Hamedanchi <14048882+mohammad-binary@users.noreply.github.com>

--- lib/Binary/WebSocketAPI.pm
+++ lib/Binary/WebSocketAPI.pm
@@ -514,6 +514,7 @@ sub startup {
         ['copy_stop',          {require_auth => 'trade'}],
         ['app_markup_details', {require_auth => 'read'}],
         ['account_security',   {require_auth => 'admin'}],
+        ['notification_event', {require_auth => 'admin'}],
         [
             'service_token',
             {
