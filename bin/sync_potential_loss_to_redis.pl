7daa75bb62c5958520b33390c566982c26fbfce2
bruce-binary <bruce@binary.com>
2019-09-10 06:22:50 +0000

    remove landing company hard coding

--- bin/sync_potential_loss_to_redis.pl
+++ bin/sync_potential_loss_to_redis.pl
@@ -3,14 +3,15 @@
 use strict;
 use warnings;
 
-use BOM::CompanyLimits;
 use BOM::CompanyLimits::SyncLoss;
+use LandingCompany::Registry;
 
-foreach my $lc (BOM::CompanyLimits::get_supported_landing_companies()) {
+foreach my $lc (LandingCompany::Registry::all()) {
     my $landing_company = $lc->{short};
-    # Assume one-to-one correlation between broker code and landing company
-    my $broker_code = $lc->{broker_codes}[0];
-    my $output = BOM::CompanyLimits::SyncLoss::sync_potential_loss_to_redis($broker_code, $landing_company);
-    print "Update potential loss from $broker_code clientdb ($landing_company)... Result: $output\n";
+    foreach my $broker_code (@{$lc->{broker_codes}}) {
+        my $output = BOM::CompanyLimits::SyncLoss::sync_potential_loss_to_redis($broker_code, $landing_company);
+        $output = $output->[1] if ref $output eq 'ARRAY';
+        print "Update potential loss from $broker_code clientdb ($landing_company)... Result: $output\n";
+    }
 }
 
