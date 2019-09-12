78deaffe2afb70dfb3c73a2db2926ef517878e76
bruce-binary <bruce@binary.com>
2019-09-12 08:40:46 +0000

    getting currency convert mock to work

--- lib/BOM/CompanyLimits/LossTypes.pm
+++ lib/BOM/CompanyLimits/LossTypes.pm
@@ -3,26 +3,26 @@ package BOM::CompanyLimits::LossTypes;
 use strict;
 use warnings;
 
-use ExchangeRates::CurrencyConverter qw(in_usd);
+use ExchangeRates::CurrencyConverter;
 
 sub calc_realized_loss {
     my ($contract) = @_;
     my $bet_data = $contract->{bet_data};
 
-    return in_usd($bet_data->{sell_price} - $bet_data->{buy_price}, $contract->{account_data}->{currency_code});
+    return ExchangeRates::CurrencyConverter::in_usd($bet_data->{sell_price} - $bet_data->{buy_price}, $contract->{account_data}->{currency_code});
 }
 
 sub calc_potential_loss {
     my ($contract) = @_;
     my $bet_data = $contract->{bet_data};
 
-    return in_usd($bet_data->{payout_price} - $bet_data->{buy_price}, $contract->{account_data}->{currency_code});
+    return ExchangeRates::CurrencyConverter::in_usd($bet_data->{payout_price} - $bet_data->{buy_price}, $contract->{account_data}->{currency_code});
 }
 
 sub calc_turnover {
     my ($contract) = @_;
 
-    return in_usd($contract->{bet_data}->{buy_price}, $contract->{account_data}->{currency_code});
+    return ExchangeRates::CurrencyConverter::in_usd($contract->{bet_data}->{buy_price}, $contract->{account_data}->{currency_code});
 }
 
 1;
