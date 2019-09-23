38fbe5ba79ce94f8f7113c5b98e7dff7974e8fc4
bruce-binary <bruce@binary.com>
2019-09-23 03:04:10 +0000

    use log::any

--- bin/expire_redis_loss_hashes.pl
+++ bin/expire_redis_loss_hashes.pl
@@ -3,12 +3,14 @@
 use strict;
 use warnings;
 
-use BOM::CompanyLimits::SyncLoss;
+use Log::Any qw($log);
 use Data::Dumper;
+use BOM::CompanyLimits::SyncLoss;
+use Log::Any::Adapter qw(Stdout), log_level => 'info';
 
-my %output = BOM::CompanyLimits::SyncLoss::reset_daily_loss_hashes();
+my $output = BOM::CompanyLimits::SyncLoss::reset_daily_loss_hashes();
 
-print 'Expire result (1 if timeout is set, 0 if key does not exist):', Dumper(\%output);
+$log->info('Expire result (1 if timeout is set, 0 if key does not exist):', Dumper($output));
 
 
 
