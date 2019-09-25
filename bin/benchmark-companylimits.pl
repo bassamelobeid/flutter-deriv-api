4d132a86bd4b51ea11ed272e903233367fa1ef3e
Torsten Förtsch <torsten@binary.com>
2019-09-25 05:00:17 +0000

    min/max

--- bin/benchmark-companylimits.pl
+++ bin/benchmark-companylimits.pl
@@ -7,15 +7,17 @@ my ($nsend, $nrecv) = (0, 0);
 
 BEGIN {
     unless ($ENV{nobw}) {
-        *CORE::GLOBAL::send = sub {
+        # We need to use prototyped subs here because the
+        # CORE functions are also prototyped.
+        *CORE::GLOBAL::send = sub (*$$;$) {
             my $n = &CORE::send;
 
             $nsend += $n if ${*{$_[0]}}{' benchmark '};
 
             return $n;
         };
-        *CORE::GLOBAL::recv = sub {
-            my $n = CORE::recv $_[0], $_[1], $_[2], $_[3];
+        *CORE::GLOBAL::recv = sub (*\$$$) {
+            my $n = &CORE::recv;
 
             $nrecv += length $_[1] if ${*{$_[0]}}{' benchmark '};
 
@@ -35,13 +37,15 @@ my $nuid   = 5000;
 my $meth   = '_add_buys';
 my $batch  = 1;
 
-... unless GetOptions('fork=i'     => \$nproc,
-                      'requests=i' => \$nreq,
-                      'prefix=s'   => \$prefix,
-                      'users=i'    => \$nuid,
-                      'method=s'   => \$meth,
-                      'batch=i'    => \$batch,
-    );
+unless (GetOptions('fork=i'     => \$nproc,
+                   'requests=i' => \$nreq,
+                   'prefix=s'   => \$prefix,
+                   'users=i'    => \$nuid,
+                   'method=s'   => \$meth,
+                   'batch=i'    => \$batch,
+                  )) {
+    ...;
+}
 
 {
     my $x = int ($nreq / $nproc);
@@ -51,8 +55,13 @@ my $batch  = 1;
     $nreq = $x;                 # this is now the number of reqs per thread
 }
 
-my @pids;
+# We are using a pipe for the children to report back their values.
+# A pipe guarantees that at least PIPE_BUF bytes are written atomically.
+# POSIX requires PIPE_BUF to be at least 512. So, if all messages sent
+# to the pipe are less than 512 bytes long, no further locking is
+# required. (see pipe(7))
 pipe my ($r, $w);
+my @pids;
 for (1..$nproc) {
     my $pid;
     select undef, undef, undef, 0.1 until defined($pid=fork);
@@ -74,19 +83,23 @@ for (@pids) {
 
 sub parent {
     my @vals;
-    my ($totsum, $totsq, $totn, $totsnd, $totrcv);
+    my ($totsum, $totsq, $totn, $totmin, $totmax, $totsnd, $totrcv)
+        = (0, 0, 0, +inf, -inf, 0, 0);
     for (1..$nproc) {
         warn ("Unexpected EOF on pipe: $!\n"), last
             unless defined (my $l = readline $r);
-        my ($sum, $sumsq, $n, $nsnd, $nrcv) = split / /, $l;
+        my ($sum, $sumsq, $n, $min, $max, $nsnd, $nrcv) = split / /, $l;
         $totsum += $sum;
         $totsq += $sumsq;
         $totn += $n;
         $totsnd += $nsnd;
         $totrcv += $nrcv;
+        $totmin = $min if $min < $totmin;
+        $totmax = $max if $max > $totmax;
     }
-    printf("total number of requests: %d, parallel: %d, avg time per req: %.3f msec, stddev: %.3f\n",
-           $totn, $nproc, $totsum/$totn, sqrt($totsq/$totn - ($totsum/$totn)**2));
+    printf("total number of requests: %d, parallel: %d, avg time per req: %.3f msec, stddev: %.3f, ".
+           "min: %.3f, max: %.3f\n",
+           $totn, $nproc, $totsum/$totn, sqrt($totsq/$totn - ($totsum/$totn)**2)), $totmin, $totmax;
     unless ($ENV{nobw}) {
         printf("total bytes sent to redis: %d, number of bytes received from redis: %d\n",
                $totsnd, $totrcv);
@@ -121,16 +134,18 @@ sub chld {
     # my @res = $x->$meth(map {C->new} 1..2);
     # use Data::Dumper; print +Data::Dumper->new([\@res], ['res'])->Useqq(1)->Sortkeys(1)->Dump;
 
-    my ($sum, $sumsq) = (0, 0);
+    my ($sum, $sumsq, $min, $max) = (0, 0, +inf, -inf);
     for (1..$nreq) {
         my $start = [Time::HiRes::gettimeofday];
         $x->$meth(map {C->new} 1..$batch);
         my $tm = Time::HiRes::tv_interval($start) * 1000; # in millisec
         $sum += $tm;
         $sumsq += $tm ** 2;
+        $min = $tm if $tm < $min;
+        $max = $tm if $tm > $max;
     }
 
-    print $w "$sum $sumsq $nreq $nsend $nrecv\n";
+    print $w "$sum $sumsq $nreq $min $max $nsend $nrecv\n";
     close $w;
     return 0;
 }
