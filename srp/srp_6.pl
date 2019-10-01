ab20444a6f6e93a5110052336e2d08d7545c665b
Tazeek <tazeek.rakib@gmail.com>
2019-10-01 03:46:58 +0000

    Added link

--- srp/srp_6.pl
+++ srp/srp_6.pl
@@ -10,20 +10,17 @@ my $brand = request()->brand;
 sub send_email {
     my ($client_email, $balance) = @_;
     
-    # If there is balance, mark as unwelcome. Otherwise, disable
-    my $status = $balance == 0 ? 'disabled' : 'unwelcome';
-    
     my $email_content = localize('Dear Valued Customer,') . "\n\n" 
     $email_content .= localize("We regret to inform you that to remain compliant with applicable international laws governing online trading, we will be closing all clients' accounts in France.");
     
-    if ($balance == 0) {
+    if ($status eq 'disabled') {
         $email_content .= localize('As there is no balance in your account, we have disabled your account.') . "\n\n";
     } else {
         $email_content .= localize('Please withdraw your money from your Binary.com account by going to this cashier’s section: https://www.binary.com/en/cashier/forwardws.html?action=withdraw') . "\n\n";
     }
     
     $email_content .= localize('We would like to thank you for your support thus far.') . "\n\n";
-    $email_content .= localize('Please contact us at support@binary.com if you have any questions.') . "\n\n";
+    $email_content .= localize('Please contact us at <a href="mailto:support@binary.com">support@binary.com</a> if you have any questions.') . "\n\n";
     
     send_email({
         from                  => $brand->emails('support'),
@@ -43,7 +40,7 @@ my @fr_residence_clients = @{
     $clientdb->run(
         fixup => sub {
             $_->selectall_arrayref(
-                "SELECT cli.email AS email, COALESCE(ta.balance, 0) AS balance
+                "SELECT cli.loginid, cli.email AS email, COALESCE(ta.balance, 0) AS balance
                 FROM betonmarkets.client as cli
                 left join transaction.account AS ta ON cli.loginid = ta.client_loginid
                  where cli.residence = 'fr'",
@@ -58,7 +55,20 @@ $clientdb->txn(
             my $email = $fr_client->{email};
             my $balance = $fr_client->{balance};
             
-            send_email($email, $balance);
+            # If there is balance, mark as unwelcome. Otherwise, disable
+            my $status = $balance == 0 ? 'disabled' : 'unwelcome';
+            
+            send_email($email, $status);
+            
+            # Insert status
+            $clientdb->run(
+                ping => sub {
+                    my $sth = $_->prepare(
+                        'INSERT INTO betonmarkets.client_status(client_loginid, status_code, staff_name, reason) VALUES (?,?,?,?)'
+                    );
+                    $sth->execute($fr_client->{loginid}, $status, 'SYSTEM', 'No binary options for french clients');
+                }
+            );
         }
     }
 );
