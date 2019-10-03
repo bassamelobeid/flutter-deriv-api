7577a8c932c4a03047feabc9c4783c00533a054d
Tazeek <tazeek.rakib@gmail.com>
2019-10-03 03:13:17 +0000

    Adjusted the SQL statement

--- srp/srp_6.pl
+++ srp/srp_6.pl
@@ -10,7 +10,7 @@ my @fr_residence_clients = @{
     $clientdb->run(
         fixup => sub {
             $_->selectall_arrayref(
-                "SELECT cli.loginid, cli.first_name, cli.email AS email, COALESCE(ta.balance, 0) AS balance
+                "SELECT cli.loginid, COALESCE(ta.balance, 0) AS balance
                 FROM betonmarkets.client as cli
                 left join transaction.account AS ta ON cli.loginid = ta.client_loginid
                  where cli.residence = 'fr'",
@@ -23,9 +23,7 @@ $clientdb->txn(
     fixup => sub {
         foreach my $fr_client (@fr_residence_clients) {
 
-            my $email = $fr_client->{email};
             my $balance = $fr_client->{balance};
-            my $cl_first_name = $fr_client->{first_name};
             
             # If there is balance, mark as unwelcome. Otherwise, disable
             my $status = $balance == 0 ? 'disabled' : 'unwelcome';
