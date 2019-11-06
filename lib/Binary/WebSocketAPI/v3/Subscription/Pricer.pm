2a47934decc2f114d99239468749f342a9ce6d34
Jun Bon <junbon@binary.com>
2019-11-06 06:34:29 +0000

    fixes price streaming issue

--- lib/Binary/WebSocketAPI/v3/Subscription/Pricer.pm
+++ lib/Binary/WebSocketAPI/v3/Subscription/Pricer.pm
@@ -127,12 +127,11 @@ sub handle_message {
 requires 'do_handle_message';
 
 sub _non_binary_price_adjustment {
-    my ($self, $c, $contract_parameters, $results) = @_;
+    my ($self, $c, $contract_parameters, $results, $theo_price) = @_;
 
     my $t = [gettimeofday];
     #do app markup adjustment here
     my $app_markup_percentage = $contract_parameters->{app_markup_percentage} // 0;
-    my $theo_price            = $contract_parameters->{theo_price}            // 0;
     my $multiplier            = $contract_parameters->{multiplier}            // 0;
 
     my $app_markup_per_unit = $theo_price * $app_markup_percentage / 100;
@@ -152,9 +151,7 @@ sub _non_binary_price_adjustment {
 }
 
 sub _binary_price_adjustment {
-    my ($self, $c, $contract_parameters, $results) = @_;
-
-    my $resp_theo_probability = $contract_parameters->{theo_probability};
+    my ($self, $c, $contract_parameters, $results, $resp_theo_probability) = @_;
 
     # log the instances when pricing server doesn't return theo probability
     unless (defined $resp_theo_probability) {
@@ -227,17 +224,14 @@ sub _price_stream_results_adjustment {
     my ($self, $c, $cache, $results) = @_;
 
     my $contract_parameters = $cache->{contract_parameters};
-    my $price_adjustment    = delete $contract_parameters->{price_adjustment};
     # We are handling pricing adjustment for 3 different scenarios here:
     # 1. adjustment for binary contracts where we expect theo probability to be present
     # 2. adjustment for non-binary where we we expect theo price
     # 3. no adjustment needed. Some contract does not require any pricing adjustment on the websocket layer.
-    return $results unless $price_adjustment;
-
-    if ($price_adjustment eq 'non_binary') {
-        return $self->_non_binary_price_adjustment($c, $contract_parameters, $results);
-    } elsif ($price_adjustment eq 'binary') {
-        return $self->_binary_price_adjustment($c, $contract_parameters, $results);
+    if (my $theo_price = delete $results->{theo_price}) {
+        return $self->_non_binary_price_adjustment($c, $contract_parameters, $results, $theo_price);
+    } elsif (my $theo_probability = delete $results->{theo_probability}) {
+        return $self->_binary_price_adjustment($c, $contract_parameters, $results, $theo_probability);
     }
 
     return $results;
