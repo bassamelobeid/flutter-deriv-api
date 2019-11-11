8d0311a1d53c336a487a1bafafc5a2aea0622454
Jun Bon <junbon@binary.com>
2019-11-11 02:39:04 +0000

    syntax

--- lib/BOM/Transaction/ContractUpdate.pm
+++ lib/BOM/Transaction/ContractUpdate.pm
@@ -155,7 +155,7 @@ sub _validate_update_parameter {
 
     # We need to limit the number of updates per second.
     # Currently, we only allow one update per second which I think is reason.
-    if ($self->$order_type and $new_order->order_date->epoch <= $contract->$order_type->order_date->epoch) {
+    if ($contract->$order_type and $new_order->order_date->epoch <= $contract->$order_type->order_date->epoch) {
         return {
             code              => 'TooFrequentUpdate',
             message_to_client => localize('Only one update per second is allowed.'),
