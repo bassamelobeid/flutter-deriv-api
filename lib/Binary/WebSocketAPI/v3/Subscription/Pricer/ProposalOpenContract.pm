53de0b99289519de3cb4c3e124ab0d8511fcda94
Jun Bon <junbon@binary.com>
2019-11-22 06:33:21 +0000

    fixes

--- lib/Binary/WebSocketAPI/v3/Subscription/Pricer/ProposalOpenContract.pm
+++ lib/Binary/WebSocketAPI/v3/Subscription/Pricer/ProposalOpenContract.pm
@@ -34,10 +34,10 @@ sub do_handle_message {
         $message->{purchase_time}   = $passed_fields->{purchase_time};
         $message->{is_sold}         = $passed_fields->{is_sold};
         if ($message->{buy_price} and $message->{bid_price} and $message->{currency}) {
-            my $cost_of_cancellation = delete $message->{cost_of_cancellation} // 0;
+            my $deal_cancellation = $message->{deal_cancellation};
             # we need to exclude cost of cancellation in profit/loss calculation to avoid confusion since
             # pnl always refers to the main contract.
-            my $main_contract_price = $message->{buy_price} - $cost_of_cancellation;
+            my $main_contract_price = $message->{buy_price} - $deal_cancellation->{ask_price};
             $message->{profit} = formatnumber('price', $message->{currency}, $message->{bid_price} - $main_contract_price);
             $message->{profit_percentage} = roundcommon(0.01, $message->{profit} / $main_contract_price * 100);
         }
