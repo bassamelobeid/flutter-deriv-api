8c6036765586dac4a0fd115abb08046fe8178541
Raunak Kathuria <raunakkathuria@gmail.com>
2019-12-18 11:34:13 +0800

    Revert "binary-tanya:tanya/remove_ukgc_code (#287)" (#296)
    This reverts commit 8bcb13c784d36b7de4056ef3d4bc17615153e797.

--- lib/BOM/User/Client/Status.pm
+++ lib/BOM/User/Client/Status.pm
@@ -24,7 +24,7 @@ my @status_codes = qw(
     professional_requested  professional professional_rejected  tnc_approval
     migrated_single_email  duplicate_account  proveid_pending  proveid_requested
     require3ds  skip_3ds  ok  ico_only  allowed_other_card  can_authenticate
-    social_signup  trusted pa_withdrawal_explicitly_allowed
+    social_signup  trusted pa_withdrawal_explicitly_allowed ukgc_authenticated
     address_verified no_withdrawal_or_trading
 );
 
