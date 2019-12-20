5c2d5af0283736aeaec536334aaf43c38ddc468c
Jun Bon <junbon@binary.com>
2019-12-20 03:03:24 +0000

    changed comment

--- lib/BOM/Pricing/PriceDaemon.pm
+++ lib/BOM/Pricing/PriceDaemon.pm
@@ -203,7 +203,7 @@ sub run {
                 {tags => $self->tags('contract_type:' . $contract_type_string, 'currency:' . $params->{currency})});
         }
 
-        # On websocket clients are subscribing to proposal open contract with "CONTRACT_PRICE::123123_virtual" as the key
+        # On websocket clients are subscribing to proposal open contract with "CONTRACT_PRICE::123123::virtual" as the key
         my $redis_channel = $params->{contract_id} ? join('::', 'CONTRACT_PRICE', $params->{contract_id}, $params->{landing_company}) : $key->[1];
         my $subscribers_count = $redis->publish($redis_channel, encode_json_utf8($response));
         # if None was subscribed, so delete the job
