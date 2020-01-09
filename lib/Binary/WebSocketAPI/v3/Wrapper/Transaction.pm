145ebc5f25e650d997d12957bd8e3812159e3966
Jun Bon <junbon@binary.com>
2020-01-09 10:13:45 +0000

    do not update poc if the request only requests for history

--- lib/Binary/WebSocketAPI/v3/Wrapper/Transaction.pm
+++ lib/Binary/WebSocketAPI/v3/Wrapper/Transaction.pm
@@ -37,20 +37,23 @@ sub contract_update_handler {
 
     # do not send this back
     delete $api_response->{updated_queue};
-    my $new_poc_params = _get_poc_params(delete $api_response->{contract_details});
+    if (my $details = delete $api_response->{contract_details}) {
+        my $new_poc_params = _get_poc_params($details);
+        if (%$new_poc_params) {
+            my $redis = Binary::WebSocketAPI::v3::Subscription::Pricer::subscription_manager()->redis;
 
-    my $redis = Binary::WebSocketAPI::v3::Subscription::Pricer::subscription_manager()->redis;
+            my ($new_pricer_args, $contract_id) = @{Binary::WebSocketAPI::v3::Wrapper::Pricer::get_pricer_args($c, $new_poc_params)};
 
-    my ($new_pricer_args, $contract_id) = @{Binary::WebSocketAPI::v3::Wrapper::Pricer::get_pricer_args($c, $new_poc_params)};
+            my $old_key = $redis->get($contract_id);
+            # can't use redis->multi & exec here because it is not supported by Mojo::Redis2
+            $redis->del($old_key) if $old_key;
+            # for pricer queue
+            $redis->set($new_pricer_args, $contract_id);
+            # for future update
+            $redis->set($contract_id, $new_pricer_args);
+        }
 
-    my $old_key = $redis->get($contract_id);
-    # can't use redis->multi & exec here because it is not supported by Mojo::Redis2
-    $redis->del($old_key) if $old_key;
-    # for pricer queue
-    $redis->set($new_pricer_args, $contract_id);
-    # for future update
-    $redis->set($contract_id, $new_pricer_args);
-
+    }
     return undef;
 }
 
