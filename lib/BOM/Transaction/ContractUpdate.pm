7ccce623c16935fc2f5fbeffc26f10d7c2aef189
Jun Bon <junbon@binary.com>
2020-01-09 11:53:42 +0000

    fixes error message

--- lib/BOM/Transaction/ContractUpdate.pm
+++ lib/BOM/Transaction/ContractUpdate.pm
@@ -69,7 +69,7 @@ sub BUILD {
     unless ($contract) {
         $self->validation_error({
             code              => 'ContractNotFound',
-            message_to_client => localize('Contract not found for contract id: [_1].', $self->contract_id),
+            message_to_client => localize('No open contract found for contract id: [_1].', $self->contract_id),
         });
     }
 
