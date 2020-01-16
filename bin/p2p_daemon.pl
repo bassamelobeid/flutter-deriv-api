178a8b726eb93aa770b531a37a2293f4a44dd125
Raunak Kathuria <raunakkathuria@gmail.com>
2020-01-16 08:44:59 +0800

    fix logging, use log adapter (#214)
    Co-authored-by: Jean-Yves Sireau <jy@binary.com>

--- bin/p2p_daemon.pl
+++ bin/p2p_daemon.pl
@@ -9,11 +9,20 @@ use BOM::Platform::Event::Emitter;
 use LandingCompany::Registry;
 use BOM::Database::ClientDB;
 use Time::HiRes;
+use Getopt::Long;
+use Log::Any qw($log);
 
-use Log::Any '$log';
 use BOM::Config::Runtime;
 
-use Log::Any qw($log);
+binmode STDOUT, ':encoding(UTF-8)';
+binmode STDERR, ':encoding(UTF-8)';
+
+require Log::Any::Adapter;
+GetOptions('l|log=s' => \my $log_level) or die;
+
+$log_level ||= 'info';
+
+Log::Any::Adapter->import(qw(Stderr), log_level => $log_level);
 
 =head1 Name
 
@@ -34,44 +43,44 @@ use constant POLLING_INTERVAL => 60;
 use constant ACTIVE_LANDING_COMPANIES => ['svg'];
 
 my @dbs;
-for my $landing_company (@{ ACTIVE_LANDING_COMPANIES() }) {
+for my $landing_company (@{ACTIVE_LANDING_COMPANIES()}) {
     my @broker_codes = LandingCompany::Registry::get($landing_company)->broker_codes->@*;
     for my $broker_code (@broker_codes) {
         push @dbs, BOM::Database::ClientDB->new({broker_code => $broker_code});
     }
 }
 
-
 my $app_config = BOM::Config::Runtime->instance->app_config;
 
-my $loop = IO::Async::Loop->new;
+my $loop     = IO::Async::Loop->new;
 my $shutdown = $loop->new_future;
 $shutdown->on_ready(sub {
-    $log->infof('Shut down');
+    $log->info('Shut down');
 });
 $loop->watch_signal(INT => sub {
     $shutdown->done;
 });
 
-$log->infof('Starting P2P polling');
 (async sub {
-    $log->infof('Starting P2P polling');
-    until($shutdown->is_ready) {
+    $log->info('Starting P2P polling');
+    until ($shutdown->is_ready) {
         $app_config->check_for_update;
-        for my $cur_db ( @dbs ) {
+        for my $cur_db (@dbs) {
             # Stop quering db when feature is disabled
             last if $app_config->system->suspend->p2p || !$app_config->payments->p2p->enabled;
+
+            $log->debug('P2P: Checking for expired orders');
             my $sth = $cur_db->db->dbh->prepare('SELECT id, client_loginid FROM p2p.order_list_expired()');
             $sth->execute();
 
-            while ( my $order_data = $sth->fetchrow_hashref ) {
+            while (my $order_data = $sth->fetchrow_hashref) {
+                $log->debugf('P2P: Emitting event to mark order as expired for order %s', $order_data->{id});
                 BOM::Platform::Event::Emitter::emit(
                     p2p_order_expired => {
                         client_loginid => $order_data->{client_loginid},
                         order_id       => $order_data->{id},
-                        expiry_started =>  [Time::HiRes::gettimeofday],
-                    }
-                );
+                        expiry_started => [Time::HiRes::gettimeofday],
+                    });
             }
             $sth->finish;
         }
