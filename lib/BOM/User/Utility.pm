4e8615e6af677ee393b3aecdf4c737e0df32bbac
Diego Ramírez <57363189+diego-binary@users.noreply.github.com>
2020-02-06 03:01:11 -0300

    gamstop/add_timing_metrics_to_gamstop_requests (#320)
    * Add timing measure for gamstop requests
    
    Signed-off-by: Diego Ramírez <diego.ramirez@binary.com>
    
    * Update variable name

--- lib/BOM/User/Utility.pm
+++ lib/BOM/User/Utility.pm
@@ -8,12 +8,13 @@ use feature qw(state);
 
 use Crypt::CBC;
 use Crypt::NamedKeys;
-use DataDog::DogStatsd::Helper qw(stats_inc);
+use DataDog::DogStatsd::Helper qw(stats_inc stats_timing);
 use DateTime;
 use Date::Utility;
 use Encode;
 use Encode::Detect::Detector;
 use Syntax::Keyword::Try;
+use Time::HiRes qw(gettimeofday tv_interval);
 use Webservice::GAMSTOP;
 use Email::Address::UseXS;
 use Email::Stuffer;
@@ -86,6 +87,7 @@ sub set_gamstop_self_exclusion {
     return undef unless $landing_company_config;
 
     my $gamstop_response;
+    my $starting_time = [gettimeofday];
     try {
         my $instance = Webservice::GAMSTOP->new(
             api_url => $gamstop_config->{api_uri},
@@ -105,6 +107,7 @@ sub set_gamstop_self_exclusion {
     catch {
         stats_inc('GAMSTOP_CONNECT_FAILURE') if $@ =~ /^Error/;
     }
+    stats_timing('GAMSTOP_REQUEST_TIME', tv_interval($starting_time) * 1000, {tags => ["landing_company:$lc"]});
 
     return undef unless $gamstop_response;
 
