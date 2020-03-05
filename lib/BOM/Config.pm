eb95af9fd20e7f6932d7880e802d1806f1a2901c
chengyang-binary <chengyang@binary.com>
2020-03-05 04:51:33 +0000

    remove unused

--- lib/BOM/Config.pm
+++ lib/BOM/Config.pm
@@ -198,9 +198,4 @@ sub onfido_supported_documents {
     return $config;
 }
 
-sub restricted_countries_list {
-    state $config = YAML::XS::LoadFile('/home/git/regentmarkets/bom-config/share/restricted_countries.yml');
-    return $config;
-}
-
 1;
