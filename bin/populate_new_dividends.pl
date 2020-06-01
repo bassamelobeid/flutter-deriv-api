592f8e45939040d1a6578eebdbe9a68e806a1059
amin roosta <amin.roosta@outlook.com>
2020-06-01 17:49:38 +0800

    amin/1s-volatility-indices (#234)
    * 1s volatility indices
    
    * use ARGV

--- bin/populate_new_dividends.pl
+++ bin/populate_new_dividends.pl
@@ -8,7 +8,9 @@ use Date::Utility;
 use BOM::Config::Chronicle;
 use Quant::Framework::Asset;
 
-for my $symbol (qw(1HZ100V 1HZ10V)) {
+# usage:
+# perl ./bin/populate_new_dividends.pl 1HZ25V 1HZ50V 1HZ75V
+for my $symbol (@ARGV) {
     my $otc_dividend = Quant::Framework::Asset->new(
         symbol           => $symbol,
         rates            => {365 => 0},
