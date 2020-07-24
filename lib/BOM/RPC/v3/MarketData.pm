0d5ba4f1b36099d52901e23cdd7d0b41ae4cfff7
Shahrizal Shaari <shahrizal@binary.com>
2020-07-24 15:18:16 +0800

    A new economic event api (#2779)
    * A new economic event api
    
    * add rpc code for EE
    
    * Let's add test first
    
    * add schema test as well
    
    * fixing error
    
    * edit test
    
    * fix error
    
    * edit test
    
    * add actual and previous
    
    * update latest code
    
    * update schema
    
    * clean up
    
    * JB Feedback: move chronicle related things into QF
    
    * Feedback: function name
    
    * Feedback: date validation
    
    * rerun
    
    * Revert "Feedback: date validation"
    
    This reverts commit 41133d698b56454a9b4ab9916ae574a12c5feeab.
    
    Co-authored-by: Nobody User <nobody@qa31.regentmarkets.com>

--- lib/BOM/RPC/v3/MarketData.pm
+++ lib/BOM/RPC/v3/MarketData.pm
@@ -16,6 +16,8 @@ use warnings;
 use Format::Util::Numbers qw(formatnumber);
 use Scalar::Util qw(looks_like_number);
 use List::Util qw(any);
+use Date::Utility;
+use BOM::Config::Chronicle;
 
 use LandingCompany::Registry;
 use ExchangeRates::CurrencyConverter qw(convert_currency);
@@ -23,6 +25,10 @@ use ExchangeRates::CurrencyConverter qw(convert_currency);
 use BOM::RPC::Registry '-dsl';
 use BOM::Platform::Context qw (localize);
 use BOM::RPC::v3::Utility;
+
+use constant {
+    EE_LOOKUP_PERIOD => 14,    #14 days in the future, 14 days in the past
+};
 
 =head2 exchange_rates
 
@@ -73,5 +79,72 @@ rpc exchange_rates => sub {
         rates         => \%rates_hash,
     };
 };
+
+rpc economic_calendar => sub {
+    my $params = shift;
+
+    my $now            = Date::Utility->new();
+    my $start_date_arg = $params->{args}->{start_date};
+    my $end_date_arg   = $params->{args}->{end_date};
+    my $currency       = $params->{args}->{currency};
+
+    my ($start_date, $end_date);
+    my $today_start    = $now->truncate_to_day();
+    my $today_end      = $today_start->plus_time_interval('23h59m59s');
+    my $min_start_date = $today_start->minus_time_interval(EE_LOOKUP_PERIOD . 'd');
+    my $max_end_date   = $today_end->plus_time_interval(EE_LOOKUP_PERIOD . 'd');
+    $start_date = $today_start unless defined $start_date_arg;
+    $end_date   = $today_end   unless defined $end_date_arg;
+
+    if (defined $start_date_arg) {
+        return BOM::RPC::v3::Utility::create_error({
+                code              => 'InputValidationFailed',
+                message_to_client => localize("Input validation failed: [_1]", "end_date"),
+                details           => {
+                    end_date => localize("Please enter end date."),
+                },
+            }) unless defined $end_date_arg;
+        $start_date = Date::Utility->new($start_date_arg);
+        return _default_invalid_date_error("start_date", $min_start_date->epoch(), $max_end_date->epoch())
+            if $start_date->is_before($min_start_date)
+            or $start_date->is_after($max_end_date);
+    } else {
+        return BOM::RPC::v3::Utility::create_error({
+                code              => 'InputValidationFailed',
+                message_to_client => localize("Input validation failed: [_1]", "start_date"),
+                details           => {
+                    start_date => localize("Please enter start date."),
+                },
+            }) if defined $end_date_arg;
+    }
+
+    if (defined $end_date_arg) {
+        $end_date = Date::Utility->new($end_date_arg);
+        return _default_invalid_date_error("end_date", $start_date->epoch(), $max_end_date->epoch())
+            if $end_date->is_before($start_date)
+            or $end_date->is_after($max_end_date)
+            or $end_date->is_before($min_start_date);
+    }
+
+    my $economic_calendar = Quant::Framework::EconomicEventCalendar->new({chronicle_reader => BOM::Config::Chronicle::get_chronicle_reader()});
+    my $events = $economic_calendar->get_economic_event_calendar_for_display({
+        currency   => $currency,
+        start_date => $start_date,
+        end_date   => $end_date
+    });
+
+    return {'events' => $events};
+};
+
+sub _default_invalid_date_error {
+    my ($field, $min, $max) = @_;
+    return BOM::RPC::v3::Utility::create_error({
+            code              => 'InputValidationFailed',
+            message_to_client => localize("Input validation failed: [_1]", $field),
+            details           => {
+                $field => localize("The date you entered is not allowed. Please enter a date between [_1] and [_2].", $min, $max),
+            },
+        });
+}
 
 1;
--- t/BOM/RPC/21_market_data.t
+++ t/BOM/RPC/21_market_data.t
@@ -12,6 +12,11 @@ use Format::Util::Numbers qw(formatnumber);
 
 use LandingCompany::Registry;
 
+use Quant::Framework::EconomicEventCalendar;
+use BOM::Test::Data::Utility::UnitTestMarketData;
+use Try::Tiny;
+use BOM::Config::Chronicle;
+
 use BOM::Test::RPC::Client;
 use BOM::RPC::v3::MarketData;
 use Email::Stuffer::TestLinks;
@@ -56,4 +61,90 @@ subtest 'exchange rates' => sub {
     }
     cmp_ok($result->{rates}->{LTC}, '==', 0.00833333, 'correct rate for LTC');
 };
+
+subtest 'economic_calendar' => sub {
+
+    my $today = Date::Utility->new()->truncate_to_day();
+    my $start = $today->minus_time_interval("1d");
+    my $end   = $today->plus_time_interval("1d");
+
+    my $chronicle_writer = BOM::Config::Chronicle::get_chronicle_writer();
+
+    $chronicle_writer->set(
+        'economic_events',
+        'ECONOMIC_EVENTS_CALENDAR',
+        [{
+                symbol       => 'USD',
+                release_date => $today->plus_time_interval("1h")->epoch,
+                impact       => "1",
+                forecast     => "49.6",
+                source       => 'forexfactory',
+                event_name   => 'Final Manufacturing PMI',
+            },
+            {
+                symbol       => 'JPY',
+                release_date => $today->plus_time_interval("2d")->epoch,
+                impact       => "2",
+                forecast     => "-31",
+                source       => 'bloomberg',
+                event_name   => 'Tankan Manufacturing Index',
+            }
+        ],
+        Date::Utility->new,
+        0, 86400
+    );
+
+    $result = $c->call_ok(
+        'economic_calendar',
+        {
+            args => {
+                "start_date" => $start->epoch(),
+                "end_date"   => $end->epoch()}});
+
+    ok $result->has_no_system_error->has_no_error, 'RPC called without system errors';
+
+    my %expected = (
+        'release_date' => 1594170000,
+        'forecast'     => '49.6',
+        'impact'       => '1',
+        'currency'     => 'USD',
+        'event_name'   => 'Final Manufacturing PMI'
+    );
+
+    cmp_ok(scalar @{$result->response->result->{events}}, '==', 1, 'Correct number of events is returned');
+
+    $start  = $today->plus_time_interval("1d");
+    $end    = $today->minus_time_interval("1d");
+    $result = $c->call_ok(
+        'economic_calendar',
+        {
+            args => {
+                "start_date" => $start->epoch(),
+                "end_date"   => $end->epoch()}});
+
+    ok $result->error_code_is('InputValidationFailed'), 'Must not be earlier than start_date';
+
+    $start  = $today->minus_time_interval("30d");
+    $end    = $today->plus_time_interval("1d");
+    $result = $c->call_ok(
+        'economic_calendar',
+        {
+            args => {
+                "start_date" => $start->epoch(),
+                "end_date"   => $end->epoch()}});
+
+    ok $result->error_code_is('InputValidationFailed'), 'Start date should not exceed 14 days in the past from now';
+
+    $start  = $today->minus_time_interval("1d");
+    $end    = $today->plus_time_interval("30d");
+    $result = $c->call_ok(
+        'economic_calendar',
+        {
+            args => {
+                "start_date" => $start->epoch(),
+                "end_date"   => $end->epoch()}});
+
+    ok $result->error_code_is('InputValidationFailed'), 'End date should not exceed 15 days in the future from now';
+};
+
 done_testing();
