b21a4712a810d418897e3582e3dfdd5155687ef9
junbon-binary <junbon@binary.com>
2020-07-29 15:39:34 +0800

    added tool to disable auto b-booking of financial svg (#2025)

--- lib/BOM/DynamicSettings.pm
+++ lib/BOM/DynamicSettings.pm
@@ -207,6 +207,7 @@ sub get_settings_by_group {
                 system.mt5.suspend.all
                 system.mt5.suspend.deposits
                 system.mt5.suspend.withdrawals
+                system.mt5.suspend.auto_Bbook_svg_financial
                 )
         ],
         quant => [qw(
