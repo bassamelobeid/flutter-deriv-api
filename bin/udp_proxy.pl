e2c2040823ea9dc2da0cb1f8e8a879694e55164e
alan-fs <58261167+alan-fs@users.noreply.github.com>
2021-01-21 11:55:37 +0330

    devops/improving_udp_proxy_service (#1490)
    * adding script to broadcast
    
    * read host and port from command args
    
    * check listening port is available or not
    
    * remove extar files
    
    * Update bin/udp_proxy.pl
    
    Co-authored-by: Tom Molesworth <tom@binary.com>
    
    * Update bin/udp_proxy.pl
    
    Co-authored-by: Tom Molesworth <tom@binary.com>
    
    * extract email template
    
    * remove extar file change
    
    * passing test
    
    * add currency to mx_verification email
    
    * solving the problem of having repeated log messages and also changing the way that recconect of clinets is handled
    
    * remove extra changes
    
    * give a name to process
    
    * put ryu inside try/catch block
    
    * dummy
    
    * devops/improving_udp_proxy_service
    
    * checking ports every 10 seconds
    
    * rerun-6-JAN-2021-1
    
    Co-authored-by: Tom Molesworth <tom@binary.com>
    Co-authored-by: deriv-sysadmin-automation <69662586+deriv-sysadmin-automation@users.noreply.github.com>

--- bin/udp_proxy.pl
+++ bin/udp_proxy.pl
@@ -5,9 +5,11 @@ use Getopt::Long;
 use Pod::Usage;
 use Syntax::Keyword::Try;
 use IO::Async::Loop;
+use Future::AsyncAwait;
 use Ryu::Async;
 use Log::Any qw($log);
 use DateTime;
+use IO::Socket;
 
 GetOptions(
     'help|?'      => sub { pod2usage() },
@@ -38,58 +40,54 @@ die "Missing --sendto parameter, try --help\n" unless $udp_sendto;
 require Log::Any::Adapter;
 Log::Any::Adapter->import(qw(Stderr), log_level => $log_level);
 
+$0 = "udp_proxy";
+
 my $loop = IO::Async::Loop->new;
-$loop->add(my $ryu = Ryu::Async->new);
-my $srv;
+my ($srv, $ryu);
 my %active_clients = ();
 
 try {
     my ($listen_host, $listen_port) = split /:/, $udp_listen;
+    $loop->add($ryu = Ryu::Async->new);
     $srv = $ryu->udp_server(
         host => $listen_host,
         port => $listen_port,
     );
     $srv->port->get;
-    $srv->incoming->completed->on_fail(
-        sub {
-            my $err = shift;
-            my $dt  = DateTime->now;
-            die "[$dt] UDP server failed - " . $err;
-        });
 
     # Forward to these ports
     $udp_sendto = [split /,/ => join ',' => @$udp_sendto];
-    setup_udp_client($ryu, $srv, $_) for $udp_sendto->@*;
+    setup_udp_client($_) for $udp_sendto->@*;
+
+    check_ports($listen_host, $listen_port);
 
     $SIG{INT} = $SIG{TERM} = sub {
         $SIG{INT} = $SIG{TERM} = sub {
             $log->errorf('Second INT/TERM received after first, hard exit');
             exit 1;
         };
-        try {
-            $srv->incoming->finish;
-            $loop->delay_future(after => 2)->on_ready(
-                sub {
-                    foreach my $key (keys %active_clients) {
-                        $active_clients{$key}->incoming->finish;
-                        $active_clients{$key}->outgoing->source->finish;
-                    }
-                    $loop->stop;
-                })->retain;
-        } catch {
-            $log->errorf('Exception while trying to handle shutdown signal: %s', $@);
-        }
+        destructor();
     };
 
-    $loop->run;
+    (
+        async sub {
+            await $srv->incoming->completed;
+        })->()->get;
+
 } catch ($e) {
+    destructor();
     my $dt = DateTime->now;
     $log->errorf("[$dt] UDP proxy failed - $e");
-}
+};
 
 sub setup_udp_client {
-    my ($ryu, $srv, $host_port) = @_;
+    my ($host_port, $is_reconnect) = @_;
     my @uri = split /:/, $host_port;
+    my $dt  = DateTime->now;
+    if (!is_alive($uri[0], $uri[1])) {
+        $log->errorf("[$dt] UDP client [$host_port] failed to connect.") unless $is_reconnect;
+        return;
+    }
     my $client = $ryu->udp_client(
         host => $uri[0],
         port => $uri[1],
@@ -99,16 +97,54 @@ sub setup_udp_client {
     $client->incoming->completed->on_fail(
         sub {
             my $err = shift;
-            my $dt  = DateTime->now;
-            $log->errorf("[$dt] UDP client failed - " . $err);
+            $dt = DateTime->now;
+            $log->errorf("[$dt] UDP client [$host_port] failed - " . $err);
             $client->incoming->finish;
             $client->outgoing->source->finish;
             $client = undef;
-            # preventing error log flooding
-            $loop->delay_future(after => 10)->on_ready(
-                sub {
-                    setup_udp_client($ryu, $srv, $host_port);
-                })->retain;
+            delete $active_clients{$host_port};
         });
     $active_clients{$host_port} = $client;
+    $log->infof("[$dt] UDP client [$host_port] connected.");
 }
+
+sub check_ports {
+    my ($listen_host, $listen_port) = @_;
+    $loop->delay_future(after => 10)->on_ready(
+        sub {
+            if (!is_alive($listen_host, $listen_port)) {
+                die $log->errorf("Server listening port $listen_port is closed.");
+            }
+            foreach my $host_port ($udp_sendto->@*) {
+                setup_udp_client($host_port, 1) if (not exists($active_clients{$host_port}));
+            }
+            check_ports($listen_host, $listen_port);
+        })->retain;
+}
+
+sub is_alive {
+    my ($host, $port) = @_;
+    my $sock = new IO::Socket::INET(
+        LocalHost => $host,
+        LocalPort => $port,
+        Proto     => 'udp'
+    );
+    if ($sock) {
+        $sock->close();
+        return 0;
+    }
+    return 1;
+}
+
+sub destructor {
+    $srv->incoming->finish;
+    $loop->delay_future(after => 2)->on_ready(
+        sub {
+            foreach my $key (keys %active_clients) {
+                $active_clients{$key}->incoming->finish;
+                $active_clients{$key}->outgoing->source->finish;
+            }
+            $loop->stop;
+        })->get;
+}
+
