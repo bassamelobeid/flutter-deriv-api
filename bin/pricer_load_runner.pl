5e371ad7329b8856bc50f706d1fd65b3c7e80b1a
Michael Mueller <52896557+michaelmueller-binary@users.noreply.github.com>
2021-02-01 16:10:35 +1100

    Loadtest/update_datadog_dashboard_url (#801)
    * loadtest/update_datadog_dashboard_url
    
    * Update URL to point to Loadtest instance of Datadog
    
    * trigger tests
    
    * trigger tests
    
    * trigger tests

--- bin/pricer_load_runner.pl
+++ bin/pricer_load_runner.pl
@@ -396,7 +396,7 @@ sub email_result {
         }
     }
     $body .= "
-    Datadog link =  https://app.datadoghq.com/dashboard/8w7-rtw-jse/qaloastest-pricer-queue?from_ts=$dashboard_start_time&live=false&to_ts=$dashboard_end_time; 
+    Datadog link = https://app.datadoghq.com/dashboard/27a-7ws-tk3/pricer-daily-load-testing?from_ts=$dashboard_start_time&live=false&to_ts=$dashboard_end_time; 
     ";
     my $email_stuffer =
       Email::Stuffer->from('loadtest@binary.com')->to(@$mails_to)
