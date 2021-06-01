73518f86ce8c317f5b4eaeeb4e96fee794fd8301
felipe-binary <64797927+felipe-binary@users.noreply.github.com>
2021-06-01 03:13:08 -0400

    idv/get_account_status (#911)
    * skeleton package and test
    
    * trigger ci with notes
    
    * improve pod
    
    * improve pod coverage
    
    * trigger ci with notes
    
    * tidy up
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes

---
+++ lib/BOM/User/IdentityVerification.pm
@@ -0,0 +1,88 @@
+package BOM::User::IdentityVerification;
+
+=head1 DESCRIPTION
+
+Identity Verification services DB wrapper and utilities.
+
+Note this module is meant to be decoupled from L<BOM::User> or L<BOM::User::Client>.
+
+=cut
+
+use strict;
+use warnings;
+
+use BOM::Config::Redis;
+use Moo;
+
+use constant IDV_REQUEST_PER_USER_PREFIX => 'IDV::REQUEST::PER::USER::';
+
+=head2 user_id
+
+The current IDV user.
+
+=cut
+
+has 'user_id' => (
+    is       => 'ro',
+    required => 1,
+);
+
+=head2 get_rejected_reasons
+
+Parses and extract the latest IDV rejection reasons for the current user.
+
+Returns,
+    an arrayref of possible reasons why the IDV attempt has been rejected
+
+=cut
+
+sub get_rejected_reasons {
+    return [];    # TODO: proper implementation
+}
+
+=head2 submissions_left
+
+Returns the submissions left for the user.
+
+Returns,
+    an integer representing the submissions left for the user involved.
+
+=cut
+
+sub submissions_left {
+    my $self             = shift;
+    my $redis            = BOM::Config::Redis::redis_events();
+    my $request_per_user = $redis->get(IDV_REQUEST_PER_USER_PREFIX . $self->user_id) // 0;
+    my $submissions_left = $self->limit_per_user() - $request_per_user;
+    return $submissions_left;
+}
+
+=head2 limit_per_user
+
+Provides a central point for onfido resubmissions limit per user in the specified
+timeframe.
+
+Returns,
+    an integer representing the onfido submission requests allowed per user
+
+=cut
+
+sub limit_per_user {
+    return $ENV{IDV_REQUEST_PER_USER_LIMIT} // 0;    # TODO: proper implementation
+}
+
+=head2 reported_properties
+
+Returns the user detected properties, from the latest IDV request, as a hashref.
+
+It takes the following arguments:
+
+Returns a hashref containing detected properties.
+
+=cut
+
+sub reported_properties {
+    return {};    # TODO: proper implementation
+}
+
+1;
---
+++ t/BOM/User/024_identity_verification.t
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::Deep;
+
+use BOM::User::IdentityVerification;
+
+my $idv = BOM::User::IdentityVerification->new(user_id => 'space_cowboy');
+
+isa_ok $idv, 'BOM::User::IdentityVerification';
+is $idv->user_id, 'space_cowboy', 'Adios vaquero';
+
+# you gotta carry that weight (of incomplete implementation)
+# TODO: proper testing once the proper implementation arrives
+
+subtest 'Rejected Reasons' => sub {
+    cmp_bag $idv->get_rejected_reasons, [], 'Expected rejected reasons';
+};
+
+subtest 'Submissions Left' => sub {
+    is $idv->submissions_left, 0, 'Expected submissions left';
+};
+
+subtest 'Limit per user' => sub {
+    is $idv->limit_per_user, 0, 'Expected limit per user';
+};
+
+subtest 'Reported properties' => sub {
+    cmp_deeply $idv->reported_properties, {}, 'Expected reported properties';
+};
+
+done_testing();
