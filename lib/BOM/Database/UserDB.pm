73efaa48405413d6ce6982bc4e635348d1b2cae5
billmarriott-binary <38449124+billmarriott-binary@users.noreply.github.com>
2021-06-29 13:16:33 +0800

    userdb/use_replica (#939)
    * wip
    
    * tidy
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes

--- lib/BOM/Database/UserDB.pm
+++ lib/BOM/Database/UserDB.pm
@@ -6,7 +6,10 @@ use BOM::Database::Rose::DB;
 
 sub rose_db {
     my %overrides  = @_;
+    my $type       = delete $overrides{operation} // 'write';
+    my $db_type    = $type eq 'replica' ? '_replica' : '';
     my $db_postfix = $ENV{DB_POSTFIX} // '';
+
     BOM::Database::Rose::DB->register_db(
         connect_options => {
             AutoCommit => 1,
@@ -15,9 +18,9 @@ sub rose_db {
         },
         schema   => 'users',
         domain   => 'userdb',
-        type     => 'write',
+        type     => $type,
         driver   => 'Pg',
-        database => "userdb$db_postfix",
+        database => "userdb$db_type$db_postfix",
         port     => 6432,
         username => 'write',
         host     => '/var/run/postgresql',
@@ -27,10 +30,17 @@ sub rose_db {
 
     my $db = BOM::Database::Rose::DB->new_or_cached(
         domain => 'userdb',
-        type   => 'write',
+        type   => $type,
     );
 
     $db->dbic->dbh->selectall_arrayref('SELECT audit.set_metadata(?::TEXT)', undef, $ENV{AUDIT_STAFF_NAME} // 'system');
+
+    if ((BOM::Config->on_qa() or BOM::Config->on_ci()) and $type eq 'replica') {
+        # Currently in QA/CI environments, the database is setup such that user is able
+        # to write to replicas. Until we can more accurately mimic production setup,
+        # we simulate this replica readonly behaviour as such:
+        $db->dbic->dbh->do("SET default_transaction_read_only TO 'on'");
+    }
 
     return $db;
 }
