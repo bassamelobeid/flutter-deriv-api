542556891278f21968b8f009fdc5da8c6081bd8c
Michael Mueller <52896557+michaelmueller-binary@users.noreply.github.com>
2021-08-11 13:04:55 +1000

    api/make_playground_update_script_work_for_api_deriv (#4457)
    * duplicate Code in script so it updates api.deriv.com at the same time as developers.binary.com
    
    * api/make_playground_update_script_work_for_deriv
    
    * api/make_playground_update_script_work_for_deriv
    
    * trigger tests
    
    * api/make_playground_update_script_work_for_api_deriv
    
    * api/make_playground_update_script_work_for_api_deriv
    
    * trigger tests
    
    * trigger tests
    
    * trigger tests
    
    * trigger tests
    
    Co-authored-by: Michael.Mueller <michael.mueller@deriv.com>

--- script/websocket_schema_update.sh
+++ script/websocket_schema_update.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
+# This script formats and copies the JSON schema files used for the API Playground
+# from binary-websocket-api to the respective repositories. 
+# 07/2021 we are currently copying for two sites api.deriv.com and developers.binary.com.
+# developers.binary.com will soon be retired and redirect to api.deriv.com.
 
 set -ex
 
 rm -rf /tmp/websockets
+rm -rf /tmp/deriv-websockets
 
 git clone git@github.com:binary-com/websockets /tmp/websockets
+git clone git@github.com:binary-com/deriv-developers-portal /tmp/deriv-websockets
+
 
 cd /tmp/websockets
 git config --local user.email "sysadmin@binary.com"
 git config --local user.name "CircleCI"
 
 rsync /home/git/regentmarkets/binary-websocket-api/config /tmp/websockets/ --delete -a
+rsync /home/git/regentmarkets/binary-websocket-api/config /tmp/deriv-websockets/ --delete -a
 
 # Generate api list yml file, and remove json files of excluded methods
 /home/git/regentmarkets/binary-websocket-api/script/websocket_api_list.pl /tmp/websockets/
 
+cd /tmp/deriv-websockets
+/home/git/regentmarkets/binary-websocket-api/script/websocket_api_list.pl /tmp/deriv-websockets/
+
+
+
+cd /tmp/websockets
 # Show what we're changing - anyone can easily check version control for this,
 # but this diff usually is not too long.
 git diff | cat
@@ -24,6 +38,16 @@ git add -A
 # in such cases but we don't want to fail either. So, we catch the
 # output and investigate what went wrong with the commit.
 # The tee command is added to see the message in the log.
+X="$(git commit -m "JSON Schema Update")" ||
+tee /dev/stderr <<<"$X" | grep -q 'nothing to commit'
+git push origin HEAD
+
+
+# repeat the push for api.deriv.com 
+cd /tmp/deriv-websockets
+git diff | cat
+
+git add -A
 X="$(git commit -m "JSON Schema Update")" ||
 tee /dev/stderr <<<"$X" | grep -q 'nothing to commit'
 git push origin HEAD
