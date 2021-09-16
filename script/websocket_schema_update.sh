386437c393bfdd3413b59b25909845f2efdee822
Michael Mueller <52896557+michaelmueller-binary@users.noreply.github.com>
2021-09-16 11:03:09 +1000

    Update script/websocket_schema_update.sh
    Co-authored-by: Diego Ramírez <57363189+diego-binary@users.noreply.github.com>

--- script/websocket_schema_update.sh
+++ script/websocket_schema_update.sh
@@ -7,7 +7,7 @@
 # To test this fork binary-com/websockets and binary-com/deriv-developers-portal then supply your git username as an argument
 
 
-GITORG=${1:-binary-codddm}
+GITORG=${1:-binary-com}
 set -ex
 
 rm -rf /tmp/websockets
