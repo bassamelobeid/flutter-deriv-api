8890a5fe1ec13db587796ff083e2c02b9e80d5d1
Kirill Sysoev <55120154+kirill-binary@users.noreply.github.com>
2021-10-20 14:54:45 +0800

    Payment methods api/p2p (#3761)
    * Adding p2p
    
    * git notes
    
    * Tidy
    
    * critic fixes
    
    * tidy
    
    * remove landing companies sub
    
    * Review fixes
    
    * Add p2p.get_trade_band_cfg
    
    * Git notes
    
    * Git notes
    
    * add p2p_limits sub
    
    * Add test
    
    * variable renaming
    
    * Adding different limits in tests
    
    * Tidy
    
    * Rebuild
    
    * Rebuild
    
    * Returns information about p2p payment method based on residence of the client
    
    * Updates tests
    
    Co-authored-by: Youssef <Youssef@deriv.com>

--- lib/BOM/RPC/v3/PaymentMethods.pm
+++ lib/BOM/RPC/v3/PaymentMethods.pm
@@ -14,16 +14,20 @@ use strict;
 use warnings;
 
 use DataDog::DogStatsd::Helper qw(stats_inc);
+use List::Util qw(any uniq);
 use Syntax::Keyword::Try;
 
 use BOM::Config;
 use BOM::Config::Redis;
+use BOM::Config::Runtime;
 
 use BOM::Platform::Context qw(localize request);
 use BOM::Platform::Doughflow qw(get_payment_methods);
 use BOM::RPC::Registry '-dsl';
 use BOM::RPC::v3::Utility;
 use BOM::User::Client;
+use Brands;
+use LandingCompany::Registry;
 
 =head2 payment_methods
 
@@ -61,14 +65,20 @@ rpc "payment_methods", sub {
     my $token_details = $params->{token_details};
     my $brand         = request()->brand->name;
 
-    $country = BOM::User::Client->new({loginid => $token_details->{loginid}})->residence
-        if $token_details;
+    my $client;
+    if ($token_details and exists $token_details->{loginid}) {
+        $client  = BOM::User::Client->new({loginid => $token_details->{loginid}});
+        $country = $client->residence;
+    }
+
     try {
-        my $ret = get_payment_methods($country, $brand);
+        my $payment_methods_list = get_payment_methods($country, $brand);
+        my $p2p_payment_methods  = get_p2p_as_payment_method($country, $client);
 
-        stats_inc('bom_rpc.v_3.no_payment_methods_found.count') unless scalar @$ret;
+        push @$payment_methods_list, $p2p_payment_methods if defined($p2p_payment_methods);
+        stats_inc('bom_rpc.v_3.no_payment_methods_found.count') unless scalar @$payment_methods_list;
 
-        return $ret;
+        return $payment_methods_list;
     } catch ($error) {
         if ($error =~ m/Unknown country code/) {
             return BOM::RPC::v3::Utility::create_error({
@@ -79,5 +89,113 @@ rpc "payment_methods", sub {
         die $error;
     }
 };
+
+=head2 get_p2p_as_payment_method
+
+Returns p2p as payment method following the same structure used in Doughflow.
+
+Parameters supported:
+
+=over 4
+
+=item * C<country> - A string with the country as two letter code (ISO 3166-Alpha2).
+
+=item * C<client> - the L<BOM::User::Client> instance
+
+=back
+
+Returns a hash of the p2p information payment methods or undef in case of p2p is not supported in the country were passed.
+
+It returns hashref described in L<BOM::Platform::Doughflow::get_payment_methods>
+
+=cut
+
+sub get_p2p_as_payment_method {
+    my ($country, $client) = @_;
+
+    return undef unless $country;
+
+    my @short_codes = request()->brand->countries_instance->real_company_for_country($country);
+    my ($p2p_lc) = grep { $_->{p2p_available} } map { LandingCompany::Registry::get($_) } @short_codes;
+
+    my @restricted_countries = BOM::Config::Runtime->instance->app_config->payments->p2p->restricted_countries->@*;
+    return undef if (!$p2p_lc || (any { $_ eq lc($country) } @restricted_countries));
+
+    my @supported_currencies = map { uc($_) } @{BOM::Config::Runtime->instance->app_config->payments->p2p->available_for_currencies};
+    my $limits               = p2p_limits(\@supported_currencies, $p2p_lc->{broker_codes}[0], $client, $country);
+
+    return +{
+        supported_currencies => \@supported_currencies,
+        deposit_limits       => $limits->{deposit_limits},
+        deposit_time         => '',
+        description          => localize("DP2P is Deriv's peer-to-peer deposit and withdrawal service"),
+        display_name         => 'DP2P',
+        id                   => 'DP2P',
+        payment_processor    => '',
+        predefined_amounts   => [5, 10, 100, 300, 500],
+        signup_link          => '',
+        type_display_name    => 'P2P',
+        type                 => '',
+        withdraw_limits      => $limits->{withdraw_limits},
+        withdrawal_time      => '',
+    };
+}
+
+=head2 p2p_limits
+
+Gets the min and max p2p deposit and withdraw limits
+
+Takes the following argument:
+
+=over 4
+
+=item * C<landing_companies> - landing company of the client or country in request 
+
+=item * C<supported_currencies> - array ref of the supported currencies for p2p
+
+=item * C<client> - the L<BOM::User::Client> instance
+
+=item * C<country> - A string with the country as two letter code (ISO 3166-Alpha2)
+
+=back
+
+Returns hashref with C<deposit_limits> and C<withdraw_limits> as string key where each consist of hashrefs that have the following structure. 
+A string key with the 3-letter currency code, e.g. B<USD>, B<EUR>. The attributes for this hash ref are:
+
+=over 4
+
+=item * C<min> - Minimum amount of money required to deposit.
+
+=item * C<max> - Maximum amount of money required to deposit.
+
+=back
+
+=cut
+
+sub p2p_limits {
+    my ($supported_currencies, $broker_code, $client, $country) = @_;
+
+    $client //= BOM::Database::ClientDB->new({broker_code => $broker_code});
+
+    my $res = {};
+    for my $currency (@$supported_currencies) {
+        my ($buy_limit, $sell_limit) = $client->db->dbic->run(
+            fixup => sub {
+                return $_->selectrow_array("SELECT daily_buy_limit, daily_sell_limit FROM p2p.get_trade_band_cfg(?,?,?)",
+                    undef, $country, 'low', $currency);
+            });
+
+        $res->{deposit_limits}->{$currency} = {
+            'max' => $buy_limit,
+            'min' => 0
+        };
+        $res->{withdraw_limits}->{$currency} = {
+            'max' => $sell_limit,
+            'min' => 0
+        };
+    }
+
+    return $res;
+}
 
 1;
--- t/BOM/RPC/60_PaymentMethods/01_payment_methods.t
+++ t/BOM/RPC/60_PaymentMethods/01_payment_methods.t
@@ -7,9 +7,15 @@ use RedisDB;
 # test dependencies
 use Test::MockModule;
 use Test::Most;
+use Test::Deep qw( cmp_deeply );
 
 use BOM::Test::Data::Utility::UnitTestDatabase qw(:init);
+
+use BOM::Database::ClientDB;
+
 use BOM::Test::RPC::QueueClient;
+
+use BOM::RPC::v3::PaymentMethods;
 
 require Test::NoWarnings;
 
@@ -53,6 +59,81 @@ subtest 'PaymentMethods' => sub {
     $params->{token}   = BOM::Platform::Token::API->new->create_token($client_cr->loginid, 'test token123');
     $rpc_ct->call_ok('payment_methods', $params)->has_no_system_error->has_no_error;
 };
+
+subtest 'get_p2p_as_payment_method' => sub {
+
+    my $expected = {
+        supported_currencies => ["USD"],
+        deposit_limits       => {
+            "USD" => {
+                "max" => 100,
+                "min" => 0,
+            }
+        },
+        deposit_time       => '',
+        description        => "DP2P is Deriv's peer-to-peer deposit and withdrawal service",
+        display_name       => 'DP2P',
+        id                 => 'DP2P',
+        payment_processor  => '',
+        predefined_amounts => [5, 10, 100, 300, 500],
+        signup_link        => '',
+        type_display_name  => 'P2P',
+        type               => '',
+        withdraw_limits    => {
+            "USD" => {
+                "max" => 100,
+                "min" => 0,
+            }
+        },
+        withdrawal_time => '',
+    };
+
+    my $client       = $client_cr;
+    my $country_code = $client->residence;
+
+    my $p2p_payment_methods = BOM::RPC::v3::PaymentMethods::get_p2p_as_payment_method($country_code, $client);
+    cmp_deeply($p2p_payment_methods, $expected, 'Call with client param only.');
+
+    set_p2p_max_band(70);
+    $expected->{deposit_limits}->{USD}->{max}  = 70;
+    $expected->{withdraw_limits}->{USD}->{max} = 70;
+
+    my $supported_countries = {
+        'Indonesia' => 'id',
+        'Brazil'    => 'br',
+        'Egypt'     => 'eg'
+    };
+
+    for my $country (keys %$supported_countries) {
+        $country_code = $supported_countries->{$country};
+        $client       = undef;
+
+        $p2p_payment_methods = BOM::RPC::v3::PaymentMethods::get_p2p_as_payment_method($country_code, $client);
+        cmp_deeply($p2p_payment_methods, $expected, "Call with country param only with supported country of $country.");
+    }
+
+    my $unsupported_countries = {
+        'Sweden'         => 'se',
+        'Spain'          => 'es',
+        'United Kingdom' => 'gb'
+    };
+    $expected = undef;
+
+    for my $country (keys %$unsupported_countries) {
+        $country_code = $unsupported_countries->{$country};
+        $client       = undef;
+
+        $p2p_payment_methods = BOM::RPC::v3::PaymentMethods::get_p2p_as_payment_method($country_code, $client);
+        cmp_deeply($p2p_payment_methods, $expected, "Call with country param only with unsupported country of  $country.");
+    }
+
+};
+
+sub set_p2p_max_band {
+    my $max_band = shift;
+
+    $client_cr->db->dbic->dbh->do("UPDATE p2p.p2p_country_trade_band SET  max_daily_buy = ?, max_daily_sell = ?", undef, $max_band, $max_band);
+}
 
 done_testing();
 
