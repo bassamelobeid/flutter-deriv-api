8874669c3b345edd7f0bf5326fe375c4bd264b12
Diego Ramírez <57363189+diego-binary@users.noreply.github.com>
2021-10-20 04:44:16 -0300

    deriv_partners/ws_api_endpoint (#4625)
    * deriv_partners/ws_api_endpoint [skip ci]
    
    * trigger ci with notes
    
    * adding new_account_affiliate endpoint
    
    * fix structure
    
    * remove auth_required
    
    * add affiliate_plan
    
    * update example
    
    * fix structure
    
    * retest
    
    * retest
    
    * refactor to affiliate_account_add + auth required + hidden
    
    * authr required + hidden
    
    * update Actions.pm
    
    * trigger ci with notes
    
    * additionalProperties: false
    
    * trigger ci with notes
    
    * fix schema
    
    * trigger ci with notes
    
    * adjust receive.json
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    Co-authored-by: Felipe Martinez <felipe@binary.com>

--- lib/Binary/WebSocketAPI/Actions.pm
+++ lib/Binary/WebSocketAPI/Actions.pm
@@ -379,6 +379,13 @@ sub actions_config {
             }
         ],
         [
+            'affiliate_account_add',
+            {
+                stash_params => [qw/ server_name client_ip user_agent /],
+                msg_group    => 'account',
+            }
+        ],
+        [
             'new_account_maltainvest',
             {
                 stash_params => [qw/ server_name client_ip user_agent /],
