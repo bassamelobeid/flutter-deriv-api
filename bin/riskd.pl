1f62a9e7ee1a6bd59145d0509dd6d959ba342900
Shantanu Patil <shantanu@regentmarkets.com>
2021-12-07 10:12:10 +0530

    backoffice_log_config (#2792)
    * backoffice_log_config
    
    * backoffice_log_config
    
    * backoffice_log_config
    
    * add json stderr
    
    * make tidy
    
    * change all log config that use backoffice
    
    * remove json config for scripts that are not binary services
    
    * trigger ci
    
    Co-authored-by: Nobody User <nobody@qa69.regentmarkets.com>

--- bin/riskd.pl
+++ bin/riskd.pl
@@ -1,7 +1,11 @@
 #!/etc/rmg/bin/perl
 use strict;
 use warnings;
-use Log::Any::Adapter ('DERIV', log_level => $ENV{RISKD_LOG_LEVEL} // 'info');
+use Log::Any::Adapter (
+    'DERIV',
+    log_level => $ENV{RISKD_LOG_LEVEL} // 'info',
+    stderr    => 'json'
+);
 use BOM::Backoffice::Script::Riskd;
 use Metrics::Any::Adapter qw(DogStatsd);
 
