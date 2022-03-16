ff18f20642cb328c27485e4e31e5f7cc83b4bd73
Amanda Chan <59076062+amanda-binary@users.noreply.github.com>
2022-03-16 13:20:36 +0800

    Revert "poo/upload_api (#2785)" (#3000)
    This reverts commit a7887ee8e89bd1507edb2d25090405a000aed597.

--- subs/subs_backoffice_clientdetails.pm
+++ subs/subs_backoffice_clientdetails.pm
@@ -44,7 +44,6 @@ use BOM::Backoffice::Request qw(request);
 use BOM::User::Onfido;
 use BOM::User::IdentityVerification;
 use BOM::RPC::v3::Accounts;
-use BOM::Platform::Doughflow;
 use BOM::Platform::Client::IdentityVerification;
 use BOM::User::RiskScreen;
 
@@ -686,8 +685,6 @@ SQL
         is_compliance                      => BOM::Backoffice::Auth0::has_authorisation(['Compliance']),
         risk_disclaimer_updated_at         => $risk_disclaimer_resubmission_updated_at,
         risk_disclaimer_updated_by         => $risk_disclaimer_resubmission_updated_by,
-        payment_methods                    => BOM::Platform::Doughflow::get_payment_methods($client->residence),
-        proof_of_ownership_list            => $client->proof_of_ownership->list(),
         disallow_residence_change          => @countries_disallow_residence_change
     };
 
@@ -1255,7 +1252,7 @@ sub show_client_id_docs {
             : "";
 
         $links .=
-            qq{<tr><td width="20" dir="rtl" $expired_poi_hint > &#9658; </td><td style="width:400px;overflow:hidden;"><a class="link" href="$url" data-document-id="$id" target="_blank">$file_name</a></td>$age_display$input};
+            qq{<tr><td width="20" dir="rtl" $expired_poi_hint > &#9658; </td><td style="width:400px;overflow:hidden;"><a class="link" href="$url" target="_blank">$file_name</a></td>$age_display$input};
 
         $links .= qq{<td><input type="checkbox" class='files_checkbox' name="document_list" value="$id-$loginid-$file_name"><td>};
 
