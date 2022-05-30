61bb915c12cc52ed0c392038129d57cd1a62811f
felipe-binary <64797927+felipe-binary@users.noreply.github.com>
2022-05-30 06:14:53 -0400

    payment_record/refactor (#1338)
    * payment_record/refactor [skip ci]
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * refactor to zset
    
    * minor fixes
    
    * add comment
    
    * improve comment
    
    * fix comment
    
    * update test with edge cases
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * simplify the keys
    
    * filter pr
    
    * improve testing
    
    * add deprecated fn for parallel check
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * trigger ci with notes
    
    * add legacy flag checker
    
    * add trimmer/break away from legacy redis key
    
    * payment_record/refactor [skip ci]
    
    * trigger ci with card-info
    
    * Update lib/BOM/User/PaymentRecord.pm
    
    Co-authored-by: Diego Ramírez <57363189+diego-binary@users.noreply.github.com>
    
    * Update lib/BOM/User/PaymentRecord.pm
    
    Co-authored-by: Diego Ramírez <57363189+diego-binary@users.noreply.github.com>
    
    * fix comment
    
    * trigger ci with card-info
    
    * trigger ci with card-info
    
    * keep it simpler -tot -cur
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    Co-authored-by: Diego Ramírez <57363189+diego-binary@users.noreply.github.com>

--- lib/BOM/User/PaymentRecord.pm
+++ lib/BOM/User/PaymentRecord.pm
@@ -5,14 +5,19 @@ use warnings;
 
 use BOM::Config::Redis;
 use Date::Utility;
-use Digest::SHA qw/sha256_hex/;
 use List::Util qw/first max/;
 
 use constant LIFETIME_IN_DAYS           => 90;
-use constant LIFETIME_IN_SECONDS        => LIFETIME_IN_DAYS * 24 * 60 * 60;
-use constant PAYMENT_KEY_PREFIX         => 'PAYMENT_RECORD';
+use constant SECONDS_IN_A_DAY           => 86400;
+use constant LIFETIME_IN_SECONDS        => LIFETIME_IN_DAYS * SECONDS_IN_A_DAY;
+use constant PAYMENT_KEY_PREFIX         => 'PAYMENT_RECORD_V2';
 use constant PAYMENT_KEY_SEPARATOR      => '::';
 use constant PAYMENT_KEY_USER_ID_PREFIX => 'UID';
+# Think of this array as an only append structure, we never change the existing elements, we never delete them nor we flip them around.
+# Otherwise data corruption and agony!
+# pp: payment processor, pm: payment method, pt: payment type, id: payment method identifier
+use constant PAYMENT_SERIALIZE_FIELDS => [qw/pp pm pt id/];
+use constant PAYMENT_UNDEF_SYMBOL     => '^';
 
 =head2 new
 
@@ -24,8 +29,6 @@ Takes the following arguments as named parameters
 
 =item * C<user_id> - binary/deriv user id
 
-=item * C<payment_type> - the payment type being processed
-
 =back
 
 Returns a bless reference object
@@ -35,66 +38,261 @@ Returns a bless reference object
 sub new {
     my ($class, %args) = @_;
 
-    die 'user_id is mandatory'      unless $args{user_id};
-    die 'payment_type is mandatory' unless $args{payment_type};
+    die 'user_id is mandatory' unless $args{user_id};
 
     return bless {
-        user_id      => $args{user_id},
-        payment_type => $args{payment_type},
+        user_id => $args{user_id},
     }, $class;
 }
 
 =head2 storage_key
 
-    $obj->storage_key();
-
-Returns the key used to identify this payment in the underlying storage engine if it's possible to build it, 0 otherwise.
+Returns the redis key for the current user id.
 
 =cut
 
 sub storage_key : method {
     my ($self) = @_;
 
-    return _build_storage_key(
-        payment_type => $self->{payment_type},
-        user_id      => $self->{user_id});
+    return join(PAYMENT_KEY_SEPARATOR, PAYMENT_KEY_PREFIX, PAYMENT_KEY_USER_ID_PREFIX, $self->{user_id});
 }
 
 =head2 add_payment
 
-    $obj->add_payment();
-
-Saves the hashed account identifier on redis
+Stores the payment as its payload representation into the Redis ZSET.
 
 Takes the following arguments as named parameters
 
 =over 4
 
-=item * C<account_identifier> - account identifier user, for example account number, card number
+=item * C<pp> - payment processor
+
+=item * C<pm> - payment method
+
+=item * C<pt> - payment type
+
+=item * C<id> - client's payment method identifier.
 
 =back
+
+Returns C<1> on success.
 
 =cut
 
 sub add_payment : method {
     my ($self, %args) = @_;
-    my $account_identifier = $args{account_identifier};
 
-    return 0 unless $account_identifier;
-
+    my $payload     = $self->get_payload(\%args);
     my $storage_key = $self->storage_key();
-    return 0 unless $storage_key;
 
     my $redis = _get_redis();
     $redis->multi;
-    $redis->pfadd($storage_key, sha256_hex($account_identifier));
+    $redis->zadd($storage_key, time, $payload);
     # we set the expiry of the whole key
     # we extend expiry whenever the same key is updated
+    # note we will use a cronjob script to trim the zsets and keep the memory tamed
     $redis->expire($storage_key, LIFETIME_IN_SECONDS);
     $redis->exec;
 
     return 1;
 }
+
+=head2 from_payload
+
+Given a payload string, recovers the equivalent hashref structure.
+
+It takes:
+
+=over 4
+
+=item * C<$payload> - the payload as a string
+
+=back
+
+Returns a hashref.
+
+=cut
+
+sub from_payload {
+    my (undef, $payload) = @_;
+
+    my %payment;
+
+    @payment{@{+PAYMENT_SERIALIZE_FIELDS}} = map { $_ eq PAYMENT_UNDEF_SYMBOL ? undef : $_ } split(/\|/, $payload);
+
+    return \%payment;
+}
+
+=head2 get_payload
+
+Generates the payload for the payment, this is a pipe `|` separated string of each element of the
+payment. If one component of the array is undef we will use the `^` symbol to represent it.
+
+It takes the following args:
+
+=over 4
+
+=item * C<pp> - payment processor
+
+=item * C<pm> - payment method
+
+=item * C<pt> - payment type
+
+=item * C<id> - payment method identifier
+
+=back
+
+Returns a pipe separated string that represents the current payment.
+
+=cut
+
+sub get_payload {
+    my (undef, $args) = @_;
+
+    return join('|', map { $_ // PAYMENT_UNDEF_SYMBOL } @$args{@{+PAYMENT_SERIALIZE_FIELDS}});
+}
+
+=head2 get_payments
+
+Returns the subset of payments made in the defined period as hashrefs.
+
+It takes:
+
+=over 4
+
+=item * C<$period> - Number of days to get the data for
+
+=back
+
+Returns an arrayref of payments hashrefs.
+
+=cut
+
+sub get_payments : method {
+    my ($self, $period) = @_;
+
+    my $payments = $self->get_raw_payments($period);
+
+    return [map { $self->from_payload($_) } $payments->@*];
+}
+
+=head2 get_raw_payments
+
+Returns the subset of payments made in the defined period as strings.
+
+It takes:
+
+=over 4
+
+=item * C<$period> - Number of days to get the data for
+
+=back
+
+Returns an arrayref of payments strings.
+
+=cut
+
+sub get_raw_payments {
+    my ($self, $period) = @_;
+
+    return [] unless $period;
+
+    return [] if $period > LIFETIME_IN_DAYS;
+
+    my $storage_key = $self->storage_key();
+
+    my $lower_bound = time - SECONDS_IN_A_DAY * $period;
+
+    return _get_redis()->zrangebyscore($storage_key, $lower_bound, '+Inf');
+}
+
+=head2 filter_payments
+
+Given a payment object and arrayref of payments strings, filter out those that do not match.
+
+Note that a non existant key will be treated as wildcard instead of undef.
+
+It takes:
+
+=over 4
+
+=item C<$payment> - a payment hashref
+
+=item C<$resulset> - the payments string arrayref.
+
+=back
+
+Returns a subset o C<$resultset> with only those payments that had a match.
+
+=cut
+
+sub filter_payments {
+    my (undef, $payment, $resultset) = @_;
+    my $regex = [];
+
+    for (qw/pp pm pt id/) {
+        my $re_comp;
+
+        if (exists $payment->{$_}) {
+            if (defined $payment->{$_}) {
+                my $value = $payment->{$_};
+                $re_comp = "\Q$value\E";
+            } else {
+                $re_comp = "\Q^\E";
+            }
+        } else {
+            $re_comp = '(.*)?';
+        }
+
+        push $regex->@*, $re_comp;
+    }
+
+    my $regex_str = join "\Q|\E", $regex->@*;
+
+    return [grep { $_ =~ /^$regex_str/ } $resultset->@*];
+}
+
+=head2 trimmer
+
+Trims all the payment records zset into its LIFETIME_IN_DAYS boundary.
+
+The scan command was used, note this command ensure all the matched keys will get
+iterated over a full sweep of the cursor, the cursor will return to 0 when the sweep is finished.
+
+Note this is not a class method.
+
+=cut
+
+sub trimmer {
+    my $upper_bound = time - LIFETIME_IN_SECONDS;
+    my $redis       = _get_redis();
+    my $cursor      = 0;
+    my $keys;
+
+    do {
+        ($cursor, $keys) = $redis->scan($cursor, 'MATCH', +PAYMENT_KEY_PREFIX . '*')->@*;
+
+        $redis->zremrangebyscore($_, '-Inf', $upper_bound) for $keys->@*;
+
+    } while ($cursor > 0);
+
+    return 1;
+}
+
+=head2 _get_redis
+
+Returns the connection object to the payments redis instance
+
+=cut
+
+sub _get_redis {
+    return BOM::Config::Redis::redis_payment_write();
+}
+
+=head1 DEPRECATED
+
+From this points the functions defined are deprecated and we must consider removing them once
+the legacy redis keys are all expired on production servers.
 
 =head2 get_distinct_payment_accounts_for_time_period
 
@@ -119,6 +317,8 @@ sub get_distinct_payment_accounts_for_time_period : method {
 
     return 0 unless $args{period};
 
+    return 0 unless $args{payment_type};
+
     die 'Payment record required period length is greater than our storage lifetime of ' . LIFETIME_IN_DAYS . ' days.'
         if $args{period} > LIFETIME_IN_DAYS;
 
@@ -127,112 +327,10 @@ sub get_distinct_payment_accounts_for_time_period : method {
     return _get_redis()->pfcount(
         @{
             _get_keys_for_time_period(
-                payment_type => $self->{payment_type},
+                payment_type => $args{payment_type},
                 user_id      => $self->{user_id},
                 period       => $args{period},
             )});
-}
-
-=head2 set_flag
-
-Sets a flag on the user's payment record, optionally sets it with a timeout after which the flag is going to be deleted
-
-Takes the following as named parameters
-
-=over 4
-
-=item * C<name>
-
-The name of the flag
-
-=item * C<expire>
-
-Time in seconds after the flag is going to be deleted
-
-=back
-
-=cut
-
-sub set_flag {
-    my ($self, %args) = @_;
-    my $flag_name   = $args{name}   // die 'name is mandatory';
-    my $flag_expire = $args{expire} // 0;
-    my $flag_key    = _build_flag_key(
-        user_id => $self->{user_id},
-        name    => $flag_name
-    );
-
-    my $redis = _get_redis();
-    $redis->multi;
-    $redis->set($flag_key, 1);
-    $redis->expire($flag_key, $flag_expire) if $flag_expire;
-    $redis->exec;
-
-    return 1;
-}
-
-=head2 is_flagged
-
-
-Takes the following as positional parameter
-
-=over 4
-
-=item * C<name>
-
-The name of the flag
-
-=back
-
-=cut
-
-sub is_flagged {
-    my ($self, $flag_name) = @_;
-
-    return _get_redis()->get(
-        _build_flag_key(
-            user_id => $self->{user_id},
-            name    => $flag_name
-        )) // 0;
-}
-
-=head2 _build_flag_key
-
-Takes the following as named parameters
-
-=over 4
-
-=item * C<user_id>
-
-The id of the user who made the payment
-
-=item * C<name>
-
-The name of the flag
-
-=back
-
-=cut
-
-sub _build_flag_key {
-    my %args = @_;
-
-    return join(
-        PAYMENT_KEY_SEPARATOR,         #
-        PAYMENT_KEY_PREFIX,            #
-        PAYMENT_KEY_USER_ID_PREFIX,    #
-        $args{user_id},                #
-        $args{name});
-}
-
-=head2 _get_redis
-
-Returns the connection object to the payments redis instance
-
-=cut
-
-sub _get_redis {
-    return BOM::Config::Redis::redis_payment_write();
 }
 
 =head2 _get_keys_for_time_period
@@ -293,6 +391,53 @@ sub _build_storage_key {
     }
 
     return join(PAYMENT_KEY_SEPARATOR, @key_parts);
+}
+
+=head2 is_flagged
+
+Takes the following as positional parameter
+
+=over 4
+
+=item * C<name> The name of the flag
+
+=back
+
+=cut
+
+sub is_flagged {
+    my ($self, $flag_name) = @_;
+
+    return _get_redis()->get(
+        _build_flag_key(
+            user_id => $self->{user_id},
+            name    => $flag_name
+        )) // 0;
+}
+
+=head2 _build_flag_key
+
+Takes the following as named parameters
+
+=over 4
+
+=item * C<user_id> The id of the user who made the payment
+
+=item * C<name> The name of the flag
+
+=back
+
+=cut
+
+sub _build_flag_key {
+    my %args = @_;
+
+    return join(
+        PAYMENT_KEY_SEPARATOR,         #
+        PAYMENT_KEY_PREFIX,            #
+        PAYMENT_KEY_USER_ID_PREFIX,    #
+        $args{user_id},                #
+        $args{name});
 }
 
 1;
