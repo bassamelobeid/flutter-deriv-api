ceb5b73234ac59a2a8b8243a281fb3f4e2484814
felipe-binary <64797927+felipe-binary@users.noreply.github.com>
2022-06-14 05:33:59 -0400

    onfido/sanctioned_countries (#639)
    * add sanctioned countries
    
    * add more countries
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * state $country_details [ci]
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * s/santioned/disabled/g [ci]
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * retest [ci]
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info

--- lib/BOM/Config/Onfido.pm
+++ lib/BOM/Config/Onfido.pm
@@ -12,6 +12,7 @@ A repository that consists data related to Onfido.
 
 use strict;
 use warnings;
+use feature "state";
 
 use JSON::MaybeUTF8 qw(:v1);
 use Locale::Codes::Country qw(country_code2code);
@@ -41,9 +42,9 @@ sub supported_documents_for_country {
     $country_code = uc(country_code2code($country_code, 'alpha-2', 'alpha-3') // '');
     return [] unless $country_code;
 
-    my %country_details = _get_country_details();
+    my $country_details = _get_country_details();
 
-    return $country_details{$country_code}->{doc_types_list} // [];
+    return $country_details->{$country_code}->{doc_types_list} // [];
 }
 
 =head2 is_country_supported
@@ -55,11 +56,29 @@ Returns 1 if country is supported and 0 if it is not supported
 sub is_country_supported {
     my $country_code = shift;
 
+    return 0 if is_disabled_country($country_code);
+
     $country_code = uc(country_code2code($country_code, 'alpha-2', 'alpha-3') // '');
 
-    my %country_details = _get_country_details();
+    my $country_details = _get_country_details();
 
-    return $country_details{$country_code}->{doc_types_list} ? 1 : 0;
+    return $country_details->{$country_code}->{doc_types_list} ? 1 : 0;
+}
+
+=head2 is_disabled_country
+
+Returns 1 if the country is disabled, 0 otherwise.
+
+=cut
+
+sub is_disabled_country {
+    my $country_code = shift;
+
+    $country_code = uc(country_code2code($country_code, 'alpha-2', 'alpha-3') // '');
+
+    my $country_details = _get_country_details();
+
+    return $country_details->{$country_code}->{disabled} ? 1 : 0;
 }
 
 =head2 _get_country_details
@@ -69,8 +88,8 @@ Changes the format into hash
 =cut
 
 sub _get_country_details {
-
-    return map { $_->{country_code} => $_ } @{supported_documents_list()};
+    state $country_details = +{map { $_->{country_code} => $_ } @{supported_documents_list()}};
+    return $country_details;
 }
 
 1;
--- t/BOM/onfido_doc_supp.t
+++ t/BOM/onfido_doc_supp.t
@@ -7,6 +7,8 @@ use Test::Fatal;
 use Test::Exception;
 
 use BOM::Config::Onfido;
+use Locale::Codes::Country qw(country_code2code);
+use Test::Deep;
 
 my $id_supported_docs = ['Driving Licence', 'National Identity Card', 'Passport'];
 my $ng_supported_docs = ['Driving Licence', 'National Identity Card', 'Passport', 'Voter Id'];
@@ -28,6 +30,35 @@ subtest 'Invalid country ' => sub {
     lives_ok { BOM::Config::Onfido::is_country_supported('I213') } 'Invalid county, but it wont die';
     lives_ok { BOM::Config::Onfido::is_country_supported(123) } 'Invalid county, but it wont die';
     is_deeply(BOM::Config::Onfido::supported_documents_for_country(123), [], 'Invalid county, returns empty list');
+};
+
+subtest 'disabled countries' => sub {
+    my $config = BOM::Config::Onfido::supported_documents_list();
+
+    my $disabled_countries = [map { $_->{disabled} ? $_->{country_code} : () } values $config->@*];
+
+    my $expected_disabled_countries = [
+        qw/
+            by
+            af
+            cd
+            iq
+            ly
+            sy
+            ru
+            cn
+            ir
+            kp
+            /
+    ];
+
+    for my $cc ($expected_disabled_countries->@*) {
+        ok BOM::Config::Onfido::is_disabled_country($cc),   "$cc is disabled";
+        ok !BOM::Config::Onfido::is_country_supported($cc), "$cc is unsupported";
+    }
+
+    cmp_bag $disabled_countries, [map { uc(country_code2code($_, 'alpha-2', 'alpha-3')); } $expected_disabled_countries->@*],
+        'disabled countries full list';
 };
 
 done_testing();
