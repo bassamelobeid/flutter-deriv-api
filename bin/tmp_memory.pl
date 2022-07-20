0ac6d45ff566799a27099d2958c362e869a8b0e6
chylli-binary <chylli@binary.com>
2022-07-20 17:18:21 +0800

    update [ci skip]

--- bin/tmp_memory.pl
+++ bin/tmp_memory.pl
@@ -7,6 +7,7 @@ my $t = Proc::ProcessTable->new;
 
 foreach my $p (@{$t->table}) {
     print "--------------------------------\n";
+    next unless $p->{cmnd} =~ /rpc_redis/;
     foreach my $f ($t->fields){
         print $f, ":  ", $p->{$f}, "\n";
     }
