8ddb046756e5f861455ac4f34b61a5311701f436
chylli-binary <chylli@binary.com>
2022-07-22 11:05:27 +0800

    add delta [ci skip]

--- bin/tmp_memory.pl
+++ bin/tmp_memory.pl
@@ -3,8 +3,12 @@ use warnings;
 # Dump all the information in the current process table
 use Proc::ProcessTable;
 use DataDog::DogStatsd::Helper qw(stats_gauge);
-dd_memory();
+use feature qw(state);
+dd_memory(1,'forex');
+sleep 10;
+dd_memory(0, 'forex');
 sub dd_memory{
+    my ($start, $market) = @_;
     my $t = Proc::ProcessTable->new;
     #my @fields = $t->fields;
     #print "@fields\n";
@@ -31,19 +35,35 @@ sub dd_memory{
             dd_prefix => 'memory.pricer_load_runner'
         },
         {
+            # TODO when finish, this process will no there, so we must process it before kill
             regexp => qr/proposal_sub.pl/,
             dd_prefix => 'memory.proposal_sub'
         },
 
     );
-    foreach my $p (@{$t->table}) {
-        foreach my $cfg (@process_cfg){
+    state %data;
+    if($start){
+        %data = ();
+    }
+    # sort to keep them same order
+    my @processes = sort {$a->{pid} <=> $b->{pid}} $t->table->@*;
+    foreach my $cfg_idx ($#process_cfg){
+        my $cfg = $process_cfg[$cfg_idx];
+        # the idx of processes that have same name
+        my $idx = 0;
+        foreach my $p (@processes) {
             next unless $p->{cmndline} =~ $cfg->{regexp};
             next if ($cfg->{ppid} // '') eq 'not 1' && $p->{ppid} != 1;
-            $cfg->{idx}++;
+            $idx++;
             print "$p->{cmndline}:$p->{pid}\n";
             foreach my $f (qw(size rss)){
-                stats_gauge("$cfg->{dd_prefix}.$f", $p->{$f}, {tags => ["tag:$cfg->{idx}"]});
+                stats_gauge("$cfg->{dd_prefix}.$f", $p->{$f}, {tags => ["tag:idx$idx", "tag:$market"]});
+                if($start){
+                    $data{$cfg_idx}{$f}{$idx}{start} = $p->{$f};
+                }
+                else{
+                    stats_gauge("$cfg->{dd_prefix}.${f}delta", $p->{$f} - $data{$cfg_idx}{$f}{$idx}{start}, {tags => ["tag:idx$idx", "tag:$market"]});
+                }
             }
         }
     }
