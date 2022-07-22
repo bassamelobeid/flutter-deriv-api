3d61e43d3650125f2ab796dac1244c98b82338a5
chylli-binary <chylli@binary.com>
2022-07-22 08:41:11 +0800

    apply dd_memory

--- bin/pricer_load_runner.pl
+++ bin/pricer_load_runner.pl
@@ -527,6 +527,7 @@ sub start_subscription {
 
     my $whole_command = [$command, '-s', $subscriptions, '-a', $app_id, '-c', 5, '-r', $check_time, '-m', $market];
     say "start command '@$whole_command'";
+    dd_memory();
     $process = $loop->open_process(
         command => $whole_command,
         stdout  => {
@@ -555,28 +556,50 @@ sub start_subscription {
             }
         },
         on_finish => sub {
-
+            dd_memory();
         });
 }
 
 sub dd_memory{
     my $t = Proc::ProcessTable->new;
-
+    #my @fields = $t->fields;
+    #print "@fields\n";
     my @process_cfg = (
         {
             regexp => qr/binary_rpc_redis\.pl.*category=general/,
             dd_prefix => 'memory.rpc_redis_general'
         },
         {
-             regexp => qr/binary_rpc_redis\.pl.*category=tick/,
+            regexp => qr/binary_rpc_redis\.pl.*category=tick/,
             dd_prefix => 'memory.rpc_redis_tick'
         },
+        {
+            regexp => qr/price_queue\.pl/,
+            dd_prefix => 'memory.price_queue',
+        },
+        {
+            regexp => qr/price_daemon\.pl/,
+            dd_prefix => 'memory.price_daemon',
+            ppid => 'not 1', # price_daemon will fork a subprocess as a worker, the parent process does nothing, so ignore it.
+        },
+        {
+            regexp => qr/pricer_load_runner\.pl/,
+            dd_prefix => 'memory.pricer_load_runner'
+        },
+        {
+            regexp => qr/proposal_sub.pl/,
+            dd_prefix => 'memory.proposal_sub'
+        },
+
     );
     foreach my $p (@{$t->table}) {
         foreach my $cfg (@process_cfg){
             next unless $p->{cmndline} =~ $cfg->{regexp};
+            next if ($cfg->{ppid} // '') eq 'not 1' && $p->{ppid} != 1;
+            $cfg->{idx}++;
+            print "$p->{cmndline}:$p->{pid}\n";
             foreach my $f (qw(size rss)){
-                stats_gauge("$cfg->{dd_pefix}.$f", $p->{$f}, {tags => ['tag:' . $market]});
+                stats_gauge("$cfg->{dd_prefix}.$f", $p->{$f}, {tags => ["tag:$cfg->{idx}"]});
             }
         }
     }
--- bin/tmp_memory.pl
+++ bin/tmp_memory.pl
@@ -6,22 +6,44 @@ use DataDog::DogStatsd::Helper qw(stats_gauge);
 dd_memory();
 sub dd_memory{
     my $t = Proc::ProcessTable->new;
-
+    #my @fields = $t->fields;
+    #print "@fields\n";
     my @process_cfg = (
         {
             regexp => qr/binary_rpc_redis\.pl.*category=general/,
             dd_prefix => 'memory.rpc_redis_general'
         },
         {
-             regexp => qr/binary_rpc_redis\.pl.*category=general/,
-            dd_prefix => 'memory.rpc_redis_general'
+            regexp => qr/binary_rpc_redis\.pl.*category=tick/,
+            dd_prefix => 'memory.rpc_redis_tick'
         },
+        {
+            regexp => qr/price_queue\.pl/,
+            dd_prefix => 'memory.price_queue',
+        },
+        {
+            regexp => qr/price_daemon\.pl/,
+            dd_prefix => 'memory.price_daemon',
+            ppid => 'not 1', # price_daemon will fork a subprocess as a worker, the parent process does nothing, so ignore it.
+        },
+        {
+            regexp => qr/pricer_load_runner\.pl/,
+            dd_prefix => 'memory.pricer_load_runner'
+        },
+        {
+            regexp => qr/proposal_sub.pl/,
+            dd_prefix => 'memory.proposal_sub'
+        },
+
     );
     foreach my $p (@{$t->table}) {
         foreach my $cfg (@process_cfg){
             next unless $p->{cmndline} =~ $cfg->{regexp};
+            next if ($cfg->{ppid} // '') eq 'not 1' && $p->{ppid} != 1;
+            $cfg->{idx}++;
+            print "$p->{cmndline}:$p->{pid}\n";
             foreach my $f (qw(size rss)){
-                stats_gauge("$cfg->{dd_prefix}.$f", $p->{$f});
+                stats_gauge("$cfg->{dd_prefix}.$f", $p->{$f}, {tags => ["tag:$cfg->{idx}"]});
             }
         }
     }
