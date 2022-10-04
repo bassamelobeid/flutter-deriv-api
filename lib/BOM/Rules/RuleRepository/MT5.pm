4adeab8a7568d198fccf5a8f78db1e86700f303e
davidperumal-deriv <90030597+davidperumal-deriv@users.noreply.github.com>
2022-10-04 14:59:17 +0800

    backend_changes_for_new_kyc_flow_for_bvi_vanuatu (#158)
    * WIP
    
    * Update rule
    
    * weizhe-deriv/mt5_creation_bvi_vanuatu
    
    * added param test on fail
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP - Updated wording
    
    * Added jurisdiction check expiry rules [ci]
    
    * [ci]
    
    * fixed undef value warning [ci]
    
    * Added missing module [ci]
    
    * removed unused module [ci]
    
    * backend_changes_for_new_kyc_flow_for_bvi_vanuatu
    
    * trigger tests [ci] 2022-09-06 12:22:13
    
    * trigger tests [ci] 2022-09-06 23:15:28
    
    * trigger tests [ci] 2022-09-07 07:50:45
    
    * trigger tests [ci] 2022-09-07 08:13:06
    
    * Updated rule logic [ci]
    
    * [ci]
    
    * Added comment [ci]
    
    * Added existence check for mt5_id [ci]
    
    * Added check for edge case of false poa_pending [ci]
    
    * corrected EOF [ci]
    
    * trigger tests [ci] 2022-09-08 00:55:22
    
    * trigger tests [ci] 2022-09-08 07:36:08
    
    * trigger tests [ci] 2022-09-08 07:57:32
    
    * trigger tests [ci] 2022-09-08 07:59:26
    
    * trigger tests [ci] 2022-09-08 08:14:17
    
    * [ci]
    
    * trigger tests [ci] 2022-09-09 05:09:14
    
    * trigger tests [ci] 2022-09-12 01:48:22
    
    * trigger tests [ci] 2022-09-12 01:59:53
    
    * [ci]
    
    * [ci]
    
    * pa/sibling_commission_withdrawal (#151)
    
    * wip
    
    * pa/sibling_commission_withdrawal
    
    * trigger tests [ci] 2022-08-22 09:07:45
    
    * [ci] fix rounding
    
    * cs_agents_paymentagent_withdraw (#153)
    
    * cs_agents_paymentagent_withdraw
    
    * [ci] new rule for internal agents
    
    * typo
    
    * trigger tests [ci] 2022-08-24 01:48:58
    
    * Update lib/BOM/Rules/RuleRepository/Client.pm
    
    Co-authored-by: billmarriott-binary <38449124+billmarriott-binary@users.noreply.github.com>
    
    * Update config/actions/cashier.yml
    
    Co-authored-by: billmarriott-binary <38449124+billmarriott-binary@users.noreply.github.com>
    
    * Update Client.pm
    
    * Update client.t
    
    * [ci] error code renamed
    
    * [ci] comment changed
    
    Co-authored-by: billmarriott-binary <38449124+billmarriott-binary@users.noreply.github.com>
    
    * fixes
    
    * trigger tests [ci] 2022-09-01 03:55:22
    
    Co-authored-by: mat-fs <53427463+mat-fs@users.noreply.github.com>
    
    * trigger tests [ci] 2022-09-12 02:37:20
    
    * Refactor logic to be jurisdiction independent [ci]
    
    * trigger tests [ci] 2022-09-13 02:26:41
    
    * trigger tests [ci] 2022-09-13 03:01:19
    
    * trigger tests [ci] 2022-09-13 03:12:08
    
    * trigger tests [ci] 2022-09-13 08:51:30
    
    * trigger tests [ci] 2022-09-14 02:46:58
    
    * trigger tests [ci] 2022-09-14 02:49:03
    
    * modified datetime retrival method [ci]
    
    * bug fixes for mixed juris state [ci]
    
    * trigger tests [ci] 2022-09-19 05:22:14
    
    * trigger tests [ci] 2022-09-19 08:31:53
    
    * trigger tests [ci] 2022-09-19 08:58:07
    
    * trigger tests [ci] 2022-09-20 06:12:57
    
    * trigger tests [ci] 2022-09-20 06:39:20
    
    * trigger tests [ci] 2022-09-20 07:51:36
    
    * trigger tests [ci] 2022-09-20 08:12:09
    
    * trigger tests [ci] 2022-09-20 08:19:53
    
    * trigger tests [ci] 2022-09-20 08:56:51
    
    * trigger tests [ci] 2022-09-20 12:15:42
    
    * trigger tests [ci] 2022-09-20 12:17:38
    
    * trigger tests [ci] 2022-09-21 07:45:33
    
    * trigger tests [ci] 2022-09-21 08:19:10
    
    * trigger tests [ci] 2022-09-21 08:56:22
    
    * trigger tests [ci] 2022-09-22 06:12:15
    
    * trigger tests [ci] 2022-09-22 07:27:26
    
    * trigger tests [ci] 2022-09-22 09:11:00
    
    * trigger tests [ci] 2022-09-23 03:32:11
    
    * trigger tests [ci] 2022-09-26 03:25:06
    
    * trigger tests [ci] 2022-09-28 06:42:21
    
    Co-authored-by: weizhe-deriv <weizhe@deriv.com>
    Co-authored-by: weizhe-deriv <103245445+weizhe-deriv@users.noreply.github.com>
    Co-authored-by: billmarriott-binary <38449124+billmarriott-binary@users.noreply.github.com>
    Co-authored-by: mat-fs <53427463+mat-fs@users.noreply.github.com>

---
+++ lib/BOM/Rules/RuleRepository/MT5.pm
@@ -0,0 +1,73 @@
+package BOM::Rules::RuleRepository::MT5;
+
+=head1 NAME
+
+BOM::Rules::RuleRepositry::MT5
+
+=head1 DESCRIPTION
+
+This modules declares rules and regulations pertaining the MT5.
+
+=cut
+
+use strict;
+use warnings;
+
+use BOM::Rules::Registry qw(rule);
+use Time::Moment;
+use Date::Utility;
+use List::Util qw( any );
+
+use constant JURISDICTION_DAYS_LIMIT => {
+    bvi     => 10,
+    vanuatu => 5
+};
+
+rule 'mt5_account.account_poa_status_allowed' => {
+    description => 'Checks if mt5 accounts are considered valid by poa status and jurisdiction',
+    code        => sub {
+        my ($self, $context, $args) = @_;
+        my $client = $context->client($args);
+        my $new_mt5_jurisdiction =
+            $args->{new_mt5_jurisdiction};    # new_mt5_jurisdiction + loginid_details as parameter for new mt5 account (without an id)
+        my $mt5_id           = $args->{mt5_id};               # mt5_id + loginid_details as parameter for existing mt5 account
+        my %loginid_details  = %{$args->{loginid_details}};
+        my $error_message    = 'POAVerificationFailed';
+        my $mt5_jurisdiction = $new_mt5_jurisdiction;
+        ($mt5_jurisdiction) = $loginid_details{$mt5_id}->{attributes}->{group} =~ m/(bvi|vanuatu)/g
+            if defined $mt5_id and exists $loginid_details{$mt5_id};
+
+        return 1 if $client->get_poa_status() eq 'verified';
+
+        if (defined $mt5_jurisdiction) {
+            return 1 unless exists JURISDICTION_DAYS_LIMIT()->{$mt5_jurisdiction};
+        } else {
+            return 1;
+        }
+
+        if (defined $mt5_id) {
+            my $selected_mt5_status = $loginid_details{$mt5_id}->{status} // 'active';
+            $self->fail($error_message, params => {failed_by_expiry => 0}) if $selected_mt5_status eq 'poa_failed';
+            return 1 unless any { $selected_mt5_status eq $_ } qw/poa_pending poa_rejected/;
+        }
+
+        my @mt5_accounts =
+            grep {
+                    ($loginid_details{$_}->{platform} // '') eq 'mt5'
+                and $loginid_details{$_}->{account_type} eq 'real'
+                and $loginid_details{$_}->{attributes}->{group} =~ m/$mt5_jurisdiction/
+            } keys %loginid_details;
+
+        my $current_datetime = Date::Utility->new(Date::Utility->new->datetime);
+        foreach my $mt5_account_id (@mt5_accounts) {
+            my $mt5_account = $loginid_details{$mt5_account_id};
+            return 1 if not defined $mt5_account->{status};
+            my $mt5_creation_datetime = Date::Utility->new($mt5_account->{creation_stamp});
+            my $days_elapsed          = $current_datetime->days_between($mt5_creation_datetime);
+            my $poa_failed_by_expiry  = $days_elapsed <= JURISDICTION_DAYS_LIMIT()->{$mt5_jurisdiction} ? 0 : 1;
+            $self->fail($error_message, params => {failed_by_expiry => 1}) if $poa_failed_by_expiry;
+        }
+
+        return 1;
+    },
+};
