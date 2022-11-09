ab61465985363faac5c46282995047bc8bc5ad0e
heshan-deriv <109588077+heshan-deriv@users.noreply.github.com>
2022-11-09 12:44:11 +0400

    account/store client UI name (#168)
    * Adding the trading_hub as an allowed field to change with the set_settings API call
    
    * trigger tests [ci] 2022-10-05 16:13:51
    
    * Make tidy
    
    * trigger tests [ci] 2022-10-05 16:24:53
    
    * trigger tests [ci] 2022-10-06 06:42:49
    
    * trigger tests [ci] 2022-10-06 07:56:22
    
    * trigger tests [ci] 2022-10-06 08:10:29
    
    * trigger tests [ci] 2022-10-11 11:53:57
    
    * trigger tests [ci] 2022-10-19 10:59:03
    
    * trigger tests [ci] 2022-10-19 14:04:54
    
    * trigger tests [ci] 2022-10-21 06:52:35
    
    * trigger tests [ci] 2022-10-28 07:58:38
    
    * trigger tests [ci] 2022-10-28 08:07:56
    
    * trigger tests [ci] 2022-11-02 07:56:46
    
    * trigger tests [ci] 2022-11-02 08:36:27
    
    * trigger tests [ci] 2022-11-03 11:45:46
    
    * trigger tests [ci] 2022-11-04 07:10:33
    
    * trigger tests [ci] 2022-11-07 08:39:02
    
    * trigger tests [ci] 2022-11-07 08:57:04
    
    * trigger tests [ci] 2022-11-07 11:09:18

--- lib/BOM/Rules/RuleRepository/Profile.pm
+++ lib/BOM/Rules/RuleRepository/Profile.pm
@@ -258,7 +258,8 @@ rule 'profile.fields_allowed_to_change' => {
         # Ideally, I think we should refactor this part instead of hard-coding it. We have $client->immutable_fields logic in place.
         # The logic for fields allowed to be changed should be !$client->immutable_fields. But, I do not have a 100% understanding of
         # client profile rules.
-        my $allowed_fields_for_virtual = qr/set_settings|loginid|email_consent|residence|allow_copiers|preferred_language|feature_flag|citizen/;
+        my $allowed_fields_for_virtual =
+            qr/set_settings|loginid|email_consent|residence|allow_copiers|preferred_language|feature_flag|citizen|trading_hub/;
         for (keys %$args) {
             $self->fail(
                 'PermissionDenied',
