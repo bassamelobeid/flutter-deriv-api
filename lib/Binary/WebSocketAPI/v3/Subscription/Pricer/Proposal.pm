010412b42dcdf0d561a5c4509da6015ca8a6f4d0
qinfeng-deriv <91937565+qinfeng-deriv@users.noreply.github.com>
2022-12-01 15:25:54 +0800

    vanilla/api_changes (#5386)
    * wip
    
    * vanilla/api_changes
    
    * vanilla/clientdb_changes
    
    * trigger tests [ci] 2022-10-18 02:39:30
    
    * add send.json
    
    * trigger tests [ci] 2022-10-26 02:14:10
    
    * trigger tests [ci] 2022-10-26 02:30:45
    
    * trigger tests [ci] 2022-10-27 06:49:03
    
    * trigger tests [ci] 2022-10-27 08:49:20
    
    * trigger tests [ci] 2022-11-01 05:23:08
    
    * trigger tests [ci] 2022-11-01 05:43:25
    
    * trigger tests [ci] 2022-11-01 07:12:20
    
    * add number of contracts and strike price choices response [ci]
    
    * trigger tests [ci] 2022-11-08 01:58:02
    
    * fix make structure [ci]
    
    * add description [ci]
    
    * remove number of contracts and strike price choices for proposal open contract [ci]
    
    * remove longcode cache [ci]
    
    * trigger tests [ci] 2022-11-10 09:50:30
    
    * add min and max stake [ci]
    
    * change to barrier choices
    
    * trigger tests [ci] 2022-11-16 02:58:49
    
    * trigger tests [ci] 2022-11-16 04:15:54
    
    * trigger tests [ci] 2022-11-30 03:19:51

--- lib/Binary/WebSocketAPI/v3/Subscription/Pricer/Proposal.pm
+++ lib/Binary/WebSocketAPI/v3/Subscription/Pricer/Proposal.pm
@@ -40,7 +40,7 @@ sub do_handle_message {
                 $type    => {
                     %$response,
                     id       => $self->uuid,
-                    longcode => $c->l($self->cache->{longcode}),
+                    longcode => $c->l($response->{longcode}),
                 },
                 subscription => {id => $self->uuid},
             };
