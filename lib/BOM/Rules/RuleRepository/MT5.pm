f75aea355088de0e38184e9b408282126bb5805a
weizhe-deriv <103245445+weizhe-deriv@users.noreply.github.com>
2023-01-11 13:19:51 +0800

    weizhe-deriv/jurisdiction_milestone2 (#173)
    * weizhe-deriv/jurisdiction_milestone2
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Added poa_pending fail scenario
    
    * WIP
    
    * WIP
    
    * tidy
    
    * Fix missing parameter
    
    * updated error message
    
    * trigger tests [ci] 2022-11-08 09:30:47
    
    * trigger tests [ci] 2022-11-08 09:34:48
    
    * trigger tests [ci] 2022-11-08 09:44:33
    
    * updated pod coverage [ci]
    
    * trigger tests [ci] 2022-11-10 09:31:30
    
    * trigger tests [ci] 2022-11-10 10:03:55
    
    * trigger tests [ci] 2022-11-14 13:52:16
    
    * trigger tests [ci] 2022-11-15 06:20:52
    
    * refactor [ci]
    
    * [ci]
    
    * trigger tests [ci] 2022-11-25 06:09:49
    
    * trigger tests [ci] 2022-11-29 08:36:24
    
    * trigger tests [ci] 2022-12-15 06:26:44
    
    * trigger tests [ci] 2022-12-19 09:31:54
    
    * trigger tests [ci] 2023-01-04 08:36:34
    
    * trigger tests [ci] 2023-01-04 08:57:43

--- lib/BOM/Rules/RuleRepository/MT5.pm
+++ lib/BOM/Rules/RuleRepository/MT5.pm
@@ -23,6 +23,12 @@ use constant JURISDICTION_DAYS_LIMIT => {
     vanuatu => 5
 };
 
+use constant JURISDICTION_PROOF_REQUIREMENT => {
+    bvi         => ['poi'],
+    vanuatu     => ['poi'],
+    labuan      => ['poi', 'poa'],
+    maltainvest => ['poi', 'poa']};
+
 rule 'mt5_account.account_poa_status_allowed' => {
     description => 'Checks if mt5 accounts are considered valid by poa status and jurisdiction',
     code        => sub {
@@ -40,7 +46,8 @@ rule 'mt5_account.account_poa_status_allowed' => {
             ($mt5_jurisdiction) = $loginid_details{$mt5_id}->{attributes}->{group} =~ m/(bvi|vanuatu)/g;
         }
 
-        return 1 if $client->get_poa_status() eq 'verified';
+        my $poa_status = $client->get_poa_status();
+        return 1 if $poa_status eq 'verified';
 
         if (defined $mt5_jurisdiction) {
             return 1 unless exists JURISDICTION_DAYS_LIMIT()->{$mt5_jurisdiction};
@@ -50,8 +57,8 @@ rule 'mt5_account.account_poa_status_allowed' => {
 
         if (defined $mt5_id) {
             my $selected_mt5_status = $loginid_details{$mt5_id}->{status} // 'active';
-            $self->fail($error_message, params => {failed_by_expiry => 0}) if $selected_mt5_status eq 'poa_failed';
-            return 1 unless any { $selected_mt5_status eq $_ } qw/poa_pending poa_rejected/;
+            $self->fail($error_message) if $selected_mt5_status eq 'poa_failed';
+            return 1 unless any { $selected_mt5_status eq $_ } qw/poa_pending poa_rejected proof_failed verification_pending/;
         }
 
         my @mt5_accounts =
@@ -62,15 +69,88 @@ rule 'mt5_account.account_poa_status_allowed' => {
             } keys %loginid_details;
 
         my $current_datetime = Date::Utility->new(Date::Utility->new->datetime);
+        if (defined $mt5_id and exists $loginid_details{$mt5_id}) {
+            return 1 unless defined $loginid_details{$mt5_id}->{attributes}->{group};
+            ($mt5_jurisdiction) = $loginid_details{$mt5_id}->{attributes}->{group} =~ m/(bvi|vanuatu)/g;
+        }
+
         foreach my $mt5_account_id (@mt5_accounts) {
             my $mt5_account = $loginid_details{$mt5_account_id};
             return 1 if not defined $mt5_account->{status};
             my $mt5_creation_datetime = Date::Utility->new($mt5_account->{creation_stamp});
             my $days_elapsed          = $current_datetime->days_between($mt5_creation_datetime);
             my $poa_failed_by_expiry  = $days_elapsed <= JURISDICTION_DAYS_LIMIT()->{$mt5_jurisdiction} ? 0 : 1;
-            $self->fail($error_message, params => {failed_by_expiry => 1}) if $poa_failed_by_expiry;
+            $self->fail($error_message, params => {mt5_status => 'poa_failed'}) if $poa_failed_by_expiry;
         }
+
+        my $poi_status = $client->get_poi_status_jurisdiction($mt5_jurisdiction);
+        $self->fail($error_message, params => {mt5_status => 'poa_pending'}) if $poi_status eq 'verified';
 
         return 1;
     },
 };
+
+rule 'mt5_account.account_proof_status_allowed' => {
+    description => 'Checks if mt5 account are considered valid by poi and poa',
+    code        => sub {
+        my ($self, $context, $args) = @_;
+        my $client = $context->client($args);
+        my $new_mt5_jurisdiction =
+            $args->{new_mt5_jurisdiction};    # new_mt5_jurisdiction + loginid_details as parameter for new mt5 account (without an id)
+        my $mt5_id           = $args->{mt5_id};               # mt5_id + loginid_details as parameter for existing mt5 account
+        my %loginid_details  = %{$args->{loginid_details}};
+        my $error_message    = 'ProofRequirementError';
+        my $mt5_jurisdiction = $new_mt5_jurisdiction;
+
+        my %proof_check = (
+            poi => sub {
+                $client->get_poi_status_jurisdiction(shift);
+            },
+            poa => sub {
+                $client->get_poa_status();
+            },
+        );
+
+        if (defined $mt5_id and exists $loginid_details{$mt5_id}) {
+            return 1 unless defined $loginid_details{$mt5_id}->{attributes}->{group};
+            ($mt5_jurisdiction) = $loginid_details{$mt5_id}->{attributes}->{group} =~ m/(bvi|vanuatu|labuan|maltainvest)/g;
+        }
+
+        if (defined $mt5_jurisdiction) {
+            return 1 unless exists JURISDICTION_PROOF_REQUIREMENT()->{$mt5_jurisdiction};
+        } else {
+            return 1;
+        }
+
+        my $required_proof = JURISDICTION_PROOF_REQUIREMENT()->{$mt5_jurisdiction};
+
+        # Status to consider - verification_pending, verified, poa_failed.
+
+        my %proof_status = map { $_ => {$proof_check{$_}->($mt5_jurisdiction) => 1} } @{$required_proof};
+
+        $self->fail($error_message, params => {mt5_status => 'proof_failed'}) if any { _is_proof_failed($proof_status{$_}) } @{$required_proof};
+
+        $self->fail($error_message, params => {mt5_status => 'verification_pending'})
+            if any { ($proof_status{$_}->{pending} // 0) == 1 } @{$required_proof};
+
+        return 1;
+    },
+};
+
+=head2 _is_proof_failed
+
+Check if current proof status considered failed. Anything other than 'verified' or 'pending' considered failed.
+
+=over 4
+
+=item * C<proof_status> - The current poi/poa's verification result.
+
+=back
+
+=cut
+
+sub _is_proof_failed {
+    my $proof_status = shift;
+    return 0 if any { ($proof_status->{$_} // 0) == 1 } ('verified', 'pending');
+    return 1;
+}
