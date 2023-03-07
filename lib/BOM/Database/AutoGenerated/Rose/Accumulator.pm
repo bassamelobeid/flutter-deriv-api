cb219387c005e28dde60324afe3b6f73e6ed8d3c
babak-deriv <80316838+babak-deriv@users.noreply.github.com>
2023-03-07 13:17:26 +0330

    Accumulator/improve_contract_dynamic_config_management_and_RM_tool (#1477)
    * adding growth_rate column to bet.accumulator
    
    * trigger tests [ci] 2023-01-19 07:26:01
    
    * trigger tests [ci] 2023-01-19 07:37:58
    
    * trigger tests [ci] 2023-01-19 07:40:16
    
    * trigger tests [ci] 2023-01-19 12:28:08
    
    * trigger tests [ci] 2023-01-19 13:21:30
    
    * trigger tests [ci] 2023-01-19 13:33:55
    
    * trigger tests [ci] 2023-01-19 18:07:05
    
    * trigger tests [ci] 2023-01-24 09:09:12
    
    * trigger tests [ci] 2023-01-24 12:10:37
    
    * trigger tests [ci] 2023-01-24 12:14:30
    
    * trigger tests [ci] 2023-01-24 12:18:58
    
    * trigger tests [ci] 2023-01-24 12:27:18
    
    * trigger tests [ci] 2023-01-24 12:31:59
    
    * trigger tests [ci] 2023-01-24 12:44:22
    
    * trigger tests [ci] 2023-01-24 12:52:22
    
    * trigger tests [ci] 2023-01-24 12:55:34
    
    * trigger tests [ci] 2023-01-24 13:32:42
    
    * trigger tests [ci] 2023-01-24 15:47:11
    
    * trigger tests [ci] 2023-01-24 15:48:02
    
    * trigger tests [ci] 2023-01-24 16:11:27
    
    * trigger tests [ci] 2023-01-24 16:16:20
    
    * trigger tests [ci] 2023-01-24 16:21:48
    
    * trigger tests [ci] 2023-01-24 16:44:45
    
    * trigger tests [ci] 2023-01-24 16:51:34
    
    * trigger tests [ci] 2023-01-24 16:54:56
    
    * trigger tests [ci] 2023-01-24 16:59:40
    
    * trigger tests [ci] 2023-01-24 17:02:55
    
    * trigger tests [ci] 2023-01-24 17:05:07
    
    * trigger tests [ci] 2023-01-24 17:08:54
    
    * trigger tests [ci] 2023-01-24 17:11:17
    
    * trigger tests [ci] 2023-01-24 17:13:36
    
    * trigger tests [ci] 2023-01-24 17:19:25
    
    * trigger tests [ci] 2023-01-24 17:23:05
    
    * trigger tests [ci] 2023-01-25 04:50:47
    
    * trigger tests [ci] 2023-01-25 19:38:13
    
    * trigger tests [ci] 2023-01-25 19:47:55
    
    * trigger tests [ci] 2023-01-27 11:23:13
    
    * trigger tests [ci] 2023-02-02 08:38:53
    
    * trigger tests [ci] 2023-02-05 11:59:10
    
    * trigger tests [ci] 2023-02-05 13:25:31
    
    * trigger tests [ci] 2023-02-07 16:57:46
    
    * trigger tests [ci] 2023-02-08 15:00:54
    
    * trigger tests [ci] 2023-02-10 05:37:27
    
    * trigger tests [ci] 2023-02-20 07:55:52
    
    * trigger tests [ci] 2023-02-23 16:59:43
    
    * trigger tests [ci] 2023-03-07 07:20:07
    
    * trigger tests [ci] 2023-03-07 08:58:13
    
    * trigger tests [ci] 2023-03-07 09:04:17

--- lib/BOM/Database/AutoGenerated/Rose/Accumulator.pm
+++ lib/BOM/Database/AutoGenerated/Rose/Accumulator.pm
@@ -15,6 +15,7 @@ __PACKAGE__->meta->setup(
         },
         take_profit_order_date   => {type => 'timestamp'},
         take_profit_order_amount => {type => 'numeric'},
+        growth_rate              => {type => 'numeric'},
     ],
 
     primary_key_columns => ['financial_market_bet_id'],
