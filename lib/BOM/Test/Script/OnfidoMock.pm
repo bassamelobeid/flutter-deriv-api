94f505aae2a23fd0ebd76cfd786ae937bbcd82ed
felipe-deriv <64797927+felipe-deriv@users.noreply.github.com>
2023-06-06 00:29:58 -0400

    onfido/cpan_testers_fix (#1131)
    * onfido/cpan_testers_fix [dummy pr] [skip ci]
    
    * trigger [ci] with card-info
    
    * retest [ci]
    
    * onfido mock update [ci]
    
    * trigger [ci] with card-info
    
    * prove the critic [ci]
    
    * debug [ci]
    
    * fix [ci]
    
    * Fix mock server script path [ci]
    
    * trigger [ci] with card-info
    
    * remove useless import [ci]
    
    * trigger [ci] with card-info

--- lib/BOM/Test/Script/OnfidoMock.pm
+++ lib/BOM/Test/Script/OnfidoMock.pm
@@ -11,16 +11,18 @@ my $pid;
 
 BEGIN {
     ## no critic (Variables::RequireLocalizedPunctuationVars)
-    $ENV{ONFIDO_URL} = 'http://localhost:4039';
+    $ENV{ONFIDO_URL} = 'http://localhost:3000';
     $pid = fork();
     die "fork error " unless defined($pid);
     unless ($pid) {
         local $ENV{NO_PURGE_REDIS} = 1;
-        exec($^X, '-MBOM::Test', '/home/git/regentmarkets/cpan/local/bin/morbo',
-            '-l', $ENV{ONFIDO_URL}, '-m', 'production', '/home/git/regentmarkets/cpan/local/bin/mock_onfido.pl');
+
+        open(STDOUT, '>', '/dev/null');
+        open(STDERR, '>', '/dev/null');
+        exec($^X, '/home/git/regentmarkets/cpan/local/bin/mock_onfido.pl');
     }
 
-    sleep 1;    # waiting for server start
+    sleep 3;    # waiting for server start
 }
 
 END {
