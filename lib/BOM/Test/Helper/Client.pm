eeafb212347585b1641dacce4d16899563ee3ebf
Kirill Sysoev <55120154+kirill-deriv@users.noreply.github.com>
2023-07-12 13:46:01 +0800

    Adds wallet factory (#1133)
    * Adds wallet factory
    
    * [ci] Makes tidy
    
    * [ci] trigger tests
    
    * [ci] update mock factory
    
    * trigger tests [ci] 2023-06-09 08:10:33
    
    * trigger tests [ci] 2023-06-09 08:22:56
    
    * trigger tests [ci] 2023-06-12 01:23:29
    
    * [ci] Address review remarks
    
    * trigger tests [ci] 2023-06-12 06:31:53
    
    * trigger tests [ci] 2023-06-12 08:41:42
    
    * trigger tests [ci] 2023-06-12 10:00:25
    
    * trigger tests [ci] 2023-06-13 06:33:52
    
    * trigger tests [ci] 2023-06-14 01:22:14
    
    * trigger tests [ci] 2023-06-14 01:25:20
    
    * trigger tests [ci] 2023-06-20 05:36:39
    
    * trigger tests [ci] 2023-06-21 04:13:45
    
    * trigger tests [ci] 2023-06-21 05:28:03
    
    * trigger tests [ci] 2023-06-23 02:18:02
    
    * trigger tests [ci] 2023-06-26 05:33:03
    
    * trigger tests [ci] 2023-07-03 08:14:02
    
    * trigger tests [ci] 2023-07-05 03:25:22
    
    * trigger tests [ci] 2023-07-10 09:27:32
    
    * trigger tests [ci] 2023-07-12 04:24:06

--- lib/BOM/Test/Helper/Client.pm
+++ lib/BOM/Test/Helper/Client.pm
@@ -11,6 +11,8 @@ use BOM::User::Password;
 use Test::More;
 use BOM::Test::Data::Utility::UnitTestDatabase;
 use BOM::Database::ClientDB;
+use JSON::MaybeUTF8 qw(encode_json_utf8);
+use BOM::Platform::Token::API;
 
 our @EXPORT_OK = qw( create_client top_up close_all_open_contracts);
 
@@ -90,6 +92,99 @@ sub create_doughflow_methods {
         "INSERT INTO payment.doughflow_method (payment_processor, reversible, withdrawal_supported) 
         VALUES ('reversible', TRUE, FALSE), ('nonreversible', FALSE, TRUE)"
     );
+}
+
+=head2 create_wallet_factory
+
+Creates User object and generator function for creating wallets accounts 
+
+=cut 
+
+my $user_counter = 0;
+
+sub create_wallet_factory {
+    my ($residence, $address_state) = @_;
+
+    my $user = BOM::User->create(
+        email          => 'test_email' . $user_counter++ . '@example.com',
+        password       => BOM::User::Password::hashpw('Abcd3s3!@'),
+        email_verified => 1
+    );
+
+    my $wallet_generator = sub {
+        my ($broker_code, $account_type, $currency) = @_;
+        my $client = $user->create_wallet(
+            broker_code               => $broker_code,
+            account_type              => $account_type,
+            email                     => $user->email,
+            residence                 => $residence,
+            last_name                 => 'Test' . rand(999),
+            first_name                => 'Test1' . rand(999),
+            date_of_birth             => '1987-09-04',
+            address_line_1            => 'Sovetskaya street',
+            address_city              => 'Samara',
+            address_state             => $address_state,
+            address_postcode          => '112233',
+            secret_question           => '',
+            secret_answer             => '',
+            account_opening_reason    => 'Speculative',
+            tax_residence             => $residence,
+            tax_identification_number => '111-222-333',
+            citizen                   => $residence,
+            client_password           => BOM::User::Password::hashpw('Abcd3s3!@'),
+            phone                     => '',
+            non_pep_declaration_time  => DateTime->now,
+            salutation                => 'Mr.',
+
+        );
+        $client->set_default_account($currency);
+
+        if ($broker_code eq 'MFW') {
+            fill_up_maltainvest_financial_assestment($client);
+        }
+
+        return $client, BOM::Platform::Token::API->new->create_token($client->loginid, 'test token');
+    };
+
+    return $user, $wallet_generator;
+
+}
+
+=head2 fill_up_maltainvest_financial_assestment
+
+Fills valid maltainvest financial assestment for provided client object
+
+=cut 
+
+sub fill_up_maltainvest_financial_assestment {
+    my ($client) = @_;
+
+    $client->financial_assessment({
+            data => encode_json_utf8(
+                +{
+                    "risk_tolerance"                           => "Yes",
+                    "source_of_experience"                     => "I have an academic degree, professional certification, and/or work experience.",
+                    "cfd_experience"                           => "Less than a year",
+                    "cfd_frequency"                            => "1 - 5 transactions in the past 12 months",
+                    "trading_experience_financial_instruments" => "Less than a year",
+                    "trading_frequency_financial_instruments"  => "1 - 5 transactions in the past 12 months",
+                    "cfd_trading_definition"                   => "Speculate on the price movement.",
+                    "leverage_impact_trading"                  => "Leverage lets you open larger positions for a fraction of the trade's value.",
+                    "leverage_trading_high_risk_stop_loss"     =>
+                        "Close your trade automatically when the loss is more than or equal to a specific amount.",
+                    "required_initial_margin" => "When opening a Leveraged CFD trade.",
+                    "employment_industry"     => "Finance",
+                    "education_level"         => "Secondary",
+                    "income_source"           => "Self-Employed",
+                    "net_income"              => '$25,000 - $50,000',
+                    "estimated_worth"         => '$100,000 - $250,000',
+                    "account_turnover"        => '$25,000 - $50,000',
+                    "occupation"              => 'Managers',
+                    "employment_status"       => "Self-Employed",
+                    "source_of_wealth"        => "Company Ownership",
+                })});
+    $client->status->set('financial_risk_approval', 'SYSTEM', 'Client accepted financial risk disclosure');
+    $client->save();
 }
 
 1;
