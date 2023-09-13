2c29d8ec8a89aac37489a9a02dfdf29969bb35af
qinfeng-deriv <91937565+qinfeng-deriv@users.noreply.github.com>
2023-09-13 17:07:37 +0800

    feed/removing_bom_market_dependency (#1141)
    * move
    
    * save
    
    * move unit test to bom-market
    
    * trigger tests [ci] 2023-08-09 06:05:18
    
    * trigger tests [ci] 2023-08-09 06:09:06
    
    * fix syntax test and perl_runtidy [ci]
    
    * symbols_for_intraday_fx(1) [ci]
    
    * trigger tests [ci] 2023-09-06 01:49:34
    
    * trigger tests [ci] 2023-09-06 01:53:12
    
    * trigger tests [ci] 2023-09-13 08:18:13
    
    * trigger tests [ci] 2023-09-13 08:55:15

---
+++ lib/BOM/Market/Script/DecimateChecker.pm
@@ -0,0 +1,32 @@
+package BOM::Market::Script::DecimateChecker;
+
+use strict;
+use warnings;
+
+use BOM::Market::DecimateChecker;
+use Syntax::Keyword::Try;
+use Future::AsyncAwait;
+use Log::Any qw($log);
+use IO::Async::Loop;
+
+# Set service name
+$0 = 'feed-decimate-async';    ## no critic
+
+async sub run {
+    STDOUT->autoflush(1);
+
+    my $loop = IO::Async::Loop->new;
+    my $app  = BOM::Market::DecimateChecker->new();
+    $log->info("$0 is running");
+    try {
+        $loop->add($app);
+        await $app->run;
+    } catch ($e) {
+        $log->warnf('Failed | %s', $e);
+    }
+
+    $loop->remove($app);
+    return Future->done;
+}
+
+1;
