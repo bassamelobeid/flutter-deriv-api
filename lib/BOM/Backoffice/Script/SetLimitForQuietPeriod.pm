b7cd879da0e689bd5c7f39209f4bb6bdb253809c
Tanvir Faisal <126654333+tanvir-deriv@users.noreply.github.com>
2023-09-25 16:19:41 +0800

    set_limit/rm_72905_extreme_risk_on_forex (#3906)
    * [ci skip] Work in progress - extended the current script
    
    * updated start_time and end_time issue
    
    * [skip ci]Added DST and non-DST way to set up extreme risk for Forex
    
    * set_limit/rm_72905_extreme_risk_on_forex
    
    * [ci]Tidy up the code
    
    * [ci]Added import + logic updated considering DST/non-DST
    
    * [ci]Tidy up the code + latest master pulled
    
    * [ci]Trigerring CI
    
    * [ci]Triggering CI
    
    * [skip ci]Method added for removal of keys
    
    * [ci]Updated code to include sub routine for repeated code
    
    * [ci]Tidy up the code
    
    * [ci]Updated code for production
    
    * [ci]POD added for new sub-routine
    
    * [ci]Removed unnecessary string
    
    * [skip ci]added unit test
    
    * [skip ci]Updated the unit test for mocking chronicle writer
    
    * [ci]Updated test and removed unnecessary comments
    
    * [ci]Removed commented lines
    
    * [ci]Tidy up the code
    
    * [ci]Code reviews - updated
    
    * [ci]Trigerring CI
    
    * [ci]Trigerring CI
    
    * [ci]Test is being updated...WIP...
    
    * [skip ci]WIP...
    
    * [ci] Modularized code to different bit of subroutines to increase maintainability - Code review suggestions followed.
    
    * [ci]Code updated to pass syntax_diff unit test
    
    * [ci]Added unit tests for code changes i.e. new subroutines - Code review updates
    
    * [ci]Triggering CI
    
    * [ci]changes for testing - set the logic condition checking to always true
    
    * [ci]Reverted to expected Production code after QA testing was completed

--- lib/BOM/Backoffice/Script/SetLimitForQuietPeriod.pm
+++ lib/BOM/Backoffice/Script/SetLimitForQuietPeriod.pm
@@ -5,6 +5,8 @@ use BOM::Config;
 use BOM::Platform::RiskProfile;
 use BOM::Config::Runtime;
 use BOM::Platform::Email qw(send_email);
+
+use Quant::Framework::VolSurface::Utils qw(NY1700_rollover_date_on);
 use Date::Utility;
 use Digest::MD5 qw(md5_hex);
 use JSON::MaybeXS;
@@ -12,62 +14,40 @@ use JSON::MaybeXS;
 sub documentation { return 'This script is to set limit for quiet period'; }
 
 sub script_run {
-    my $self = shift;
-    my $json = JSON::MaybeXS->new(
-        pretty    => 1,
-        canonical => 1
-    );
-
-    my $quants_config = BOM::Config::Runtime->instance->app_config;
-    $quants_config->chronicle_writer(BOM::Config::Chronicle::get_chronicle_writer());
-    my $current                  = $quants_config->get('quants.custom_product_profiles');
-    my $current_product_profiles = $json->decode($current);
+    my $self                     = shift;
+    my $current                  = quants_config_handler()->get('quants.custom_product_profiles');
+    my $current_product_profiles = json_handler()->decode($current);
 
     #Adding ultra_short to the list to protect us from Ukranian clients.
     #As of Jan 17th, we have 104 Ukranian clients hitting our profit
     #using 1 minute contract.
 
     foreach my $duration (qw(intraday tick ultra_short)) {
-
         my ($todo, $risk_profile, $to_remove, $between);
         my $now          = Date::Utility->new;
         my $cut_off_hour = $now->is_dst_in_zone('Europe/London') ? '06' : '07';
+
         foreach my $type (({market => 'forex'}, {submarket => 'forex_basket,commodity_basket'})) {
-
             my ($value) = values $type->%*;
-            # Since we are setting start and end time, we just need to do this once a day at 00
+
             if ($duration eq 'intraday') {
+                $risk_profile = 'extreme_risk';
                 if ($now->hour == 00) {
-                    $todo         = "set extreme_risk_${value}_${duration}_trade_asian_hour";
-                    $risk_profile = 'extreme_risk';
-                    my $uniq_key = substr(md5_hex('new' . $todo), 0, 16);
+                    $todo = "set extreme_risk_${value}_${duration}_trade_asian_hour";
+                    my $uniq_key = unique_key_generator($todo);
+                    my ($new_limit, $between) = build_new_limit($duration, 'asian_hour', $todo, $risk_profile, $now, $type);
+                    set_new_limit($uniq_key, $current_product_profiles, $todo, $new_limit);
+                    send_notification_email($new_limit, $todo . ' between ' . $between);
 
-                    my $start_time = $now->truncate_to_day;
-                    my $end_time   = $start_time->plus_time_interval('7h');
-
-                    my %new_limit = (
-                        risk_profile => $risk_profile,
-                        expiry_type  => $duration,
-                        name         => $todo,
-                        updated_by   => 'cron job',
-                        start_time   => $start_time->datetime_yyyymmdd_hhmmss,
-                        end_time     => $end_time->datetime_yyyymmdd_hhmmss,
-                        updated_on   => Date::Utility->new->datetime,
-                        $type->%*,
-                    );
-
-                    #removing old limit for intraday set previous day at 00 hour
-                    my @removing_keys =
-                        grep { $current_product_profiles->{$_}->{updated_by} eq 'cron job' and $current_product_profiles->{$_}->{name} eq $todo }
-                        keys %$current_product_profiles;
-                    delete @{$current_product_profiles}{@removing_keys};
-
-                    $current_product_profiles->{$uniq_key} = \%new_limit;
-                    $quants_config->set({'quants.custom_product_profiles' => $json->encode($current_product_profiles)});
-
-                    $between = $start_time->hour . ' to ' . $end_time->hour . ' GMT';
-                    send_notification_email(\%new_limit, $todo . ' between ' . $between);
+                    if ($value eq 'forex') {
+                        $todo = "set extreme_risk_${value}_${duration}_mean_reversion_trade";
+                        my $uniq_key = unique_key_generator($todo);
+                        my ($new_limit, $between) = build_new_limit($duration, 'mean_reversion', $todo, $risk_profile, $now, $type);
+                        set_new_limit($uniq_key, $current_product_profiles, $todo, $new_limit);
+                        send_notification_email($new_limit, $todo . ' between ' . $between);
+                    }
                 }
+
                 next;
             }
 
@@ -82,18 +62,12 @@ sub script_run {
                 $to_remove    = "set extreme_risk_${value}_${duration}_trade";
                 $risk_profile = 'high_risk';
                 $between      = $cut_off_hour . ' to 00GMT';
+
             } else {
+
                 next;
             }
-            my $uniq_key = substr(md5_hex('new' . $todo), 0, 16);
-
-            #removing old limit (tick trade/ ultra short duration)
-            my @removing_keys =
-                grep { $current_product_profiles->{$_}->{updated_by} eq 'cron job' and $current_product_profiles->{$_}->{name} eq $to_remove }
-                keys %$current_product_profiles;
-            delete @{$current_product_profiles}{@removing_keys};
-
-            $quants_config->set({'quants.custom_product_profiles' => $json->encode($current_product_profiles)});
+            my $uniq_key = unique_key_generator($todo);
 
             #imposing new limit on forex and basket indices (tick trade/ ultra short duration)
             my %new_limit = (
@@ -105,14 +79,130 @@ sub script_run {
                 $type->%*,
             );
 
-            $current_product_profiles->{$uniq_key} = \%new_limit;
-            $quants_config->set({'quants.custom_product_profiles' => $json->encode($current_product_profiles)});
+            set_new_limit($uniq_key, $current_product_profiles, $to_remove, \%new_limit);
             send_notification_email(\%new_limit, $todo . ' between ' . $between);
         }
 
     }
 
     return 1;
+}
+
+=head2 json_handler
+
+This sub routine handles json encoding and decoding
+
+=cut
+
+sub json_handler {
+    return JSON::MaybeXS->new(
+        pretty    => 1,
+        canonical => 1
+    );
+}
+
+=head2 quants_config_handler
+
+This sub routine handles getting and setting of quants configuration
+
+=cut
+
+sub quants_config_handler {
+    my $quants_config = BOM::Config::Runtime->instance->app_config;
+    $quants_config->chronicle_writer(BOM::Config::Chronicle::get_chronicle_writer());
+    return $quants_config;
+}
+
+=head2 unique_key_generator
+
+This sub routine generates unique key using $key
+
+=cut
+
+sub unique_key_generator {
+    my $key = shift;
+    return substr(md5_hex('new' . $key), 0, 16);
+}
+
+=head2 calculate_time_duration_for_limit
+
+This sub routine sets up start time and end time for new limits
+
+=cut
+
+sub calculate_time_duration_for_limit {
+    my $now         = shift;
+    my $limit_cause = shift;
+
+    my $start_time = $now->truncate_to_day;
+    my $end_time   = $start_time->plus_time_interval('7h');
+
+    if ($limit_cause eq 'mean_reversion') {
+        my $rollover_date_time = NY1700_rollover_date_on($now->truncate_to_day);
+        my $start_time         = $rollover_date_time->plus_time_interval('1h');
+        my $end_time           = $rollover_date_time->plus_time_interval('3h');
+
+        return ($start_time, $end_time);
+
+    }
+
+    return ($start_time, $end_time);
+
+}
+
+=head2 build_new_limit
+
+This sub routine processes the params and builds a hash for the new limit
+
+=cut
+
+sub build_new_limit {
+    my $duration     = shift;
+    my $limit_cause  = shift;
+    my $todo         = shift;
+    my $risk_profile = shift;
+    my $now          = shift;
+    my $type         = shift;
+
+    my $between;
+    my ($start_time, $end_time) = calculate_time_duration_for_limit($now, $limit_cause);
+    $between = $start_time->hour . ' to ' . $end_time->hour . ' GMT';
+
+    my %new_limit = (
+        risk_profile => $risk_profile,
+        expiry_type  => $duration,
+        name         => $todo,
+        updated_by   => 'cron job',
+        start_time   => $start_time->datetime_yyyymmdd_hhmmss,
+        end_time     => $end_time->datetime_yyyymmdd_hhmmss,
+        updated_on   => Date::Utility->new->datetime,
+        $type->%*,
+    );
+
+    return (\%new_limit, $between);
+}
+
+=head2 set_new_limit
+
+This sub routine removes existing limits based on given parameters and set up new limits
+
+=cut
+
+sub set_new_limit {
+    my $uniq_key                 = shift;
+    my $current_product_profiles = shift;
+    my $to_remove                = shift;
+    my $new_limit                = shift;
+
+    my @removing_keys =
+        grep { $current_product_profiles->{$_}->{updated_by} eq 'cron job' and $current_product_profiles->{$_}->{name} eq $to_remove }
+        keys %$current_product_profiles;
+
+    delete @{$current_product_profiles}{@removing_keys};
+
+    $current_product_profiles->{$uniq_key} = $new_limit;
+    quants_config_handler()->set({'quants.custom_product_profiles' => json_handler()->encode($current_product_profiles)});
+
 }
 
 sub send_notification_email {
