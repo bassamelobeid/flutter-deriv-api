3743c9e6d49dbe3f70021a81555f2af6c8fc94de
Ahmad Aizuddin <57475261+ahmad-deriv@users.noreply.github.com>
2023-10-17 17:13:15 +0800

    Revert "appstore/transferable_accounts (#863)" (#933)
    This reverts commit a72202e14fb61c2e4a138d963ab38c9dc7c9accf.

--- lib/BOM/Config/AccountType.pm
+++ lib/BOM/Config/AccountType.pm
@@ -129,14 +129,6 @@ Return list of supported regulations by account type
 
 has $regulations : reader;
 
-=head2 transfers
-
-Returns type of transfer supported by the account
-
-=cut
-
-has $transfers : reader;
-
 =head2 is_cashier
 
 Returns type of transfer supported by the account
@@ -311,8 +303,7 @@ BUILD {
     }
 
     $regulations = +{map { $_ => 1 } $args{regulations}->@*};
-    $transfers   = $category_name eq 'trading' ? 'all' : $args{transfers} // 'none';
-    $is_cashier  = $args{is_cashier}                                      // 0;
+    $is_cashier  = $args{is_cashier} // 0;
 
     (
         $groups, $type_broker_codes, $linkable_to_different_currency,
