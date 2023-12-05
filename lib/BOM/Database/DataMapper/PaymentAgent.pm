35f678582d9b39700a7bcc9cb1fc8e2d8ad9ed64
Hani Kh <108520819+hani-deriv@users.noreply.github.com>
2023-12-05 16:51:09 +0800

    Payment agents optimization (#1870)
    * add sub to call db function
    
    * add test case and remove broker code
    
    * add linked details mapper
    
    * amend POD's and remove unnecessary return
    
    * trigger tests 2023-11-22 06:59:18
    
    * trigger tests 2023-11-22 07:25:06
    
    * trigger tests 2023-11-22 09:56:26

--- lib/BOM/Database/DataMapper/PaymentAgent.pm
+++ lib/BOM/Database/DataMapper/PaymentAgent.pm
@@ -81,6 +81,83 @@ sub get_payment_agents_linked_to_client {
         });
 }
 
+=head2 get_payment_agents_details_full
+
+Return a list of payment agents' full details each as a hashref based on the provided country and currency.
+
+Takes the following parameters:
+
+=over 4
+
+=item * country_code - L<2-character ISO country code|https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2> to restrict search (agents with no country will not be included)
+
+=item * currency - Three letter currency code. For example USD.
+
+=item * is_listed - Indicate which payment agents you want to retrieve. whether they appear on binary site or not or both. For example ('t','f', NULL)
+
+=item * details_fields_mapper - A hashref of the fields that need to be mapped. The key is the field name and the value is the field name of the linked details.
+
+=back
+
+=cut
+
+sub get_payment_agents_details_full {
+    my ($self, %args) = @_;
+
+    my ($country_code, $currency, $is_listed, $details_field_mapper) = @args{qw/ country_code currency is_listed details_field_mapper/};
+
+    if (!defined($country_code)) {
+        die "country code should be specified";
+    }
+
+    my $db_rows = $self->db->dbic->run(
+        fixup => sub {
+            $_->selectall_arrayref(
+                'SELECT * FROM betonmarkets.get_payment_agents_by_country_full(?, ?, ?, ?)',
+                {Slice => {}},
+                $country_code, 't', $currency, $is_listed
+            );
+        });
+
+    if ($details_field_mapper) {
+        $self->map_linked_details($db_rows, $details_field_mapper);
+    }
+
+    my %indexed_by_loginid = map { $_->{client_loginid} => $_ } $db_rows->@*;
+    return \%indexed_by_loginid;
+}
+
+=head2 map_linked_details
+
+Loop thorugh the db_rows and convert the array result of linked details to hashref based on the details_fields_mapper
+Example map { urls => 'url'}
+Db rows before
+{urls => ['http://www.MyPAMyAdventure.com/','http://www.MyPAMyAdventure2.com/']}
+Result {urls => [{url => 'http://www.MyPAMyAdventure.com/'},{url => 'http://www.MyPAMyAdventure2.com/'}]}
+
+=over 4
+
+=item C<$db_rows> - An array of hashref of the db rows which contains fields that need to be mapped
+
+=item C<$details_field_mapper> - A hashref of the fields that need to be mapped. The key is the field name and the value is the field name of the linked details.
+
+=back
+
+=cut
+
+sub map_linked_details {
+    my ($self, $db_rows, $details_field_mapper) = @_;
+    for my $row ($db_rows->@*) {
+        for my $field (keys $details_field_mapper->%*) {
+            $row->{$field} = [
+                map {
+                    { $details_field_mapper->{$field} => $_ }
+                } $row->{$field}->@*
+            ];
+        }
+    }
+}
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
 
