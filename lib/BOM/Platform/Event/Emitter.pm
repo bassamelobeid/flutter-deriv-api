2fe1849874f42508430c2499dfdb496e8e9d5ac6
Laura Vazquez <120387181+laura-vazquez-deriv@users.noreply.github.com>
2023-12-18 04:52:16 -0300

    squad_branch_kyc15_v2 (#3176)
    * idv/stream_consumer [skip ci]
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * stream new message towards the nodejs services [ci]
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    * trigger [ci] with card-info
    
    ---------
    
    Co-authored-by: Felipe Martinez <felipe@regentmarkets.com>

--- lib/BOM/Platform/Event/Emitter.pm
+++ lib/BOM/Platform/Event/Emitter.pm
@@ -65,6 +65,8 @@ my %event_stream_mapping = (
     derivez_inactive_notification       => 'BULK_EVENTS_STREAM',
     onfido_check_completed              => 'DOCUMENT_AUTHENTICATION_STREAM',
     monolith_hello                      => 'NODEJS_STREAM',
+    idv_webhook                         => 'NODEJS_STREAM',
+    idv_verification                    => 'NODEJS_STREAM',
 );
 
 my $config = LoadFile('/etc/rmg/redis-events.yml');
