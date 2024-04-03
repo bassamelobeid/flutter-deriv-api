b8764921a50c5d598fadedbf831ed9cdeadd6ad1
Shafi-deriv <132987426+Shafi-deriv@users.noreply.github.com>
2024-04-03 12:44:42 +0400

    fix_setup_integration_testing_commission_database (#2050)
    * fix setup of integration database
    
    * fix setup of integration database
    
    * fix setup of integration database
    
    * trigger tests 2024-02-20 06:09:08
    
    * make tidy [ci]
    
    * [DT]
    
    * Empty commit [DT]
    
    * Empty commit [DT]

--- lib/BOM/Database/CommissionDB.pm
+++ lib/BOM/Database/CommissionDB.pm
@@ -17,6 +17,8 @@ sub rose_db {
     # connect to commission db by default unless replica is specified
     my $database = 'commission';
     $database .= '-replica' if $operation eq 'replica';
+    my $db_postfix = $ENV{DB_POSTFIX} // '';
+    $database .= $db_postfix;
 
     BOM::Database::Rose::DB->register_db(
         connect_options => {
