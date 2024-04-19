276f17d4b94f5d0b2a2c4cc9a91b5042df7bf7f4
shaydo-deriv <129739908+shaydo-deriv@users.noreply.github.com>
2024-04-19 09:18:48 +0200

    move-get-bid-to-bom (#2851)
    * move get-bid logic from bom-pricing into bom
    
    * remove number of contracts for Turbos and Vanilla
    
    see regentmarkets/bom-pricing#1568

--- lib/BOM/Contract.pm
+++ lib/BOM/Contract.pm
@@ -5,6 +5,16 @@ use warnings;
 
 use Moose;
 
+=head2 inner_contract
+
+reference to C<BOM::Product::Contract> object that this BOM::Contract object
+wraps. Long term goal is not to have it, if you use it anywhere outside of
+C<bom> repo you need to think about refactoring your code. The name
+C<inner_contract> is specifically chosen to enable you to quickly find the
+places in the code that require refactoring.
+
+=cut
+
 has inner_contract => (
     is       => 'ro',
     required => 1,
@@ -12,14 +22,57 @@ has inner_contract => (
 
 =head2 metadata
 
-Returns contract metadata as a hashref. Includes contract category and type,
-symbol and market, barrier category, expiry type, start type, duration
+Returns contract metadata as a hashref. Includes the following fields:
+
+=over 4
+
+=item contract_category
+
+=item underlying_symbol
+
+=item barrier_category 
+
+=item expiry_type      
+
+=item start_type       
+
+=item contract_duration
+
+=item for_sale         
+
+=item contract_type    
+
+=item market           
+
+=back
 
 =cut
 
 sub metadata {
     my ($self, @args) = @_;
     return $self->inner_contract->metadata(@args);
+}
+
+=head2 longcode
+
+returns textual description of the contract. The value needs to be passed to
+C<BOM::Platform::Context::localize> before it can be forwarded to users as it
+is not a string.
+
+=cut
+
+sub longcode {
+    return shift->inner_contract->longcode;
+}
+
+=head2 is_expired
+
+returns true if the contract has expired
+
+=cut
+
+sub is_expired {
+    return shift->inner_contract->is_expired;
 }
 
 no Moose;
--- lib/BOM/Contract/Pricer.pm
+++ lib/BOM/Contract/Pricer.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Format::Util::Numbers qw(formatnumber roundcommon);
 use JSON::MaybeXS;
+use List::Util qw(max);
 
 our @EXPORT_OK = qw( price_contract );
 
@@ -12,7 +13,14 @@ our @EXPORT_OK = qw( price_contract );
 
 Calculate ask price for the contract and return a hashref with all relevant
 details including: longcode, payout, ask price, spot time, start and expiry
-dates and other contract specific parameters.
+dates and other contract specific parameters. Accepts two arguments: the
+contract and a hashref containing following optional parameters:
+
+=over 4
+
+=item update -- produce update message, that might skip some details
+
+=back
 
 =cut
 
@@ -186,6 +194,265 @@ sub calc_ask_price_detailed {
             if $contract->inner_contract->code =~
             /^(MULTUP|MULTDOWN|CALLSPREAD|PUTSPREAD|ACCU|VANILLALONGCALL|VANILLALONGPUT|TURBOSLONG|TURBOSSHORT)$/;
     }
+
+    return $response;
+}
+
+=head2 calc_bid_price_detailed
+
+Calculate bid price for the contract and return a hashref with all the relevant
+details.  Accepts two arguments: the contract and a hashref containing the
+following parameters:
+
+=over 4
+
+=item is_sold  Boolean  Whether the contract is sold or not.
+
+=item is_expired  Boolean  Whether the contract is expired or not.
+
+=item sell_price   Numeric Price at which contract was sold, only available
+when contract has been sold.
+
+=item sell_time   Integer Epoch time of when the contract was sold (only
+present for contracts already sold).
+
+=back
+
+Returns a contract proposal response as a  Hashref
+
+=cut
+
+my @spot_list = qw(entry_tick entry_spot exit_tick sell_spot current_spot);
+
+sub calc_bid_price_detailed {
+    my ($class, $contract, $params) = @_;
+    my $inner              = $contract->inner_contract;
+    my $is_valid_to_settle = $inner->is_settleable;
+    my $underlying         = $inner->underlying;
+
+    my $response = {
+        current_spot_time   => 0 + $inner->current_tick->epoch,
+        underlying          => $underlying->symbol,
+        display_name        => $underlying->display_name,
+        is_expired          => $inner->is_expired,
+        is_forward_starting => $inner->starts_as_forward_starting,
+        is_path_dependent   => $inner->is_path_dependent,
+        is_intraday         => $inner->is_intraday,
+        date_start          => 0 + $inner->date_start->epoch,
+        date_expiry         => 0 + $inner->date_expiry->epoch,
+        date_settlement     => 0 + $inner->date_settlement->epoch,
+        currency            => $inner->currency,
+        longcode            => $inner->longcode,
+        shortcode           => $inner->shortcode,
+        contract_type       => $inner->code,
+        bid_price           => formatnumber('price', $inner->currency, $inner->bid_price),
+        is_settleable       => $is_valid_to_settle,
+        barrier_count       => $inner->two_barriers ? 2 : 1,
+        is_valid_to_cancel  => $inner->is_valid_to_cancel,
+        expiry_time         => $inner->date_expiry->epoch,
+    };
+    if (!$inner->uses_barrier) {
+        $response->{barrier_count} = 0;
+        $response->{barrier}       = undef;
+    }
+
+    if ($inner->reset_spot) {
+        $response->{reset_time}    = 0 + $inner->reset_spot->epoch;
+        $response->{reset_barrier} = $underlying->pipsized_value($inner->reset_spot->quote);
+    }
+
+    $response->{multiplier}   = $inner->multiplier   if $inner->can('multiplier');
+    $response->{current_spot} = $inner->current_spot if $underlying->feed_license eq 'realtime';
+    $response->{tick_count}   = $inner->tick_count   if $inner->expiry_type eq 'tick';
+
+    if ($inner->is_binary) {
+        $response->{payout} = $inner->payout;
+    } elsif ($inner->can('maximum_payout')) {
+        $response->{payout} = $inner->maximum_payout;
+    }
+
+    if ($params->{is_sold} and $params->{is_expired}) {
+        # here sell_price is used to parse the status of contracts that settled from Back Office
+        # For non binary (except accumulator), there is no concept of won or lost, hence will return empty status if it is already expired and sold
+        $response->{status} = undef;
+        if ($inner->is_binary) {
+            $response->{status} = ($params->{sell_price} == $inner->payout ? "won" : "lost");
+        }
+    } elsif ($params->{is_sold} and not $params->{is_expired}) {
+        $response->{status} = 'sold';
+    } else {    # not sold
+        $response->{status} = 'open';
+    }
+
+    # overwrite the above status if contract is cancelled
+    $response->{status} = 'cancelled' if $inner->is_cancelled;
+
+    if ($inner->entry_spot) {
+        my $entry_spot = $underlying->pipsized_value($inner->entry_spot);
+        $response->{entry_tick}      = $entry_spot;
+        $response->{entry_spot}      = $entry_spot;
+        $response->{entry_tick_time} = 0 + $inner->entry_spot_epoch;
+    }
+
+    if ($inner->two_barriers and $inner->high_barrier) {
+        # supplied_type 'difference' and 'relative' will need entry spot to calculate absolute barrier value
+        if ($inner->high_barrier->supplied_type eq 'absolute' or $inner->entry_spot) {
+            $response->{high_barrier} = $inner->high_barrier->as_absolute;
+            $response->{low_barrier}  = $inner->low_barrier->as_absolute;
+        }
+    } elsif ($inner->can('barrier') and $inner->barrier) {
+        if ($inner->barrier->supplied_type eq 'absolute' or $inner->barrier->supplied_type eq 'digit') {
+            $response->{barrier} = $inner->barrier->as_absolute;
+        } elsif ($inner->entry_spot) {
+            $response->{barrier} = $inner->barrier->as_absolute;
+        }
+    }
+
+    # for multiplier, we want to return the orders and insurance details.
+    if ($inner->category_code eq 'multiplier') {
+        # If the caller is not from price daemon, we need:
+        # 1. sorted orders as array reference ($contract->available_orders) for PRICER_ARGS
+        # 2. available order for display in the websocket api response ($contract->available_orders_for_display)
+        $response->{limit_order} = $inner->available_orders_for_display;
+        # commission in payout currency amount
+        $response->{commission} = $inner->commission_amount;
+        # deal cancellation
+        if ($inner->cancellation) {
+            $response->{cancellation} = {
+                ask_price   => $inner->cancellation_price,
+                date_expiry => $inner->cancellation_expiry->epoch,
+            };
+        }
+    }
+
+    # for accumulator, we want to return maximum_ticks and growth_rate and limit_order.
+    if ($inner->category_code eq 'accumulator') {
+        if ($inner->take_profit) {
+            $response->{limit_order} = {
+                'take_profit' => {
+                    'display_name' => 'Take profit',
+                    'order_date'   => $inner->take_profit->{date}->epoch,
+                    'order_amount' => $inner->take_profit->{amount}}};
+        }
+        $response->{growth_rate}               = $inner->growth_rate;
+        $response->{tick_count}                = $inner->max_duration;
+        $response->{tick_passed}               = $inner->tick_count_after_entry;
+        $response->{high_barrier}              = $inner->display_high_barrier if $inner->display_high_barrier;
+        $response->{low_barrier}               = $inner->display_low_barrier  if $inner->display_low_barrier;
+        $response->{current_spot_high_barrier} = $inner->current_spot_high_barrier;
+        $response->{current_spot_low_barrier}  = $inner->current_spot_low_barrier;
+        $response->{barrier_spot_distance}     = $inner->barrier_spot_distance;
+
+        #in the first few ticks of the contract bid_price will be less than stake
+        #but we don't want to show that to users
+        $response->{bid_price} = max($response->{bid_price}, $inner->_user_input_stake) unless $inner->is_expired;
+
+        #status of accumulator is determined differently from other non-binary contracts
+        if ($params->{is_sold} and $params->{is_expired}) {
+            $response->{status} = ($inner->pnl >= 0 ? "won" : "lost");
+        } elsif ($params->{is_sold} and not $params->{is_expired}) {
+            #user can only sell the contract if pnl > 0, so it will considered as a 'win'
+            $response->{status} = 'won';
+        } else {    # not sold
+            $response->{status} = 'open';
+        }
+    }
+
+    if ($inner->category_code eq 'turbos') {
+        if ($inner->take_profit) {
+            $response->{limit_order} = {
+                'take_profit' => {
+                    'display_name' => 'Take profit',
+                    'order_date'   => $inner->take_profit->{date}->epoch,
+                    'order_amount' => $inner->take_profit->{amount}}};
+        }
+        $response->{barrier}                     = $inner->display_barrier;
+        $response->{display_number_of_contracts} = $inner->number_of_contracts;
+
+        # status of turbos is determined differently from other non-binary contracts
+        if ($params->{is_sold} and $params->{is_expired}) {
+            $response->{status} = ($inner->pnl >= 0 ? "won" : "lost");
+        } elsif ($params->{is_sold} and not $params->{is_expired}) {
+            $response->{status} = 'sold';
+        } else {
+            $response->{status} = 'open';
+        }
+    }
+
+    if ($inner->category_code eq 'vanilla') {
+        $response->{display_number_of_contracts} = $inner->number_of_contracts;
+    }
+
+    if (    $inner->exit_tick
+        and $inner->is_valid_exit_tick
+        and $inner->is_after_settlement)
+    {
+        $response->{exit_tick}      = $inner->underlying->pipsized_value($inner->exit_tick->quote);
+        $response->{exit_tick_time} = 0 + $inner->exit_tick->epoch;
+    }
+
+    if ($is_valid_to_settle || $inner->is_sold) {
+        $response->{audit_details} = $inner->audit_details($params->{sell_time});
+    }
+
+    # sell_spot and sell_spot_time are updated if the contract is sold
+    # or when the contract is expired.
+    #exit_tick to be returned on these scenario:
+    # - sell back early (tick at sell time)
+    # - hit tick for an American contract
+    # - latest tick at the expiry time of a European contract.
+    # TODO: Planning to phase out sell_spot in the next API version.
+
+    my $contract_close_tick;
+    # contract expire before the expiry time
+    if ($params->{sell_time} and $params->{sell_time} < $inner->date_expiry->epoch) {
+        if (    $inner->is_path_dependent
+            and $inner->close_tick
+            and $inner->close_tick->epoch <= $params->{sell_time})
+        {
+            $contract_close_tick = $inner->close_tick;
+        }
+
+        if ((!$inner->is_path_dependent) and ($inner->can('close_tick'))) {
+            # using close_tick if the non path dependent contract has the method defined
+            # since tick_at is not reliable for sell at market contracts
+            $contract_close_tick = $inner->close_tick;
+        }
+
+        # client sold early
+        $contract_close_tick = $inner->underlying->tick_at($params->{sell_time}, {allow_inconsistent => 1})
+            unless defined $contract_close_tick;
+    } elsif ($inner->is_expired) {
+        # it could be that the contract is not sold until/after expiry for path dependent
+        $contract_close_tick = $inner->close_tick if $inner->is_path_dependent;
+        $contract_close_tick = $inner->exit_tick  if not $contract_close_tick and $inner->exit_tick and $inner->is_valid_exit_tick;
+    }
+
+    # if the contract is still open, $contract_close_tick will be undefined
+    if (defined $contract_close_tick) {
+        foreach my $key ($params->{is_sold} ? qw(sell_spot exit_tick) : qw(exit_tick)) {
+            $response->{$key} = $inner->underlying->pipsized_value($contract_close_tick->quote);
+            $response->{$key . '_time'} = 0 + $contract_close_tick->epoch;
+        }
+    }
+
+    if ($inner->tick_expiry) {
+
+        $response->{tick_stream} = $inner->tick_stream;
+
+        if ($inner->category->code eq 'highlowticks' and $inner->selected_tick) {
+            my $selected_tick = $inner->selected_tick;
+            $response->{selected_tick} = 0 + $selected_tick;
+
+            if ($inner->supplied_barrier) {
+                $response->{selected_spot} = 0 + $inner->supplied_barrier;
+            }
+        }
+    }
+
+    $response->{$_ . '_display_value'} = $inner->underlying->pipsized_value($response->{$_}) for (grep { defined $response->{$_} } @spot_list);
+    # makes sure they are numbers
+    $response->{$_} += 0 for (grep { defined $response->{$_} } @spot_list);
 
     return $response;
 }
